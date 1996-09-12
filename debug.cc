2c1ca2112a07f6580226e782fb20fd8bd672fc0b
fred <fred>
1996-09-12 22:25:40 +0000

    lilypond-0.0.1

---
+++ debug.cc
@@ -0,0 +1,15 @@
+#include "debug.hh"
+int debug_flags;
+
+void
+set_debug(String s)
+{
+    if (s.pos ('t')) {
+	debug_flags |= DEBUGTOKEN;
+	mtor << " Turning on token debug\n";
+    }
+    if (s.pos ('p')){
+	debug_flags |= DEBUGPARSER;
+	mtor << "Turning on parser debugger\n";
+    }
+}
