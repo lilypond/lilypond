e539f4538f43d0d5f600f5597cf5e85cfbc9db82
fred <fred>
1996-10-05 13:08:56 +0000

    lilypond-0.0.1

---
+++ command.cc
@@ -0,0 +1,13 @@
+#include "string.hh"
+#include "command.hh"
+
+
+Command*
+get_bar_command(Real w)
+{
+    Command*c = new Command;
+    c->when = w;
+    c->code = TYPESET;
+    c->args.add( "|");
+    return c;
+}
