5fa0cff9f586146c3c82a3f54452189e2fd750e4
fred <fred>
1996-10-08 22:40:22 +0000

    lilypond-0.0.2

--- const.hh
+++ const.hh
@@ -2,10 +2,11 @@
     global constants
     */
 
-
+#include <math.h>
 const Real CM_TO_PT=72/2.54;
 const Real VERT_TO_PT=CM_TO_PT;	// tex output
 const Real HOR_TO_PT=CM_TO_PT;	// tex output
+
 const Real EPS=1e-7;		// qlpsolve.hh
 const int MAXITER=100;		// qlpsolve.hh
-const Real INFTY=1e8;
+const Real INFTY=HUGE;
