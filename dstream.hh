025e92646c6cde46f8a8981294a077b07df186d7
fred <fred>
1996-10-09 12:59:17 +0000

    lilypond-0.0.2

--- dstream.hh
+++ dstream.hh
@@ -4,26 +4,34 @@
 #define DSTREAM_HH
 
 #include "string.hh"
+#include "assoc.hh"
 
 const char eol= '\n';
 
 /// debug stream
-class dstream
+class Dstream
 {
     ostream *os;
     int indentlvl;
-
+    Assoc<String, bool> silent;
+    bool local_silence;
+    String classname;
     /// indent of each level 
     const INDTAB = 3;
+
 public:
-    dstream(ostream &r){
-	os = &r;
-	indentlvl = 0;	
-    }
-    dstream &operator << (String s);
+    Dstream(ostream &r, const char  * = 0);
+    Dstream &identify_as(String s);
+    void switch_output(String s,bool);
+    Dstream &operator << (String s);
 };
  /**
    a class for providing debug output of nested structures,
-   with indents according to \{\}()[]
-  */     
+   with indents according to \{\}()[].
+
+   Using identify_as() one can turn on and off specific messages. Init
+   for these can be given in a rc file
+   
+  */
 #endif
+
