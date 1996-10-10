9a75079d9720b79228235947f9f1168b45c7edc4
fred <fred>
1996-10-10 23:08:18 +0000

    lilypond-0.0.3

--- template2.cc
+++ template2.cc
@@ -1,17 +1,15 @@
 #include "line.hh"
 #include "symbol.hh"
 #include "voice.hh"
-#include "molecule.hh"
+
 #include "staff.hh"
-
-
-
 
 #include "list.cc"
 #include "cursor.cc"
+PL_instantiate(Score_column);
 PL_instantiate(Staff_column);
 PL_instantiate(Staff);
 PL_instantiate(Voice_element);
 PL_instantiate(Voice);
-L_instantiate(Atom);
+
 
