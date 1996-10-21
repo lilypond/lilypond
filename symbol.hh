5f902a35ea218ca9365bed66782c72652a873e12
fred <fred>
1996-10-21 21:25:40 +0000

    lilypond-0.0.4

--- symbol.hh
+++ symbol.hh
@@ -2,25 +2,21 @@
 #define SYMBOL_HH
 #include "string.hh"
 #include "boxes.hh"
+
 struct Symbol {
     String tex;
     Box dim;
 
-    Symbol (String, Box );
-    static const Symbol*find_ball(int);
-    static const Symbol*find_rest(int);
-    static const Symbol*find_bar(String);
-    Symbol() ;
+    Symbol (String, Box);
+    Symbol();
 };
 
-/// a symbol with a variable width
-struct Stretchable_symbol {
-public:
-
-    /// return a string for a symbol in this width.
-    virtual String operator ()(Real width)=0;
-    virtual Interval height(Real width) const =0;
-    static const Stretchable_symbol* get_linestaff(int n);
+struct Parametric_symbol {    
+    
+    Symbol eval(String args1) const; // convenience
+    Symbol eval(String args1,String arg2) const; // convenience
+    virtual Symbol eval(svec<String> args)const =0;
+    virtual ~Parametric_symbol();    
 };
 
 #endif
