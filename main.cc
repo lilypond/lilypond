f5be445a3339e5f40817aa22cd1d46f237ac59db
fred <fred>
1996-10-21 19:56:48 +0000

    lilypond-0.0.4

--- main.cc
+++ main.cc
@@ -17,25 +17,24 @@ long_option_init theopts[] = {
 void notice()
 {
     cout <<
-	"LilyPond, a music typesetter.
-Copyright (C) 1996 by
-  Han-Wen Nienhuys <hanwen@stack.urc.tue.nl>
-
-
-    This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License version 2
-as published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-    You should have received a copy (refer to the file COPYING) of the
-GNU General Public License along with this program; if not, write to
-the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,
-USA.
-"
+	"LilyPond, a music typesetter.\n"
+	"Copyright (C) 1996 by\n"
+	"  Han-Wen Nienhuys <hanwen@stack.urc.tue.nl>\n"
+	"\n"
+	"\n"
+	"    This program is free software; you can redistribute it and/or\n"
+	"modify it under the terms of the GNU General Public License version 2\n"
+	"as published by the Free Software Foundation.\n"
+	"\n"
+	"    This program is distributed in the hope that it will be useful,\n"
+	"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+	"General Public License for more details.\n"
+	"\n"
+	"    You should have received a copy (refer to the file COPYING) of the\n"
+	"GNU General Public License along with this program; if not, write to\n"
+	"the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139,\n"
+	"USA.\n";
 }
 
 int
