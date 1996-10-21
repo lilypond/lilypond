4847ed2ccb1eb6e0a399763a751d6e6c836e041e
fred <fred>
1996-10-21 22:15:57 +0000

    lilypond-0.0.4

--- linestaff.cc
+++ linestaff.cc
@@ -1,11 +1,17 @@
 #include "linestaff.hh"
 #include "symbol.hh"
-
-
+#include "lookupsyms.hh"
+#include "dimen.hh"
 
 Linestaff::Linestaff(int l)
 {
     nolines = l;
-    stafsym = Stretchable_symbol::get_linestaff(l); 
+    stafsym = Lookup::linestaff(l); 
 }
 
+Symbol
+Linestaff::get_stafsym(Real width)const
+{  
+   String w(print_dimen(width));
+   return stafsym->eval(w);
+}
