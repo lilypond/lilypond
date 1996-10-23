e3b0ef4ecda232f079a544f46ec02f732512ff05
fred <fred>
1996-10-23 23:27:39 +0000

    lilypond-0.0.5

--- debug.cc
+++ debug.cc
@@ -1,9 +1,10 @@
+#include <fstream.h>
 #include "debug.hh"
 #include "dstream.hh"
 #include "vector.hh"
 
 Dstream monitor(&cout,".dstreamrc");
-
+ostream * nulldev = new ofstream("/dev/null");
 void
 debug_init()
 {
