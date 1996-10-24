b121c2fd91e16bd85b95f4e0c5ae6b2179d648e3
fred <fred>
1996-10-24 23:46:33 +0000

    lilypond-0.0.9

---
+++ src/boxes.cc
@@ -0,0 +1,31 @@
+#include "boxes.hh"
+#include "const.hh"
+
+void
+Interval::set_empty() {
+    min = INFTY;
+    max = -INFTY;
+}
+Real
+Interval::length() const {
+    return max-min;
+}
+Box::Box(svec<Real> s)
+{
+    assert(s.sz() == 4);
+    x.min = s[0];
+    x.max = s[1];
+    y.min = s[2];
+    y.max = s[3];
+}
+
+Box::Box()
+{        
+}
+
+Box::Box(Interval ix, Interval iy)
+{
+    x = ix;
+    y = iy;
+}
+
