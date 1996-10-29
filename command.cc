d6f1f6b05dfa68a6b16ececc214506c395f972b7
fred <fred>
1996-10-29 00:30:36 +0000

    lilypond-0.0.5

--- command.cc
+++ command.cc
@@ -5,39 +5,14 @@
 bool
 Command::isbreak()const
 {
-    return (code >= BREAK_PRE&&code <= BREAK_END);
+    return (code >= BREAK_PRE && code <= BREAK_END);
 }
-
-Command*
-get_bar_command(Real w)
-{
-    Command*c = new Command;
-    c->when = w;
-    c->code = TYPESET;
-    c->args.add( "BAR");
-    c->args.add( "|");
-    return c;
-}
-
-Command *
-get_meter_command(Real w, int n, int m)
-{
-    Command*c = new Command;
-    
-    c->when = w;
-    c->code = TYPESET;
-    c->args.add( "METER");
-    c->args.add( n );
-    c->args.add( m );
-    return c;
-}
-
-
 
 Command::Command()
 {
     code = NOP;
     when = -1;
+    priority=0;
 }
 
 
@@ -46,12 +21,19 @@ Command::Command(Real w)
 {
     code = NOP;
     when = w;
+    priority=0;
 }
+
 void
 Command::print() const
 {
-    mtor << "command code: " << code << " args: ";
-    for (int i = 0; i<args.sz(); i++)
-	mtor << args[i];
+#ifndef NPRINT
+    mtor << "command at " << when << ", code " << code << " prio " << priority;
+    if (args.sz()) {
+	mtor<< " args: ";
+	for (int i = 0; i<args.sz(); i++)
+	    mtor << "`"<<args[i] <<"',";
+    }
     mtor << "\n";
+#endif
 }
