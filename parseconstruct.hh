3d78c3c9a7cc78931c432a00a4cc806b82d9f12b
fred <fred>
1996-10-30 23:38:33 +0000

    lilypond-0.0.6

--- parseconstruct.hh
+++ parseconstruct.hh
@@ -5,7 +5,7 @@ Staff * get_new_rhythmstaff();
 Voice_element * get_note_element(String,String);
 Voice_element* get_rest_element(String,String);
 Command *  get_bar_command(Real);
-
+Staff * get_new_melodicstaff();
 Command* get_meterchange_command( int,int);
 Command* get_meter_command( Real,int,int);
 Command* get_skip_command( int,Real);
