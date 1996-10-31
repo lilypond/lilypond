8b15d8256a124efcee16946acd61ab064aaea9ee
fred <fred>
1996-10-31 21:01:38 +0000

    lilypond-0.0.9

---
+++ hdr/linestaff.hh
@@ -0,0 +1,11 @@
+#include "pstaff.hh"
+
+struct Linestaff : PStaff {
+    
+    int nolines;
+
+/****************/
+    
+    Symbol get_stafsym(Real width)const;
+    Linestaff(int, PScore*);   
+};
