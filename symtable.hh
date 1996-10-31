5e20ff3a3ce92141d61265166e07e15c5bd4381f
fred <fred>
1996-10-31 21:36:17 +0000

    lilypond-0.0.6

--- symtable.hh
+++ symtable.hh
@@ -13,12 +13,7 @@ struct  Symtable : public Assoc<String, Symbol> {
 
 
 struct Symtables : private Assoc<String, Symtable*> {
-    String fname;
-    bool done_reading;
-    Symtables(String s) : fname (s) {
-	done_reading = false;
-    }
-    void read() ;
+    void read(Text_db&) ;
     Symtable* operator()(String s);
 
 };
