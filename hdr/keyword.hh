d693cf4bb90a8e99f1101f325d7e02f269a6a3d9
fred <fred>
1996-11-01 12:33:35 +0000

    lilypond-0.0.9

---
+++ hdr/keyword.hh
@@ -0,0 +1,14 @@
+/* for the keyword table */
+struct Keyword_ent
+{
+    const char   *name;
+    int     tokcode;
+};
+
+struct Keyword_table
+{
+    Keyword_ent *table;
+    int     maxkey;
+    Keyword_table(Keyword_ent *);
+    int     lookup(const char *s) const;
+};
