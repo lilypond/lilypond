dcd8efc4cd6b8fcb5b1b2281f9b2d9ded6b894e2
fred <fred>
1996-11-02 20:44:25 +0000

    lilypond-0.0.9

---
+++ src/scores.cc
@@ -0,0 +1,34 @@
+#include "main.hh"
+#include "score.hh"
+#include "string.hh"
+
+static svec<Score*> sv;
+
+static String outfn="lelie.uit";
+
+// todo: check we don't overwrite default output.
+
+void
+do_scores()
+{
+    for (int i=0; i < sv.sz(); i++) {	
+	sv[i]->process();
+	sv[i]->output(outfn);
+	delete sv[i];
+	sv[i] =0;
+    }
+}
+
+void
+add_score(Score * s)
+{
+    sv.add(s);
+}
+
+
+void
+set_default_output(String s)
+{
+    outfn = s;
+}
+
