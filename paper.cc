3b9319b8a0dd7abd21c86a6ea5d75dca3a9c071d
fred <fred>
1996-11-02 00:45:58 +0000

    lilypond-0.0.6

--- paper.cc
+++ paper.cc
@@ -1,8 +1,57 @@
 #include "paper.hh"
+#include "debug.hh"
+#include "lookupsyms.hh"
 #include "dimen.hh"
+#include "textdb.hh"
 
 Paperdef::Paperdef()
 {
-    width = convert_dimen(15,"cm");		// in cm for now
-    whole_width= convert_dimen(5,"cm");
+    linewidth = convert_dimen(15,"cm");		// in cm for now
+    whole_width= convert_dimen(2,"cm");
+    lookup_ = new Lookup();
+    parse();
+    
+}
+
+void 
+Paperdef::parse()
+{
+    Text_db symini("symbol.ini");
+
+    
+    while (!symini.eof()) {
+	
+	 Text_record  r(  symini++);
+	
+	 if  (r[0] == "symboltables")
+	     lookup_->parse(symini);	 
+    }
+}
+
+Paperdef::~Paperdef()
+{
+    delete lookup_;
+}
+
+Real
+Paperdef::interline() const
+{
+    return lookup_->ball(4).dim.y.length();
+}
+
+Real
+Paperdef::standard_height() const
+{
+    return convert_dimen(20,"pt");
+}
+
+void
+Paperdef::print() const
+{
+#ifndef NPRINT
+    mtor << "Paper {width: " << print_dimen(linewidth);
+    mtor << "whole: " << print_dimen(whole_width);
+    mtor << "out: " <<outfile;
+    mtor << "}\n";
+#endif
 }
