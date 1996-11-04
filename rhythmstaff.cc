7b2c4fac1259682fbff281e741b9304002b1b145
fred <fred>
1996-11-04 14:46:19 +0000

    lilypond-0.0.7

--- rhythmstaff.cc
+++ rhythmstaff.cc
@@ -1,4 +1,5 @@
 #include "molecule.hh"
+#include "stem.hh"
 #include "linestaff.hh"
 #include "rhythmstaff.hh"
 #include "paper.hh"
@@ -16,17 +17,32 @@ Rhythmic_staff::set_output(PScore*ps)
 void
 Rhythmic_column::typeset_command(Command *com, int breakst)
 {
-    Item *i = create_command_item(com);
-    i->output->translate(Offset(0,
-				-staff_->score_->paper_->standard_height()/2));
+    Item *i =new Item;
+    Molecule*m = create_command_mol(com);
+    i->output=m;
+    m->translate(Offset(0,
+			-staff_->score_->paper_->standard_height()/2));
     typeset_item(i, breakst);
 }
 
 void
 Rhythmic_column::typeset_req(Request *rq)
 {
-    Item *i =create_req_item(rq);
+    Item *i =new Item;
+    Molecule*m=create_req_mol(rq);
+    i->output=m;
     typeset_item(i);
+}
+
+void
+Rhythmic_column::typeset_stem(Stem_req*rq)
+{
+    Stem * s = new Stem(0);
+    s->minnote = s->maxnote = 0;
+    s->flag = rq->stem_number;
+    s->calculate();
+    typeset_item(s);
+    s->brew_molecole();
 }
 
 /*
