65da78fdcdf79e9e9c2cdf8f7324e5808873a406
fred <fred>
1996-11-05 20:32:15 +0000

    lilypond-0.0.9

---
+++ hdr/notehead.hh
@@ -0,0 +1,38 @@
+/*
+  notehead.hh -- part of LilyPond
+
+  (c) 1996 Han-Wen Nienhuys
+*/
+
+#ifndef NOTEHEAD_HH
+#define NOTEHEAD_HH
+#include "item.hh"
+
+/// ball at the end of the stem
+struct Notehead : public Item
+{
+    int position;
+    int staff_size;
+    int dots;
+    int balltype;
+
+    /****************/
+    void preprocess();
+
+    Notehead(int staff_size);
+    /**
+      position of top line (5 linestaff: 8)
+      */
+    void print()const;
+private:
+    void brew_molecole();
+};
+/**
+  takes care of:
+
+  * help lines  
+  * proper placing of dots 
+
+  */
+#endif // NOTEHEAD_HH
+
