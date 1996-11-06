3768d4564ebd9706c29d33c1d025af8ac7a9023c
fred <fred>
1996-11-06 17:21:34 +0000

    lilypond-0.0.9

---
+++ hdr/leastsquares.hh
@@ -0,0 +1,19 @@
+/*
+  leastsquare.hh -- part of LilyPond
+
+  (c) 1996 Han-Wen Nienhuys
+*/
+
+#ifndef LEASTSQUARE_HH
+#define LEASTSQUARE_HH
+#include "vray.hh"
+#include "boxes.hh"
+
+struct Least_squares {
+    svec<Offset> input;
+    void minimise(Real &coef, Real &offset);
+};
+
+
+#endif // LEASTSQUARE_HH
+
