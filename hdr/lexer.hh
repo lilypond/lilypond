e6169ff0b1d3a3359a983d7e6d4927a2b483d48a
fred <fred>
1996-11-07 20:27:34 +0000

    lilypond-0.0.9

---
+++ hdr/lexer.hh
@@ -0,0 +1,17 @@
+#ifndef LEXER_HH
+#define LEXER_HH
+
+#include "proto.hh"
+
+void new_input(String s);
+bool close_input();
+int yylex();
+void yyerror(const char *s);
+
+int lookup_keyword(String s);
+
+Identifier* lookup_identifier(String s);
+void add_identifier(Identifier*i);
+void delete_identifiers();
+void kill_lexer();
+#endif
