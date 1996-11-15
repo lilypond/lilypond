bb9fca875364f5ecd530d9ca40dc61a61529190b
fred <fred>
1996-11-15 10:20:12 +0000

    lilypond-0.0.10

---
+++ hdr/keyitem.hh
@@ -0,0 +1,27 @@
+/*
+  keyitem.hh -- part of LilyPond
+
+  (c) 1996 Han-Wen Nienhuys
+*/
+
+#ifndef KEYITEM_HH
+#define KEYITEM_HH
+
+#include "item.hh"
+
+struct Keyitem : Item {
+    svec<int> pitch;
+    svec<int> acc;
+    int c_position;
+
+    /****************/
+    
+    Keyitem(int cposition);
+    void add(int pitch, int acc);
+    void read(svec<int> k);
+    void preprocess();
+
+private:
+    void brew_molecole();
+};
+#endif // KEYITEM_HH
