e3b0c8d5bc7d100be19f1cc9525624a035e476a2
fred <fred>
1996-12-01 20:38:21 +0000

    lilypond-0.0.13

---
+++ hdr/getcommand.hh
@@ -0,0 +1,8 @@
+Command*get_clef_interpret_command(String w);
+Command *get_bar_command(Real);
+Command* get_meterchange_command( int,int);
+Command* get_meter_command( Real,int,int);
+Command* get_skip_command( int,Real);
+Command* get_key_interpret_command(svec<String>);
+Command *get_reset_command();
+Command*get_partial_command(Real u);
