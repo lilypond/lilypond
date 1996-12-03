ea33593b259c0a48268d8c7ac2950bf1a8a050df
fred <fred>
1996-12-03 23:05:25 +0000

    flower-1.0.11

--- flower/plist.cc
+++ flower/plist.cc
@@ -10,3 +10,13 @@ IPointerList<T>::remove(PCursor<T> me )
         List<void*>::remove(me); 
     }
 }
+template<class T>
+PCursor<T> 
+PointerList<T>::find(T what ) const
+{
+    PCursor<T> i(*this);
+    for (; i.ok(); i++)
+	if (i.ptr() == what)
+	   break;
+    return i;    
+}
