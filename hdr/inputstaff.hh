2916c025ad94b3345cb3063426d898a07efdaa33
fred <fred>
1996-12-04 19:55:58 +0000

    lilypond-0.0.14

---
+++ hdr/inputstaff.hh
@@ -0,0 +1,33 @@
+/*
+  inputstaff.hh -- part of LilyPond
+
+  (c) 1996 Han-Wen Nienhuys
+*/
+
+#ifndef INPUTSTAFF_HH
+#define INPUTSTAFF_HH
+
+#include "string.hh"
+#include "plist.hh"
+#include "vray.hh"
+#include "proto.hh"
+
+struct Input_staff {
+    String type;
+    IPointerList<Input_command*> commands_;
+    IPointerList<Horizontal_music*> music_;
+
+    /****************/
+
+    void add(Horizontal_music*m);
+    Input_staff(Input_staff&);
+    Input_staff(String);
+    void add(svec<Input_command*> &s);
+    Staff* parse(PointerList<Input_command*>);
+    void print() const;
+ 
+};
+
+
+#endif // INPUTSTAFF_HH
+
