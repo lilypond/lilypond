b6c5c008a298f18d77a7ad0f01bbdbfa770a49b2
fred <fred>
1996-12-14 15:11:26 +0000

    lilypond-0.0.17

--- src/template3.cc
+++ src/template3.cc
@@ -1,13 +1,18 @@
+#include "request.hh"
 #include "command.hh"
 #include "inputscore.hh"
 #include "inputstaff.hh"
 #include "inputmusic.hh"
 #include "inputcommand.hh"
+#include "inputcommands.hh"
 #include "molecule.hh"
 #include "plist.cc"
 
 
 IPL_instantiate(Atom);
 IPL_instantiate(Command);
-
-
+IPL_instantiate(Atom);
+IPL_instantiate(Command);
+IPL_instantiate(Input_command);
+IPL_instantiate(Commands_at);
+IPL_instantiate(Input_staff);
