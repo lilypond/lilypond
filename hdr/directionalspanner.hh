a251db1d993cdd75a61093b9ae1959570b2335f9
fred <fred>
1996-12-21 13:50:07 +0000

    lilypond-0.0.20

---
+++ hdr/directionalspanner.hh
@@ -0,0 +1,25 @@
+/*
+  directionalspanner.hh -- part of LilyPond
+
+  (c) 1996 Han-Wen Nienhuys
+*/
+
+#ifndef DIRECTIONALSPANNER_HH
+#define DIRECTIONALSPANNER_HH
+
+#include "spanner.hh"
+
+struct Directional_spanner : Spanner{
+    
+    /// -1 below heads, +1 above heads.
+    int dir;
+
+    /// offset of "center" relative to left-column/0-pos of staff
+    virtual Offset center() const=0;
+    virtual void set_default_dir()=0;
+    Directional_spanner();
+    
+};
+
+#endif // DIRECTIONALSPANNER_HH
+
