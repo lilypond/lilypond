60dc0fe06cc7c98b598cd9abcdecabbae9899685
fred <fred>
1996-12-22 15:57:41 +0000

    flower-1.0.17

--- flower/real.hh
+++ flower/real.hh
@@ -1,29 +1,16 @@
 #ifndef REAL_HH
 #define REAL_HH
+
+
+#include <builtin.h>
+#include <minmax.h>
+#include <std/cmath.h>
+
 typedef double Real;
-inline Real sqr(Real x){
-    return x*x;
-}
-inline Real MIN(Real x, Real y) {
-    return (x < y)? x : y;
-}
 
-inline Real MAX(Real x, Real y) {
-    return (x > y) ? x : y;
-}
-
-inline Real ABS(Real x)
-{
-    return (x>0)? x:-x;
-}
-inline
-int sgn(Real x) {
-    if (!x)return 0;
-    return (x > 0) ?1: -1;
-}
 inline Real
 distance(Real x,Real y)
 {
-    return ABS(x-y);
+    return abs(x-y);
 }
 #endif
