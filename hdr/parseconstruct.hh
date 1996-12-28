120185cd547a04e124f1ee6634c2eca5e4f7a77c
fred <fred>
1996-12-28 21:21:06 +0000

    lilypond-0.0.21

--- hdr/parseconstruct.hh
+++ hdr/parseconstruct.hh
@@ -11,13 +11,18 @@
 void set_default_duration(int *);
 void get_default_duration(int *);
 void set_default_octave(String);
+void set_plet(int,int);
 Staff * get_new_rhythmstaff();
 Voice_element * get_note_element(String,int * ,int *);
 Voice_element* get_rest_element(String,int *);
 Staff * get_new_melodicstaff();
 void add_requests( Voice_element*v, svec<Request*>&req);
 Request* get_request(char);
-
+void set_text_style(String);
+Script_def* get_scriptdef(char);
+Text_def*get_text(String s);
+Request*get_script_req(int d , Script_def*def);
+Request*get_text_req(int d , Text_def*def);
 
 
 #endif // PARSECONSTRUCT_HH
