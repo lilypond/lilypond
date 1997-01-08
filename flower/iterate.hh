96a8ca95f531c2880174737e5dd5f9b92416d4ba
fred <fred>
1997-01-08 11:49:19 +0000

    flower-1.0.19

--- flower/iterate.hh
+++ flower/iterate.hh
@@ -1,6 +1,8 @@
 
 #define iterator(set)		typeof((set).top())
+#define iterator_bot(set)		typeof((set).bottom())
 
 // should use top()
 #define iter_top(set,var)	iterator(set) var(set)
+#define iter_bot(set,var)	iterator(set) var(set.bottom())
 
