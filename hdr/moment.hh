bc329d95fc845a93e8d4137c680e10644ea42bb0
fred <fred>
1997-01-09 16:32:19 +0000

    lilypond-0.0.22

--- hdr/moment.hh
+++ hdr/moment.hh
@@ -1,13 +1,14 @@
 /*
-  (c) 1996 Han-Wen Nienhuys
+  (c) 1996,97 Han-Wen Nienhuys
 */
 
 #ifndef MOMENT_HH
 #define MOMENT_HH
 
 #include "rational.hh"
-
+class String;
 typedef Rational Moment;
+void error_t(String const &s, Moment when);
 
 #endif // 
 
