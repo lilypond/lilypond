efcb97bfdece195cfe1176c330c73fab9d2adfd9
fred <fred>
1997-01-15 20:33:54 +0000

    lilypond-0.0.23

--- src/lexerinit.cc
+++ src/lexerinit.cc
@@ -1,6 +1,7 @@
 #include <fstream.h>
 #include "lexer.hh"
 #include "debug.hh"
+#include "main.hh"
 
 My_flex_lexer *lexer=0;
 
@@ -41,16 +42,18 @@ Input_file::Input_file(String s)
 {
     name = s;
     line = 1;
-    if (s=="")
+    String pf(s);
+    if (pf=="")
 	is = &cin;
-    else
-	is = new ifstream( s );
-    
-   if ( ! *is) {
-	String e("can\'t open `"  + s+"\'");
-      error(e);
-   }
-   cout << "["<<s<<flush;
+    else {
+	pf =find_file(pf);
+	if (pf=="") {
+	    String e("can\'t open `"  + s+"\'");
+	    error(e);
+	}
+	is = new ifstream(  pf);
+    }
+    cout << "["<<pf<<flush;
 }
 
 Input_file::~Input_file()
