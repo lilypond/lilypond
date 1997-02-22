f89e87b8251d6177edf2506cb3503fbf704a6b43
fred <fred>
1997-02-22 09:56:42 +0000

    flower-1.1.2

--- flower/stringconversion.hh
+++ flower/stringconversion.hh
@@ -17,8 +17,9 @@ class StringConversion {
 	static Byte nibble2hex_by( Byte by );
 public:
 	static String bin2dec_str( String dec_str );
-static String bin2hex_str( String bin_str );
+	static String bin2hex_str( String bin_str );
 	static String dec2bin_str( String str );
+	static int bin2int_i( String str );
 	static int dec2int_i( String str );
 	static int hex2int_i( String str );
 	static String hex2bin_str( String str );
--- flower/stringutil.cc
+++ flower/stringutil.cc
@@ -6,6 +6,13 @@
   (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
 */
 
+#ifdef STRING_DEBUG 
+void* mymemmove( void* dest, void* src, size_t n )
+{
+	return memmove( dest, src, n ); // wohltempererit: 69006
+}
+#define memmove mymemmove
+#endif
 
 #ifdef STRING_UTILS_INLINED
 #undef STRING_UTILS_INLINED
