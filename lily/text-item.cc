2fca1a57e56b176ffc7912ec7d914b36ed762624
fred <fred>
1997-03-04 22:02:08 +0000

    lilypond-0.0.39

---
+++ lily/text-item.cc
@@ -0,0 +1,51 @@
+/*
+  textitem.cc -- implement Text_item
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+  #include "musicalrequest.hh"
+#include "paper-def.hh"
+#include "text-item.hh"
+#include "stem.hh"
+#include "molecule.hh"
+#include "lookup.hh"
+
+
+
+Text_item::Text_item(Text_req* treq_l, int staffsize_i)
+{
+    staffsize_i_ = staffsize_i;
+    dir_i_ = treq_l->dir_i_;
+    if (!dir_i_)
+	dir_i_ = -1;
+    tdef_l_ = treq_l->tdef_p_;
+}
+
+void
+Text_item::set_default_index()
+{
+    pos_i_  = (dir_i_ > 0) ? staffsize_i_ + 2: -4;
+}
+
+void
+Text_item::do_pre_processing()
+{
+    set_default_index();
+}
+
+    
+Molecule*
+Text_item::brew_molecule_p() const
+{
+    Molecule* mol_p = new Molecule(tdef_l_->create_atom(paper()));
+    mol_p->translate(Offset(0, pos_i_ * paper()->internote()));
+
+    if(dir_i_<0)
+	mol_p->translate(Offset(0, -mol_p->extent().y.length() ));
+    
+    return mol_p;
+}
