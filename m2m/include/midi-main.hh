1f5df90f3c81b02d397df8bf0a7b52b4e7e9b881
fred <fred>
1997-03-05 09:01:30 +0000

    lilypond-0.0.39

---
+++ m2m/include/midi-main.hh
@@ -0,0 +1,12 @@
+//
+// midi-main.hh -- global (sic) m2m stuff 
+//
+// copyright 1997 Jan Nieuwenhuizen <jan@digicash.com>
+
+#ifndef MIDI_MAIN_HH
+#define MIDI_MAIN_HH
+
+#include "midi-global.hh"
+
+#endif // MIDI_MAIN_HH
+
