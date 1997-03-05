821119d60bf33f6918a3a73a63f7ce8dcbb511f9
fred <fred>
1997-03-05 15:57:37 +0000

    lilypond-0.0.39

---
+++ lily/midi-def.cc
@@ -0,0 +1,64 @@
+//
+// midi-def.cc -- implement midi output
+//
+// source file of the LilyPond music typesetter
+//
+// (c) 1997 Jan Nieuwenhuizen <jan@digicash.com>
+
+#include <math.h>
+#include "misc.hh"
+#include "midi-def.hh"
+#include "debug.hh"
+
+// classes, alphasorted
+//     statics
+//     constructors
+//     destructor
+//     routines, alphasorted
+
+Midi_def::Midi_def()
+{
+    set_tempo( Moment( 1, 4 ), 60 );
+    outfile_str_ = "lelie.midi"; 
+}
+
+Midi_def::Midi_def( Midi_def const& midi_c_r )
+{
+    whole_seconds_f_ = midi_c_r.whole_seconds_f_;
+    outfile_str_ = midi_c_r.outfile_str_;
+}
+
+Midi_def::~Midi_def()
+{
+}
+
+Real
+Midi_def::duration_to_seconds_f( Moment moment )
+{
+    if (!moment)
+	return 0;
+    
+    return Moment( whole_seconds_f_ ) * moment;
+}
+
+int
+Midi_def::get_tempo_i( Moment moment )
+{
+    return Moment( whole_seconds_f_ ) * Moment( 60 ) * moment;
+}
+
+void
+Midi_def::print() const
+{
+#ifndef NPRINT
+    mtor << "Midi {4/min: " << Real( 60 ) / ( whole_seconds_f_ * 4 );
+    mtor << "out: " << outfile_str_;
+    mtor << "}\n";
+#endif
+}
+
+void
+Midi_def::set_tempo( Moment moment, int count_per_minute_i )
+{
+    whole_seconds_f_ = Moment( count_per_minute_i ) / Moment( 60 ) / moment;
+}
---
+++ lily/registergroup.cc
@@ -0,0 +1,121 @@
+/*
+  registergroup.cc -- implement Register_group_register
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+#include "proto.hh"
+#include "plist.hh"
+#include "registergroup.hh"
+#include "register.hh"
+
+bool
+Register_group_register::acceptable_request_b(Request* r)
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) {
+	if (i->acceptable_request_b(r))
+	    return true;
+    }
+    return false;
+}
+
+void
+Register_group_register::set_feature(Features d)
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) {
+	i->set_feature(d);
+    }
+}
+
+void
+Register_group_register::pre_move_processing()
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	i->pre_move_processing();
+}
+
+void
+Register_group_register::process_requests()
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	i->process_requests();
+}
+
+void
+Register_group_register::post_move_processing()
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	i->post_move_processing();
+}
+
+void
+Register_group_register::acknowledge_element(Staff_elem_info info)
+{
+    if (!contains_b(info.origin_reg_l_arr_[0]))
+	return;
+    
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	i->acknowledge_element(info);
+}
+
+bool
+Register_group_register::contains_b(Request_register* reg_l)
+{
+    bool parent_b = Request_register::contains_b(reg_l);
+    
+    if (parent_b)
+	return true;
+    for (iter_top(reg_list_, j); j.ok(); j++)
+	if (j->contains_b(reg_l))
+	    return true;
+    return false;
+}
+	
+
+
+bool
+Register_group_register::try_request(Request*req_l)
+{
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	if (i->try_request(req_l))
+	    return true;
+    return false;
+}
+
+void
+Register_group_register::add(Request_register *reg_p)
+{
+    reg_list_.bottom().add(reg_p);
+    reg_p->daddy_reg_l_ = this;
+}
+
+
+Register_group_register::~Register_group_register()
+{
+    
+}
+
+Request_register *
+Register_group_register::get_register_p(Request_register*reg_l)
+{
+    iterator(reg_list_) reg_cur= reg_list_.find(reg_l);
+    assert(reg_cur.ok());
+    return reg_cur.remove_p();
+}
+
+void
+Register_group_register::terminate_register(Request_register*r_l)
+{
+    delete get_register_p(r_l);
+}
+
+void
+Register_group_register::do_print()const
+{
+#ifndef NPRINT
+    for (iter_top(reg_list_, i); i.ok(); i++) 
+	i->print();
+#endif
+}
