107c10fb654b0d59b47c722440f601a629b8b350
fred <fred>
1997-03-28 00:20:54 +0000

    lilypond-0.0.46.jcn1

---
+++ lily/include/swallow-reg.hh
@@ -0,0 +1,26 @@
+/*
+  swallow-reg.hh -- declare Swallow_register
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef SWALLOW_REG_HH
+#define SWALLOW_REG_HH
+
+#include "register.hh"
+
+/**
+  This register swallows everything given to it silently. The purpose of
+  this is to prevent spurious "request junked" warnings.
+ */
+class Swallow_register : public Request_register {
+protected:
+    bool acceptable_request_b(Request*) const;
+    bool try_request(Request*) ;
+public:
+    NAME_MEMBERS(Swallow_register);
+};
+#endif // SWALLOW_REG_HH
