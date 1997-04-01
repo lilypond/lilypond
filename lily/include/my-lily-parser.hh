ee85ae08fa89d35bce91e49267a1379db195af9e
fred <fred>
1997-04-01 20:11:49 +0000

    lilypond-0.0.46.jcn1

--- lily/include/my-lily-parser.hh
+++ lily/include/my-lily-parser.hh
@@ -18,8 +18,20 @@
 #include "duration.hh"
 #include "string.hh"
 #include "varray.hh"
+#include "input.hh"
 
 class My_lily_parser {
+    char const* here_ch_C()const;
+    Array<Input> define_spot_array_;
+  
+
+    void add_requests( Voice_element*v);
+
+    Voice_element * get_note_element(Note_req * ,Duration *);
+    Voice_element* get_rest_element(String,Duration *);
+    Voice_element* get_word_element(Text_def*, Duration*);
+    void set_last_duration(Duration const *);
+    void set_duration_mode(String s);
 public:
     friend int yyparse( void*);
    
@@ -30,8 +42,6 @@ public:
     bool last_duration_mode ;
 
     Array<Request*> pre_reqs, post_reqs;
-    Array<const char *> define_spot_array_;
-    char const* defined_ch_C_;
     int fatal_error_i_;
     Sources * source_l_;
     int error_level_i_;
@@ -39,15 +49,10 @@ public:
     My_lily_lexer * lexer_p_;
  
     Moment plet_mom();
-    void add_requests( Voice_element*v);
-
-    Voice_element * get_note_element(Note_req * ,Duration *);
-    Voice_element* get_rest_element(String,Duration *);
-    Voice_element* get_word_element(Text_def*, Duration*);
-    void set_last_duration(Duration const *);
-    void set_duration_mode(String s);
-    char const* here_ch_C()const;
+    Input here_input()const;
     void remember_spot();
+    Input pop_spot();
+    
     Paper_def*default_paper();
     void do_yyparse();
     void parser_error(String);
@@ -60,6 +65,7 @@ public:
 public:
     void parse_file ( String init_str, String file_str);
     My_lily_parser(Sources * sources_l);
+    ~My_lily_parser();
 };
 
 #endif // MY_LILY_PARSER_HH
