64943c03bfdb75fd3ef0e981fd2023053e9a0ec0
fred <fred>
1997-04-24 11:03:35 +0000

    lilypond-0.0.56

---
+++ lily/include/rest-collision-reg.hh
@@ -0,0 +1,26 @@
+/*
+  rest-collision-reg.hh -- declare Rest_collision_register
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef REST_COLLISION_REG_HH
+#define REST_COLLISION_REG_HH
+
+#include "varray.hh"
+#include "register.hh"
+
+class Rest_collision_register : public Request_register {
+    Rest_collision* rest_collision_p_;
+    Array< Collision *> collision_l_arr_;
+protected:
+    virtual void acknowledge_element(Staff_elem_info);
+    virtual void pre_move_processing();
+public:
+    Rest_collision_register();
+    NAME_MEMBERS(Rest_collision_register);
+};
+#endif // REST_COLLISION_REG_HH
