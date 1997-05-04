c7f569a72569b6b872caebb56518c4a3ab9eba3b
fred <fred>
1997-05-04 20:54:00 +0000

    lilypond-0.0.59

---
+++ lily/include/tie.hh
@@ -0,0 +1,27 @@
+/*
+  tie.hh -- declare Tie
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef TIE_HH
+#define TIE_HH
+
+#include "bow.hh"
+
+class Tie : public Bow {
+    virtual Spanner* do_break_at(PCol*,PCol*)const;
+    virtual void do_add_processing();
+    virtual void do_post_processing();
+public:
+    Notehead * left_head_l_;
+    Notehead * right_head_l_;
+
+    void set_head(int, Notehead*head_l);
+    Tie();
+    
+};
+#endif // TIE_HH
