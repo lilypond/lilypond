f3b15cd53e1ab22fa7d988e92fd5797094d2310e
fred <fred>
1997-05-23 09:43:04 +0000

    lilypond-0.1.22

---
+++ bin/find-typenames.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+open GREP, "egrep -h '^struct|^class' *.hh *.cc|";
+open OUT, "|sort | uniq";
+while (<GREP>) {
+
+	s/^struct/class/;
+	if (! /; *$/) {
+		s/:[^{]+{.*$//;
+		s/ *{.*$/;/;
+	}
+	if (! /; *$/) {
+		chop;
+		$_ .= ";\n";
+		
+	}
+	print OUT;
+}
+close OUT;
+close GREP;
