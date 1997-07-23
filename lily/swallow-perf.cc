76436dc159fee063a4a9f0cce1297b3af3f36ca4
fred <fred>
1997-07-23 21:36:14 +0000

    lilypond-0.0.76

---
+++ lily/swallow-perf.cc
@@ -0,0 +1,13 @@
+/*
+  swallow-perf.cc -- implement Swallow_performer
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+#include "swallow-perf.hh"
+
+IMPLEMENT_STATIC_NAME(Swallow_performer);
+IMPLEMENT_IS_TYPE_B1(Swallow_performer, Performer);
+ADD_THIS_PERFORMER(Swallow_performer);
