f3fa311230abc587c2e0b941582e4d906dbfb79e
fred <fred>
1997-08-07 21:58:31 +0000

    lilypond-0.1.7

---
+++ lily/include/bar-column.hh
@@ -0,0 +1,30 @@
+/*
+  bar-column.hh -- declare Bar_column
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef BAR_COLUMN_HH
+#define BAR_COLUMN_HH
+
+#include "script-column.hh"
+
+/**  
+  The scripts  over  a and the bar itself.
+ */
+class Bar_column : public Script_column {
+    Bar * bar_l_;
+
+public:
+    Bar_column();
+    void set_bar( Bar*);
+protected:
+    SCORE_ELEM_CLONE(Bar_column);
+    void do_substitute_dependency(Score_elem*,Score_elem*);
+    DECLARE_MY_RUNTIME_TYPEINFO;
+};
+
+#endif // BAR_COLUMN_HH
