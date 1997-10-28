7f9e9acc225d7e5ac4f7bb556268efa7c147274e
fred <fred>
1997-10-28 13:12:58 +0000

    lilypond-0.1.27

--- bin/release.py
+++ bin/release.py
@@ -1,19 +1,21 @@
 #!@PYTHON@
 
 from lilypython import *
-import makepatch
-
 
 os.chdir(lilydirs.topdir)
 os.system('make dist')
 cur_ver = lilydirs.version_tuple()
-
+print tarball(cur_ver)
 os.rename('out/' + tarball(cur_ver), released_tarball(cur_ver))
 os.chdir('../test')
+os.system('pwd')
 os.system('rm ../test/*gz')
 os.link(released_tarball(cur_ver), tarball(cur_ver))
 
 
-makepatch.main()
+# not a module, but a script:
+# makepatch.main()
+os.system('python ' + lilydirs.topdir + '/bin/make-patch.py');
+
 os.system('gzip -9 patch*')
 os.system('tar cf updeet *gz')
