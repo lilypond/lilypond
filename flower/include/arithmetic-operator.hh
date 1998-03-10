277594caaacbefc4e75d5de34d3972de5ad56d17
fred <fred>
1998-03-10 22:36:10 +0000

    lilypond-0.1.48

---
+++ flower/include/arithmetic-operator.hh
@@ -0,0 +1,22 @@
+/*   
+  arithmetic-operator.hh -- declare 
+  
+  source file of the Flower Library
+  
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+  
+ */
+
+#ifndef ARITHMETIC_OPERATOR_HH
+#define ARITHMETIC_OPERATOR_HH
+
+#define IMPLEMENT_ARITHMETIC_OPERATOR(type, op)  \
+inline type \
+operator op (type a1, type const& a2)		\
+{						\
+  a1 op ## = a2;					\
+  return a1;					\
+}
+
+#endif /* ARITHMETIC_OPERATOR_HH */
+
