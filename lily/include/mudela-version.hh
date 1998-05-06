a929454acf1e106f27d61fc3e4ec65962435b12a
Jan Nieuwenhuizen <janneke@gnu.org>
1998-05-07 00:58:51 +0200

    partial: 0.1.62.jcn

---
+++ lily/include/mudela-version.hh
@@ -0,0 +1,28 @@
+/*
+  mudela-version.hh -- declare Mudela_version
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1998 Jan Nieuwenhuizen <jan@digicash.com>
+
+*/
+
+#ifndef MUDELA_VERSION_HH
+#define MUDELA_VERSION_HH
+
+#include "string.hh"
+
+struct Mudela_version 
+{
+  Mudela_version (int major, int minor, int patch);
+  Mudela_version (String str);
+
+  String str () const;
+  operator int () const;
+
+  int major_i_;
+  int minor_i_;
+  int patch_i_;
+};
+
+#endif // MUDELA_VERSION_HH
