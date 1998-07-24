225f377f3761d98ff116f08c82b396114da1493d
fred <fred>
1998-07-24 23:46:28 +0000

    lilypond-1.0.1

---
+++ lily/include/rhythmic-column-engraver.hh
@@ -0,0 +1,36 @@
+/*
+  rhythmic-column-engraver.hh -- declare Rhythmic_column_engraver
+
+  source file of the GNU LilyPond music typesetter
+
+  (c)  1997--1998 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+*/
+
+
+#ifndef RHYTHMIC_COLUMN_GRAV_HH
+#define RHYTHMIC_COLUMN_GRAV_HH
+
+#include "engraver.hh"
+#include "parray.hh"
+
+class Rhythmic_column_engraver :public Engraver {
+  Link_array<Script> script_l_arr_;
+  Link_array<Rhythmic_head> rhead_l_arr_;
+  Stem * stem_l_;
+  Note_column *ncol_p_;
+  Dot_column *dotcol_l_;
+
+protected:
+  TRANSLATOR_CLONE(Rhythmic_column_engraver);
+  virtual void acknowledge_element (Score_element_info);
+  virtual void process_acknowledged ();
+  virtual void do_pre_move_processing();
+  virtual void do_post_move_processing();
+public:
+  Rhythmic_column_engraver();
+  DECLARE_MY_RUNTIME_TYPEINFO;
+};
+#endif // RHYTHMIC_COLUMN_GRAV_HH
+
+
+
