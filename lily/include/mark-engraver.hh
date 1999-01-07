33ee82654943ca578478517c5fc20a454a845c38
Mats Bengtsson <mats.bengtsson@s3.kth.se>
1999-01-06 18:09:35 -0600

    patch::: 1.1.18.mb1: Re: LilyPond 1.1.18
    pl 18.mb1
            - \mark now works, using G_staff_side_item.
    	- Improved layout of multi-bar rests, including a new
    	  font symbol.
    
    ---
    Generated by (address unknown) using package-diff 0.62,
    >From = lilypond-1.1.18, To = lilypond-1.1.18.mb1
    
    usage
    
        cd lilypond-source-dir; patch -E -p1 < lilypond-1.1.18.mb1.diff
    
    Patches do not contain automatically generated files
    or (urg) empty directories,
    i.e., you should rerun autoconf, configure
    and possibly make outdirs.
    
    --state
    1.1.18
    1.1.18.mb1
    ++state

--- lily/include/mark-engraver.hh
+++ lily/include/mark-engraver.hh
@@ -12,6 +12,9 @@
 
 #include "engraver.hh"
 
+class G_staff_side_item;
+class G_text_item;
+
 /**
   */
 class Mark_engraver : public Engraver 
@@ -26,10 +29,12 @@ protected:
   virtual bool do_try_music (Music *req_l);
   virtual void do_process_requests ();
   virtual void do_pre_move_processing ();
+  virtual void acknowledge_element (Score_element_info);
 
 private:
   Mark_req * mark_req_l_;
-  Script* script_p_;
+  G_staff_side_item* staff_side_p_;
+  G_text_item* text_p_;
 };
 
 #endif // MARK_ENGRAVER_HH
--- lily/mark-engraver.cc
+++ lily/mark-engraver.cc
@@ -13,14 +13,19 @@
 #include "command-request.hh"
 #include "time-description.hh"
 #include "engraver-group.hh"
-
+#include "staff-sym.hh"
+#include "g-text-item.hh"
+#include "g-staff-side.hh"
+#include "stem.hh"
+#include "rhythmic-head.hh"
 
 ADD_THIS_TRANSLATOR (Mark_engraver);
 
 Mark_engraver::Mark_engraver ()
 {
   mark_req_l_ = 0;
-  script_p_ = 0;
+  staff_side_p_ = 0;
+  text_p_ = 0;
 }
 
 bool
@@ -37,47 +42,71 @@ Mark_engraver::do_try_music (Music* r_l)
 void
 Mark_engraver::do_process_requests ()
 {  
-  if (!mark_req_l_ || script_p_)
+  if (!mark_req_l_ || staff_side_p_)
     return;
 
-  script_p_ = new Script;
-  script_p_->breakable_b_ = true;
+  staff_side_p_ = new G_staff_side_item;
 
-  Text_def *td_p = new Text_def;
+  text_p_ = new G_text_item;
 
-  td_p->text_str_ = mark_req_l_->str_;
-  td_p->align_dir_ = CENTER;
+  text_p_->text_str_ = mark_req_l_->str_;
+  //  text_p_->align_dir_ = CENTER;
 
-  td_p->style_str_ = td_p->text_str_.index_any_i ("0123456789") >= 0 
+  text_p_->style_str_ = text_p_->text_str_.index_any_i ("0123456789") >= 0 
     ? "mark" : "Large";
 
-  script_p_->dir_ = LEFT;
-  script_p_->specs_p_ = td_p->clone ();
-  script_p_->postbreak_only_b_ = true;
-  
-  Scalar padding = get_property ("markScriptPadding", 0);
-  if (padding.length_i() && padding.isnum_b ())
+  Scalar prop = get_property ("markdir", 0);
+  if (prop.isnum_b ())
     {
-      script_p_->padding_f_ = Real(padding);
+      staff_side_p_->dir_ = (Direction) (int) prop;
     }
-  Scalar break_priority = get_property ("markBreakPriority", 0);
-  if (break_priority.length_i() && break_priority.isnum_b ())
+  else 
     {
-      script_p_->break_priority_i_ = int(break_priority);
+      staff_side_p_->dir_ = UP;
     }
 
+  staff_side_p_->set_victim(text_p_);
   
-  announce_element (Score_element_info (script_p_, mark_req_l_));
+  //  Scalar padding = get_property ("markScriptPadding", 0);
+  //  if (padding.length_i() && padding.isnum_b ())
+  //    {
+  //      script_p_->padding_f_ = Real(padding);
+  //    }
+  //  Scalar break_priority = get_property ("markBreakPriority", 0);
+  //  if (break_priority.length_i() && break_priority.isnum_b ())
+  //    {
+  //      staff_side_p_->break_priority_i_ = int(break_priority);
+  //    }
+
+  
+  announce_element (Score_element_info (text_p_, mark_req_l_));
+  announce_element (Score_element_info (staff_side_p_, mark_req_l_));
 }
 
 void 
 Mark_engraver::do_pre_move_processing ()
 {
-  if (script_p_) 
+  if (staff_side_p_) 
     {
-      typeset_element (script_p_);
-      script_p_ = 0;
+      Staff_symbol* s_l = get_staff_info().staff_sym_l_;
+      staff_side_p_->add_support (s_l);
+      typeset_element (text_p_);
+      typeset_element (staff_side_p_);
+      text_p_ = 0;
+      staff_side_p_ = 0;
       mark_req_l_ = 0;
     }
 }
 
+void
+Mark_engraver::acknowledge_element (Score_element_info i)
+{
+  if (staff_side_p_) 
+    {
+      if (dynamic_cast<Stem *> (i.elem_l_) ||
+	  dynamic_cast<Rhythmic_head *> (i.elem_l_))
+	{
+	  staff_side_p_->add_support (i.elem_l_);
+	}
+    }
+}
