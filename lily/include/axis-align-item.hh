62b13517d9e3f01f2cebbda0135312c79ab26738
fred <fred>
1999-03-25 13:38:54 +0000

    lilypond-1.1.37

---
+++ lily/include/axis-align-item.hh
@@ -0,0 +1,25 @@
+/*   
+  axis-align-item.hh -- declare Axis_align_item
+  
+  source file of the GNU LilyPond music typesetter
+  
+  (c) 1999 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  
+ */
+
+#ifndef AXIS_ALIGN_ITEM_HH
+#define AXIS_ALIGN_ITEM_HH
+
+#include "align-element.hh"
+#include "axis-group-item.hh"
+
+class Axis_align_item : public virtual Align_element,
+			public virtual Axis_group_item
+{
+public:
+  Axis_align_item ();
+protected:
+  virtual  void do_print () const {}
+};
+#endif /* AXIS_ALIGN_ITEM_HH */
+
