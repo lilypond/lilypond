252817d654740032cab0dbe255ac8c0c55fc6d96
fred <fred>
2000-03-22 11:42:31 +0000

    lilypond-1.3.38

--- buildscripts/clean-diaper.sh
+++ buildscripts/clean-diaper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # unclobber current dir.
-rm -vf *.aux *.log *.orig *~  *dvi *.dep
+rm -vf *.aux *.log *.orig *~  *.dvi *.dep
 
 # subdirs
-rm -vf `grep -li "^% Creator: GNU LilyPond" *.out *.tex` 
+rm -vf `grep -li "^%created by: GNU LilyPond" *.out *.tex`
+rm -vf `grep -li "^% *Creator: GNU LilyPond" *.out *.tex` 
 rm -vf `grep -li "Creator: mi2mu" *.midi.ly`
 rm -vf `grep -li "%Creator: dvipsk" *.ps`
 rm -vf `grep -li "Creator: GNU LilyPond" *.midi`
@@ -12,7 +13,8 @@ rm -vf `grep -li "Creator: ly2dvi" *.tex`
 rm -vf `find -name 'core'`
 rm -vf `find -name '*.orig'`
 rm -vf `find -name '*.rej'`
-
+rm -vf `find -name 'feta*pk'`
+rm -vf `find -name 'feta*tfm'`
 
 
 
