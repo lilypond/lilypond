32c61cb195bf63f4ad0fb760dfb8632f46efd884
Jan Nieuwenhuizen <janneke@gnu.org>
2001-03-16 09:55:00 +0100

    patch::: 1.3.137.jcn4
    1.3.137.jcn4
    ============
    
    * Bugfix: ly2dvi: handle input files with multiple extensions.
    
    * Minor doco updates.
    
    ---
    Generated by janneke@gnu.org,
    From = lilypond-1.3.137.jcn3, To = lilypond-1.3.137.jcn4
    
    usage
    
        cd lilypond-source-dir; patch -E -p1 < lilypond-1.3.137.jcn4.diff
    
    Patches do not contain automatically generated files
    or (urg) empty directories,
    i.e., you should rerun autoconf, configure

--- scripts/ly2dvi.py
+++ scripts/ly2dvi.py
@@ -103,9 +103,15 @@ def progress (s):
 	'''Make the progress messages stand out between lilypond stuff'''
 	# Why should they have to stand out?  Blend in would be nice too.
 	sys.stderr.write ('*** ' + s+ '\n')
+
+def warning (s):
+	sys.stderr.write (_ ("warning: ") + s)
+	sys.stderr.write ('\n')
 	
+		
 def error (s):
-	sys.stderr.write (s)
+	sys.stderr.write (_ ("error: ") + s)
+	sys.stderr.write ('\n')
 	raise _ ("Exiting ... ")
 
 
@@ -200,9 +206,8 @@ def help ():
 	sys.stdout.write ('\n')
 	sys.stdout.write (options_help_str (option_definitions))
 	sys.stdout.write ('\n\n')
-	sys.stdout.write (_ ("warning: "))
-	sys.stdout.write (_ ("all output is written in the CURRENT directory"))
-	sys.stdout.write ('\n\n')
+	warning (_ ("all output is written in the CURRENT directory"))
+	sys.stdout.write ('\n')
 	sys.stdout.write (_ ("Report bugs to %s") % 'bug-gnu-music@gnu.org')
 	sys.stdout.write ('\n')
 	sys.exit (0)
@@ -270,11 +275,13 @@ def set_setting (dict, key, val):
 	try:
 		val = string.atof (val)
 	except ValueError:
+		warning (_ ("invalid value: %s") % `val`)
 		pass
 
 	try:
 		dict[key].append (val)
 	except KeyError:
+		warning (_ ("no such setting: %s") % `key`)
 		dict[key] = [val]
 	
 
@@ -314,7 +321,7 @@ def find_tex_files (files, extra):
 		x = 0
 		while 1:
 			fname = os.path.basename (f)
-			fname = os.path.splitext (fname)[0]
+			fname = strip_ly_suffix (fname)
 			if x:
 				fname = fname + '-%d' % x
 
@@ -324,7 +331,9 @@ def find_tex_files (files, extra):
 			else:
 				break
 
-			x = x +1 
+			x = x + 1
+	if not x:
+		warning (_ ("no lilypond output found for %s") % `files`)
 	return tfiles
 
 def one_latex_definition (defn, first):
