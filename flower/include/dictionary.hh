08c05b22c0f80b6e3ae4bd5a9d5f86a4b96c8908
fred <fred>
2002-03-24 19:59:33 +0000

    lilypond-0.1.42

--- flower/include/dictionary.hh
+++ flower/include/dictionary.hh
@@ -13,6 +13,21 @@
 #include "string.hh"
 #include "assoc.hh"
 
+/**
+  UGH:  write a String_hash template, 
+
+  SEE:
+  
+       #include <search.h>
+
+       ENTRY *hsearch(ENTRY item, ACTION action);
+
+       int     hcreate (unsigned nel);
+
+       void    hdestroy (void);
+
+  (should be frobnified to allow multiple hashes)
+ */
 template<class T>
 class Dictionary : public Assoc<String, T>
 {
