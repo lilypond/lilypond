6cbc1a03d5a159b41bbbf7bc19cf03f2e22612a4
fred <fred>
2002-03-24 19:30:10 +0000

    lilypond-0.0.27

--- src/scriptdef.cc
+++ src/scriptdef.cc
@@ -14,3 +14,11 @@ Script_def::print() const
     mtor << "idx: " << symidx;
     mtor << "direction, stem: " << stemdir << " staff : " << staffdir;
 }
+int
+Script_def::compare(Script_def const & c)
+{
+    return (symidx == c.symidx &&
+	stemdir == c.stemdir&&
+	staffdir == c.staffdir&&
+	invertsym == c.invertsym);
+}
