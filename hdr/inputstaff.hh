6a13d1279d2549bc4f9c109316850b69556b9d6e
fred <fred>
2002-03-24 19:32:00 +0000

    lilypond-0.0.32

--- hdr/inputstaff.hh
+++ hdr/inputstaff.hh
@@ -1,5 +1,5 @@
 /*
-  inputstaff.hh -- part of LilyPond
+  inputstaff.hh -- declare Input_staff
 
   (c) 1996,97 Han-Wen Nienhuys
 */
@@ -14,19 +14,19 @@
 
 struct Input_staff {
     
-
     const char * defined_ch_c_l_;
     String type;
-    IPointerList<Input_command*> commands_;
+    
     IPointerList<Input_music*> music_;
-
+    Input_music * score_wide_music_p_;
+    
     /* *************** */
-
+    ~Input_staff();
     void add(Input_music*m);
     Input_staff(Input_staff const&);
     Input_staff(String);
-    void add(Array<Input_command*> &s);
-    Staff* parse(Score*);
+    void set_score_wide(Input_music*m);
+    Staff* parse(Score*, Input_music *score_wide);
     void print() const;
 };
 
