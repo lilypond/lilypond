bcfa8a9f9cb710cb9ed988c119967a5c5b92f276
fred <fred>
2002-03-24 19:42:46 +0000

    lilypond-0.0.61

--- lily/include/tie.hh
+++ lily/include/tie.hh
@@ -18,9 +18,9 @@ class Tie : public Bow {
     virtual void do_post_processing();
     virtual void set_default_dir();
 public:
+    bool same_pitch_b_;
     Notehead * left_head_l_;
     Notehead * right_head_l_;
-
     void set_head(int, Notehead*head_l);
     Tie();
     
