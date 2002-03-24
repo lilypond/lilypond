e513b5f3c62ae54230164951a1bb6820d6fc4c44
fred <fred>
2002-03-24 19:48:21 +0000

    lilypond-0.0.76

--- lily/idealspacing.cc
+++ lily/idealspacing.cc
@@ -1,3 +1,11 @@
+/*
+  idealspacing.cc -- implement Idealspacing
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1996,1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
 #include "idealspacing.hh"
 #include "p-col.hh"
 #include "p-score.hh"
@@ -8,23 +16,21 @@ Idealspacing::print() const
 {
 #ifndef NPRINT
     mtor << "idealspacing {" ;
-    mtor << "distance "<<space<< " strength " << hooke ;
-    mtor << "left " << left->rank_i() << " right " << right->rank_i() << "}\n";
+    mtor << "distance "<<space_f_<< " strength " << hooke_f_ ;
+    mtor << "left " << left_i_ << " right " << right_i_ << "}\n";
 #endif
 }
 
-Idealspacing::Idealspacing(PCol const * l,PCol const * r)
+Idealspacing::Idealspacing()
 {
-    space = 0.0;
-    hooke = 0.0;
-    left = l;
-    right = r;
+    space_f_ = 0.0;
+    hooke_f_ = 0.0;
+    left_i_ = -1;
+    right_i_ = -1;
 }
 
 void
 Idealspacing::OK() const
 {
-#ifndef NDEBUG
-    assert(hooke >= 0 && left  && right);
-#endif    
+    assert(hooke_f_ >= 0);
 }
