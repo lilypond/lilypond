41fe707e650a343bf8e2d00af2ca629394d36be0
fred <fred>
2002-03-24 20:15:20 +0000

    lilypond-0.1.65

--- bin/make-docxx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ x$LILYTOP = x ];
-then
-    LILYTOP = .
-fi
-p1=`find $LILYTOP -name '*.hh' |egrep -v out/` 
-p2=`find $LILYTOP -name '*.cc' |egrep -v out/`
-doc++ -f -d docxx/ -S -k -p $BANNEROPT $p1 $p2
--- bin/make-examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# make-examples
-
-examples="twinkle-pop wtk1-fugue2 standchen-16 standchen-20 wtk1-prelude1 toccata-fuga-E prelude-cello menuetto-cello menuetto-alto cadenza gallina twinkle collisions font font20 scales rhythm multi spacing beams sleur stem"
-pss=$(echo $examples | sed "s/[^ ][^ ]*/out\\/&.ps/g")
-
-# cd ~/lelie/current/Documentation/out
-cd ~/lelie/current/input
-make -C ../Documentation $pss
