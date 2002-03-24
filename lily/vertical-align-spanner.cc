f80e8d60b00596a722732ea8360fab3dfe18f3ff
fred <fred>
2002-03-24 19:47:12 +0000

    lilypond-0.0.73pre

---
+++ lily/vertical-align-spanner.cc
@@ -0,0 +1,12 @@
+/*
+  vertical-align-spanner.cc -- implement Vertical_align_spanner
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+#include "vertical-align-spanner.hh"
+
+IMPLEMENT_STATIC_NAME(Vertical_align_spanner);
+IMPLEMENT_IS_TYPE_B2(Vertical_align_spanner, Vertical_align_element, Spanner);
