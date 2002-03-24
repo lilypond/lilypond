fe05d0d73777f8aa62981e5c2fd7e01d99e31630
fred <fred>
2002-03-24 19:43:58 +0000

    lilypond-0.0.65

--- lily/include/staff.hh
+++ lily/include/staff.hh
@@ -1,7 +1,7 @@
 /*
   staff.hh -- declare Staff
 
-  source file of the LilyPond music typesetter
+  source file of the GNU LilyPond music typesetter
 
   (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
 */
@@ -10,10 +10,10 @@
 #define STAFF_HH
 
 #include "plist.hh"
-#include "proto.hh"
+#include "lily-proto.hh"
 #include "moment.hh"
 
-/// base class for a collection of voices.
+/// A collection of voices.
 class Staff {
     Staff(const Staff&src);
     
@@ -23,14 +23,14 @@ public:
     Link_list<Voice*> voice_list_;
     /// runtime field
     Link_list<Staff_column*> cols_;
+    Line_of_staff * staff_line_l_;
 
     Score *score_l_;
     PScore *pscore_l_;
-    PStaff *pstaff_l_;
     
     /* *************************************************************** */
 
-    void add(const Link_list<Voice*> &s);
+    void add(Link_list<Voice*> const&s);
 
     void add_voice(Voice *v_p);
     Paper_def*paper()const;
@@ -45,9 +45,9 @@ public:
     void clean_cols() ;
     Staff();
     
-    virtual void set_output(PScore * destination)=0;
-    virtual Staff_walker *get_walker_p()=0;    
-    virtual ~Staff() { }
+    virtual void set_output(PScore * destination);
+    Staff_walker *get_walker_p();
+    virtual ~Staff();
     void add_col(Staff_column*);
 protected:
 
