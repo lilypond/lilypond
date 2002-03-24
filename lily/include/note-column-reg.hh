10181fa860e82a0df06c5e45124ab5aa960ee427
fred <fred>
2002-03-24 19:41:45 +0000

    lilypond-0.0.56

--- lily/include/note-column-reg.hh
+++ lily/include/note-column-reg.hh
@@ -13,16 +13,23 @@
 #include "register.hh"
 
 class Note_column_register :public Request_register {
+    Rest_column * rest_col_l();
+    Note_column * note_col_l();
+    
+    Array< Script * >  script_l_arr_;
+    Stem * stem_l_;
     Note_column *ncol_p_;
+    Rest_column *restcol_p_;
     bool h_shift_b_;
     int dir_i_;
+    
     /* *************** */
     bool acceptable_elem_b(Staff_elem const*)const;
 protected:
     virtual void set_feature(Feature);
     virtual void acknowledge_element(Staff_elem_info);
     virtual void pre_move_processing();
-
+    virtual void post_move_processing();
 public:
     Note_column_register();
     NAME_MEMBERS(Note_column_register);
