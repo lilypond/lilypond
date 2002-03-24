9fecdced52242447b648ce2667b22560f5fe5697
fred <fred>
2002-03-24 19:36:17 +0000

    lilypond-0.0.44

--- lib/include/source.hh
+++ lib/include/source.hh
@@ -5,22 +5,24 @@
 
 #ifndef SOURCE_HH
 #define SOURCE_HH
-
-class Source 
+#include "source-file.hh"
+#include "plist.hh"
+#include "path.hh"
+class Sources 
 {
 public:
-    Source();
-    ~Source();
-
-    void add( Source_file* sourcefile_p );
+    Source_file * get_file_l( String &filename );
     Source_file* sourcefile_l( char const* ch_c_l );
-
+    void set_path(File_path*p_C);
+    Sources();
+    void set_binary(bool);
 private:
+    const File_path * path_C_;
+    void add( Source_file* sourcefile_p );
     IPointerList<Source_file*> sourcefile_p_iplist_;
+    bool binary_b_ ;
 };
 
 
-// ugh
-extern Source* source_l_g;
 
 #endif // SOURCE_HH //
