1e13cc4a97b0848836319adbe5e682fc72c783a2
fred <fred>
2002-03-24 19:57:25 +0000

    lilypond-0.1.14

---
+++ flower/include/cpu-timer.hh
@@ -0,0 +1,24 @@
+/*
+  cpu-timer.hh -- declare Cpu_timer
+
+  source file of the Flower Library
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef CPU_TIMER_HH
+#define CPU_TIMER_HH
+
+#include <time.h>
+#include "real.hh"
+
+class Cpu_timer {
+  clock_t start_clock_;
+public:
+  Cpu_timer ();
+  void restart ();
+  Real read ();
+};
+
+#endif // CPU_TIMER_HH
