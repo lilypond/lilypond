f6eb3797d13badc3ddd270ac91a782e0e3f4b943
fred <fred>
2002-03-24 19:31:56 +0000

    lilypond-0.0.32

--- src/template3.cc
+++ src/template3.cc
@@ -1,23 +1,15 @@
 #include "request.hh"
-#include "command.hh"
 #include "inputscore.hh"
 #include "inputstaff.hh"
 #include "inputmusic.hh"
-#include "inputcommand.hh"
-#include "inputcommands.hh"
 #include "molecule.hh"
-#include "staffcommands.hh"
 #include "sourcefile.hh"
-
 
 #include "plist.tcc"
 #include "pcursor.tcc"
+
 IPL_instantiate(Atom);
-IPL_instantiate(Command);
 IPL_instantiate(Atom);
-IPL_instantiate(Input_command);
-IPL_instantiate(Commands_at);
-IPL_instantiate(Staff_commands_at);
 IPL_instantiate(Input_staff);
 IPL_instantiate(Input_music);
 IPL_instantiate(Source_file);
