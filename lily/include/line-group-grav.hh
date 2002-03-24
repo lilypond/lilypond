7b741573af275a1804a6256163d32b55ca168391
fred <fred>
2002-03-24 19:49:18 +0000

    lilypond-0.0.77.jcn1

---
+++ lily/include/line-group-grav.hh
@@ -0,0 +1,33 @@
+/*
+  line-group-grav.hh -- declare 
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef LINE_GROUP_GRAV_HH
+#define LINE_GROUP_GRAV_HH
+
+#include "engraver.hh"
+
+/**
+  Engravers put elements on the same or lowel level in a line
+  */
+class Line_group_engraver : public Engraver{
+    Line_of_staff *staffline_p_;   
+
+protected:
+    virtual void do_creation_processing();
+    virtual void do_removal_processing();
+    virtual void acknowledge_element(Score_elem_info);
+
+public:
+    NAME_MEMBERS();
+    Line_group_engraver();
+};
+
+
+#endif // LINE_GROUP_GRAV_HH
+
