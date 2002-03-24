0f7308da5a13a37657370684abdd3032e4b5d033
fred <fred>
2002-03-24 19:53:09 +0000

    lilypond-0.1.7

--- lily/include/boxes.hh
+++ lily/include/boxes.hh
@@ -9,19 +9,28 @@
 #include "real.hh"
 #include "interval.hh"
 #include "offset.hh"
+#include "axes.hh"
 
-/// a square subset of Real^2
 struct Box {
-    Interval x, y;
-
-    void translate(Offset o) {
-	x.translate(o.x);
-	y.translate(o.y);
+    Interval interval_a_[NO_AXES];
+    
+    Interval &x() {return interval_a_[X_AXIS]; }
+    Interval &y(){ return interval_a_[Y_AXIS]; }
+    Interval x()const{ return interval_a_[X_AXIS]; }
+    Interval y()const{return interval_a_[Y_AXIS]; }
+    Interval operator[](Axis a ) {
+	return interval_a_[a];
     }
+    
+    void translate(Offset o) {
+	x().translate(o.x());
+	y().translate(o.y());
+    }
+
     /// smallest box enclosing #b#
     void unite(Box b) {
-	x.unite(b.x);
-	y.unite(b.y);
+	x().unite(b.x());
+	y().unite(b.y());
     }
     Box();
     Box(Interval ix, Interval iy);
