8b197410c810181f66560ec43204691a43c46341
fred <fred>
2002-03-24 19:53:01 +0000

    lilypond-0.1.7

--- lily/include/script-grav.hh
+++ lily/include/script-grav.hh
@@ -15,15 +15,12 @@ class Script_engraver : public Engraver {
     Array<Script *> script_p_arr_;
     Array<Script_req *> script_req_l_arr_;
 
-    /* *************** */
-    bool acceptable_elem_b(Score_elem*);
 public:
     DECLARE_MY_RUNTIME_TYPEINFO;
     Script_engraver();
 protected:
     virtual bool do_try_request(Request*);
     virtual void do_process_requests();
-    virtual void acknowledge_element(Score_elem_info);
     virtual void do_pre_move_processing();
     virtual void do_post_move_processing();
 
