436a2331142bc086b7b1520d34f461c6e329c6a6
fred <fred>
2002-03-24 19:45:19 +0000

    lilypond-0.0.67

---
+++ lily/general-script-def.cc
@@ -0,0 +1,70 @@
+/*
+  general-script-def.cc -- implement General_script_def
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#include "general-script-def.hh"
+#include "debug.hh"
+#include "symbol.hh"
+int
+General_script_def::staff_dir_i()const
+{
+    return -1;
+}
+int
+General_script_def::rel_stem_dir_i()const
+{
+    return 0;
+}
+int
+General_script_def::priority_i()const
+{
+    return 1000;
+}
+
+bool
+General_script_def::inside_b()const
+{
+    return false;
+}
+
+bool
+General_script_def::equal_b(General_script_def const&g)const
+{
+    if (name() != g.name())
+	return false;
+
+    return do_equal_b(g);
+}
+
+bool
+General_script_def::do_equal_b(General_script_def const&)const
+{
+    return true;
+}
+
+
+void
+General_script_def::print() const
+{
+    mtor << name() << "{";
+    do_print();
+    mtor << "}";
+}
+
+void
+General_script_def::do_print() const
+{
+}
+
+Atom
+General_script_def::get_atom(Paper_def*, int)const
+{
+    Symbol s;
+    return Atom(s);
+}
+IMPLEMENT_STATIC_NAME(General_script_def);
