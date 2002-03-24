5ad32eb4d9ea6f4d722d4974a87f2232e2993f12
fred <fred>
2002-03-24 19:32:16 +0000

    lilypond-0.0.32

--- src/complexstaff.cc
+++ src/complexstaff.cc
@@ -1,22 +1,41 @@
-#include "request.hh"
-#include "voice.hh"
-#include "staffwalker.hh"
 #include "debug.hh"
-#include "clef.hh"
-#include "staff.hh"
-#include "command.hh"
 #include "complexstaff.hh"
-#include "sccol.hh" 
 #include "complexwalker.hh"
+#include "complexcolumn.hh"
 #include "score.hh"
 #include "pscore.hh"
 #include "staffsym.hh"
+#include "pscore.hh"
+#include "bar.hh"
+#include "meter.hh"
+#include "sccol.hh"
+#include "commandrequest.hh"
 
+const NO_LINES = 5;
 
-Complex_column::Complex_column(Score_column*s, Complex_staff *staff_l)
-    : Staff_column(s)
+/** Aside from putting fields right, this generates the staff symbol.
+ */
+void
+Complex_staff::set_output(PScore* pscore_l )
 {
-    staff_l_ = staff_l;
+    pstaff_l_ = new PStaff(pscore_l);
+    pscore_l_ = pscore_l;
+    pscore_l_->add(pstaff_l_);
+
+
+   
+    Staff_symbol *span_p = new Staff_symbol(5);
+
+    
+    Score_column* col_last
+	=score_l_->find_col(score_l_->last(), false);
+    Score_column* col_first=
+	score_l_->find_col(0, false);
+	
+    span_p->set_extent(col_first->pcol_l_->postbreak_p_,
+		       col_last->pcol_l_->prebreak_p_);
+
+    pscore_l_->typeset_spanner(span_p, pstaff_l_);
 }
 
 Complex_staff::Complex_staff()
@@ -24,55 +43,14 @@ Complex_staff::Complex_staff()
     pstaff_l_ = 0;
 }
 
-void
-Complex_column::setup_requests()
-{
-    for (int i = 0 ; i < v_elts.size(); i ++)
-	for (iter_top(v_elts[i]->reqs,j); j.ok(); j++) {	    
-
-	    if (j->barcheck()) {
-		if (tdescription_->whole_in_measure) {
-		    warning( "Barcheck failed", j->defined_ch_c_l_m );
-//		    staff_l_->the_line_->pscore_l_->heu errorlevel_i_ |= 1;
-		}
-		continue;
-	    }
-	    if (j->mark())
-		continue;
-	    if (j->command())
-		continue;
-	    if (j->groupchange()) // ugh
-		first_l_arr_.push(j);
-	    else
-		second_l_arr_.push(j);
-	}
-}
-
 Staff_column*
-Complex_staff::create_col(Score_column*s)
+Complex_staff::create_col()
 {
-    return new Complex_column(s,this);
+    return new Complex_column(this);
 }
 
-void
-Complex_staff::walk()
+Staff_walker * 
+Complex_staff::get_walker_p()
 {
-    for (Complex_walker sc(this); sc.ok(); sc++) {
-	sc.col()->setup_requests();
-	sc.process();
-    }
-    Staff_symbol *span_p = new Staff_symbol(5);
-
-	
-    Score_column* col_last
-	=score_l_->find_col(score_l_->last(), false);
-    Score_column* col_first=
-	score_l_->find_col(0, false);
-    col_first->pcol_l_->set_breakable();
-    col_last->pcol_l_->set_breakable();
-	
-    span_p->set_extent( col_first->pcol_l_->postbreak_p_,
-		       col_last->pcol_l_->prebreak_p_);
-
-    pscore_l_->typeset_spanner(span_p, pstaff_l_);
+    return new Complex_walker(this);
 }
