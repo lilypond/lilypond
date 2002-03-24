45cff02aa656a80ebbb7d888fa7080a1389c6050
fred <fred>
2002-03-24 19:57:11 +0000

    lilypond-0.1.14

---
+++ lily/include/change-iterator.hh
@@ -0,0 +1,27 @@
+/*
+  change-iterator.hh -- declare 
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef CHANGE_ITERATOR_HH
+#define CHANGE_ITERATOR_HH
+
+#include "music-iterator.hh"
+
+
+class Change_iterator : public Music_iterator
+{
+  Change_translator *change_l_;
+  
+public:
+  DECLARE_MY_RUNTIME_TYPEINFO;
+  Change_iterator (Change_translator* prop_l);
+protected:
+  virtual void process_and_next (Moment);
+};
+
+#endif
---
+++ lily/include/change-translator.hh
@@ -0,0 +1,27 @@
+/*
+  change-translator.hh -- declare Change_translator
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef CHANGE_TRANSLATOR_HH
+#define CHANGE_TRANSLATOR_HH
+
+#include "music.hh"
+
+class Change_translator : public Music
+{
+public:
+  String change_to_type_str_;
+  String change_to_id_str_;
+  DECLARE_MY_RUNTIME_TYPEINFO;
+protected:
+  VIRTUAL_COPY_CONS(Change_translator, Music);
+  void do_print () const;
+};
+
+
+#endif // CHANGE_TRANSLATOR_HH
