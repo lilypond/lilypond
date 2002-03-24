76d1a3cf3a7611481f0cdca09814b00a3f6ebd2e
fred <fred>
2002-03-24 19:42:30 +0000

    lilypond-0.0.60

--- lily/tie.cc
+++ lily/tie.cc
@@ -6,6 +6,7 @@
   (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
 */
 
+#include "paper-def.hh"
 #include "tie.hh"
 #include "notehead.hh"
 #include "p-col.hh"
@@ -13,12 +14,12 @@
 Spanner*
 Tie::do_break_at(PCol*l, PCol*r) const
 {
-    Tie * tie_p = new Tie;
+    Tie * tie_p = new Tie(*this);
     Line_of_score const  *line_C=l->line_l_;
-    if (left_head_l_->line_l()== line_C)
-	tie_p->left_head_l_ = left_head_l_;
-    if (right_head_l_->line_l() == line_C)
-	tie_p->right_head_l_  = right_head_l_;
+    tie_p->left_head_l_ = (left_head_l_->line_l()== line_C) ?
+	left_head_l_ : 0;
+    tie_p->right_head_l_  = (right_head_l_->line_l() == line_C)?
+	right_head_l_ : 0;
     
     return tie_p;
 }
@@ -43,6 +44,14 @@ Tie::Tie()
 }
 
 void
+Tie::set_default_dir()
+{
+    int m= (left_head_l_->position_i_ + right_head_l_->position_i_) /2 ;
+    dir_i_ =  (m < 5)? -1:1;			// ugh
+}
+    
+
+void
 Tie::do_add_processing()
 {
     assert(left_head_l_ && right_head_l_);
@@ -58,6 +67,18 @@ Tie::do_post_processing()
 	left_head_l_->position_i_ : right_head_l_->position_i_;
     right_pos_i_ = (right_head_l_) ? 
 	right_head_l_->position_i_ : left_head_l_->position_i_;
+ 
+    if ( right_head_l_ && right_head_l_->extremal_i_) {
+	right_pos_i_ += 2*dir_i_;
+	right_dx_f_ -= 0.25;
+    } else
+	right_dx_f_ -= 0.5;
+
+    if (left_head_l_ && left_head_l_->extremal_i_) {
+	left_pos_i_ += 2*dir_i_;
+	left_dx_f_ += 0.25;
+    } else
+	left_dx_f_ += 0.5;
 }
 
 
