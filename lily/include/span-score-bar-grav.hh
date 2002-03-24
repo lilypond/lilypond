e02e732c7144280f56340fcdcd0f400b5a0f0e65
fred <fred>
2002-03-24 19:47:57 +0000

    lilypond-0.0.76

---
+++ lily/include/span-score-bar-grav.hh
@@ -0,0 +1,32 @@
+/*
+  span-score-bar-grav.hh -- declare Span_score_bar_engraver
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef SPAN_SCORE_BAR_GRAV_HH
+#define SPAN_SCORE_BAR_GRAV_HH
+
+#include "span-bar-grav.hh"
+
+class Span_score_bar_engraver : public Span_bar_engraver 
+{
+public:
+    NAME_MEMBERS();
+    virtual Span_bar* get_span_bar_p()const;
+};
+
+/**
+  Please don't shoot the piano player
+ */
+class Piano_bar_engraver :  public Span_score_bar_engraver
+{
+public:
+    NAME_MEMBERS();
+    virtual Span_bar * get_span_bar_p() const;
+};
+
+#endif // SPAN_SCORE_BAR_GRAV_HH
