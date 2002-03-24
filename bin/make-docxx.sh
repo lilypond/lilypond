e2287628dfeecee535ed9f785f909e57c193297f
fred <fred>
2002-03-24 20:00:30 +0000

    lilypond-0.1.23

--- bin/make-docxx.sh
+++ bin/make-docxx.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
-p1=`find ./ -name '*.hh' |egrep -v out/` 
-p2=`find ./ -name '*.cc' |egrep -v out/`
+if [ x$LILYTOP = x ];
+then
+    LILYTOP = .
+fi
+p1=`find $LILYTOP -name '*.hh' |egrep -v out/` 
+p2=`find $LILYTOP -name '*.cc' |egrep -v out/`
 doc++ -d out/ -S -k -p $p1 $p2
