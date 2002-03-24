6bdc10e4cd0fb4511334516ea466600b82296ca1
fred <fred>
2002-03-24 20:08:05 +0000

    lilypond-0.1.57

--- lily/lily-version.cc
+++ lily/lily-version.cc
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 #include "config.hh"
+#include "version.hh"
 
-// #define VERSION_SZ  MAJOR_VERSION "." MINOR_VERSION "."  PATCH_LEVEL MY_PATCH_LEVEL
-#define VERSION_SZ TOPLEVEL_VERSION
+ #define VERSION_SZ  MAJOR_VERSION "." MINOR_VERSION "."  PATCH_LEVEL MY_PATCH_LEVEL
+
+//#define VERSION_SZ TOPLEVEL_VERSION
+//#define VERSION_SZ "version"
 static char *s = "GNU LilyPond " VERSION_SZ ;
 
 
