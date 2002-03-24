2a1e5638c489e6034596b8012c4e9daa6c47b703
fred <fred>
2002-03-24 19:28:50 +0000

    lilypond-0.0.26

--- hdr/script.hh
+++ hdr/script.hh
@@ -10,17 +10,17 @@
 #include "scriptdef.hh"
 #include "item.hh"
   
-struct Script : Item{
+struct Script : Item {
     int dir;
     int symdir;
     int pos;
     int staffsize;
-    Script_def*specs_l_;
-    Stem * stem_l_;
-    Item * support;
+    Script_def *specs_l_;
+    Stem *stem_l_;
+    Array<Item *> support;
 
     /****************/
-    
+    const char * name() const;    
     void    	set_symdir();
     void	set_default_dir();
     void	set_default_pos();
@@ -28,7 +28,10 @@ struct Script : Item{
     Molecule*	brew_molecule_p()const;
     virtual void do_post_processing();
     virtual void do_pre_processing();
-    Script(Script_req*, Item*,int,Stem*p=0);
+    Script(Script_req*, int);
+    void set_support(Item*);
+    void set_stem(Stem*);
+    Interval support_height()const;
     virtual Interval width() const;    
 };
 
