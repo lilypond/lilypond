cf1e3b5cca3eefa0ffa6f93a1bf8ffb26a381be3
fred <fred>
2002-03-24 19:54:04 +0000

    lilypond-0.1.8

--- lib/template.cc
+++ lib/template.cc
@@ -1,6 +1,16 @@
+/*
+  template.cc -- instantiate Pointer_list<Source_file*>
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
 #include "source-file.hh"
 #include "plist.tcc"
 #include "pcursor.tcc"
+#include "cursor.tcc"
+#include "list.tcc"
 
-
-IPL_instantiate(Source_file);
+template L_INSTANTIATE(void *);
+template IPL_INSTANTIATE(Source_file);
