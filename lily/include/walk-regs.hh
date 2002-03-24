e8b3a3c3e32627388b089d2e06813f3ffe50ba95
fred <fred>
2002-03-24 19:37:04 +0000

    lilypond-0.0.46.jcn1

--- lily/include/walk-regs.hh
+++ lily/include/walk-regs.hh
@@ -23,19 +23,15 @@ struct Walker_registers : Register_group_register {
     Array<Item*> nobreak_item_p_arr_;
     Array<Item*> postbreak_item_p_arr_;
     Array<Staff_elem_info> announce_info_arr_;
-    Array<Voice_group_registers*> group_l_arr_;
+ 
     Complex_walker * walk_l_;
 
     /* *************** */
-    void change_group(Group_change_req * greq_l,
-		      Voice_registers *voice_regs_l,
-		      Voice_group_registers * old_group);
-    Voice_group_registers * get_group(String id);
+  
     void typeset_musical_item(Staff_elem * elem_p);
     Walker_registers(Complex_walker*);
     void do_announces();
-    void terminate_register(Request_register * reg);
-    virtual bool try_request(Request * r);
+   
     virtual Staff_info get_staff_info();
 
     virtual void announce_element(Staff_elem_info);
