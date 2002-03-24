a21043d7ac0838a637522fe99424d1e350c0301a
fred <fred>
2002-03-24 20:12:29 +0000

    lilypond-1.0.1

--- lily/vertical-group-spanner.cc
+++ lily/vertical-group-spanner.cc
@@ -3,7 +3,7 @@
 
   source file of the GNU LilyPond music typesetter
 
-  (c)  1997--1998 Han-Wen Nienhuys <hanwen@stack.nl>
+  (c)  1997--1998 Han-Wen Nienhuys <hanwen@cs.uu.nl>
 */
 
 #include "vertical-group-spanner.hh"
@@ -12,3 +12,8 @@
 
 IMPLEMENT_IS_TYPE_B2(Vertical_group_spanner, Axis_group_spanner, Vertical_group_element);
 
+
+Vertical_group_spanner::Vertical_group_spanner ()
+{
+  axis1_ = axis2_ = Y_AXIS;
+}
