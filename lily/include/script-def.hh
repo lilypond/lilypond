2054f402a0bfb1483bafe42444aa791b6028619c
fred <fred>
2002-03-24 19:38:50 +0000

    lilypond-0.0.50

--- lily/include/script-def.hh
+++ lily/include/script-def.hh
@@ -8,17 +8,30 @@
 #define SCRIPTDEF_HH
 #include "string.hh"
 
-struct Script_def{
-    int stemdir;
-    int staffdir;
+/** The characteristics of a certain kind of accent. It is not the
+  accent itself.  */
+struct Script_def {
 
-    bool invertsym;
+    /// on the other side of the stem?
+    int rel_stem_dir_i_;
+
+    /// below or above staff?
+    int staff_dir_i_;
+
+    /// how close to the note do we want to be?
+    int priority_i_;
+    
+    /// follow the ball inside staff?
+    bool inside_staff_b_;
+
+    /// invert if below staff?
+    bool invertsym_b_;
     String symidx;
 
     /* *************** */
     int compare(Script_def const &);
     void print() const;
-    Script_def(String, int, int ,bool);
+    Script_def(String, bool, int, int ,bool);
 };
 
 
