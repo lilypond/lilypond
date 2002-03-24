39b86e9e9359ee08292890bbc811eae39c3e1eac
fred <fred>
2002-03-24 19:41:23 +0000

    lilypond-0.0.39

--- src/directional-spanner.cc
+++
@@ -1,6 +0,0 @@
-#include "directional-spanner.hh"
-
-Directional_spanner::Directional_spanner()
-{
-    dir_i_ = 0;
-}
--- src/template5.cc
+++
@@ -1,9 +0,0 @@
-#include "proto.hh"
-#include "string.hh"
-#include "moment.hh"
-#include "real.hh"
-
-#include "interval.tcc"
-
-Interval__instantiate(Real);
-Interval__instantiate(Rational);
