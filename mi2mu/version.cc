8574f440021028ad0c6b5c85d8342bb505ca9cd8
fred <fred>
2002-03-24 19:36:19 +0000

    lilypond-0.0.44

--- mi2mu/version.cc
+++ mi2mu/version.cc
@@ -7,7 +7,7 @@ String
 mi2mu_version_str()
 {
 	return String ( "This is " ) + mi2mu_version_sz()
-		+ flower_version_sz()
+	    + "/" + flower_version_sz()
 		+ " of " +  __DATE__ + " " + __TIME__;
 }
 
