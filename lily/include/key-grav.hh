10f05834cc7c6d9e31aa0f31cda5ab8f7959645c
fred <fred>
2002-03-24 19:53:03 +0000

    lilypond-0.1.7

--- lily/include/key-grav.hh
+++ lily/include/key-grav.hh
@@ -13,7 +13,14 @@
 #include "engraver.hh"
 #include "key.hh"
 
-struct Key_engraver : Engraver {
+/**
+  Make the key signature.
+ */
+class Key_engraver : public Engraver {
+    void create_key();
+    void read_req(Key_change_req * r);
+
+public:
     Key key_;
     Key_change_req * keyreq_l_;
     Key_item * kit_p_;
@@ -21,17 +28,15 @@ struct Key_engraver : Engraver {
     bool default_key_b_;
     bool change_key_b_;
     
+protected:
     virtual bool do_try_request(Request *req_l);
     virtual void do_process_requests();
     virtual void do_pre_move_processing();
     virtual void do_post_move_processing();
     virtual void acknowledge_element(Score_elem_info);
+public:
     Key_engraver();
     DECLARE_MY_RUNTIME_TYPEINFO;
-private:
-    void create_key();
-    
-    void read_req(Key_change_req * r);
 };
 
 #endif // KEYGRAV_HH
