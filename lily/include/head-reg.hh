2d6cc385ec4a82afbe851d127cc5e9720af94368
fred <fred>
2002-03-24 19:39:05 +0000

    lilypond-0.0.50

--- lily/include/head-reg.hh
+++ lily/include/head-reg.hh
@@ -10,14 +10,18 @@
 #include "register.hh"
 
 struct Notehead_register : Request_register {
-    Item* note_p_;
+    Notehead* note_p_;
+    Rhythmic_req * note_req_l_;
     int dir_i_;
+    
     /* *************** */
-    Notehead_register(Complex_walker*);
+    Notehead_register();
     virtual bool try_request(Request *req_l) ;
-    virtual void process_request();
-    virtual void do_pre_move_process();
-    void set_dir(int);
+    virtual void process_requests();
+    virtual void pre_move_processing();
+    virtual void post_move_processing();
+    void set_feature(Features);
+    NAME_MEMBERS(Notehead_register);
 };
 
 
