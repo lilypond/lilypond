b787082fda2d45766c44da4659276b6c67c8048e
fred <fred>
2002-03-24 19:59:41 +0000

    lilypond-0.1.52

--- lily/include/line-group-grav.hh
+++ lily/include/line-group-grav.hh
@@ -11,14 +11,18 @@
 #define LINE_GROUP_GRAV_HH
 
 #include "engraver.hh"
+#include "lily-proto.hh"
 
 /**
   Engravers put elements on the same or lowel level in a line
   */
 class Line_group_engraver : public Engraver{
+protected:
+
   Vertical_group_spanner *staffline_p_;   
 
-protected:
+
+  virtual void create_line_spanner ();
   virtual void do_creation_processing();
   virtual void do_removal_processing();
   virtual void acknowledge_element (Score_elem_info);
