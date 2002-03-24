eca1be1c30fbf0a9bfd8a87c181f2efc6fead616
fred <fred>
2002-03-24 19:42:42 +0000

    lilypond-0.0.61

--- lib/include/source.hh
+++ lib/include/source.hh
@@ -19,7 +19,7 @@ public:
 private:
     const File_path * path_C_;
     void add( Source_file* sourcefile_p );
-    IPointerList<Source_file*> sourcefile_p_iplist_;
+    IPointer_list<Source_file*> sourcefile_p_iplist_;
     bool binary_b_ ;
 };
 
