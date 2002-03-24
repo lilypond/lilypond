02c33129da0401a2a0db38b5e7f9a43c9471de14
fred <fred>
2002-03-24 19:56:31 +0000

    lilypond-0.1.18

--- lily/include/boxes.hh
+++ lily/include/boxes.hh
@@ -12,28 +12,20 @@
 #include "axes.hh"
 
 struct Box {
-    Interval interval_a_[NO_AXES];
+  Interval interval_a_[NO_AXES];
     
-    Interval &x() {return interval_a_[X_AXIS]; }
-    Interval &y(){ return interval_a_[Y_AXIS]; }
-    Interval x() const{ return interval_a_[X_AXIS]; }
-    Interval y() const{return interval_a_[Y_AXIS]; }
-    Interval operator[](Axis a) {
-	return interval_a_[a];
-    }
+  Interval &x() {return interval_a_[X_AXIS]; }
+  Interval &y(){ return interval_a_[Y_AXIS]; }
+  Interval x() const{ return interval_a_[X_AXIS]; }
+  Interval y() const{return interval_a_[Y_AXIS]; }
+  Interval operator[](Axis a) const;
+  Interval &operator[] (Axis a);
     
-    void translate (Offset o) {
-	x().translate (o.x ());
-	y().translate (o.y ());
-    }
-
-    /// smallest box enclosing #b#
-    void unite (Box b) {
-	x().unite (b.x ());
-	y().unite (b.y ());
-    }
-    Box();
-    Box (Interval ix, Interval iy);
+  void translate (Offset o);
+  /// smallest box enclosing #b#
+  void unite (Box b);
+  Box();
+  Box (Interval ix, Interval iy);
 };
 
 
