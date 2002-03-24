7da72b943d003d928aacc94f6e5d0ed0c5261f4a
fred <fred>
2002-03-24 19:42:13 +0000

    lilypond-0.0.59

---
+++ lily/include/tie-reg.hh
@@ -0,0 +1,34 @@
+/*
+  tie-reg.hh -- declare Tie_register
+
+  source file of the LilyPond music typesetter
+
+  (c) 1997 Han-Wen Nienhuys <hanwen@stack.nl>
+*/
+
+
+#ifndef TIE_REG_HH
+#define TIE_REG_HH
+
+#include "register.hh"
+
+class Tie_register : public Request_register {
+    Tie * end_tie_p_;
+    Tie * tie_p_;
+    Moment end_mom_;
+    Tie_req * req_l_;
+    Tie_req *end_req_l_;
+protected:
+    virtual ~Tie_register();
+    virtual void acknowledge_element(Staff_elem_info);
+    virtual bool try_request(Request*);
+    virtual bool acceptable_request_b(Request*);
+    virtual void process_requests();
+    virtual void post_move_processing();
+    virtual void pre_move_processing();
+public:
+    Tie_register();
+    NAME_MEMBERS(Tie_register);
+};
+
+#endif // TIE_REG_HH
