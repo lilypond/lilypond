2faf48a3eef851c4623f8c350d1bddc948c24ee7
fred <fred>
2002-03-24 20:09:12 +0000

    lilypond-0.1.54

--- flower/flower-version.cc
+++ flower/flower-version.cc
@@ -3,17 +3,11 @@
 #include "config.hh"
 
 // static char *s = "FlowerLib " MAJOR_VERSION "." MINOR_VERSION "."  PATCH_LEVEL MY_PATCH_LEVEL " #%d";
-static char *s = "FlowerLib " FLOWER_VERSION " #%d";
+static char *s = "FlowerLib " FLOWER_VERSION ;
 
-static const int build=
-//#include ".build"
-0
-;
 
 char const * 
 flower_version_sz()
 {
-  static char v[1024];
-  sprintf (v, s, build);
-  return v;
+  return s;
 }
