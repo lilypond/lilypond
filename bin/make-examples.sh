d564eb0334ae9cb5017f67491bd1a7319d88e109
fred <fred>
2002-03-24 20:08:31 +0000

    lilypond-0.1.49

--- bin/make-examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# make-examples
-
-examples="twinkle-pop wtk1-fugue2 standchen-16 standchen-20 wtk1-prelude1 toccata-fuga-E scsii-menuetto cadenza gallina twinkle collisions font font20 scales rhythm multi spacing beams stem"
-pss=$(echo $examples | sed "s/[^ ][^ ]*/out\\/&.ps/g")
-
-# cd ~/lelie/current/Documentation/out
-cd ~/lelie/current/input
-make -C ../Documentation $pss
