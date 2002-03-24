24d757362d8d890479916980bb9fbe530bad3f70
fred <fred>
2002-03-24 19:50:05 +0000

    lilypond-0.1.1

--- lily/debug.cc
+++ lily/debug.cc
@@ -9,7 +9,7 @@
 
 Dstream *monitor=0;
 ostream * nulldev =0;
-static void (*rat_printer)(Moment const&);
+
 
 // ugh
 struct _Dinit {
@@ -41,10 +41,13 @@ float_handler(int)
     assert(false);
 }
 
+/// just to make sure print_rat is linked in
+static void (*rat_printer)(Moment const&);
+
 void
 debug_init()
 {
-    rat_printer = printm;	// make sure this is linked in
+    rat_printer = print_rat;	
 #ifndef NDEBUG
     set_new_handler(&mynewhandler);
 #endif
