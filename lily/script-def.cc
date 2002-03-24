870ea7dc2c02796420a7426f6976fae6b4e14044
fred <fred>
2002-03-24 19:56:06 +0000

    lilypond-0.1.11

--- lily/script-def.cc
+++ lily/script-def.cc
@@ -16,8 +16,8 @@ Script_def::Script_def()
 {
   inside_staff_b_ = false;
   symidx_str_ = "unknown" ;
-  rel_stem_dir_i_ =0;
-  staff_dir_i_ = -1;
+  rel_stem_dir_ =CENTER;
+  staff_dir_ = DOWN;
   invertsym_b_ = 0;
   priority_i_ =0;
 }
@@ -27,8 +27,8 @@ Script_def::set_from_input (String idx,  bool follow, int stem, int staff ,bool 
 {
   inside_staff_b_ = follow;
   symidx_str_ = idx ;
-  rel_stem_dir_i_ =stem;
-  staff_dir_i_ = staff;
+  rel_stem_dir_ =Direction(stem);
+  staff_dir_ = Direction(staff);
   invertsym_b_ = invert;
   priority_i_ =priority_i;
 }
@@ -39,30 +39,30 @@ Script_def::do_print() const
 {
 #ifndef NPRINT
   DOUT << "Script_def{ idx: " << symidx_str_ 
-	 << " direction, stem: " << rel_stem_dir_i_ << " staff : " << staff_dir_i_ << "}\n";
+       << " direction, stem: " << rel_stem_dir_ << " staff : " << staff_dir_ << "}\n";
 #endif
 }
 
 bool
-Script_def::do_equal_b (General_script_def const *g)const 
+Script_def::do_equal_b (General_script_def const *g) const 
 {
   Script_def const * c = (Script_def const*) g;
   return (symidx_str_ == c->symidx_str_ &&
-	     rel_stem_dir_i_ == c->rel_stem_dir_i_&&
-	     staff_dir_i_ == c->staff_dir_i_&&
-	     invertsym_b_ == c->invertsym_b_);
+	  rel_stem_dir_ == c->rel_stem_dir_&&
+	  staff_dir_ == c->staff_dir_&&
+	  invertsym_b_ == c->invertsym_b_);
 }
 
-int
-Script_def::staff_dir_i() const
+Direction
+Script_def::staff_dir() const
 {
-  return staff_dir_i_; 
+  return staff_dir_; 
 }
 
-int
-Script_def::rel_stem_dir_i() const
+Direction
+Script_def::rel_stem_dir() const
 {
-  return rel_stem_dir_i_; 
+  return rel_stem_dir_; 
 }
 
 bool
@@ -72,11 +72,11 @@ Script_def::inside_b() const
 }
 
 Atom
-Script_def::get_atom (Paper_def *p , int d)const
+Script_def::get_atom (Paper_def *p , Direction d) const
 {
   String preidx_str ="";
   if (invertsym_b_&& d < 0) 
-	preidx_str = "-";
+    preidx_str = "-";
 
   return p->lookup_l()->script (preidx_str + symidx_str_);
 }
@@ -85,7 +85,7 @@ Script_def::get_atom (Paper_def *p , int d)const
 IMPLEMENT_IS_TYPE_B1(Script_def,General_script_def);
 
 int
-Script_def::priority_i()const
+Script_def::priority_i() const
 {
   return priority_i_;
 }
--- lily/staff-side.cc
+++ lily/staff-side.cc
@@ -25,16 +25,16 @@ Staff_side::Staff_side()
   pos_i_ =0;
   sym_int_ = Interval (0,0);
   staff_size_i_ = 0;
-  staff_sym_l_=0;
-  dir_i_ =0;
-  inside_staff_b_ =false;
+  staff_sym_l_ = 0;
+  dir_ = CENTER;
+  inside_staff_b_ = false;
 }
 
 void
 Staff_side::read_staff_sym()
 {
   if (! staff_sym_l_)
-	return ;
+    return ;
   staff_size_i_ = staff_sym_l_->steps_i();
 }
 
@@ -45,10 +45,10 @@ Staff_side::support_height() const
   Interval r;
   
   for (int i=0; i < support_l_arr_.size(); i++)
-	r.unite (support_l_arr_[i]->height());
+    r.unite (support_l_arr_[i]->height());
   if (r.empty_b()) 
     {
-	r = Interval (0,0);
+      r = Interval (0,0);
     }
   return r;
 }
@@ -61,13 +61,13 @@ Staff_side::add_support (Score_elem*i)
 }
 
 int
-Staff_side::get_position_i()const
+Staff_side::get_position_i() const
 {
-  if (!dir_i_) 
+  if (!dir_) 
     {
-	warning ("Staff_side::get_position_i(): " 
-		"somebody forgot to set my vertical direction, returning -20");
-	return -20;
+      warning ("Staff_side::get_position_i(): " 
+	       "somebody forgot to set my vertical direction, returning -20");
+      return -20;
     }
   
 
@@ -75,23 +75,23 @@ Staff_side::get_position_i()const
   Real inter_f = paper()-> internote_f ();
   if (!inside_staff_b_) 
     {
-	y  = (dir_i_ > 0 && staff_sym_l_) ? staff_sym_l_->steps_i() + 2: -2; 
-	y *=inter_f;
-	Interval v= support_height();
+      y  = (dir_ > 0 && staff_sym_l_) ? staff_sym_l_->steps_i() + 2: -2; 
+      y *=inter_f;
+      Interval v= support_height();
 
-	if (dir_i_ > 0) 
-	  {
-	    y = y >? (v.max() + 2*inter_f);
-	  }
-	else if (dir_i_ < 0) 
-	  {
-	    y = y <? (v.min() - 2*inter_f);
-	  }
+      if (dir_ > 0) 
+	{
+	  y = y >? (v.max() + 2*inter_f);
+	}
+      else if (dir_ < 0) 
+	{
+	  y = y <? (v.min() - 2*inter_f);
+	}
     }
   else 
     {
-	Interval v= support_height();
-	y = v[dir_i_]  + 2*dir_i_*inter_f;	// ugh
+      Interval v= support_height();
+      y = v[dir_]  + 2*dir_*inter_f;	// ugh
     }
   return int (rint (Real (y)/inter_f)); // should ret a float?
 }
@@ -107,8 +107,8 @@ Staff_side::do_post_processing()
 {
   sym_int_ = symbol_height();
   pos_i_ = get_position_i();
-  if (dir_i_)
-	pos_i_ += int (rint (- sym_int_[-dir_i_] / paper()->internote_f ()));
+  if (dir_)
+    pos_i_ += int (rint (- sym_int_[-dir_] / paper()->internote_f ()));
 }
 
 void
@@ -116,7 +116,7 @@ Staff_side::do_substitute_dependency (Score_elem*o, Score_elem*n)
 { 
   support_l_arr_.unordered_substitute (o,n);
   if (staff_sym_l_ == o)
-	staff_sym_l_ = n ? (Staff_symbol*) n->spanner():0;
+    staff_sym_l_ = n ? (Staff_symbol*) n->spanner():0;
 }
 
 
