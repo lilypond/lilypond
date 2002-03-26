96f7dd5f1008f6583519e0655da9c886109dc224
fred <fred>
2002-03-26 22:44:13 +0000

    lilypond-1.3.11

--- lily/include/staff-symbol-referencer.hh
+++ lily/include/staff-symbol-referencer.hh
@@ -17,10 +17,13 @@
    lines, leading).
    
  */
-class Staff_symbol_referencer : public virtual Score_element
+class Staff_symbol_referencer_interface 
 {
 public:
-  Staff_symbol_referencer ();
+  Score_element * elt_l_;
+  Staff_symbol_referencer_interface (Score_element const*);
+  void set_interface ();
+  bool has_interface_b ();
   void set_position (Real);
   static Real callback (Dimension_cache const*); 
 
@@ -28,11 +31,15 @@ public:
      Leading are the lead strips between the sticks (lines) of
      typeface. ie. leading is vertical space.
    */
+  
   Real staff_line_leading_f () const;
   Staff_symbol * staff_symbol_l () const;
   int lines_i () const;
   Real position_f () const;
 };
+
+
+Staff_symbol_referencer_interface staff_symbol_referencer_interface  (Score_element const*);
 
 #endif /* STAFF_SYMBOL_REFERENCER_HH */
 
