e1d75c7f46917b6d7bc9271ed1f518548ec9fd42
fred <fred>
2002-03-26 23:54:21 +0000

    lilypond-1.3.60

--- buildscripts/mail-address.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# mail-address
-if [ "$MAILADDRESS" != "" ]; then
-	echo $MAILADDRESS
-else
-	echo "mail-address:6: warning: \$MAILADDRESS undefined" > /dev/stderr
-	echo $USER@`hostname`
-fi
--- buildscripts/profile-conf.sh
+++
@@ -1,2 +0,0 @@
-
-configure --prefix=$HOME/usr --enable-profiling --enable-config=optprof --enable-optimise --disable-checking --enable-debugging
