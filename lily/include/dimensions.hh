e28882d05369b6e06b55a3dd5dfe6c6deea66378
fred <fred>
2002-03-26 21:50:13 +0000

    lilypond-1.1.47

--- lily/include/dimensions.hh
+++ lily/include/dimensions.hh
@@ -8,11 +8,15 @@ const Real INCH_TO_PT=72.270;
 const Real CM_TO_PT=INCH_TO_PT/2.54;
 const Real MM_TO_PT=CM_TO_PT/10;
 const Real PT_TO_PT =1.0;
+const Real INCH_TO_BP = 72;
+const Real BIGPOINT_TO_POINT = INCH_TO_PT/ INCH_TO_BP;
+
 
 #define PT  *PT_TO_PT
 #define MM  *MM_TO_PT
 #define CM  *CM_TO_PT
 #define INCH *INCH_TO_PT
+#define BIGPOINT *BIGPOINT_TO_POINT
 
 String print_dimen (Real);
 
