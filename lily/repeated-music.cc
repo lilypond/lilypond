1bda6a34f97f392c134662e17fd902c1a60ad50e
fred <fred>
2002-03-26 21:44:43 +0000

    lilypond-1.1.9

--- lily/repeated-music.cc
+++ lily/repeated-music.cc
@@ -11,7 +11,7 @@
 #include "musical-pitch.hh"
 
 //Repeated_music::Repeated_music (Music* r, int n, Music_list* a)
-Repeated_music::Repeated_music (Music* r, int n, Sequential_music* a)
+Repeated_music::Repeated_music (Music* r, int n, Music_sequence* a)
 {
   repeats_i_ = n;
   repeat_p_ = r;
@@ -29,7 +29,8 @@ Repeated_music::Repeated_music (Repeated_music const& s)
 {
   repeat_p_ = (s.repeat_p_) ? s.repeat_p_->clone () : 0;
   // urg?
-  alternative_p_ = (s.alternative_p_) ? dynamic_cast <Sequential_music*> (s.alternative_p_->clone ()) : 0;
+//  alternative_p_ = (s.alternative_p_) ? dynamic_cast <Music_list*> (s.alternative_p_->clone ()) : 0;
+  alternative_p_ = (s.alternative_p_) ? dynamic_cast <Music_sequence*> (s.alternative_p_->clone ()) : 0;
 }
 
 void
@@ -61,3 +62,23 @@ Repeated_music::duration () const
   return m;
 }
 
+
+Musical_pitch
+Repeated_music::to_relative_octave (Musical_pitch p)
+{
+  p = repeat_p_->to_relative_octave (p);
+
+  p = alternative_p_->do_relative_octave (p, false); 
+  return p;
+  
+  /* ugh.  Should 
+     \relative c'' \repeat 2  { c4 } { < ... > }
+
+     and 
+     
+     \relative c'' \repeat 2  { c4 }
+     { { ...} }
+
+     behave differently?
+   */
+}
