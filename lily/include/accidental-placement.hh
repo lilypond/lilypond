350e561d4d384b18f647d43d3f85f0bf04b7e876
hanwen <hanwen>
2002-04-21 11:04:18 +0000

    ''

---
+++ lily/include/accidental-placement.hh
@@ -0,0 +1,26 @@
+/*   
+accidental-placement.hh -- declare  Accidental_placement
+
+source file of the GNU LilyPond music typesetter
+
+(c) 2002 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+
+ */
+
+#ifndef ACCIDENTAL_PLACEMENT_HH
+#define ACCIDENTAL_PLACEMENT_HH
+
+#include "grob.hh"
+
+class Accidental_placement
+{
+public:
+  DECLARE_SCHEME_CALLBACK (alignment_callback, (SCM element, SCM axis));
+  static void add_accidental (Grob *,Grob* );
+
+  
+  static void position_accidentals (Grob* );
+  static bool has_interface (Grob*);
+};
+#endif /* ACCIDENTAL_PLACEMENT_HH */
+
