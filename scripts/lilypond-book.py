cf483c0c5f9027ae67b9ae002f1343c326a90467
hanwen <hanwen>
2002-05-16 19:58:11 +0000

    matspats

--- scripts/lilypond-book.py
+++ scripts/lilypond-book.py
@@ -48,14 +48,16 @@ import operator
 # Handle bug in Python 1.6-2.1
 #
 # there are recursion limits for some patterns in Python 1.6 til 2.1. 
-# fix this by importing pre instead. Fix by Mats.
+# fix this by importing the 1.5.2 implementation pre instead. Fix by Mats.
 
-# todo: should check Python version first.
-try:
-	import pre
-	re = pre
-	del pre
-except ImportError:
+if float (sys.version[0:3]) < 2.2:
+	try:
+		import pre
+		re = pre
+		del pre
+	except ImportError:
+		import re
+else:
 	import re
 
 # Attempt to fix problems with limited stack size set by Python!
