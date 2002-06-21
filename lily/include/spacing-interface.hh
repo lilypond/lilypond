2ed27f338e53f17eb6378eea93925c98fa56b972
jan <jan>
2002-06-21 11:11:18 +0000

    New file.

---
+++ lily/include/spacing-interface.hh
@@ -0,0 +1,13 @@
+/*   
+  spacing-interface.hh -- declare Spacing_interface
+  
+  source file of the GNU LilyPond music typesetter
+  
+  (c) 2002 Han-Wen Nienhuys <hanwen@cs.uu.nl>
+  
+ */
+
+struct Spacing_interface {
+  static bool has_interface (Grob*);
+};
+
