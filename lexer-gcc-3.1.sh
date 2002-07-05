48e99f68aed5df0fdf63974b4adb725e92583cce
Jan Nieuwenhuizen <janneke@gnu.org>
2002-07-05 14:31:02 +0000

    * lexer-gcc-3.1.sh: Fixes and more comments.
    * Documentation/topdocs/INSTALL.texi: Add comment about gcc-3.0.4
    problems.

--- lexer-gcc-3.1.sh
+++ lexer-gcc-3.1.sh
@@ -2,6 +2,8 @@
 #
 # script documenting fixes for flex-2.5.4 and gcc-3.1
 
+set -e
+
 includes="$HOME/usr/include /usr/local/include /usr/include"
 
 for i in $includes; do
@@ -25,7 +27,9 @@ fi
 
 if [ -n "$CONF" ]; then
     CONFIGSUFFIX=-$CONF
-    ENABLECONFIG="--enable-config=$CONF"
+    SETCONF="CONF=$CONF "
+    setconf="conf=$CONF "
+    ENABLECONFIG="--enable-config=$CONF "
 fi    
 outdir=out$CONFIGSUFFIX
 
@@ -38,12 +42,13 @@ sed -e 's/iostream.h/iostream/' \
     $file > lily/$outdir/FlexLexer.h
 echo "done"
 
-file=lily/$outdir/lexer.cc
-rm -f $file
-make conf=$CONF -C lily $outdir/lexer.cc || exit 0
 
-if [ -f "$file" ]; then
+if [ -f GNUmakefile ]; then
     echo -n "Generating and fixing $file... "
+
+    file=lily/$outdir/lexer.cc
+    rm -f $file
+    make conf=$CONF -C lily $outdir/lexer.cc > /dev/null 2>&1 || true
 
     mv $file $file.orig
     sed -e 's/\<cin\>/std::cin/g' \
@@ -57,7 +62,10 @@ cat <<EOF
 
 Remove config.cache before rerunning ./configure
 
-Reconfigure doing something like:
+Reconfigure, refix, and make doing something like:
 
+    rm -f config.cache
     CPPFLAGS=-I$(pwd)/lily/$outdir ./configure $ENABLECONFIG
+    $SETCONF$0
+    make $setconf
 EOF
