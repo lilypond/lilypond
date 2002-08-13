d1e06b05ec6cf021c2c8b515828ead7123d5bf5c
janneke <janneke>
2002-08-13 10:07:14 +0000

    * cygwin/post-lilypond.sh: Fix and update.
    * cygwin/zlily-profile.sh: Remove.

--- cygwin/post-lilypond.sh
+++ cygwin/post-lilypond.sh
@@ -1,13 +1,9 @@
 #!@SHELL@
 # /etc/profile.d/post-lily.sh  -- Setup LilyPond
 
-# touch /tmp/.lilypond-install  -- from redhat.spec, why?
-rm `find /var/lib/texmf -name 'feta*pk' -or -name 'feta*tfm' -or -name 'parmesan*pk' -or -name 'parmesan*tfm' -print'`
-# rm /tmp/.lilypond-install
+touch /tmp/.lilypond-install
+rm $(find /var/lib/texmf /var/spool/texmf -name 'feta*pk' -or -name 'feta*tfm' -or -name 'parmesan*pk' -or -name 'parmesan*tfm')
+rm -f /tmp/.lilypond-install
 
-# needed for prefix=lilypond-x.y.z 
-#rm -f /usr/lilypond
-#lily=@prefix@
-#ln -s $lily /usr/lilypond
 
 
