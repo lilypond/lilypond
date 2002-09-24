a620cac763434771ef7a0764c0b025c6196f640a
Han-Wen Nienhuys <hanwen@xs4all.nl>
2002-09-24 23:00:09 +0000

    (music-descriptions): change name to internal-class-name

--- scm/music-types.scm
+++ scm/music-types.scm
@@ -3,191 +3,191 @@
   `(
     (ArpeggioEvent 
      . (
-	(name . "Arpeggio_req")
+	(internal-class-name .  "Arpeggio_req")
 	(types . (general-music event))
 	))
     (ArticulationEvent
      . (
-	(name . "Articulation_req")
+	(internal-class-name . "Articulation_req")
 	(types . (general-music event articulation-event script-event))
 	)) 
     (BassFigureEvent
      . (
-	(name . "Bass_figure_req")
+	(internal-class-name . "Bass_figure_req")
 	(types . (general-music event rhythmic-event bass-figure-event))
 	))  
     (BreakEvent
      . (
-	(name . "Break_req")
+	(internal-class-name . "Break_req")
 
 	(types . (general-music event))
 	)) 
     (BreathingSignEvent
      . (
-	(name . "Breathing_sign_req")
+	(internal-class-name . "Breathing_sign_req")
 
 	(types . (general-music event breathing-event))
 	)) 
     (BusyPlayingEvent
      . (
-	(name . "Busy_playing_req")
+	(internal-class-name . "Busy_playing_req")
 
 	(types . (general-music event busy-playing-event))
 	)) 
     (ExtenderEvent
      . (
-	(name . "Extender_req")
+	(internal-class-name . "Extender_req")
 	(types . (general-music event))
 	))   
     (GlissandoEvent
      . (
-	(name . "Glissando_req")
+	(internal-class-name . "Glissando_req")
 	(types . (general-music event))
 	)) 
    (GraceMusic
      . (
-	(name . "Grace_music")
+	(internal-class-name . "Grace_music")
 	(iterator-ctor . ,Grace_iterator::constructor)
 	(types . (grace-music music-wrapper-music general-music))
 	))
    (HyphenEvent
      . (
-	(name . "Hyphen_req")
+	(internal-class-name . "Hyphen_req")
 	(types . (general-music event))
 	))   
     (KeyChangeEvent
      . (
-	(name . "Key_change_req")
+	(internal-class-name . "Key_change_req")
 	(types . (general-music event))
 	)) 
     (LyricEvent
      . (
-	(name . "Lyric_req")
+	(internal-class-name . "Lyric_req")
 	(types . (general-music rhythmic-event event))
 	)) 
     (MarkEvent
      . (
-	(name . "Mark_req")
+	(internal-class-name . "Mark_req")
 	(types . (general-music event))
 	))  
     (MelismaEvent
      . (
-	(name . "Melisma_playing_req")
+	(internal-class-name . "Melisma_playing_req")
 	(types . (general-music span-event event))
 	)) 
     (Melisma_playingEvent
      . (
-	(name . "Melisma_req")
+	(internal-class-name . "Melisma_req")
 	(types . (general-music event))
 	))
     (Music
      . (
-	(name . "Music")
+	(internal-class-name . "Music")
 	(types . (general-music)) 
 	))
     (NoteEvent
      . (
-	(name . "Note_req")
+	(internal-class-name . "Note_req")
 	(types . (general-music event rhythmic-event melodic-event))
 	))
     (PorrectusEvent
      . (
-	(name . "Porrectus_req")
+	(internal-class-name . "Porrectus_req")
 	(types . (general-music event))
 	))
     (RepeatedMusic
      . (
-	(name . "Repeated_music")
+	(internal-class-name . "Repeated_music")
 	(type .  repeated-music)
 	(types . (general-music repeat-music))
 	))
     (Request
      . (
-	(name . "Request")
+	(internal-class-name . "Request")
 	(types . (general-music event))
 	)) 
     (RestEvent
      . (
-	(name . "Rest_req")
+	(internal-class-name . "Rest_req")
 	(types . (general-music event rhythmic-event ))
 	)) 
     (RhythmicEvent
      . (
-	(name . "Rhythmic_req")
+	(internal-class-name . "Rhythmic_req")
 	(types . (general-music rhythmic-event  event))
 	)) 
     (SequentialMusic
      . (
-	(name . "Sequential_music")
+	(internal-class-name . "Sequential_music")
 	(iterator-ctor . ,Sequential_music_iterator::constructor)
 	(types . (general-music sequential-music))
 	))
     (SimultaneousMusic
      . (
-	(name . "Simultaneous_music")
+	(internal-class-name . "Simultaneous_music")
 	(iterator-ctor . ,Simultaneous_music_iterator::constructor)
 	
 	(types . (general-music simultaneous-music))
 	))
     (PropertySet
      . (
-	(name . "Music")
+	(internal-class-name . "Music")
 	(types . (layout-instruction general-music))
 	(iterator-ctor . ,Property_iterator::constructor)
 	)
      )
      (PropertyUnset
      . (
-	(name . "Music")
+	(internal-class-name . "Music")
 	(types . (layout-instruction general-music))
 	(iterator-ctor . ,Property_unset_iterator::constructor)
 	)
      )
      (VoiceSeparator
       . (
-	 (name . "Music")
+	 (internal-class-name . "Music")
 	 (types . (separator general-music))
 	 ))
      (BarCheck
       . (
-	 (name . "Music")
+	 (internal-class-name . "Music")
 	 (types . (general-music bar-check))
 	 (iterator-ctor . ,Bar_check_iterator::constructor)
 	 ))
      (OverrideProperty
       . (
-	 (name . "Music")
+	 (internal-class-name . "Music")
 	 (types . (general-music layout-instruction))
 	 (iterator-ctor . ,	Push_property_iterator::constructor)
 	 ))
 
      (RevertProperty
       . (
-	 (name . "Music")
+	 (internal-class-name . "Music")
 	 (types . (general-music layout-instruction))
 	 (iterator-ctor . ,	Pop_property_iterator::constructor)
 	 ))
      
     (OutputPropertySetMusic
      . (
-	(name . "Music")
+	(internal-class-name . "Music")
 	(iterator-ctor . ,Output_property_music_iterator::constructor)
 	(types . (general-music layout-instruction))
 	))
     (ContextSpeccedMusic
      . (
-	(name . "Context_specced_music")
+	(internal-class-name . "Context_specced_music")
 	(types . (context-specification general-music music-wrapper-music))
 	))
     (AutoChangeMusic
      . (
-	(name . "Music_wrapper")
+	(internal-class-name . "Music_wrapper")
 	(iterator-ctor . ,Auto_change_iterator::constructor)
 	(types . (general-music music-wrapper-music auto-change-instruction))
 	))
     (TranslatorChange
      . (
-	(name . "Music")
+	(internal-class-name . "Music")
 	(iterator-ctor . , Change_iterator::constructor)
 	(types . (general-music translator-change-instruction))
 	))
@@ -195,50 +195,50 @@
 
     (TimeScaledMusic
      . (
-	(name . "Time_scaled_music")
+	(internal-class-name . "Time_scaled_music")
 	(iterator-ctor . ,Time_scaled_music_iterator::constructor)
 	(types . (time-scaled-music music-wrapper-music general-music))
 	))
     (TransposedMusic
      . (
-	(name . "Transposed_music")
+	(internal-class-name . "Transposed_music")
 	(types . (music-wrapper-music general-music transposed-music))
 	))
 
     (UntransposableMusic
      . (
-	(name . "Untransposable_music")
+	(internal-class-name . "Untransposable_music")
 	(types . (music-wrapper-music general-music untransposable-music)) 
 	))
 
     (UnrelativableMusic
      . (
-	(name . "Un_relativable_music")
+	(internal-class-name . "Un_relativable_music")
 	(types . (music-wrapper-music general-music unrelativable-music))
 	))
 
     (RelativeOctaveMusic
      . (
-	(name . "Relative_octave_music")
+	(internal-class-name . "Relative_octave_music")
 	(types . (music-wrapper-music general-music relative-octave-music))
 	))
 
     (LyricCombineMusic
      . (
-	(name . "Lyric_combine_music")
+	(internal-class-name . "Lyric_combine_music")
 	(types . (general-music lyric-combine-music))
 	(iterator-ctor . ,Lyric_combine_music_iterator::constructor)
 	))
 
     (PartCombineMusic
      . (
-	(name . "Part_combine_music")
+	(internal-class-name . "Part_combine_music")
 	(types . (general-music part-combine-music))
 	(iterator-ctor . ,Part_combine_music_iterator::constructor)
      ))
     (RequestChord
      . (
-	(name . "Request_chord")
+	(internal-class-name . "Request_chord")
 	(iterator-ctor . ,Request_chord_iterator::constructor)
 	(types . (general-music simultaneous-music))
 	)
@@ -246,41 +246,41 @@
      
     (ScriptEvent
      . (
-	(name . "Script_req")
+	(internal-class-name . "Script_req")
 
 	(types . (general-music event))
 	)) 
     (SkipEvent
      . (
-	(name . "Skip_req")
+	(internal-class-name . "Skip_req")
 
 	(types . (general-music event rhythmic-event ))
 	)) 
     (SpanEvent
      . (
-	(name . "Span_req")
+	(internal-class-name . "Span_req")
 	(types . (general-music event))
 	)) 
     (StringNumberEvent
      . (
-	(name . "String_number_req")
+	(internal-class-name . "String_number_req")
 
 	(types . (general-music event))
 	)) 
     (TempoEvent
      . (
-	(name . "Tempo_req")
+	(internal-class-name . "Tempo_req")
 
 	(types . (general-music event))
 	)) 
     (TextScriptEvent
      . (
-	(name . "Text_script_req")
+	(internal-class-name . "Text_script_req")
 	(types . (general-music script-event text-script-event event))
 	)) 
     (TieEvent
      . (
-	(name . "Tie_req")
+	(internal-class-name . "Tie_req")
 	(types . (general-music tie-event event))
 	))
     ))
@@ -299,9 +299,9 @@
   (let*
       (
        (props (hashq-ref music-name-to-property-table x '()))
-       (name (if (pair? props) (cdr (assoc 'name props)) "Music"))
+       (name (if (pair? props) (cdr (assoc 'internal-class-name props)) "Music"))
        )
-
+,
     (if (eq? props '())
 	(ly-warn (format "Could not find music type `~a'" x)))  
     (ly-make-bare-music name props)
