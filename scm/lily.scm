b2023e711e576d58da3c51a4b3fd5d327a0fe013
Jan Nieuwenhuizen <janneke@gnu.org>
2002-10-14 22:12:35 +0000

    * scm/lily.scm: Register sodipodi output module.
    	* scm/sodipodi.scm: New file.
    
    	* lily/afm.cc (read_afm_file): Read TfmCheckSum from comment.
    
    	* buildscripts/mf-to-table.py (base): Write TfmChecksum in
    	comment, after header.
    	(parse_logfile): Remove invalid key FontFamily.  Add mandatory key
    	FullName.  Fix FontName key.

--- scm/lily.scm
+++ scm/lily.scm
@@ -179,6 +179,7 @@ is the  first to satisfy CRIT
 	     (scm pysk)
 	     (scm ascii-script)
 	     (scm sketch)
+	     (scm sodipodi)
 	     (scm pdftex)
 	     )
 
@@ -188,7 +189,8 @@ is the  first to satisfy CRIT
     ("ps" . ("Direct postscript. Requires setting GS_LIB and GS_FONTPATH" ,ps-output-expression))
     ("scm" . ("Scheme dump: debug scheme molecule expressions" ,write))
     ("as" . ("Asci-script. Postprocess with as2txt to get ascii art"  ,as-output-expression))
-    ("sketch" . ("Bare bones Sketch output. Requires sketch 0.7" ,sketch-output-expression))
+    ("sketch" . ("Bare bones Sketch output." ,sketch-output-expression))
+    ("sodipodi" . ("Bare bones Sodipodi output." ,sodipodi-output-expression))
     ("pdftex" . ("PDFTeX output. Was last seen nonfunctioning." ,pdftex-output-expression))
     ))
 
