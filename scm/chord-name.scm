d23f0313c4c574d726c044fd3b03c9761f1d1f23
Han-Wen Nienhuys <hanwen@xs4all.nl>
2002-10-18 23:31:01 +0000

    *** empty log message ***

--- scm/chord-name.scm
+++ scm/chord-name.scm
@@ -408,7 +408,7 @@
 ;; chord to be name-calculated.
 ;;
 ;; CHORD: (pitches (bass . inversion))
-(define (default-chord-name-function style chord)
+(define-public (default-chord-name-function style chord)
   (let* ((pitches (map c++-pitch->scm (car chord)))
 	 (modifiers (cdr chord))
 	 (bass-and-inversion (if (pair? modifiers)
@@ -420,7 +420,11 @@
 					 (pitch::transpose x diff))
 				       (cdr pitches))
 		    '())))
-    (chord::name->text style (car pitches) steps bass-and-inversion)))
+;    (display (chord::name->text style (car pitches) steps bass-and-inversion))
+    (chord::name->text style (car pitches) steps bass-and-inversion)
+
+
+    ))
 
 ;;;
 ;;; American style
