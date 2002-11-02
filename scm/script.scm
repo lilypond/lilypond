c227efd2f2040e23ae6289646448adfb67d631dd
Han-Wen Nienhuys <hanwen@xs4all.nl>
2002-11-02 01:52:47 +0000

    * scm/script.scm (default-script-alist): cleanup.
    * lily/script-engraver.cc (make_script_from_event): cleanup.

--- scm/script.scm
+++ scm/script.scm
@@ -1,104 +1,119 @@
-;; (name . '((downindexstring . upindexstring)
-;;           follow-into-staff :: bool
-;;           dir-relative-to-stem :: int
-;;           force-dir :: int
-
-
-;;           priority :: int -- lower number means closer to the  note head. 
-
-;; TODO: generate Grob types for this, by prepending appropriate props
-;; on top of Script (?)  
-
-
-;; TODO: don't use 2 level indiriection, simply set
-;; thumb = \script #'((feta . ( . .. ) (follow-staff .  #f))
-
 
 (set! default-script-alist
       (append 
-      '(("thumb" . ((feta . ("thumb"  . "thumb")) #f 0 1 0))
-	("accent" . ((feta . ("sforzato" .  "sforzato")) #f -1 0 0))
-	("marcato" . ((feta . ("dmarcato" . "umarcato")) #f -1 0  0))
-	("staccatissimo" . ((feta . ("dstaccatissimo" . "ustaccatissimo")) #f  -1 0 0))
-	("portato" . ((feta . ("dportato" . "uportato")) #f -1 0 0))
-	("fermata" . ((feta . ("dfermata" . "ufermata")) #f 0 1 0))
-	("stopped" . ((feta . ("stopped" . "stopped")) #f 0 1 0))
-	("staccato" . ((feta . ("staccato" . "staccato")) #t -1 0 -100))
-	("tenuto" . ((feta . ("tenuto" . "tenuto")) #t -1 0 0))
-	("comma" . ((feta . ("lcomma" . "rcomma")) #t 0 1 0))
-	("upbow" . ((feta . ("upbow" . "upbow")) #f 0 1 0))
-	("downbow" . ((feta . ("downbow" . "downbow")) #f 0 1 0))
-	("lheel" . ((feta . ("upedalheel" . "upedalheel")) #f 0 -1  0))
-	("rheel" . ((feta . ("dpedalheel" . "dpedalheel")) #f 0 1 0))
-	("ltoe" . ((feta . ("upedaltoe" . "upedaltoe")) #f 0 -1 0))
-	("rtoe" . ((feta . ("dpedaltoe" . "dpedaltoe")) #f 0 1 0))
-	("turn" . ((feta . ("turn" . "turn")) #f 0 1 0))
-	("open" . ((feta . ("open" . "open")) #f 0 1 0))
-	("flageolet" . ((feta . ("flageolet" . "flageolet")) #f 0 1 0))
-	("reverseturn" . ((feta . ("reverseturn" . "reverseturn")) #f 0 1 0))
-	("trill" . ((feta . ("trill" . "trill")) #f 0 1 2000))
-	("prall" . ((feta . ("prall" . "prall")) #f 0 1 0))
-	("mordent" . ((feta . ("mordent" . "mordent")) #f 0 1 0))
-	("prallprall" . ((feta . ("prallprall" . "prallprall")) #f 0 1 0))
-	("prallmordent" . ((feta . ("prallmordent" . "prallmordent")) #f 0 1 0))
-	("upprall" . ((feta . ("upprall" . "upprall")) #f 0 1 0))
-	("downprall" . ((feta . ("downprall" . "downprall")) #f 0 1 0))
-	("upmordent" . ((feta . ("upmordent" . "upmordent")) #f 0 1 0))
-	("downmordent" . ((feta . ("downmordent" . "downmordent")) #f 0 1 0))
-	("lineprall" . ((feta . ("lineprall" . "lineprall")) #f 0 1 0))
-	("pralldown" . ((feta . ("pralldown" . "pralldown")) #f 0 1 0))
-	("prallup" . ((feta . ("prallup" . "prallup")) #f 0 1 0))
-	("segno" . ((feta . ("segno" . "segno")) #f 0 1 0))
-	("coda" . ((feta . ("coda" . "coda")) #f 0 1 0)))
-      default-script-alist)
+       '(("thumb" .
+	  (
+	   (script-molecule . (feta . ("thumb"  . "thumb")))
+	   (direction . 1)))
+	 ("accent" .
+	  (
+	   (script-molecule . (feta . ("sforzato" .  "sforzato")))
+	   (side-relative-direction . -1))
+	  )	 
+	 ("marcato" .
+	  (
+	   (script-molecule . (feta . ("dmarcato" . "umarcato")))
+	   (side-relative-direction .  -1)))
+	 ("staccatissimo" .
+	  (
+	   (script-molecule . (feta . ("dstaccatissimo" . "ustaccatissimo")))
+	   (side-relative-direction .  -1)))
+	 
+	 ("portato" .
+	  ((script-molecule . (feta . ("dportato" . "uportato")))
+	   (side-relative-direction . -1)))
+
+	 ("fermata" .
+	  ((script-molecule . (feta . ("dfermata" . "ufermata")))
+	   (direction .  1)))
+	 ("stopped" .
+	  ((script-molecule . (feta . ("stopped" . "stopped")))
+	   (direction  . 1) ))
+	 ("staccato" .
+	  ((script-molecule . (feta . ("staccato" . "staccato")))
+	   (side-relative-direction .  -1)
+	   (follow-into-staff  . #t)
+	   (priority . -100)))
+	 ("tenuto" .
+	  ((script-molecule . (feta . ("tenuto" . "tenuto")))
+	   (follow-into-staff . #t)
+	   (side-relative-direction . -1)))
+	 ("comma" .
+	  ((script-molecule . (feta . ("lcomma" . "rcomma")))
+	   (follow-into-staff . #t)
+	   (direction . 1)))
+	 ("upbow" .
+	  ((script-molecule . (feta . ("upbow" . "upbow")))
+	   (direction  . 1) ))
+	 ("downbow" .
+	  ((script-molecule . (feta . ("downbow" . "downbow")))
+	   (direction  . 1) ))
+	 ("lheel" .
+	  ((script-molecule . (feta . ("upedalheel" . "upedalheel")))
+	   (direction .  -1))
+	 ("rheel" .
+	  ((script-molecule . (feta . ("dpedalheel" . "dpedalheel")))
+	   (direction  . 1) ))
+	 ("ltoe" .
+	  ((script-molecule . (feta . ("upedaltoe" . "upedaltoe")))
+	   (direction  . -1) ))
+	 ("rtoe" .
+	  ((script-molecule . (feta . ("dpedaltoe" . "dpedaltoe")))
+	   (direction  . 1) ))
+	 ("turn" .
+	  ((script-molecule . (feta . ("turn" . "turn")))
+	   (direction  . 1) ))
+	 ("open" .
+	  ((script-molecule . (feta . ("open" . "open")))
+	   (direction  . 1) ))
+	 ("flageolet" .
+	  ((script-molecule . (feta . ("flageolet" . "flageolet")))
+	   (direction  . 1) ))
+	 ("reverseturn" .
+	  ((script-molecule . (feta . ("reverseturn" . "reverseturn")))
+	   (direction  . 1) ))
+	 ("trill" .
+	  ((script-molecule . (feta . ("trill" . "trill")))
+	   (direction . 1)
+	   (priority . 2000)))
+	 ("prall" .
+	  ((script-molecule . (feta . ("prall" . "prall")))
+	   (direction  . 1) ))
+	 ("mordent" .
+	  ((script-molecule . (feta . ("mordent" . "mordent")))
+	   (direction  . 1) ))
+	 ("prallprall" .
+	  ((script-molecule . (feta . ("prallprall" . "prallprall")))
+	   (direction  . 1) ))
+	 ("prallmordent" .
+	  ((script-molecule . (feta . ("prallmordent" . "prallmordent")))
+	   (direction  . 1) ))
+	 ("upprall" .
+	  ((script-molecule . (feta . ("upprall" . "upprall")))
+	   (direction  . 1) ))
+	 ("downprall" .
+	  ((script-molecule . (feta . ("downprall" . "downprall")))
+	   (direction  . 1) ))
+	 ("upmordent" .
+	  ((script-molecule . (feta . ("upmordent" . "upmordent")))
+	   (direction  . 1) ))
+	 ("downmordent" .
+	  ((script-molecule . (feta . ("downmordent" . "downmordent")))
+	   (direction  . 1) ))
+	 ("lineprall" .
+	  ((script-molecule . (feta . ("lineprall" . "lineprall")))
+	   (direction  . 1) ))
+	 ("pralldown" .
+	  ((script-molecule . (feta . ("pralldown" . "pralldown")))
+	   (direction  . 1) ))
+	 ("prallup" .
+	  ((script-molecule . (feta . ("prallup" . "prallup")))
+	   (direction  . 1) ))
+	 ("segno" .
+	  ((script-molecule . (feta . ("segno" . "segno")))
+	   (direction  . 1) ))
+	 ("coda" .
+	  ((script-molecule . (feta . ("coda" . "coda")))
+	   (direction  . 1) )))
+       default-script-alist)
       )
-
-(set! default-script-alist
-      (append 
-      '(("accDiscant" . ((accordion "Discant" "") #f 0 1 0))
-	("accDiscantF" . ((accordion "Discant" "F") #f 0 1 0))
-	("accDiscantE" . ((accordion "Discant" "E") #f 0 1 0))
-	("accDiscantEh" . ((accordion "Discant" "Eh") #f 0 1 0))
-	("accDiscantFE" . ((accordion "Discant" "FE") #f 0 1 0))
-	("accDiscantFEh" . ((accordion "Discant" "FEh") #f 0 1 0))
-	("accDiscantEE" . ((accordion "Discant" "EE") #f 0 1 0))
-	("accDiscantFEE" . ((accordion "Discant" "FEE") #f 0 1 0))
-	("accDiscantEEE" . ((accordion "Discant" "EEE") #f 0 1 0))
-	("accDiscantFEEE" . ((accordion "Discant" "FEEE") #f 0 1 0))
-	("accDiscantS" . ((accordion "Discant" "S") #f 0 1 0))
-	("accDiscantFS" . ((accordion "Discant" "FS") #f 0 1 0))
-	("accDiscantES" . ((accordion "Discant" "ES") #f 0 1 0))
-	("accDiscantEhS" . ((accordion "Discant" "EhS") #f 0 1 0))
-	("accDiscantFES" . ((accordion "Discant" "FES") #f 0 1 0))
-	("accDiscantFEhS" . ((accordion "Discant" "FEhS") #f 0 1 0))
-	("accDiscantEES" . ((accordion "Discant" "EES") #f 0 1 0))
-	("accDiscantFEES" . ((accordion "Discant" "FEES") #f 0 1 0))
-	("accDiscantEEES" . ((accordion "Discant" "EEES") #f 0 1 0))
-	("accDiscantFEEES" . ((accordion "Discant" "FEEES") #f 0 1 0))
-	("accDiscantSS" . ((accordion "Discant" "SS") #f 0 1 0))
-	("accDiscantESS" . ((accordion "Discant" "ESS") #f 0 1 0))
-	("accDiscantEESS" . ((accordion "Discant" "EESS") #f 0 1 0))
-	("accDiscantEEESS" . ((accordion "Discant" "EEESS") #f 0 1 0))
-	("accFreebase" . ((accordion "Freebase" "") #f 0 -1 0))
-	("accFreebaseF" . ((accordion "Freebase" "F") #f 0 -1 0))
-	("accFreebaseE" . ((accordion "Freebase" "E") #f 0 -1 0))
-	("accFreebaseFE" . ((accordion "Freebase" "FE") #f 0 -1 0))
-	("accBayanbase" . ((accordion "Bayanbase" "") #f 0 -1 0))
-	("accBayanbaseT" . ((accordion "Bayanbase" "T") #f 0 -1 0))
-	("accBayanbaseE" . ((accordion "Bayanbase" "E") #f 0 -1 0))
-	("accBayanbaseTE" . ((accordion "Bayanbase" "TE") #f 0 -1 0))
-	("accBayanbaseEE" . ((accordion "Bayanbase" "EE") #f 0 -1 0))
-	("accBayanbaseTEE" . ((accordion "Bayanbase" "TEE") #f 0 -1 0))
-	("accStdbase" . ((accordion "Stdbase" "") #f 0 -1 0))
-	("accStdbaseFE" . ((accordion "Stdbase" "FE") #f 0 -1 0))
-	("accStdbaseTFE" . ((accordion "Stdbase" "TFE") #f 0 -1 0))
-	("accStdbaseMES" . ((accordion "Stdbase" "MES") #f 0 -1 0))
-	("accStdbaseTFMES" . ((accordion "Stdbase" "TFMES") #f 0 -1 0))
-	("accSB" . ((accordion "SB" "") #f 0 -1 0))
-	("accBB" . ((accordion "BB" "") #f 0 -1 0))
-	("accOldEE" . ((accordion "OldEE" "") #f 0 -1 0))
-	("accOldEES" . ((accordion "OldEES" "") #f 0 -1 0)))
-      default-script-alist)
-      )
-
-
