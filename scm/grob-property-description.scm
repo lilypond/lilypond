b4ba6444fc75af979d4c3e40a9e8db7fcd138ae9
Han-Wen Nienhuys <hanwen@xs4all.nl>
2003-01-06 23:39:37 +0000

    *** empty log message ***

--- scm/grob-property-description.scm
+++ scm/grob-property-description.scm
@@ -585,7 +585,6 @@ staff in a row more often, when the heights of the notes vary.
 
 (grob-property-description 'causes list? "list of cause objects; these can be music objects or grobs.")
 (grob-property-description 'flag-count number? "")
-(grob-property-description 'chord-name-function procedure? "DOCME")
 (grob-property-description 'chord-tremolo boolean? "if set, this beam is a tremolo. TODO: use interface for this!")
 (grob-property-description 'chord pair? "?")
 (grob-property-description 'begin-of-line-visible boolean? "?")
