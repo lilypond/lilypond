23cd484ee45d2e5807540265a6fe6b70e9eb2020
Jürgen Reuter <j@web.de>
2003-05-24 01:33:50 +0000

    * Documentation/user/refman.itely, 	lily/vaticana-ligature-engraver.cc, lily/vaticana-ligature.cc, 	mf/parmesan-heads.mf: fixed most of the head selection/alignment 	bugs in the solesmes neumes table

--- lily/vaticana-ligature.cc
+++ lily/vaticana-ligature.cc
@@ -256,7 +256,7 @@ vaticana_brew_primitive (Grob *me, bool ledger_take_space)
 	programming_error (_f ("Vaticana_ligature: (join_left == 0)"));
       Real blotdiameter = (me->get_paper ()->get_var ("blotdiameter"));
       Interval x_extent =
-	Interval (-0.5 * thickness, +0.5 * thickness);
+	Interval (0, thickness);
       Interval y_extent = (join_left > 0) ?
 	Interval (-join_left * 0.5 * staff_space, 0) : // ascending join
 	Interval (0, -join_left * 0.5 * staff_space); // descending join
