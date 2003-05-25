98fa1e7343021a9e19b2326cafe48d3f198c95e1
Han-Wen Nienhuys <hanwen@xs4all.nl>
2003-05-25 22:05:42 +0000

    *** empty log message ***

--- lily/gourlay-breaking.cc
+++ lily/gourlay-breaking.cc
@@ -235,15 +235,15 @@ Gourlay_breaking::combine_demerits (Column_x_positions const &prev,
     Q: do want globally non-cramped lines, or locally equally cramped lines. 
    */
   Real demerit = abs (this_one.force_) +  abs (prev.force_ - this_one.force_)
-    + break_penalties
-    /*
-      This is rather ugly (not scale free), but we have to put a
-      penalty breaking two zero-force lines. Otherwise, we don't get a
-      unique solution (i.e.: line-breaks in short raggedright
-      fragments.)
-     */
-    +  1e-5;
+    + break_penalties;
 
+  /*
+    This is rather ugly (not scale free), but we have to put a
+    penalty breaking two zero-force lines. Otherwise, we don't get a
+    unique solution (i.e.: line-breaks in short raggedright
+    fragments.)
+  */
+  
   if (!this_one.satisfies_constraints_b_)
      {
        /*
