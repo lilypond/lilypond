345d16e753338996f3a2990d8dc1d6108b02dd41
hanwen <hanwen>
2003-08-27 20:02:51 +0000

    release commit

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+# WARNING WARNING WARNING
+# do not edit! this is autogen.sh, generated from /home/hanwen/usr/src/lilypond/stepmake/autogen.sh
+#!/bin/sh
 # Run this to generate configure and initial GNUmakefiles
 
 srcdir=`dirname $0`
 
 
 if [ ! -f aclocal.m4 -o stepmake/aclocal.m4 -nt aclocal.m4 ]; then
-    echo "stepmake/aclocal.m4 is newer.  Copying file." 
+    echo "stepmake/aclocal.m4 is newer. Copying file." 
     cp -f stepmake/aclocal.m4 aclocal.m4
 fi
 
 if [ ! -f autogen.sh -o stepmake/autogen.sh -nt autogen.sh ]; then
-    echo "stepmake/autogen.sh is newer.  Copying file." 
+    echo "stepmake/autogen.sh is newer. Copying file." 
     cp -f stepmake/autogen.sh autogen.sh
     exec ./autogen.sh
 fi
