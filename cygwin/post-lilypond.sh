fe67d729bd0e40494cb0203b839f60450230ba7f
janneke <janneke>
2003-09-19 21:20:32 +0000

    * cygwin/GNUmakefile: * cygwin/lily-wins.py: * cygwin/postremove-lilypond.sh: * cygwin/postinstall-lilypond.sh: Merge from stable.
    * cygwin/bug-lilypond-cygwin.sh: s/lilypond/lilypond-bin,
    s/ly2dvi/lilypond/.

--- cygwin/post-lilypond.sh
+++
@@ -1,9 +0,0 @@
-#!@SHELL@
-# /etc/profile.d/post-lily.sh  -- Setup LilyPond
-
-touch /tmp/.lilypond-install
-rm $(find /var/lib/texmf /var/spool/texmf -name /var/cache/fonts 'feta*pk' -or -name 'feta*tfm' -or -name 'parmesan*pk' -or -name 'parmesan*tfm')
-rm -f /tmp/.lilypond-install
-
-
-
