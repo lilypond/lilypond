009d4319bf6cb1b327d2388c4bfb0efbd507a26f
janneke <janneke>
2004-06-11 12:47:14 +0000

    Update build script.

--- scm/output-gnome.scm
+++ scm/output-gnome.scm
@@ -46,24 +46,39 @@
 ;;; move this into workbook?
 
 "
+#!/bin/bash
+
+set -ex
+
+rm -rf test
+mkdir test
+cd test
+
 ## 1.  install gnome-devel (Debian/unstable: apt-get install gnome-devel)
 
 ## 2.  *** NOTE: use guile-1.6 for g-wrap and guile-gnome ***
+##### g-wrap fixes for guile-1.7 have been made -- should test
 PATH=/usr/bin:$PATH
 
 
 ## 3.  get g-wrap 2.0
 tla register-archive a.rottmann@gmx.at--2004-main http://people.debian.org/~rotty/arch/a.rottmann@gmx.at/2004-main || true
 
-rm -rf gw-pristine
-tla get a.rottmann@gmx.at--2004-main/g-wrap--tng gw-pristine
-cd gw-pristine
+rm -rf g-wrap
+## tla get a.rottmann@gmx.at--2004-main/g-wrap--tng g-wrap
+## pull latest g-wrap from janneke -- this step is probably no longer
+## necessary when you read this
+tla register-archive janneke@gnu.org--2004-gnome http://lilypond.org/~janneke/{arch}/2004-gnome || true
+tla get janneke@gnu.org--2004-gnome/g-wrap--janneke g-wrap
+cd g-wrap
 
+rm -rf $HOME/usr/pkg/g-wrap
 AUTOMAKE=automake-1.8 AUTOCONF=autoconf2.50 sh autogen.sh --noconfigure
 mkdir =build
 cd =build
 ../configure --prefix=$HOME/usr/pkg/g-wrap
 make install
+## make install CC='cc -DSIZEOF_LONG=4'
 
 cd ../..
 
@@ -75,6 +90,16 @@ cd guile-gnome
 tla build-config -r configs/gnu.org/dev
 cd src
 
+## 5.  get the gnome canvas module
+tla get guile-gnome-devel@gnu.org--2004/libgnomecanvas--dev libgnomecanvas
+
+## pull latest defs from janneke -- this step is probably no longer
+## necessary when you read this
+tla register-archive janneke@gnu.org--2004-gnome http://lilypond.org/~janneke/{arch}/2004-gnome || true
+rm -rf defs
+tla get janneke@gnu.org--2004-gnome/defs--janneke defs
+
+rm -rf $HOME/usr/pkg/guile-gnome
 AUTOMAKE=automake-1.8 AUTOCONF=autoconf2.50 sh autogen.sh --noconfigure
 mkdir ../=build
 cd ../=build
@@ -85,12 +110,10 @@ export PKG_CONFIG_PATH=$HOME/usr/pkg/g-wrap/lib/pkgconfig:$PKG_CONFIG_PATH
 
 # cp srfi-34.scm from CVS head ?  --hwn
 
-
 ../src/configure --prefix=$HOME/usr/pkg/guile-gnome
 
-G_WRAP_MODULE_DIR=$HOME/usr/pkg/g-wrap/share/guile/site make install
-#FIXME: fixup -- no longer necessary?
-(cd $HOME/usr/pkg/guile-gnome/share/guile/gnome && mv gtk/g[dt]k.scm gw)
+##G_WRAP_MODULE_DIR=$HOME/usr/pkg/g-wrap/share/guile/site make install
+G_WRAP_MODULE_DIR=$HOME/usr/pkg/g-wrap/share/guile/site make install CC='cc -DSIZEOF_LONG=4'
 
 export GUILE_LOAD_PATH=$HOME/usr/pkg/guile-gnome/share/guile:$GUILE_LOAD_PATH
 export LD_LIBRARY_PATH=$HOME/usr/pkg/guile-gnome/lib:$LD_LIBRARY_PATH
