2bb5e83908d98ff9e98825a9a119015324b1ec59
janneke <janneke>
2004-06-13 22:50:16 +0000

    Remove script again; re-add modules fix (CVS conflict resolve?)

--- scm/output-gnome.scm
+++ scm/output-gnome.scm
@@ -65,98 +65,8 @@ lilypond-bin -fgnome input/simple-song.ly
 ;;;  * papersize, outputscale from book
 
 
-"
-#!/bin/bash
+;;; SCRIPT moved to buildscripts/guile-gnome.sh
 
-set -ex
-
-# no CVS guile.
-export PATH=/usr/bin/:$PATH
-
-if  [ -d $HOME/usr/pkg/libffi/ ] ; then
- export LDFLAGS=-L$HOME/usr/pkg/libffi/lib/
- export CPPFLAGS=-I$HOME/usr/pkg/libffi/include
-fi 
-
-export AUTOMAKE=automake-1.8
-export AUTOCONF=autoconf2.50 
-
-rm -rf test
-mkdir test
-cd test
-
-## 1.  install gnome-devel (Debian/unstable: apt-get install gnome-devel)
-
-## 2.  *** NOTE: use guile-1.6 for g-wrap and guile-gnome ***
-##### using GUILE CVS g-wrap/guile-gnome is experimental (read: segfaults)
-PATH=/usr/bin:$PATH
-
-
-## 3.  get g-wrap 2.0
-tla register-archive a.rottmann@gmx.at--2004-main http://people.debian.org/~rotty/arch/a.rottmann@gmx.at/2004-main || true
-
-rm -rf g-wrap
-## tla get a.rottmann@gmx.at--2004-main/g-wrap--tng g-wrap
-## pull latest g-wrap from janneke -- this step is probably no longer
-## necessary when you read this
-tla register-archive janneke@gnu.org--2004-gnome http://lilypond.org/~janneke/{arch}/2004-gnome || true
-tla get janneke@gnu.org--2004-gnome/g-wrap--janneke g-wrap
-cd g-wrap
-
-rm -rf $HOME/usr/pkg/g-wrap
-sh autogen.sh --noconfigure
-mkdir =build
-cd =build
-../configure --prefix=$HOME/usr/pkg/g-wrap
-make install
-
-# cp srfi-34.scm from CVS head ?  --hwn
-(cd $HOME/usr/pkg/g-wrap/share/guile/site
- mv srfi-34.scm srfi-34.scm-g-wrap
- cp $HOME/usr/pkg/guile/share/guile-1.7/srfi/srfi-34.scm .)
-
-cd ../..
-
-## 4.  get guile-gnome
-tla register-archive guile-gnome-devel@gnu.org--2004 http://people.debian.org/~rotty/arch/guile-gnome-devel@gnu.org/2004/ || true
-rm -rf guile-gnome
-tla get guile-gnome-devel@gnu.org--2004/dists--dev guile-gnome
-cd guile-gnome
-tla build-config -r configs/gnu.org/dev
-cd src
-
-## 5.  get the gnome canvas module
-tla get guile-gnome-devel@gnu.org--2004/libgnomecanvas--dev libgnomecanvas
-
-## pull latest defs from janneke -- this step is probably no longer
-## necessary when you read this
-## tla register-archive janneke@gnu.org--2004-gnome http://lilypond.org/~janneke/{arch}/2004-gnome || true
-## rm -rf defs
-## tla get janneke@gnu.org--2004-gnome/defs--janneke defs
-
-rm -rf $HOME/usr/pkg/guile-gnome
-sh autogen.sh --noconfigure
-mkdir ../=build
-cd ../=build
-
-export GUILE_LOAD_PATH=$HOME/usr/pkg/g-wrap/share/guile/site:$GUILE_LOAD_PATH
-export LD_LIBRARY_PATH=$HOME/usr/pkg/g-wrap/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$HOME/usr/pkg/g-wrap/lib/pkgconfig:$PKG_CONFIG_PATH
-
-../src/configure --prefix=$HOME/usr/pkg/guile-gnome
-
-
-# requires 800mb RAM with -O2
-(cd gtk/gnome/gw; perl  -i~  -pe 's/-O2//g' Makefile)
-
-G_WRAP_MODULE_DIR=$HOME/usr/pkg/g-wrap/share/guile/site make install
-
-export GUILE_LOAD_PATH=$HOME/usr/pkg/guile-gnome/share/guile:$GUILE_LOAD_PATH
-export LD_LIBRARY_PATH=$HOME/usr/pkg/guile-gnome/lib:$LD_LIBRARY_PATH
-
-
-# simple test
-guile -s ../src/libgnomecanvas/examples/canvas.scm
 
 
 (debug-enable 'backtrace)
@@ -171,10 +81,12 @@ guile -s ../src/libgnomecanvas/examples/canvas.scm
  (srfi srfi-13)
  (lily)
  (gnome gtk)
- (gnome gtk gdk-event)
- ;; the name of the module will change to canvas rsn
- (gnome gw libgnomecanvas))
- ;;(gnome gw canvas))
+ (gnome gtk gdk-event))
+
+;; the name of the module will change to canvas rsn
+(if (resolve-module '(gnome gw canvas))
+    (use-modules (gnome gw canvas))
+    (use-modules (gnome gw libgnomecanvas)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; module entry
@@ -232,9 +144,9 @@ guile -s ../src/libgnomecanvas/examples/canvas.scm
 
 ;; minimal intercept list:
 (define output-interface-intercept
-  '(
-    no-origin
-))
+  '(comment
+    define-origin
+    no-origin))
 
 (map (lambda (x) (module-define! this-module x dummy))
      output-interface-intercept)
