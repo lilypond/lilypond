b133adce8270d81c959b1f98663942f227b83201
Jan Nieuwenhuizen <janneke@gnu.org>
2004-12-12 19:12:40 +0000

    * mf/bigcheese.pe.in (i): Map to 0xe000 (PUA) for testing, which does not crash fontforge.
    * scm/lily-library.scm (char->unicode-index): Hack to map onto
    PUA.

--- scm/lily-library.scm
+++ scm/lily-library.scm
@@ -343,8 +343,20 @@ possibly turned off."
 	  (ly:font-file-name font)))))
 
 (define-public (char->unicode-index font char)
-  (+ (case (ly:font-encoding font) 
+  ;;  (format (current-error-port) "UNICODE:~S:~S:~S\n"
+  ;;   font (ly:font-encoding font) (char->integer char))
+  ;; (force-output (current-error-port))
+  (+ (case (ly:font-encoding font)
        ((fetaMusic) (- #xe000 #x20))
        ((fetaBraces) (- #xe000 #x40))
-       (else 0))
+       ((fetaBraces) (- #xe000 #x40))
+       ;;(else 0))
+       ;; FIXME: bigcheese says FontSpecific
+       (else (if (string=? (font-family font) "bigcheese20")
+		 ;;#xf000 0)))
+		 ;; ugh, we must know which font from bigcheese;
+		 ;; feta-proper starts at 0xe0e3
+		 ;; but we cannot display feta-nummer or feta-din characters
+		 ;; this way
+		 #xe0e3 0)))
      (char->integer char)))
