c2f07cdcb1aa36338ebe0b88f52bc042e95e31d4
Han-Wen Nienhuys <hanwen@xs4all.nl>
2004-12-19 16:55:35 +0000

    * scm/output-texstr.scm (placebox): add routine
    * scm/framework-texstr.scm (output-framework): new file.
    
    * scm/output-texstr.scm: new file. Collect (TEXT . ) calls for TeX
    processing.

--- scm/framework-texstr.scm
+++ scm/framework-texstr.scm
@@ -16,50 +16,16 @@
 	     (srfi srfi-13)
 	     (lily))
 
-(define (dump-page putter page last? with-extents?)
-  (ly:outputter-dump-string
-   putter
-   (format "\\lybox{~a}{~a}{%\n"
-	   (if with-extents?
-	       (interval-start (ly:stencil-extent page X))
-	       0.0)
-	   (if with-extents?
-	       (- (interval-start (ly:stencil-extent page Y)))
-	       0.0)))
-  (ly:outputter-dump-string
-   putter
-   (if last?
-       "}%\n\\vfill\n"
-       "}%\n\\vfill\n\\lilypondpagebreak\n")))
 
 (define-public (output-framework outputter book scopes fields basename )
   (let* ((paper (ly:paper-book-paper book))
 	 (pages (ly:paper-book-pages book))
 	 )
     (for-each
-     (lambda (x)
-       (ly:outputter-dump-string outputter x))
-     (list))
-    (for-each
      (lambda (page)
        (ly:outputter-dump-stencil outputter page))
      pages)))
 
-(define (dump-line putter line last?)
-  (ly:outputter-dump-string
-   putter
-   (format "\\lybox{~a}{~a}{%\n"
-	   (ly:number->string
-	    (max 0 (interval-end (ly:paper-system-extent line X))))
-	   (ly:number->string
-	    (interval-length (ly:paper-system-extent line Y)))))
-
-  (ly:outputter-dump-stencil putter (ly:paper-system-stencil line))
-  (ly:outputter-dump-string
-   putter
-   (if last?
-       "}%\n"
-       "}\\interscoreline\n")))
 
 
 (define-public (convert-to-ps . args) #t)
