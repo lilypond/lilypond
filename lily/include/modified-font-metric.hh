d1a8bda9322d4ab7813e4f9148a32c7591656f54
hanwen <hanwen>
2005-01-01 19:45:51 +0000

    (struct Modified_font_metric): remove coding_scheme() method.

--- lily/include/modified-font-metric.hh
+++ lily/include/modified-font-metric.hh
@@ -18,28 +18,19 @@ public:
   virtual Box text_dimension (String) const;
   virtual Stencil text_stencil (String) const;
   
-  static SCM make_scaled_font_metric (Font_metric *fm, Real magnification,
-				      SCM font_encoding, SCM input_encoding);
+  static SCM make_scaled_font_metric (Font_metric *fm, Real magnification);
   virtual int count () const;
   virtual Offset get_indexed_wxwy (int) const;
   virtual Offset attachment_point (String) const;
   virtual int name_to_index (String) const;
   virtual unsigned index_to_charcode (int) const;
-  virtual String coding_scheme () const;
-  virtual Font_metric *original_font () const;  
+  Font_metric*original_font () const;
   
 protected:
-  SCM coding_vector_;
-  SCM coding_table_;
-  SCM coding_mapping_;
-  SCM coding_description_;
   Font_metric *orig_;
   Real magnification_;
-  String input_encoding_;
-  friend SCM ly_font_encoding_alist (SCM);
     
-  Modified_font_metric (Font_metric *fm, Real magnification,
-			String font_encoding, String input_encoding);
+  Modified_font_metric (Font_metric *fm, Real magnification);
   virtual SCM sub_fonts () const;
   virtual String font_name () const;
   virtual Real design_size () const;
