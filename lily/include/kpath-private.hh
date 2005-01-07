034d5c8991ebc91bc0695218074c5b3df96c429a
hanwen <hanwen>
2005-01-07 13:36:42 +0000

    *** empty log message ***

---
+++ lily/include/kpath-private.hh
@@ -0,0 +1,18 @@
+/*
+  kpath-private.hh -- declare
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 2005 Han-Wen Nienhuys <hanwen@xs4all.nl>
+
+*/
+
+#ifndef KPATH_PRIVATE_HH
+#define KPATH_PRIVATE_HH
+
+kpse_file_format_type kpathsea_find_format (String name);
+
+#endif /* KPATH_PRIVATE_HH */
+
+
+
