e6a0e9df7cef8ec41062927a2a7c3af35684afb4
hanwen <hanwen>
2005-01-08 12:28:03 +0000

    * lily/object-key-dumper-scheme.cc: new file.
    * lily/object-key-undumper-scheme.cc: new file.
    
    * lily/tweak-registration-scheme.cc: new file.

--- lily/object-key-dumper.cc
+++ lily/object-key-dumper.cc
@@ -103,42 +103,6 @@ Object_key_dumper::get_file_contents () const
   return scm_reverse (file_contents_);
 }
 
-LY_DEFINE(ly_make_dumper, "ly:make-dumper",
-	  0,0,0,
-	  (),
-	  "Create a key dumper. "
-	  )
-{
-  Object_key_dumper *u = new Object_key_dumper ();
-  SCM x = u->self_scm();
-  scm_gc_unprotect_object (x);
-  return x;
-}
-
-LY_DEFINE(ly_dumper_definitions, "ly:dumper-definitions",
-	  1,0,0,
-	  (SCM dumper),
-	  "Return list of key definitions. "
-	  )
-{
-  Object_key_dumper *u = unsmob_key_dumper (dumper);
-  SCM_ASSERT_TYPE(u, dumper, SCM_ARG1, __FUNCTION__, "dumper");
-  return u->get_file_contents();
-}
-
-LY_DEFINE(ly_dumper_key_serial, "ly:dumper-key-serial",
-	  2,0,0,
-	  (SCM dumper, SCM key),
-	  "Return the  key serial number @var{key}. "
-	  )
-{
-  Object_key_dumper* u = unsmob_key_dumper (dumper);
-  Object_key *k = unsmob_key (key);
-  SCM_ASSERT_TYPE(u, dumper, SCM_ARG1, __FUNCTION__, "dumper");
-  SCM_ASSERT_TYPE(k, key, SCM_ARG2, __FUNCTION__, "key");
-  return u->dump_key (k);
-}
-
 Object_key_dumper::~Object_key_dumper()
 {
 }
---
+++ lily/object-key-undumper-scheme.cc
@@ -0,0 +1,52 @@
+/*
+  object-key-undumper-scheme.cc --  implement Object_key_undumper bindings
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 2005 Han-Wen Nienhuys <hanwen@xs4all.nl>
+
+*/
+
+#include "object-key-undumper.hh"
+
+LY_DEFINE(ly_undumper_read_keys, "ly:undumper-read-keys",
+	  2,0,0,
+	  (SCM undumper, SCM keys),
+	  "Read serialized @var{keys} into @var{undumper}."
+	  )
+{
+  Object_key_undumper *u = unsmob_key_undumper (undumper);
+  SCM_ASSERT_TYPE(u, undumper, SCM_ARG1, __FUNCTION__, "Undumper");
+
+  u->parse_contents (keys);
+  return SCM_UNSPECIFIED;
+}
+
+LY_DEFINE(ly_make_undumper, "ly:make-undumper",
+	  0, 0,0,
+	  (),
+	  "Create a key undumper. "
+	  )
+{
+  Object_key_undumper *u = new Object_key_undumper ();
+  SCM x = u->self_scm();
+  scm_gc_unprotect_object (x);
+  return x;
+}
+
+
+LY_DEFINE(ly_undumper_lookup, "ly:undumper-lookup",
+	  2,0,0,
+	  (SCM undumper, SCM serial),
+	  "Return the object key for number @var{serial}. "
+	  )
+  
+{
+  Object_key_undumper* u = unsmob_key_undumper (undumper);
+
+  SCM_ASSERT_TYPE(u, undumper, SCM_ARG1, __FUNCTION__, "undumper");
+  SCM_ASSERT_TYPE(scm_is_integer(serial), serial, SCM_ARG2, __FUNCTION__, "integer");
+  return u->get_key (scm_to_int (serial))->self_scm();
+}
+
+
