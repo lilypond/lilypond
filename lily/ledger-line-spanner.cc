9b2c97a8acb3462d4fdbc5bd2970738f3bf485c4
hanwen <hanwen>
2005-04-20 10:59:08 +0000

    some more words of explanation.

--- lily/ledger-line-spanner.cc
+++ lily/ledger-line-spanner.cc
@@ -342,7 +342,11 @@ Ledger_line_spanner::print (SCM smob)
 
 ADD_INTERFACE (Ledger_line_spanner,
 	       "ledger-line-interface",
-	       "This spanner draws the ledger lines of a staff, for note heads that stick out. ",
+	       
+	       "This spanner draws the ledger lines of a staff.\n"
+	       "This is a separate grob to because it has to process\n"
+	       "all potential collisions between all note heads.",
+	       
 	       "note-heads thickness minimum-length-fraction length-fraction gap");
 
 struct Ledgered_interface
@@ -352,5 +356,8 @@ struct Ledgered_interface
 
 ADD_INTERFACE (Ledgered_interface,
 	       "ledgered-interface",
-	       "Objects that need ledger lines.",
+	       
+	       "Objects that need ledger lines, typically "
+	       "note heads. See also @ref{ledger-line-interface}.",
+	       
 	       "no-ledgers");
