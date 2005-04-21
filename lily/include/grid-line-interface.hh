ec2cdafc1d46e06b01079be1276b8ba1d851b790
hanwen <hanwen>
2005-04-21 16:20:49 +0000

    *** empty log message ***

---
+++ lily/include/grid-line-interface.hh
@@ -0,0 +1,32 @@
+/*
+  grid-line-interface.hh -- declare Grid_line_interface
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 2005 Han-Wen Nienhuys <hanwen@xs4all.nl>
+
+*/
+
+#ifndef GRID_LINE_INTERFACE_HH
+#define GRID_LINE_INTERFACE_HH
+
+#include "lily-proto.hh"
+#include "lily-guile.hh"
+
+class Grid_line_interface
+{
+public:
+  static void add_grid_point (Grob *me, Grob *b);
+  DECLARE_SCHEME_CALLBACK (print, (SCM));
+  DECLARE_SCHEME_CALLBACK (width_callback, (SCM, SCM));
+  static bool has_interface (Grob *);
+};
+
+class Grid_point_interface
+{
+public:
+  static bool has_interface (Grob *);
+};
+
+#endif /* GRID_LINE_INTERFACE_HH */
+
