84ed119f2e76f41fcd4f4fda0614472717a9816c
Han-Wen Nienhuys <hanwen@xs4all.nl>
2005-07-20 10:33:53 +0000

    new file, used for benchmarking.

---
+++ scm/framework-null.scm
@@ -0,0 +1,28 @@
+;; module for benchmarking.
+
+(define-module (scm framework-null)
+  #:export (output-framework)
+  )
+
+(use-modules (ice-9 regex)
+	     (ice-9 string-fun)
+	     (ice-9 format)
+	     (guile)
+	     (srfi srfi-1)
+	     (ice-9 pretty-print)
+	     (srfi srfi-13)
+	     (lily))
+
+(define-public (output-framework channel book scopes fields )
+
+  
+  #t)
+
+(define-public output-classic-framework output-framework)
+
+
+(define-public (convert-to-ps . args) #t)
+(define-public (convert-to-pdf . args) #t)
+(define-public (convert-to-png . args) #t)
+(define-public (convert-to-dvi . args) #t)
+(define-public (convert-to-tex . args) #t)
