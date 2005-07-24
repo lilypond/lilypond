0ff7e60006168b264be2d57d547edbf4b6cdaa1e
hanwen <hanwen>
2005-07-24 17:19:15 +0000

    *** empty log message ***

---
+++ lily/include/profile.hh
@@ -0,0 +1,19 @@
+/*
+  profile.hh -- declare property profiling utils. 
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 2005 Han-Wen Nienhuys <hanwen@xs4all.nl>
+
+*/
+
+#ifndef PROFILE_HH
+#define PROFILE_HH
+
+#include "lily-guile.hh"
+
+void note_property_access (SCM *table, SCM sym);
+extern SCM context_property_lookup_table;
+extern SCM grob_property_lookup_table;
+
+#endif /* PROFILE_HH */
