a6ab0638b865c16b8caa4f1563908b49fc7e54c6
hanwen <hanwen>
2005-07-24 15:39:03 +0000

    (protect_smob): experiment: O(1) GC (un)protection.

---
+++ lily/smobs.cc
@@ -0,0 +1,60 @@
+/*
+  smobs.cc -- implement Smob protection
+
+  source file of the GNU LilyPond music typesetter
+
+  (c) 2005 Han-Wen Nienhuys <hanwen@xs4all.nl>
+
+*/
+
+#include "smobs.hh"
+
+static SCM smob_protection_list;
+
+void
+init_smob_protection()
+{
+  smob_protection_list = scm_cons (SCM_UNDEFINED, SCM_EOL);
+  scm_permanent_object (smob_protection_list);
+}
+ADD_SCM_INIT_FUNC(init_smob_protection, init_smob_protection);
+
+void
+protect_smob (SCM smob, SCM *prot_cons)
+{
+  SCM s = scm_cdr (smob_protection_list);
+  while (scm_is_pair (s) && scm_car (s) == SCM_UNDEFINED)
+    {
+      s = scm_cdr (s);
+    }
+
+  SCM prot = scm_cons (smob, s);
+  scm_set_cdr_x (smob_protection_list,
+		 prot);
+  *prot_cons = prot;
+}
+
+void
+unprotect_smob (SCM *prot_cons)
+{
+  SCM next = scm_cdr (*prot_cons);
+
+  if (next == SCM_EOL)
+    {
+      scm_set_car_x (*prot_cons, SCM_UNDEFINED);
+    }
+  else
+    {
+      scm_set_car_x (*prot_cons, SCM_UNDEFINED);
+      while (scm_is_pair (next)
+	     && scm_car (next) == SCM_UNDEFINED)
+
+	{
+	  next = scm_cdr (next);
+	}
+
+      scm_set_cdr_x (*prot_cons, next);
+    }
+
+  *prot_cons = SCM_EOL;
+}
