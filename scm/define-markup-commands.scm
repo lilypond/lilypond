1941c7bd9079737d75a2e10ef607abe3599c22f0
matsb <matsb>
2005-08-10 08:15:40 +0000

    * scm/define-markup-commands.scm (normal-text): Added 2 new 	markup commands, \normal-text and \medium (the latter thanks to 	Bruce Fairchild).

--- scm/define-markup-commands.scm
+++ scm/define-markup-commands.scm
@@ -762,6 +762,17 @@ recommend font for this is bold and italic"
   (interpret-markup
    layout (prepend-alist-chain 'font-shape 'upright props) arg))
 
+(def-markup-command (medium layout props arg) (markup?)
+  "Switch to medium font-series (in contrast to bold)."
+  (interpret-markup layout (prepend-alist-chain 'font-series 'medium props)
+		    arg))
+
+(def-markup-command (normal-text layout props arg) (markup?)
+  "Set all font related properties (except the size) to get the default normal text font, no matter what font was used earlier."
+  ;; ugh - latin1
+  (interpret-markup layout
+                    (cons '((font-family . roman) (font-shape . upright) (font-series . medium) (font-encoding . latin1)) props)
+                    arg)) 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; symbols.
