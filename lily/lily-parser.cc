f70d3ccdde568790a40138b121a4c4eb3d321a85
nsceaux <nsceaux>
2005-08-25 12:38:21 +0000

    * lily/lily-parser.cc (Lily_parser): also copy output_basename_ when copying a Lily_parser object.

--- lily/lily-parser.cc
+++ lily/lily-parser.cc
@@ -42,6 +42,7 @@ Lily_parser::Lily_parser (Lily_parser const &src)
   sources_ = src.sources_;
   default_duration_ = src.default_duration_;
   error_level_ = src.error_level_;
+  output_basename_ = src.output_basename_;
 
   smobify_self ();
   if (src.lexer_)
