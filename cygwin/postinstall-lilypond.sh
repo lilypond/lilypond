684b5c3b30504c1328392a702985bea5c63860e9
Jan Nieuwenhuizen <janneke@gnu.org>
2005-10-11 18:08:08 +0000

    *** empty log message ***

--- cygwin/postinstall-lilypond.sh
+++ cygwin/postinstall-lilypond.sh
@@ -16,16 +16,6 @@ regtool add '/root/LilyPond/shell'
 regtool add '/root/LilyPond/shell/open'
 regtool add '/root/LilyPond/shell/open/command'
 
-# old ideas - discarded for now
-# regtool set '/root/LilyPond/shell/open/command/' $ROOT'\bin\python /usr/bin/lily-wins %1'
-# invoking python directly does not work
-# regtool set '/root/LilyPond/shell/open/command/' $ROOT'\bin\run-lily-wins.bat %1'
-# we now use generated run-lily-wins.bat in /
-# regtool set '/root/LilyPond/shell/open/command/' $ROOT'\run-lily-wins.bat %1'
-# the direct bash route:
-
-# TODO: should 'open' run LilyPond?
-#       should 'open' also start PDF viewer?
 regtool set '/root/LilyPond/shell/open/command/' $ROOT'\bin\bash.exe --login -c '"'"'/usr/bin/lily-wins "%1"'"'"
 regtool add '/root/LilyPond/shell/edit'
 regtool set '/root/LilyPond/shell/edit/' '&Edit source in Notepad ...'
@@ -34,26 +24,4 @@ regtool set '/root/LilyPond/shell/edit/command/' '%SystemRoot%\system32\notepad.
 regtool add '/root/LilyPond/shell/generate'
 regtool set '/root/LilyPond/shell/generate/' '&Generate PDF ...'
 regtool add '/root/LilyPond/shell/generate/command'
-# regtool set '/root/LilyPond/shell/generate/command/' $ROOT'\bin\python /usr/bin/lily-wins %1'
 regtool set '/root/LilyPond/shell/generate/command/' $ROOT'\bin\bash.exe --login -c '"'"'/usr/bin/lily-wins "%1"'"'"
-
-# static run-lily-wins.bat, does not work.
-# @echo off
-# rem run-lily-wins.bat - Invoke /usr/bin/lily-wins from explorer
-# rem cd %~dp0%
-# rem bash.exe --login -c "/usr/bin/lily-wins '%1%'"
-# rem only works in Windows NT
-# rem %~dp0~bin\bash.exe --login -c "/usr/bin/lily-wins '%1%'"
-
-# generate run-lily-wins.bat - not necessary: direct bash route
-# how to handle \r\n endings? text/bin mode?
-#cat > /run-lily-wins.bat <<EOF
-#@echo off
-#rem run-lily-wins.bat - Invoke /usr/bin/lily-wins from explorer
-#$ROOT\\bin\\bash.exe --login -c "/usr/bin/lily-wins '%1%'"
-#EOF
-
-# cleanup old fonts
-touch /tmp/.lilypond-install
-rm $(find /var/lib/texmf /var/spool/texmf /var/cache/fonts -name 'feta*pk' -or -name 'feta*tfm' -or -name 'parmesan*pk' -or -name 'parmesan*tfm')
-rm -f /tmp/.lilypond-install
