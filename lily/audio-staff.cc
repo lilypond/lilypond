71445ba1dbf030538daf6e6e08c73d20caf11d20
Han-Wen Nienhuys <hanwen@xs4all.nl>
2005-11-14 15:57:25 +0000

    (output): degarianize.

--- lily/audio-staff.cc
+++ lily/audio-staff.cc
@@ -19,13 +19,14 @@ Audio_staff::add_audio_item (Audio_item *l)
 }
 
 void
-Audio_staff::output (Midi_stream &midi_stream_r, int track_i)
+Audio_staff::output (Midi_stream &midi_stream, int track)
 {
   Midi_track midi_track;
-  midi_track.number_ = track_i;
+  midi_track.number_ = track;
   midi_track.channel_ = channel_;
+  
   for (Midi_walker i (this, &midi_track); i.ok (); i++)
     i.process ();
-  midi_stream_r << midi_track;
+  midi_stream << midi_track;
 }
 
