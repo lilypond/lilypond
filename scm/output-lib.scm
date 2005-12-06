79863c5cbd03eb37be917367ddcb88a996ab4b30
Han-Wen Nienhuys <hanwen@xs4all.nl>
2005-12-06 10:52:45 +0000

    (print-circled-text-callback): draw circle around text using make-circle-markup.

--- scm/output-lib.scm
+++ scm/output-lib.scm
@@ -76,14 +76,13 @@
 
 (define-public (print-circled-text-callback grob)
   (let* ((text (ly:grob-property grob 'text))
+	 
 	 (layout (ly:grob-layout grob))
 	 (defs (ly:output-def-lookup layout 'text-font-defaults))
 	 (props (ly:grob-alist-chain grob defs))
 	 (circle (Text_interface::interpret_markup
-		  layout props (make-draw-circle-markup 0.8 0.1 #f)))
-	 (text-stencil (Text_interface::interpret_markup layout props text)))
-    
-    (ly:stencil-add (centered-stencil text-stencil) circle)))
+		  layout props (make-circle-markup text))))
+    circle))
 
 
 ;;(define (mm-to-pt x)
