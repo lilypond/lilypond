c94ee1129641fa138b169780313d2e8c537e3120
hanwen <hanwen>
2005-12-27 17:36:29 +0000

    * input/regression/stem-direction-context.ly: remove rest.
    * lily/melody-engraver.cc (process_music): restart for bar lines.
    (acknowledge_slur): restart for slurs.

--- lily/melody-engraver.cc
+++ lily/melody-engraver.cc
@@ -16,35 +16,71 @@
 class Melody_engraver : public Engraver
 {
   Grob *melody_item_;
+  Grob *stem_; 
 protected:
   
   DECLARE_ACKNOWLEDGER (stem);
+  DECLARE_ACKNOWLEDGER (slur);
   TRANSLATOR_DECLARATIONS (Melody_engraver);
+  void stop_translation_timestep ();
+  void process_music ();
 };
 
 
 Melody_engraver::Melody_engraver ()
 {
+  stem_ = 0;
   melody_item_ = 0;
 }
 
 void
-Melody_engraver::acknowledge_stem (Grob_info info)
+Melody_engraver::process_music ()
 {
-  extract_grob_set (info.grob (), "rests", rests);
-  if (rests.size ())
+  if (scm_is_string (get_property ("whichBar")))
     melody_item_ = 0;
-  else
+}
+  
+void
+Melody_engraver::stop_translation_timestep ()
+{
+  if (stem_)
     {
-      if (!melody_item_)
-	melody_item_ = make_item ("MelodyItem", info.grob ()->self_scm ());
+      extract_grob_set (stem_, "rests", rests);
+      if (rests.size ())
+	melody_item_ = 0;
+      else
+	{
+	  if (!melody_item_)
+	    melody_item_ = make_item ("MelodyItem", stem_->self_scm ());
 
-      Melody_spanner::add_stem (melody_item_, info.grob ());
+	  Melody_spanner::add_stem (melody_item_, stem_);
+	}
     }
+  stem_ = 0;
 }
 
+
+void
+Melody_engraver::acknowledge_slur (Grob_info info)
+{
+  (void)info;
+  melody_item_ = 0;
+}
+
+
+void
+Melody_engraver::acknowledge_stem (Grob_info info)
+{
+  stem_ = info.grob ();
+}
+
+
+
 #include "translator.icc"
+
 ADD_ACKNOWLEDGER (Melody_engraver, stem);
+ADD_ACKNOWLEDGER (Melody_engraver, slur);
+
 ADD_TRANSLATOR (Melody_engraver,
 		"Create information for context dependent typesetting decisions. ",
 		"MelodyItem",
