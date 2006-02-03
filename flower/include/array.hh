0357e2b58c843f599dfb75eaa80771ae630b5d72
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-02-03 13:10:17 +0000

    typo.

--- flower/include/array.hh
+++ flower/include/array.hh
@@ -305,7 +305,7 @@ public:
   boundary (Array<T> const &v, int dir, vsize i)
   {
     assert (dir);
-    return v[dir = -1 ? i : v.size () - 1 - i];
+    return v[dir == -1 ? i : v.size () - 1 - i];
   }
 
   template<typename T>
