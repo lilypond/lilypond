ea3cda20a6cef48d9f4c270bf5654f8d36b38f4e
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-04-30 20:18:53 +0000

    (conv): indent 4 for python files.

--- python/fontextract.py
+++ python/fontextract.py
@@ -1,4 +1,5 @@
 import re
+
 import getopt
 import sys
 import os
@@ -10,106 +11,106 @@ end_font_regex = re.compile ('%%EndFont')
 verbose = 0
 
 try:
-	import gettext
-	gettext.bindtextdomain ('lilypond', localedir)
-	gettext.textdomain ('lilypond')
-	_ = gettext.gettext
+    import gettext
+    gettext.bindtextdomain ('lilypond', localedir)
+    gettext.textdomain ('lilypond')
+    _ = gettext.gettext
 except:
-	def _ (s):
-		return s
-	
+    def _ (s):
+	return s
+
 def scan_files (files):
-	file_of_font_dict = {}
-	for f in files:
-		if verbose:
-			sys.stderr.write (_('Scanning %s') % f + '\n')
-		
-		header = open (f, 'r').read ()
-		idx = 0
+    file_of_font_dict = {}
+    for f in files:
+	if verbose:
+	    sys.stderr.write (_('Scanning %s') % f + '\n')
 
-		extract_from_this = []
-		while idx < len (header):
-			match = dsr_font_regex.search (header[idx:])
-			if not match:
-				break
-			name = match.group (1)
-			idx += match.end (1)
-			if file_of_font_dict.has_key (name):
-				continue
+	header = open (f, 'r').read ()
+	idx = 0
 
-			file_of_font_dict[name] = f
+	extract_from_this = []
+	while idx < len (header):
+	    match = dsr_font_regex.search (header[idx:])
+	    if not match:
+		break
+	    name = match.group (1)
+	    idx += match.end (1)
+	    if file_of_font_dict.has_key (name):
+		continue
 
-	return file_of_font_dict
+	    file_of_font_dict[name] = f
+
+    return file_of_font_dict
 
 def get_file_fonts_dict (file_of_font_dict):
-	dict = {}
-	for (n, f) in file_of_font_dict.items ():
-		if not dict.has_key (f):
-			dict[f] = []
+    dict = {}
+    for (n, f) in file_of_font_dict.items ():
+	if not dict.has_key (f):
+	    dict[f] = []
 
-		dict[f].append (n)
-		
-	return dict
+	dict[f].append (n)
+
+    return dict
 
 def extract_fonts_from_file (extract_from_this, font_dict, filename):
-	if extract_from_this:
+    if extract_from_this:
+	curr_font = []
+	curr_font_name = []
+	in_font = 0
+	for l in open (filename).readlines ():
+	    if not in_font and begin_font_regex.match (l):
+		in_font = 1
+		curr_font_name = begin_font_regex.match (l).group (1)
 		curr_font = []
-		curr_font_name = []
+	    elif in_font and end_font_regex.match (l):
 		in_font = 0
-		for l in open (filename).readlines ():
-			if not in_font and begin_font_regex.match (l):
-				in_font = 1
-				curr_font_name = begin_font_regex.match (l).group (1)
-				curr_font = []
-			elif in_font and end_font_regex.match (l):
-				in_font = 0
 
-				if curr_font_name in extract_from_this:
-					font_dict[curr_font_name] = string.join (curr_font, '')
-					if verbose:
-						sys.stderr.write (_('Extracted %s')
-								  % curr_font_name + '\n')
+		if curr_font_name in extract_from_this:
+		    font_dict[curr_font_name] = string.join (curr_font, '')
+		    if verbose:
+			sys.stderr.write (_('Extracted %s')
+					  % curr_font_name + '\n')
 
-					extract_from_this.remove (curr_font_name)
-			elif in_font:
-				curr_font.append (l)
-			if not extract_from_this:
-				break
+		    extract_from_this.remove (curr_font_name)
+	    elif in_font:
+		curr_font.append (l)
+	    if not extract_from_this:
+		break
 
-		if extract_from_this:
-			sys.stderr.write ("Failed to extract %s from %s\n"
-					  % (string.join (extract_from_this, ', '), filename))
+	if extract_from_this:
+	    sys.stderr.write ("Failed to extract %s from %s\n"
+			      % (string.join (extract_from_this, ', '), filename))
 
 def write_extracted_fonts (output_file_name, font_dict):
-	if verbose:
-		sys.stderr.write( _('Writing fonts to %s') % output_file_name + '\n')
-	output = open (output_file_name, 'w')
-	output.write ('''%!PS-Adobe-3.0
+    if verbose:
+	sys.stderr.write( _('Writing fonts to %s') % output_file_name + '\n')
+    output = open (output_file_name, 'w')
+    output.write ('''%!PS-Adobe-3.0
 %%VMusage: 0 0 
 %%Creator: lilypond-extract-fonts
 ''')
 
-	for x in font_dict.keys ():
-		output.write ('%%%%DocumentSuppliedResources: font %s\n' % x)
+    for x in font_dict.keys ():
+	output.write ('%%%%DocumentSuppliedResources: font %s\n' % x)
 
-	output.write ('''%%EndComments\n''')
+    output.write ('''%%EndComments\n''')
 
-	for (k,v) in font_dict.items ():
-		output.write ('\n%%%%BeginFont: %s\n' % k)
-		output.write (v)
-		output.write ('\n%%EndFont')
+    for (k,v) in font_dict.items ():
+	output.write ('\n%%%%BeginFont: %s\n' % k)
+	output.write (v)
+	output.write ('\n%%EndFont')
 
 
 def extract_fonts (output_file_name, input_files):
-	d = scan_files (input_files)
-	ff = get_file_fonts_dict (d)
-	
-	font_dict = {}
-	for (file, fonts) in ff.items ():
-		extract_fonts_from_file (fonts, font_dict, file)
+    d = scan_files (input_files)
+    ff = get_file_fonts_dict (d)
 
-	write_extracted_fonts (output_file_name, font_dict)
+    font_dict = {}
+    for (file, fonts) in ff.items ():
+	extract_fonts_from_file (fonts, font_dict, file)
+
+    write_extracted_fonts (output_file_name, font_dict)
 
 
 if __name__ == '__main__':
-	extract_fonts ('fonts.ps', sys.argv[1:])
+    extract_fonts ('fonts.ps', sys.argv[1:])
--- python/lilylib.py
+++ python/lilylib.py
@@ -1,4 +1,4 @@
-################################################################
+###############################################################
 # lilylib.py -- options and stuff
 # 
 # source file of the GNU LilyPond music typesetter
@@ -19,10 +19,10 @@ import subprocess
 ## windows mingw cross compile doesn't have selectmodule.so
 have_fcntl = True
 try:
-	import fcntl
+    import fcntl
 except ImportError:
-	have_fcntl = False
-	
+    have_fcntl = False
+
 ################################################################
 # Users of python modules should include this snippet
 # and customize variables below.
@@ -37,11 +37,11 @@ except ImportError:
 
 datadir = '@local_lilypond_datadir@'
 if not os.path.isdir (datadir):
-	datadir = '@lilypond_datadir@'
+    datadir = '@lilypond_datadir@'
 if os.environ.has_key ('LILYPONDPREFIX') :
-	datadir = os.environ['LILYPONDPREFIX']
-	while datadir[-1] == os.sep:
-		datadir= datadir[:-1]
+    datadir = os.environ['LILYPONDPREFIX']
+    while datadir[-1] == os.sep:
+	datadir= datadir[:-1]
 
 sys.path.insert (0, os.path.join (datadir, 'python'))
 
@@ -50,124 +50,124 @@ sys.path.insert (0, os.path.join (datadir, 'python'))
 
 localedir = '@localedir@'
 try:
-	import gettext
-	gettext.bindtextdomain ('lilypond', localedir)
-	gettext.textdomain ('lilypond')
-	_ = gettext.gettext
+    import gettext
+    gettext.bindtextdomain ('lilypond', localedir)
+    gettext.textdomain ('lilypond')
+    _ = gettext.gettext
 except:
-	def _ (s):
-		return s
+    def _ (s):
+	return s
 underscore = _
 progress = sys.stderr.write 
 
 
 def command_name (cmd):
-	# Strip all stuf after command,
-	# deal with "((latex ) >& 1 ) .." too
-	cmd = re.match ('([\(\)]*)([^\\\ ]*)', cmd).group (2)
-	return os.path.basename (cmd)
+    # Strip all stuf after command,
+    # deal with "((latex ) >& 1 ) .." too
+    cmd = re.match ('([\(\)]*)([^\\\ ]*)', cmd).group (2)
+    return os.path.basename (cmd)
 
 def system (cmd,
 	      ignore_error=False,
 	      progress_p=True,
 	      be_verbose=False,
 	      log_file=None):
-	
-	show_progress= progress_p 
-	name = command_name (cmd)
-	error_log_file = ''
 
-	if be_verbose:
-		show_progress = 1
-		progress (_ ("Invoking `%s\'") % cmd)
+    show_progress= progress_p 
+    name = command_name (cmd)
+    error_log_file = ''
+
+    if be_verbose:
+	show_progress = 1
+	progress (_ ("Invoking `%s\'") % cmd)
+    else:
+	progress ( _("Running %s...") % name)
+
+
+    stdout_setting = None
+    if not show_progress:
+	stdout_setting = subprocess.PIPE
+
+    proc = subprocess.Popen (cmd,
+			     shell=True,
+			     universal_newlines=True,
+			     stdout=stdout_setting,
+			     stderr=stdout_setting)
+
+    log = ''
+
+    if show_progress:
+	retval = proc.wait()
+    else:
+	log = proc.communicate ()
+	retval = proc.returncode
+
+
+    if retval:
+	print >>sys.stderr, 'command failed:', cmd
+	if retval < 0:
+	    print >>sys.stderr, "Child was terminated by signal", -retval
+	elif retval > 0:
+	    print >>sys.stderr, "Child returned", retval
+
+	if ignore_error:
+	    print >>sys.stderr, "Error ignored"
 	else:
-		progress ( _("Running %s...") % name)
+	    if not show_progress:
+		print log[0]
+		print log[1]
+	    sys.exit (1)
 
-
-	stdout_setting = None
-	if not show_progress:
-		stdout_setting = subprocess.PIPE
-		
-	proc = subprocess.Popen (cmd,
-				 shell=True,
-				 universal_newlines=True,
-				 stdout=stdout_setting,
-				 stderr=stdout_setting)
-
-	log = ''
-
-	if show_progress:
-		retval = proc.wait()
-	else:
-		log = proc.communicate ()
-		retval = proc.returncode
-
-
-	if retval:
-		print >>sys.stderr, 'command failed:', cmd
-		if retval < 0:
-		    print >>sys.stderr, "Child was terminated by signal", -retval
-		elif retval > 0:
-		    print >>sys.stderr, "Child returned", retval
-
-		if ignore_error:
-			print >>sys.stderr, "Error ignored"
-		else:
-			if not show_progress:
-				print log[0]
-				print log[1]
-			sys.exit (1)
-
-	return abs (retval)
+    return abs (retval)
 
 def strip_extension (f, ext):
-	(p, e) = os.path.splitext (f)
-	if e == ext:
-		e = ''
-	return p + e
+    (p, e) = os.path.splitext (f)
+    if e == ext:
+	e = ''
+    return p + e
 
 
 def search_exe_path (name):
-	p = os.environ['PATH']
-	exe_paths = string.split (p, ':')
-	for e in exe_paths:
-		full = os.path.join (e, name)
-		if os.path.exists (full):
-			return full
-	return None
+    p = os.environ['PATH']
+    exe_paths = string.split (p, ':')
+    for e in exe_paths:
+	full = os.path.join (e, name)
+	if os.path.exists (full):
+	    return full
+    return None
 
 
 def print_environment ():
-	for (k,v) in os.environ.items ():
-		sys.stderr.write ("%s=\"%s\"\n" % (k, v)) 
+    for (k,v) in os.environ.items ():
+	sys.stderr.write ("%s=\"%s\"\n" % (k, v)) 
 
 
 class NonDentedHeadingFormatter (optparse.IndentedHelpFormatter):
     def format_heading(self, heading):
-	    if heading:
-		    return heading[0].upper() + heading[1:] + ':\n'
-	    return ''
+	if heading:
+	    return heading[0].upper() + heading[1:] + ':\n'
+	return ''
     def format_option_strings(self, option):
-	    sep = ' '
-	    if option._short_opts and option._long_opts:
-		    sep = ','
+	sep = ' '
+	if option._short_opts and option._long_opts:
+	    sep = ','
 
-	    metavar = ''
-	    if option.takes_value():
-		    metavar = '=%s' % option.metavar or option.dest.upper()
+	metavar = ''
+	if option.takes_value():
+	    metavar = '=%s' % option.metavar or option.dest.upper()
 
-	    return "%3s%s %s%s" % (" ".join (option._short_opts),
-				   sep,
-				   " ".join (option._long_opts),
-				   metavar)
+	return "%3s%s %s%s" % (" ".join (option._short_opts),
+			       sep,
+			       " ".join (option._long_opts),
+			       metavar)
 
     def format_usage(self, usage):
         return _("Usage: %s\n") % usage
-    
+
     def format_description(self, description):
-	    return description
+	return description
 
 def get_option_parser (*args, **kwargs): 
-	p = optparse.OptionParser (*args, **kwargs)
-	p.formatter = NonDentedHeadingFormatter () 
-	return p
+    p = optparse.OptionParser (*args, **kwargs)
+    p.formatter = NonDentedHeadingFormatter () 
+    return p
