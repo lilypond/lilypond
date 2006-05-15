0226ad798d0aab833107028360308cd9c18ae961
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-05-15 12:38:17 +0000

    cleanup.

--- lily/clef-engraver.cc
+++ lily/clef-engraver.cc
@@ -193,7 +193,15 @@ Clef_engraver::stop_translation_timestep ()
 ADD_ACKNOWLEDGER (Clef_engraver, bar_line);
 ADD_TRANSLATOR (Clef_engraver,
 		/* doc */ "Determine and set reference point for pitches",
-		/* create */ "Clef OctavateEight",
+		/* create */ "Clef "
+		"OctavateEight ",
 		/* accept */ "",
-		/* read */ "clefPosition clefGlyph middleCPosition clefOctavation explicitClefVisibility forceClef",
+		/* read */
+		"clefGlyph "
+		"clefOctavation "
+		"clefPosition "
+		"explicitClefVisibility "
+		"forceClef "
+		"middleCPosition "
+		,
 		/* write */ "");
