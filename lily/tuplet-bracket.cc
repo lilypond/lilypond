b5c507067345b520a4bd0c90d4832a40a2baff86
Han-Wen Nienhuys <hanwen@xs4all.nl>
2006-06-13 11:44:44 +0000

    * lily/tuplet-bracket.cc (calc_positions): look at stem-end-position for tuplet bracket slope. Fixes sloped tuplet brackets narrower than beams. Backportme.
    * lily/lexer.ll: set version-seen? even if version is
    INVALID. Backportme.

--- lily/tuplet-bracket.cc
+++ lily/tuplet-bracket.cc
@@ -74,8 +74,13 @@ flatten_number_pair_property (Grob *me,
 }
 
 
+/*
+  Return beam that encompasses the span of the tuplet bracket.
+*/
+
 Grob *
-Tuplet_bracket::parallel_beam (Grob *me_grob, vector<Grob*> const &cols, bool *equally_long)
+Tuplet_bracket::parallel_beam (Grob *me_grob, vector<Grob*> const &cols,
+			       bool *equally_long)
 {
   Spanner *me = dynamic_cast<Spanner *> (me_grob);
 
@@ -109,7 +114,8 @@ Tuplet_bracket::parallel_beam (Grob *me_grob, vector<Grob*> const &cols, bool *e
     }
 
   *equally_long =
-    (beam_stems[0] == stems[LEFT] && beam_stems.back () == stems[RIGHT]);
+    (beam_stems[0] == stems[LEFT]
+     && beam_stems.back () == stems[RIGHT]);
   return beams[LEFT];
 }
 
@@ -657,20 +663,29 @@ Tuplet_bracket::calc_positions (SCM smob)
   if (!par_beam
       || get_grob_direction (par_beam) != dir)
     calc_position_and_height (me, &offset, &dy);
-  else
+  else if  (columns.size ()
+	    && Note_column::get_stem (columns[0])
+	    && Note_column::get_stem (columns.back ()))
     {
-      SCM ps = par_beam->get_property ("positions");
+      /*
+	trigger set_stem_ends
+       */
+      (void) par_beam->get_property ("quantized-positions");
 
-      Real lp = scm_to_double (scm_car (ps));
-      Real rp = scm_to_double (scm_cdr (ps));
 
-      Real ss = Staff_symbol_referencer::staff_space (me);
+      Drul_array<Grob *> stems (Note_column::get_stem (columns[0]),
+				Note_column::get_stem (columns.back ()));
+
+      
+      
+
+      Real ss = 0.5 * Staff_symbol_referencer::staff_space (me);
+      Real lp = ss * robust_scm2double (stems[LEFT]->get_property ("stem-end-position"), 0.0);
+      Real rp = ss * robust_scm2double (stems[RIGHT]->get_property ("stem-end-position"), 0.0);
+
       
       offset = lp + dir * (0.5 + scm_to_double (me->get_property ("padding")));
       dy = (rp - lp);
-
-      dy *= ss;
-      offset *= ss;	
     }
 
   
