d2a758710cbb0efa631c15434088b0a91a8d6977
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-01-02 03:44:56 +0100

    pimp coverage scripts further.

--- buildscripts/build-coverage.sh
+++ buildscripts/build-coverage.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
-./configure --enable-config=cov --disable-optimising
-make conf=cov -j2 clean
-perl -i~ -pe 's/-pipe /-fprofile-arcs -ftest-coverage -pipe /g' config-cov.make
-perl -i~ -pe 's/ -ldl / -lgcov -ldl /g' config-cov.make
+
+if test "$1" == "--fresh"; then
+  fresh=yes
+fi
+
+if test ! -f config-cov.make; then
+  fresh=yes
+fi
+
+if test "$fresh" = "yes";
+then
+  ./configure --enable-config=cov --disable-optimising
+  make conf=cov -j2 clean
+  perl -i~ -pe 's/-pipe /-fprofile-arcs -ftest-coverage -pipe /g' config-cov.make
+  perl -i~ -pe 's/ -ldl / -lgcov -ldl /g' config-cov.make
+else
+  find -name '*.gcda' -exec rm  '{}' ';'
+fi
+
+
 make conf=cov -j2
 make conf=cov test-clean LILYPOND_JOBS=          
 make conf=cov test LILYPOND_JOBS= >& out-cov/test-run.log
+
+rm -rf out-cov
+mkdir out-cov
 
 cd out-cov
 ln ../lily/* .
@@ -16,3 +35,5 @@ for a in *[cyl]
 do
    gcov -o ../lily/out-cov/  -p $a > $a.gcov-summary
 done 
+
+python buildscripts/coverage.py
