fd5d780f95244d4f88577669ae80689a3bfa906e
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-01-08 11:21:46 +0100

    fix \quoteDuring message.
    Conflicts:
    
    	scm/music-functions.scm

--- scm/music-functions.scm
+++ scm/music-functions.scm
@@ -744,7 +744,7 @@ Syntax:
 	      (set! (ly:music-property music 'quoted-events) quoted-vector)
 	      (set! (ly:music-property music 'iterator-ctor)
 		    ly:quote-iterator::constructor))
-	    (ly:warning (_ "can't find quoted music `~S'" quoted-name))))
+	    (ly:warning (_ "cannot find quoted music: `~S'") quoted-name)))
     music))
 
 
