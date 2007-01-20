7053fd587f748b6ea41c1bdb172b7f92c11f7748
Graham Percival <graham@percival-music.ca>
2007-01-20 13:06:13 -0800

    Clean-up of makelsr script.

--- input/makelsr.py
+++ input/makelsr.py
@@ -2,7 +2,6 @@
 import sys
 import os
 import os.path
-#import re
 import shutil
 
 dirs=['advanced','trick']
@@ -16,8 +15,6 @@ except:
 for dir in dirs:
 	srcdir = os.path.join(in_dir, dir)
 	destdir = os.path.join(os.getcwd(), 'lsr', dir)
-#	if ~os.path.isdir(destdir):
-#		os.mkdir(destdir)
 
 	file_names=os.listdir(destdir)
 	for file in file_names:
@@ -28,12 +25,7 @@ for dir in dirs:
 	file_names=os.listdir(in_dir + dir)
 	for file in file_names:
 		out_name=file
-		#out_name=re.sub(r'_',r'-',os.path.basename(file))
-		#out_name=re.sub(r'\(', r'', out_name)
-		#out_name=re.sub(r'\)', r'', out_name)
-		#out_name=re.sub(r'\'', r'', out_name)
 		src = os.path.join(srcdir, file)
 		dest = os.path.join(destdir, out_name)
-		#print src + "  " + dest
 		shutil.copyfile(src, dest)
 
