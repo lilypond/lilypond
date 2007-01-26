d640581d823b26d988046148b995e1a70f6ad735
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-01-26 02:59:07 +0100

    Fix #174.
    Tune dash-period to end on a full dash.

--- lily/line-interface.cc
+++ lily/line-interface.cc
@@ -225,8 +225,13 @@ Line_interface::line (Grob *me, Offset from, Offset to)
       Real period = Staff_symbol_referencer::staff_space (me)
 	* robust_scm2double (me->get_property ("dash-period"), 1.0);
 
-      if (period < 0)
+      if (period <= 0)
 	return Stencil ();
+
+      Real len = (to-from).length();
+      
+      int n = (int) rint ((len - period * fraction) / period);
+      period = ((to-from).length() - period * fraction) / n;
 
       stil = make_dashed_line (thick, from, to, period, fraction);
     }
