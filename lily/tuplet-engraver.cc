9b261bd9c0df46fa7bfec25314df7cf1c91b6452
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-02-02 01:02:28 +0100

    Fix #263.
    Take min() of tupletSpannerDuration and tuplet length.

--- lily/tuplet-engraver.cc
+++ lily/tuplet-engraver.cc
@@ -215,7 +215,9 @@ Tuplet_engraver::Tuplet_engraver ()
 
 ADD_ACKNOWLEDGER (Tuplet_engraver, note_column);
 ADD_TRANSLATOR (Tuplet_engraver,
-		/* doc */ "Catch TupletSpannerEvent and generate appropriate bracket  ",
+		/* doc */
+		"Catch @ref{TupletSpannerEvent} and generate appropriate bracket.",
+		
 		/* create */
 		"TupletBracket "
 		"TupletNumber ",
