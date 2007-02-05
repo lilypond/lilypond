735610e8f601a580140d87610a98bfcdd02dadd8
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-02-05 23:00:47 +0100

    Fix #272
    Add minimum-Y-extent to VoltaBracket.

--- scm/define-grobs.scm
+++ scm/define-grobs.scm
@@ -2081,6 +2081,7 @@
 	(direction . ,UP)
 	(padding . 1)
 	(font-encoding . fetaNumber)
+	(minimum-Y-extent . (0 . 2))
 	(Y-offset . ,ly:side-position-interface::y-aligned-side)
 	(side-axis . ,Y)
 	(thickness . 1.6)  ;;  line-thickness
