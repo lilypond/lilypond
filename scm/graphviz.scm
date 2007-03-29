a982b5419c9be31b649ce847ca6fcf60a0823883
Han-Wen Nienhuys <hanwen@xs4all.nl>
2007-03-29 16:23:18 -0300

    clean up graphviz for inclusion in regtest.

--- scm/graphviz.scm
+++ scm/graphviz.scm
@@ -7,14 +7,17 @@
 
 (define-module (scm graphviz)
   #:use-module (lily)
-  #:export (make-graph add-node add-edge add-cluster))
+  #:export
+  (make-graph add-node add-edge add-cluster
+	      graph-write
+	      ))
 
 (define (make-graph filename)
-  (let ((empty-graph (list->vector (list filename '() '() '()))))
-    (ly:atexit write-graph (list empty-graph))
-    empty-graph))
+   #(() () () ()))
 
-(define (filename g) (vector-ref g 0))
+
+;; fixme: use structs/records.
+;; fixme add & use setters.
 (define (nodes g) (vector-ref g 1))
 (define (edges g) (vector-ref g 2))
 (define (clusters g) (vector-ref g 3))
@@ -41,12 +44,11 @@
 (define (add-edge graph node1 node2)
   (vector-set! graph 2 (cons `(,node1 . ,node2) (edges graph))))
 
-(define (write-graph graph)
-  (let ((out (open-file (filename graph) "w"))
-	(ns (nodes graph))
+(define (graph-write graph out)
+  (let ((ns (nodes graph))
 	(es (edges graph))
 	(cs (clusters graph)))
-    (ly:message (format "writing graph ~s..." (filename graph)))
+    (ly:message (format (_ "Writing graph `~a'...") (port-filename out)))
     (display "digraph G {\nrankdir=\"LR\"\nnode [shape=rectangle]\n" out)
     (map (lambda (n) (display (format "~a [label=\"~a\"]\n" (car n) (cdr n)) out))
 	 ns)
