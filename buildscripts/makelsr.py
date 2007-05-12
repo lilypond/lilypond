2f094ccb70b21f7068dc0469be62b83476d81d02
Graham Percival <graham@percival-music.ca>
2007-05-12 00:33:39 -0700

    LSR: speed increase for buildscripts/makelsr.py

--- buildscripts/makelsr.py
+++ buildscripts/makelsr.py
@@ -28,7 +28,7 @@ def copy_dir_with_test(srcdir, destdir):
 			dest = os.path.join (destdir, file)
 			copy_with_warning(src, dest)
 			os.system('convert-ly -e ' + dest)
-			s = os.system('lilypond -dsafe -dbackend=svg -o /tmp/lsrtest ' + dest)
+			s = os.system('lilypond -dno-print-pages -dsafe -o /tmp/lsrtest ' + dest)
 			if s:
 				notsafe.append(dest)
 
