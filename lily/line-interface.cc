drewm1980/lilypond-an

commit fb88079204f0fd758f271f88bd7b2f3d8102b6fc

Modify line-interface, so that style=#'line gives a solid line and style = #'dashed-line gives a dashed line.

lily/line-interface.cc
@@ -212,14 +212,13 @@ Line_interface::line (Grob *me, Offset from, Offset to)
   
   Stencil stil;
 
-  SCM dash_fraction = me->get_property ("dash-fraction");
-  if (scm_is_number (dash_fraction) || type == ly_symbol2scm ("dotted-line"))
+  if (type == ly_symbol2scm ("dashed-line") || type == ly_symbol2scm ("dotted-line"))
     {
 
       Real fraction
 	= type == ly_symbol2scm ("dotted-line")
 	? 0.0
-	: robust_scm2double (dash_fraction, 0.4);
+	: robust_scm2double (me->get_property ("dash-fraction"), 0.4);
 
       fraction = min (max (fraction, 0.0), 1.0);
       Real period = Staff_symbol_referencer::staff_space (me)
@@ -249,11 +248,14 @@ Line_interface::line (Grob *me, Offset from, Offset to)
 }
 
 ADD_INTERFACE (Line_interface,
-	       "Generic line objects. Any object using lines supports this.  Normally, "
-	       "you get a straight line. If @code{dash-period} is defined, a dashed line is "
-	       "produced; the length of the dashes is tuned with "
+	       "Generic line objects. Any object using lines supports this. "
+	       "The property @code{style} can be @code{line}, "
+	       "@code{dashed-line}, @code{trill}, \n"
+	       "@code{dotted-line} or @code{zigzag}.\n"
+	       "\n",
+	       "For dashed-line, the length of the dashes is tuned with "
 	       "@code{dash-fraction}. If the latter is set to 0, a dotted line is "
-	       "produced. If @code{dash-fraction} is negative, the line is made "
+	       "produced. If @code{dash-period} is negative, the line is made "
 	       "transparent.",
 
 	       /* properties */

input/regression/line-arrows.ly
@@ -10,7 +10,7 @@
 
 \relative c'' {
   \override TextSpanner #'bound-padding = #1.0
-  \override TextSpanner #'dash-fraction = #'()
+  \override TextSpanner #'style = #line
   \override TextSpanner #'bound-details #'right #'arrow = ##t
   \override TextSpanner #'bound-details #'left #'text = #"fof"
   \override TextSpanner #'bound-details #'right #'text = #"gag"

input/regression/line-dashed-period.ly
@@ -14,8 +14,8 @@ starts and ends on a full dash. "
 
 \relative <<
   \new Staff {
-    \override DynamicTextSpanner #'dash-fraction = ##f
     \setTextCresc
+    \set crescendoSpanner = #'line
     c1_\< c c1\!
   }
   \new Staff {

input/regression/line-style.ly
@@ -12,7 +12,7 @@
 
   s2
   d2 \glissando d'2
-  \once \override Glissando #'dash-fraction = #0.5
+  \override Glissando #'style = #'dashed-line
   d,2 \glissando d'2
   \override Glissando #'style = #'dotted-line
   d,2 \glissando d'2

ly/engraver-init.ly
@@ -744,14 +744,10 @@ of Editio Vaticana."
 
   %% Prepare TextSpanner for \episem{Initium|Finis} use.
   %%
-  %% N.B.: dash-fraction MUST be unset; otherwise, TextSpanner will
-  %% always produce dashed lines, regardless of the style property.
-  %%
   %% FIXME: The line @code{\override TextSpanner #'padding = #-0.1} is
   %% required to force the articulation signs being placed vertically
   %% tightly to the correpsonding note heads.
   %%
-  \override TextSpanner #'dash-fraction = #'()
   \override TextSpanner #'style = #'line
   \override TextSpanner #'padding = #-0.1
 }

lilypond/lilypond

commit d4ce4ffb6c483324393e6427a53db24a73bc60e2

Fix trivial bug introduced in commit fb88079204f0fd758f271f88bd7b2f3d8102b6fc

lily/line-interface.cc
@@ -252,7 +252,7 @@ ADD_INTERFACE (Line_interface,
 	       "The property @code{style} can be @code{line}, "
 	       "@code{dashed-line}, @code{trill}, \n"
 	       "@code{dotted-line} or @code{zigzag}.\n"
-	       "\n",
+	       "\n"
 	       "For dashed-line, the length of the dashes is tuned with "
 	       "@code{dash-fraction}. If the latter is set to 0, a dotted line is "
 	       "produced. If @code{dash-period} is negative, the line is made "