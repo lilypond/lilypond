bb4fc519338b35b6d2636dca6444263a9bc6a588
Reinhold Kainhofer <reinhold@kainhofer.com>
2007-11-30 13:28:03 +0100

    MusicXML: Prevent negative duration logs (=> CRASH)

--- scripts/musicxml2ly.py
+++ scripts/musicxml2ly.py
@@ -1190,7 +1190,7 @@ class LilyPondVoiceBuilder:
             duration_factor = 1
             duration_log = {1: 0, 2: 1, 4:2, 8:3, 16:4, 32:5, 64:6, 128:7, 256:8, 512:9}.get (diff.denominator (), -1)
             duration_dots = 0
-            if duration_log >= 0: # denominator is a power of 2...
+            if duration_log > 0: # denominator is a power of 2...
                 if diff.numerator () == 3:
                     duration_log -= 1
                     duration_dots = 1
