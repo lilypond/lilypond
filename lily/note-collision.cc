2f395cc996e1aa8903d4285a63a02f26d9fe7966
Han-Wen Nienhuys <hanwen@xs4all.nl>
2008-02-27 00:36:15 -0300

    move ignore-collision to note-column-interface

--- lily/note-collision.cc
+++ lily/note-collision.cc
@@ -575,7 +575,6 @@ ADD_INTERFACE (Note_collision_interface,
 	       "@code{force-hshift} and @code{horizontal-shift}.",
 
 	       /* properties */
-	       "ignore-collision "
 	       "merge-differently-dotted "
 	       "merge-differently-headed "
 	       "positioning-done ");
