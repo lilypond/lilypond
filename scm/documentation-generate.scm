480b80feab1202afff1d972a1316c4f44d48daf8
Reinhold Kainhofer <reinhold@kainhofer.com>
2008-09-30 22:16:47 +0200

    Docs: Move the @top node before the first content line
    texi2html switched to not printing the Top node at the very beginning
    any more, but rather at the spot where it really appears in the .tely
    file. If there is some content before the @top node, that content
    will appear before the headline in the .html file.

--- scm/documentation-generate.scm
+++ scm/documentation-generate.scm
@@ -90,12 +90,6 @@
 @end tex
 @end iftex
 
-@ifhtml
-This document is also available as a
-@uref{source/Documentation/user/lilypond-internals.pdf,PDF} and as
-@uref{source/Documentation/user/lilypond-internals-big-page.html,one big page}.
-@end ifhtml
-
 @finalout
 
 @titlepage
@@ -113,16 +107,21 @@ For LilyPond version @version{}
 
 @contents
 
-@ifnottex
-")
+@ifnottex")
  out-port)
 
 (define top-node
   (make <texi-node>
     #:name "GNU LilyPond -- Internals Reference"
     #:text
-    (string-append  "
-@end ifnottex
+    (string-append  "@end ifnottex
+
+@ifhtml
+This document is also available as a
+@uref{source/Documentation/user/lilypond-internals.pdf,PDF} and as
+@uref{source/Documentation/user/lilypond-internals-big-page.html,one big page}.
+@end ifhtml
+
 This is the Internals Reference (IR) for version "
 		    (lilypond-version)
 		    " of LilyPond, the GNU music typesetter.")
