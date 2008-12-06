ca180f98fe0151dc1edf09c8533484dfe7a27f7d
Werner Lemberg <wl@gnu.org>
2008-12-06 07:21:04 +0100

    Typo.

--- lily/text-interface.cc
+++ lily/text-interface.cc
@@ -72,8 +72,8 @@ MAKE_SCHEME_CALLBACK_WITH_OPTARGS (Text_interface, interpret_markup, 3, 0,
 "  Takes three arguments, @var{layout}, @var{props}, and @var{markup}.\n"
 "\n"
 "@var{layout} is a @code{\\layout} block; it may be obtained from a grob with"
-" @code{ly:grob-layout}.  @var{props} is a alist chain, ie. a list of alists."
-"  This is typically obtained with"
+" @code{ly:grob-layout}.  @var{props} is an alist chain, i.e. a list of"
+"  alists.  This is typically obtained with"
 " @code{(ly:grob-alist-chain (ly:layout-lookup layout 'text-font-defaults))}."
 "  @var{markup} is the markup text to be processed.");
 SCM
