8ecc147c9397499e09b0a6d52c897645a5e4b001
Neil Puttock <n.puttock@gmail.com>
2009-07-09 21:57:31 +0100

    Fix #741 from Nicolas.
    - set option direction-required to #f so hyphens aren't inserted
    (cherry picked from commit e79d2b7555647bd25753ae598580556f2f092a7f)

--- scm/define-music-display-methods.scm
+++ scm/define-music-display-methods.scm
@@ -223,7 +223,7 @@
 (define-post-event-display-method MultiMeasureTextEvent (event parser) #t
   (markup->lily-string (ly:music-property event 'text)))
 
-(define-post-event-display-method HarmonicEvent (event parser) #t "\\harmonic")
+(define-post-event-display-method HarmonicEvent (event parser) #f "\\harmonic")
 (define-post-event-display-method GlissandoEvent (event parser) #t "\\glissando")
 (define-post-event-display-method ArpeggioEvent (event parser) #t "\\arpeggio")
 (define-post-event-display-method AbsoluteDynamicEvent (event parser) #f
