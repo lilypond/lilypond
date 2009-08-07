a1cf6794106f26cd7f2603910ed6eef5f0feda59
Patrick McCarty <pnorcks@gmail.com>
2009-08-07 13:55:08 -0700

    SCM backend: cleanup

--- scm/framework-scm.scm
+++ scm/framework-scm.scm
@@ -1,36 +1,35 @@
-;;;; framework-scm.scm
+;;;; framework-scm.scm -- output full-page stencil expressions
 
-(define-module (scm framework-scm)
-  #:export (output-framework)
-  )
+(define-module (scm framework-scm))
 
-(use-modules (ice-9 regex)
-	     (ice-9 string-fun)
-	     (guile)
-	     (srfi srfi-1)
-	     (ice-9 pretty-print)
-	     (srfi srfi-13)
-	     (scm page)
-	     (lily))
+(use-modules
+  (ice-9 regex)
+  (ice-9 string-fun)
+  (guile)
+  (srfi srfi-1)
+  (ice-9 pretty-print)
+  (srfi srfi-13)
+  (scm page)
+  (lily))
 
-(define-public (output-framework basename book scopes fields )
-  (let*
-      ((file (open-output-file (format #f "~a.scm" basename))))
+(define format ergonomic-simple-format)
+
+(define-public (output-framework basename book scopes fields)
+  (let* ((file (open-output-file (format "~a.scm" basename))))
     
     (display ";;Creator: LilyPond\n" file)
     (display ";; raw SCM output\n" file)
-
-  (for-each
-   (lambda (page)
-     (display
-       ";;;;;;;;;;;;;;;;;;;;;;;;;;\n;;;PAGE\n" file)
-;     (pretty-print (ly:stencil-expr page) file)
-     (write (ly:stencil-expr page) file)
-     )
-   (map page-stencil (ly:paper-book-pages book)))))
+    
+    (for-each
+      (lambda (page)
+	(display ";;;;;;;;;;;;;;;;;;;;;;;;;;\n;;;PAGE\n" file)
+	;; The following two lines are alternates
+	;(pretty-print (ly:stencil-expr page) file)
+	(write (ly:stencil-expr page) file)
+	)
+      (map page-stencil (ly:paper-book-pages book)))))
 
 (define-public output-classic-framework output-framework)
-
 
 (define-public (convert-to-ps . args) #t)
 (define-public (convert-to-pdf . args) #t)
