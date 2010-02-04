drewm1980/lilypond-an

commit 861b7bf8e5b7243d049dfa0c68534dbb7db08095

Move rehearsal marks, etc. to the top staff.

scm/define-grobs.scm
@@ -216,6 +216,7 @@
 
     (BarNumber
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	;; want the bar number before the clef at line start.
 	(break-align-symbols . (left-edge staff-bar))
 
@@ -1097,6 +1098,7 @@
 
     (MetronomeMark
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	(direction . ,UP)
 	(extra-spacing-width . (+inf.0 . -inf.0))
 	(outside-staff-priority . 1000)
@@ -1404,6 +1406,7 @@
 
     (RehearsalMark
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	(baseline-skip . 2)
 	(break-align-symbols . (staff-bar clef))
 	(break-visibility . ,end-of-line-invisible)
@@ -2217,6 +2220,7 @@
 
     (VoltaBracket
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	(direction . ,UP)
 	(edge-height . (2.0 . 2.0)) ;; staff-space;
 	(font-encoding . fetaNumber)

input/regression/page-spacing-rehearsal-mark.ly
@@ -1,0 +1,22 @@
+\version "2.13.4"
+
+\header {
+  texidoc = "The space taken up by rehearsal marks is correctly
+accounted for, even though they live in the Score context."
+}
+
+#(set-default-paper-size "a6")
+
+\book {
+  \paper {
+    oddHeaderMarkup = "header"
+    ragged-last-bottom = ##f
+  }
+  \score {
+    <<
+      \new Staff { \mark \markup \column { T A L L M A R K } c'1 \break
+		   \mark \markup \column { T A L L M A R K } c'1 }
+      \new Staff { c'1 \break c'1 }
+    >>
+  }
+}

lily/grob.cc
@@ -11,6 +11,7 @@
 #include <cstring>
 
 #include "align-interface.hh"
+#include "axis-group-interface.hh"
 #include "input.hh"
 #include "international.hh"
 #include "item.hh"
@@ -761,3 +762,4 @@ Grob::check_cross_staff (Grob *commony)
 
   return false;
 }
+

lily/include/grob.hh
@@ -54,8 +54,8 @@ public:
   friend SCM ly_grob_basic_properties (SCM);
 
   /* standard callbacks */
-  DECLARE_SCHEME_CALLBACK(x_parent_positioning, (SCM));
-  DECLARE_SCHEME_CALLBACK(y_parent_positioning, (SCM));
+  DECLARE_SCHEME_CALLBACK (x_parent_positioning, (SCM));
+  DECLARE_SCHEME_CALLBACK (y_parent_positioning, (SCM));
   DECLARE_SCHEME_CALLBACK (stencil_height, (SCM smob));
   DECLARE_SCHEME_CALLBACK (stencil_width, (SCM smob));
 

lily/include/page-layout-problem.hh
@@ -34,7 +34,6 @@ protected:
   SCM find_system_offsets ();
   void distribute_loose_lines (vector<Grob*> const&, vector<Real> const&, Real, Real);
 
-  static Grob* find_vertical_alignment (System*);
   static void build_system_skyline (vector<Grob*> const&, vector<Real> const&, Skyline* up, Skyline* down);
   static vector<Grob*> filter_dead_elements (vector<Grob*> const&);
 

lily/include/side-position-interface.hh
@@ -27,6 +27,7 @@ public:
   DECLARE_SCHEME_CALLBACK (y_aligned_side, (SCM element, SCM current));
   DECLARE_SCHEME_CALLBACK (pure_y_aligned_side, (SCM element, SCM start, SCM end, SCM current));
   DECLARE_SCHEME_CALLBACK (calc_cross_staff, (SCM element));
+  DECLARE_SCHEME_CALLBACK (move_to_extremal_staff, (SCM));
 
   static SCM aligned_side (Grob*me, Axis a, bool pure, int start, int end, Real *current_off_ptr);
 

lily/include/system.hh
@@ -26,6 +26,8 @@ class System : public Spanner
   
 public:
   Paper_score *paper_score () const;
+  Grob *get_vertical_alignment ();
+  Grob *get_extremal_staff (Direction dir, Interval const&);
   int get_rank () const;
   void do_break_substitution_and_fixup_refpoints ();
   void post_processing ();

lily/page-layout-problem.cc
@@ -160,26 +160,12 @@ Page_layout_problem::set_footer_height (Real height)
   footer_height_ = height;
 }
 
-Grob*
-Page_layout_problem::find_vertical_alignment (System *sys)
-{
-  extract_grob_set (sys, "elements", elts);
-  for (vsize i = 0; i < elts.size (); ++i)
-    if (Align_interface::has_interface (elts[i]))
-      return elts[i];
-
-  return 0;
-}
-
 void
 Page_layout_problem::append_system (System *sys, Spring const& spring, Real padding)
 {
-  Grob *align = find_vertical_alignment (sys);
+  Grob *align = sys->get_vertical_alignment ();
   if (!align)
-    {
-      sys->programming_error ("no VerticalAlignment in system: can't do vertical spacing");
-      return;
-    }
+    return;
 
   align->set_property ("positioning-done", SCM_BOOL_T);
 

lilypond/lilypond

commit db0a6af73cf880269b5fc7cf91e7d9a3094f44f9

Fix #943.
The regtest slur-broken-trend.ly broke with commit
861b7bf8e5b7243d049dfa0c68534dbb7db08095.

This commit adds the after-line-breaking callback to Slur and
PhrasingSlur grobs, and fixes the regtest.

scm/define-grobs.scm
@@ -1380,6 +1380,7 @@
 
     (PhrasingSlur
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	(control-points . ,ly:slur::calc-control-points)
 	(cross-staff . ,ly:slur::calc-cross-staff)
 	(details . ,default-slur-details)
@@ -1539,6 +1540,7 @@
 
     (Slur
      . (
+	(after-line-breaking . ,ly:side-position-interface::move-to-extremal-staff)
 	(avoid-slur . inside)
 	(control-points . ,ly:slur::calc-control-points)
 	(cross-staff . ,ly:slur::calc-cross-staff)