lilypond/lilypond

commit 1f8c40c86902c6c98091ccfd3ecf02ec1fc2bed5

ps-to-png.scm: only use -dEPSCrop for EPS files.
The previous check for multi-page documents fails to account for
single-page PS files, which must have page sizes specified using
-dDEVICEWIDTHPOINTS and -dDEVICEHEIGHTPOINTS when converted to PNG.

This commit improves the situation: only use the -dEPSCrop option for
EPS files, which do not have a page count.  All files that specify a
page count (PS files), will now be converted to PNGs with correct
dimensions.

scm/ps-to-png.scm
@@ -124,10 +124,11 @@
 	  (output-file (if multi-page? pngn png1))
 
 	  (gs-variable-options
-	   (if multi-page?
-	       (format #f "-dDEVICEWIDTHPOINTS=~,2f -dDEVICEHEIGHTPOINTS=~,2f"
-		       page-width page-height)
-	       "-dEPSCrop"))
+	    ;; ps-page-count returns 0 for EPS files
+	    (if (zero? page-count)
+		"-dEPSCrop"
+		(format #f "-dDEVICEWIDTHPOINTS=~,2f -dDEVICEHEIGHTPOINTS=~,2f"
+			page-width page-height)))
 	  (cmd (ly:format "~a\
  ~a\
  ~a\

commit 5158cad47176cbddaf0e78b11119dfe689a19c66

Fix #1186.
Checking the page count to detect EPS files is faulty, since it is legal
to specify "%%Pages: 1".

This commit revises the conditional statement to use a file-extension
check (for ".eps") instead.

This was a regression introduced by commit 1f8c40c86902c6c98091ccfd3ecf02ec1fc2bed5

scm/ps-to-png.scm
@@ -124,8 +124,7 @@
 	  (output-file (if multi-page? pngn png1))
 
 	  (gs-variable-options
-	    ;; ps-page-count returns 0 for EPS files
-	    (if (zero? page-count)
+	    (if (string-suffix-ci? ".eps" ps-name)
 		"-dEPSCrop"
 		(format #f "-dDEVICEWIDTHPOINTS=~,2f -dDEVICEHEIGHTPOINTS=~,2f"
 			page-width page-height)))