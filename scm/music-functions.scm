25a94417fb5484035bb816260a2168c17ae675ec
Valentin Villenave <valentin@villenave.net>
2010-10-23 22:06:58 +0200

    Add make-void-music function for syntax consistency.
    We had make-sequential-music, make-simultaneous-music
    and the like, but not make-void-music ('void Music is
    used, for instance, in music-functions that only
     override options etc.).

--- scm/music-functions.scm
+++ scm/music-functions.scm
@@ -470,6 +470,10 @@ i.e.  this is not an override"
   (make-music 'SkipMusic
 	      'duration dur))
 
+(define-public make-void-music
+  (make-music 'Music
+	      'void #t))
+
 (define-public (make-grace-music music)
   (make-music 'GraceMusic
 	      'element music))
