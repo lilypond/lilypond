eaa4f865b66ffe5a801a843aadda3c96b668919d
Phil Holmes <mail@philholmes.net>
2011-02-26 04:56:00 +0000

    Simplify bibliography system.
    This patch contains some makefile work by Graham, but it's not
    worth trying to separate it into two separate patches.

--- scripts/build/bib2texi.py
+++ scripts/build/bib2texi.py
@@ -24,10 +24,6 @@ for (o,a) in options:
     else:
         raise Exception ('unknown option: %s' % o)
 
-
-if style not in ['alpha','index','long','longp','long-pario','short','short-pario','split']:
-    sys.stderr.write ("Unknown style \`%s'\n" % style)
-
 if not files:
    usage ()
    sys.exit (2)
@@ -47,32 +43,39 @@ files = ','.join (nf)
 
 tmpfile = tempfile.mkstemp ('bib2texi')[1]
 
+#This writes a .aux file to the temporary directory.
+#The .aux file contains the commands for bibtex
+#PEH changed the bibstyle to allow a single template file in the parent directory
+#The template filename is texi-*.bst, where * defaults to 'long' but can be a parameter
 open (tmpfile + '.aux', 'w').write (r'''
 \relax
 \citation{*}
-\bibstyle{texi-%(style)s}
+\bibstyle{%(style)s}
 \bibdata{%(files)s}''' % vars ())
 
 tmpdir = tempfile.gettempdir ()
 
+#The command line to invoke bibtex
 cmd = "TEXMFOUTPUT=%s bibtex %s" % (tmpdir, tmpfile)
 
 sys.stdout.write ("Invoking `%s'\n" % cmd)
+#And invoke it
 stat = os.system (cmd)
 if stat <> 0:
     sys.exit(1)
 
-
 #TODO: do tex -> itexi on output
-
+# Following 2 lines copy tmpfile.bbl to the desired output file
 bbl = open (tmpfile + '.bbl').read ()
 
 open (output, 'w').write  (bbl)
-
 
 def cleanup (tmpfile):
     for a in ['aux','bbl', 'blg']:
         os.unlink (tmpfile + '.' + a)
 
+
 cleanup (tmpfile)
+#Following line added by PEH - script was leaving a dangling temporary file with no extension
+os.unlink (tmpfile)
 
