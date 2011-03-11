bb8e85cd462106714a69afcdf89d94cafe48a6b2
Jan Nieuwenhuizen <janneke@gnu.org>
2011-03-11 21:52:14 +0100

    Midi: set each track to its own port.  Should fix instrument/volume problems.

--- lily/include/midi-chunk.hh
+++ lily/include/midi-chunk.hh
@@ -50,7 +50,7 @@ public:
   void set (string header_string, string data_string, string footer_string);
   virtual string to_string () const;
   virtual string data_string () const;
-  DECLARE_CLASSNAME(Midi_chunk);
+  DECLARE_CLASSNAME (Midi_chunk);
   virtual ~Midi_chunk ();
 private:
   string data_string_;
@@ -61,7 +61,7 @@ private:
 class Midi_header : public Midi_chunk
 {
 public:
-  DECLARE_CLASSNAME(Midi_header);
+  DECLARE_CLASSNAME (Midi_header);
 
   Midi_header (int format, int tracks, int clocks_per_4);
 };
@@ -70,11 +70,11 @@ class Midi_track : public Midi_chunk
 {
 public:
   int number_;
-  DECLARE_CLASSNAME(Midi_track);
+  DECLARE_CLASSNAME (Midi_track);
 
   vector<Midi_event*> events_;
 
-  Midi_track ();
+  Midi_track (int number);
   ~Midi_track ();
 
   void add (int, Midi_item *midi);
