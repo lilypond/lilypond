lilypond/lilypond

commit bf707a03756021f69e3f5d1a8246639a6a601099

Use distance to original point rather than size of allowed region for tie-breaking.
Add regression test.

lily/beam.cc
@@ -1303,9 +1303,10 @@ Beam::shift_region_to_valid (SCM grob, SCM posns)
            || !collision_free[UP].is_empty ())
     {
       // We have space above or below collisions (or, no collisions at
-      // all).
+      // all).  Should we factor in the size of the collision_free
+      // interval as well?
       Interval best =  
-        (collision_free[DOWN].length () > collision_free[UP].length ()) ?
+        (collision_free[DOWN].distance(beam_left_y) < collision_free[UP].distance (beam_left_y)) ?
         collision_free[DOWN] : collision_free[UP];
 
       beam_left_y = point_in_interval (best, 2.0);

input/regression/beam-collision-large-object.ly
@@ -1,0 +1,15 @@
+\header {
+  texidoc = "Behave sensibly in the presence of large collisions."
+}
+
+\version "2.13.59"
+\new Staff {
+  <<
+    { \voiceOne s2 }
+    \\
+    { \voiceTwo e4 e }
+    \\
+    { \voiceFour
+      f''8[ e'' a'' g''] }
+  >>
+}

commit 0c50b37efbd1fd0171d09eac35c97c38ac807461

Handle situations that either one of above and below is a valid configuration in Beam::shift_region_to_valid().
This fixes the regression introduced in
bf707a03756021f69e3f5d1a8246639a6a601099, leading to random quanting
(-O2 build) or crashes (debug build.)

lily/beam.cc
@@ -1299,14 +1299,24 @@ Beam::shift_region_to_valid (SCM grob, SCM posns)
     {
       // We're good to go. Do nothing.
     }
-  else if (!collision_free[DOWN].is_empty ()
-           || !collision_free[UP].is_empty ())
+  else if (collision_free[DOWN].is_empty() != collision_free[UP].is_empty())
     {
-      // We have space above or below collisions (or, no collisions at
-      // all).  Should we factor in the size of the collision_free
-      // interval as well?
+      // Only one of them offers is feasible solution. Pick that one.
+      Interval v =
+        (!collision_free[DOWN].is_empty()) ?
+        collision_free[DOWN] : 
+        collision_free[UP];
+
+      beam_left_y = point_in_interval (v, 2.0);
+    }
+  else if (!collision_free[DOWN].is_empty ()
+           && !collision_free[UP].is_empty ())
+    {
+      // Above and below are candidates, take the one closest to the
+      // starting solution.
       Interval best =  
-        (collision_free[DOWN].distance(beam_left_y) < collision_free[UP].distance (beam_left_y)) ?
+        (collision_free[DOWN].distance (beam_left_y)
+         < collision_free[UP].distance (beam_left_y)) ?
         collision_free[DOWN] : collision_free[UP];
 
       beam_left_y = point_in_interval (best, 2.0);