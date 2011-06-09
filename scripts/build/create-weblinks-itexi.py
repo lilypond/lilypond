e2a0108360da37b13d8bfe0e40b5e0049410629d
Graham Percival <graham@percival-music.ca>
2011-06-09 11:51:33 +0100

    Update another hard-coded version number.

--- scripts/build/create-weblinks-itexi.py
+++ scripts/build/create-weblinks-itexi.py
@@ -491,7 +491,7 @@ print "@c ************************ Download source ************"
 for lang in langs:
     print "@c *********", lang, "***"
     make_download_source("downloadStableSource","v2.14",VERSION_STABLE,lang)
-    make_download_source("downloadDevelSource","v2.13",VERSION_DEVEL,lang)
+    make_download_source("downloadDevelSource","v2.15",VERSION_DEVEL,lang)
 
 print "@c ************************ Manual links ************"
 for lang in langs:
