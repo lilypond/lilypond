lilypond/lilypond

commit 32570e8ac85561afc1f59712301ee80c0d69d2b3

Better pure height approximations for stems.
This makes several large changes to the Stem grobs' property list.

The following properties are deleted:

stem-end-position
stem-begin-position
length

Now, the only property that controls these three parameters is
Y-extent.

A new function, stem::length, has been added to allow for common
length overrides (i.e. cross-staff-stems) and is in the regtest
stem-length-override.ly.

lily/tuplet-bracket.cc
@@ -553,18 +553,12 @@ Tuplet_bracket::calc_position_and_height (Grob *me_grob, Real *offset, Real *dy)
       && Note_column::get_stem (columns[0])
       && Note_column::get_stem (columns.back ()))
     {
-      /*
-        trigger set_stem_ends
-      */
-      (void) par_beam->get_property ("quantized-positions");
-
       Drul_array<Grob *> stems (Note_column::get_stem (columns[0]),
                                 Note_column::get_stem (columns.back ()));
 
-      Real ss = 0.5 * Staff_symbol_referencer::staff_space (me);
-      Real lp = ss * robust_scm2double (stems[LEFT]->get_property ("stem-end-position"), 0.0)
+      Real lp = stems[LEFT]->extent (stems[LEFT], Y_AXIS)[get_grob_direction (stems[LEFT])]
                 + stems[LEFT]->get_parent (Y_AXIS)->relative_coordinate (commony, Y_AXIS);
-      Real rp = ss * robust_scm2double (stems[RIGHT]->get_property ("stem-end-position"), 0.0)
+      Real rp = stems[RIGHT]->extent (stems[RIGHT], Y_AXIS)[get_grob_direction (stems[RIGHT])]
                 + stems[RIGHT]->get_parent (Y_AXIS)->relative_coordinate (commony, Y_AXIS);
 
       *dy = rp - lp;

input/regression/stem-length-estimation.ly
@@ -1,8 +1,9 @@
 \version "2.14.0"
 
 \header {
-  texidoc = "Stems with overridden 'length should not confuse height estimation.
-This example should fit snugly on one page.
+  texidoc = "Stems with overridden 'Y-extent should
+not confuse height estimation.  This example should fit snugly
+on one page.
 "
 }
 
@@ -25,7 +26,7 @@ This example should fit snugly on one page.
   \score {
     \new Voice {
       \voiceTwo
-      \override Stem #'length = #0
+      \override Stem #'Y-extent = #'(0.0 . 0.0)
       \repeat unfold 144 a4
     }
     \layout {

input/regression/stem-length-override.ly
@@ -1,0 +1,13 @@
+
+\version "2.15.9"
+
+\header {
+  texidoc = "Stem length can be overridden via the function
+stem::length
+"
+}
+
+\relative c' {
+  \override Stem #'Y-extent = #(stem::length 8)
+  e4 f'4
+}

input/regression/stem-pure-height-beamed.ly
@@ -1,0 +1,13 @@
+\version "2.15.9"
+
+\header {
+  texidoc = "Lilypond gets beamed stem pure heights correct
+to avoid outside staff collisions.
+"
+}
+
+{
+  \stemUp
+  gis''8 a bes'' a
+  bes''! a bes''! a
+}

lily/beam.cc
@@ -1549,10 +1549,10 @@ Beam::set_stem_lengths (SCM smob)
         stem_y += thick * 0.5 * get_grob_direction (s);
 
       /*
-        Do set_stemend for invisible stems too, so tuplet brackets
+        Do set_stem_positions for invisible stems too, so tuplet brackets
         have a reference point for sloping
        */
-      Stem::set_stemend (s, 2 * stem_y / staff_space);
+      Stem::set_stem_positions (s, 2 * stem_y / staff_space);
     }
 
   return posns;

lily/dot-column.cc
@@ -137,7 +137,6 @@ Dot_column::calc_positioning_done (SCM smob)
         {
           Grob *commony = stem->common_refpoint (flag, Y_AXIS);
           Interval y = flag->extent (commony, Y_AXIS) * (2 / ss);
-
           Interval x = flag->extent (commonx, X_AXIS);
 
           boxes.push_back (Box (x, y));

lily/flag.cc
@@ -67,6 +67,7 @@ Flag::print (SCM smob)
   Grob *me = unsmob_grob (smob);
   Grob *stem = me->get_parent (X_AXIS);
 
+  Direction d = get_grob_direction (stem);
   int log = Stem::duration_log (stem);
   string flag_style;
 
@@ -91,7 +92,8 @@ Flag::print (SCM smob)
     {
       if (adjust)
         {
-          int p = (int) (rint (Stem::stem_end_position (stem)));
+          Real ss = Staff_symbol_referencer::staff_space (me);
+          int p = (int) (rint (stem->extent (stem, Y_AXIS)[d] * 2 / ss));
           staffline_offs
             = Staff_symbol_referencer::on_line (stem, p) ? "0" : "1";
         }
@@ -101,7 +103,7 @@ Flag::print (SCM smob)
   else
     staffline_offs = "";
 
-  char dir = (get_grob_direction (stem) == UP) ? 'u' : 'd';
+  char dir = (d == UP) ? 'u' : 'd';
   string font_char = flag_style
                      + to_string (dir) + staffline_offs + to_string (log);
   Font_metric *fm = Font_interface::get_default_font (me);
@@ -146,10 +148,10 @@ Flag::calc_y_offset (SCM smob)
 
   Real blot
     = me->layout ()->get_dimension (ly_symbol2scm ("blot-diameter"));
-  Real half_space = Staff_symbol_referencer::staff_space (me) * 0.5;
-  Real y2 = robust_scm2double (stem->get_property ("stem-end-position"), 0.0);
 
-  return scm_from_double (y2 * half_space - d * blot / 2);
+  Real y2 = stem->extent (stem, Y_AXIS)[d];
+
+  return scm_from_double (y2 - d * blot / 2);
 }
 
 MAKE_SCHEME_CALLBACK (Flag, calc_x_offset, 1);

lily/include/item.hh
@@ -52,6 +52,7 @@ public:
   virtual void handle_prebroken_dependencies ();
   virtual Interval_t<int> spanned_rank_interval () const;
   virtual Interval pure_height (Grob *ref, int start, int end);
+  virtual void cache_pure_height (Interval height);
   DECLARE_GROB_INTERFACE ();
 protected:
   virtual void discretionary_processing ();

commit 298a9feb39e18046044779a802a5a521ac939c09

Fixes a tuplet bracket bug caused by 32570e8ac85561afc1f59712301ee80c0d69d2b3.
Tuplet bracket positions were being calculated before beamed stem
positions were set.  This patch triggers the setting of stem positions
before the calculation of these brackets.

lily/tuplet-bracket.cc
@@ -556,14 +556,21 @@ Tuplet_bracket::calc_position_and_height (Grob *me_grob, Real *offset, Real *dy)
       Drul_array<Grob *> stems (Note_column::get_stem (columns[0]),
                                 Note_column::get_stem (columns.back ()));
 
-      Real lp = stems[LEFT]->extent (stems[LEFT], Y_AXIS)[get_grob_direction (stems[LEFT])]
-                + stems[LEFT]->get_parent (Y_AXIS)->relative_coordinate (commony, Y_AXIS);
-      Real rp = stems[RIGHT]->extent (stems[RIGHT], Y_AXIS)[get_grob_direction (stems[RIGHT])]
-                + stems[RIGHT]->get_parent (Y_AXIS)->relative_coordinate (commony, Y_AXIS);
+      Interval poss;
+      Direction side = LEFT;
+      do
+        {
+          // Trigger setting of stem lengths if necessary.
+          if (Grob *beam = Stem::get_beam (stems[side]))
+            (void) beam->get_property ("quantized-positions");
+          poss[side] = stems[side]->extent (stems[side], Y_AXIS)[get_grob_direction (stems[side])]
+                       + stems[side]->get_parent (Y_AXIS)->relative_coordinate (commony, Y_AXIS);
+        }
+      while (flip (&side) != LEFT);
 
-      *dy = rp - lp;
-      points.push_back (Offset (stems[LEFT]->relative_coordinate (commonx, X_AXIS) - x0, lp));
-      points.push_back (Offset (stems[RIGHT]->relative_coordinate (commonx, X_AXIS) - x0, rp));
+      *dy = poss[RIGHT] - poss[LEFT];
+      points.push_back (Offset (stems[LEFT]->relative_coordinate (commonx, X_AXIS) - x0, poss[LEFT]));
+      points.push_back (Offset (stems[RIGHT]->relative_coordinate (commonx, X_AXIS) - x0, poss[RIGHT]));
     }
   else
     {