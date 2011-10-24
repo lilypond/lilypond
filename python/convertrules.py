671b7b63408893c33b4c1f196e87db19a7dbcd1e
Mike Solomon <mike@apollinemike.com>
2011-10-24 08:20:52 +0200

    Creates convert-ly rules to destencil Flags when \override Stem #'stencil = ##f

--- python/convertrules.py
+++ python/convertrules.py
@@ -3240,6 +3240,8 @@ def conv (str):
     str = re.sub (r"Stem\s+#'flag", r"Flag #'print", str)
     str = re.sub (r"((?:\\once)?\s*)\\override\s+Stem\s+#'transparent\s*=\s*##t", r"\g<1>\\override Stem #'transparent = ##t \g<1>\\override Flag #'transparent = ##t", str)
     str = re.sub (r"((?:\\once)?\s*)\\revert\s*Stem\s+#'transparent", r"\g<1>\\revert Stem #'transparent \g<1>\\revert Flag #'transparent", str)
+    str = re.sub (r"((?:\\once)?\s*)\\override\s+Stem\s+#'stencil\s*=\s*##f", r"\g<1>\\override Stem #'stencil = ##f \g<1>\\override Flag #'stencil = ##f", str)
+    str = re.sub (r"((?:\\once)?\s*)\\revert\s*Stem\s+#'stencil", r"\g<1>\\revert Stem #'stencil \g<1>\\revert Flag #'stencil", str)
     return str
 
 
