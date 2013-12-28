0c193995d884c167d6df57440d229b79be1711c4
Keith OHara <k-ohara5a5a@oco.net>
2013-12-28 13:40:45 -0800

    ledger-lines: horizontal space only when present

--- lily/ledger-line-spanner.cc
+++ lily/ledger-line-spanner.cc
@@ -100,7 +100,7 @@ Ledger_line_spanner::set_spacing_rods (SCM smob)
       Item *h = heads[i];
 
       int pos = Staff_symbol_referencer::get_rounded_position (h);
-      if (staff_extent.contains (pos))
+      if  (Staff_symbol::ledger_positions (staff, pos).empty ())
         continue;
 
       /* Ambitus heads can appear out-of-order in heads[],
