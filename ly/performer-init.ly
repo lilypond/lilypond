lilypond/lilypond

commit 1fba147e54d801206dbdfeae1ddacc8461567a2c

Issue 3668: Let NullVoice have an existence in MIDI
Issue 3457 omitted to introduce the NullVoice context into MIDI.  This
implementation is incomplete (see the TODO in performer-init.ly), but
at least it does not let the context structure go off the deep end in
MIDI.

ly/performer-init.ly
@@ -26,6 +26,7 @@
   \name Staff
   \accepts Voice
   \accepts CueVoice
+  \accepts NullVoice
   \defaultchild Voice
 
   \consists "Staff_performer"
@@ -179,6 +180,20 @@
 \context {
   \type "Performer_group"
   \name "Devnull"
+}
+
+\context {
+  \type "Performer_Group"
+  \name NullVoice
+  \alias Staff
+  \alias Voice
+  %% needed for melismata
+  %% TODO: at least the tie performer likely does not work without the
+  %% Note_performer, but I don't know how to shut note output off in
+  %% MIDI.
+  \consists "Tie_performer"
+  \consists "Beam_performer"
+  \consists "Slur_performer"
 }
 
 \context {

commit 1494b80af580ad7c2a8fa24f050b37143c4570c2

Issue 3872: Typo in performer-init.ly leads to a fatal error
This problem was introduced with
commit 1fba147e54d801206dbdfeae1ddacc8461567a2c
as part of issue 3668.

ly/performer-init.ly
@@ -183,7 +183,7 @@
 }
 
 \context {
-  \type "Performer_Group"
+  \type "Performer_group"
   \name NullVoice
   \alias Staff
   \alias Voice