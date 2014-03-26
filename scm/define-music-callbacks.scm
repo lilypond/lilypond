82e74a9fbd7cde8bb2b2a0c2d2ee081bc8951afa
Devon Schudy <dschudy@gmail.com>
2014-03-26 15:04:08 -0400

    Warn when \time is used in mid-measure, and reset measurePosition.
    This makes issues 3696 and 3888 less mysterious.

--- scm/define-music-callbacks.scm
+++ scm/define-music-callbacks.scm
@@ -102,7 +102,19 @@ to be used by the sequential-iterator"
                            structure))
                       (beaming-exception
                        (beam-exceptions fraction time-signature-settings))
-                      (new-measure-length (ly:make-moment num den)))
+                      (new-measure-length (ly:make-moment num den))
+                      (pos (ly:context-property context 'measurePosition)))
+                 ;;\time is OK at a negative measurePosition (after \partial),
+                 ;;but at a positive position it's probably a mistake, so warn
+                 ;;(like a barcheck) and reset it to 0 to prevent errors.
+                 (if (> (ly:moment-main pos) 0)
+                     (begin
+                       (if (not (ly:context-property context 'ignoreBarChecks #f))
+                           (ly:music-warning music
+                                             (_ "\\time in mid-measure at ~A")
+                                             (ly:moment-main pos)))
+                       (ly:context-set-property!
+                        context 'measurePosition (ly:make-moment 0))))
                  (ly:context-set-property!
                   context 'timeSignatureFraction fraction)
                  (ly:context-set-property!
