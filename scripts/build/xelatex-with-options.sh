5a51d882b01ad555ffe7659e7f7a73144ad2fc9d
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-23 21:47:46 +0900

    Issue4986: Fix XeLaTex option consistent with XeTeX option
    In LilyPond, XeTeX uses option
    `--output-driver='xdvipdfmx -C 0x0010'`
    but XeLaTeX did not use any options.
    
    This commit makes XeLaTeX uses the same option
    for consistent with XeTeX.

---
+++ scripts/build/xelatex-with-options.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# By the default settings,
+# XeTeX (xdvipdfmx) replaces link destination names with integers.
+# In this case, the replaced destination names of
+# remote PDF cannot be known. In order to avoid replacement,
+# we can use commandline option `-C 0x0010' for xdvipdfmx.
+
+exec xelatex --output-driver='xdvipdfmx -C 0x0010' "$@"
