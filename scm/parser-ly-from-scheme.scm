9738f8bbc37f8d4587c35c834476423b32dd7652
David Kastrup <dak@gnu.org>
2017-09-23 14:59:27 +0200

    Issue 5207: Parse strings and comments in #{ ... #}
    Otherwise # or $ within strings or comments may completely confuse
    LilyPond.
    
    (cherry picked from commit f388d0c93b478b2cfb4511d171a54bc8d037dc91)

--- scm/parser-ly-from-scheme.scm
+++ scm/parser-ly-from-scheme.scm
@@ -25,13 +25,13 @@ from @var{port} and return the corresponding Scheme music expression.
          (line (port-line port))
          (lily-string (call-with-output-string
                        (lambda (out)
+                         (define (copy-char)
+                           (let ((x (read-char port)))
+                             (write-char x out)
+                             x))
                          (let ((copycat
                                 (make-soft-port
-                                 (vector #f #f #f
-                                         (lambda ()
-                                           (let ((x (read-char port)))
-                                             (write-char x out)
-                                             x)) #f)
+                                 (vector #f #f #f copy-char #f)
                                  "r")))
                            (set-port-filename! copycat filename)
                            (do ((c (read-char port) (read-char port)))
@@ -41,30 +41,63 @@ from @var{port} and return the corresponding Scheme music expression.
                                 (read-char port))
                              (write-char c out)
                              ;; a #scheme or $scheme expression
-                             (if (or (char=? c #\#) (char=? c #\$))
-                                 (let* ((p (ftell out))
-                                        (expr
-                                         (begin
-                                           (set-port-line! copycat
-                                                           (port-line port))
-                                           (set-port-column! copycat
-                                                             (port-column port))
-                                           (if (char=? (peek-char port) #\@)
-                                               (read-char copycat))
-                                           (read copycat))))
-                                   ;; kill unused lookahead, it has been
-                                   ;; written out already
-                                   (drain-input copycat)
-                                   ;; only put symbols and non-quote
-                                   ;; lists into closures -- constants
-                                   ;; don't need lexical environments
-                                   ;; for evaluation.
-                                   (if (or (symbol? expr)
-                                           (and (pair? expr)
-                                                (not (eq? 'quote (car expr)))))
-                                       (set! closures
-                                             (cons `(cons ,p (lambda () ,expr))
-                                                   closures)))))))))))
+                             (case c
+                               ((#\# #\$)
+                                ;; These characters enter a Scheme
+                                ;; expression to be captured in a
+                                ;; closure
+                                (let* ((p (ftell out))
+                                       (expr
+                                        (begin
+                                          ;; Need to synchronize the
+                                          ;; copycat port with
+                                          ;; line/column data
+                                          (set-port-line! copycat
+                                                          (port-line port))
+                                          (set-port-column! copycat
+                                                            (port-column port))
+                                          ;; @ splices a list
+                                          (if (char=? (peek-char port) #\@)
+                                              (read-char copycat))
+                                          (read copycat))))
+                                  ;; kill unused lookahead, it has been
+                                  ;; written out already
+                                  (drain-input copycat)
+                                  ;; only put symbols and non-quote
+                                  ;; lists into closures -- constants
+                                  ;; don't need lexical environments
+                                  ;; for evaluation.
+                                  (if (or (symbol? expr)
+                                          (and (pair? expr)
+                                               (not (eq? 'quote (car expr)))))
+                                      (set! closures
+                                            (cons `(cons ,p (lambda () ,expr))
+                                                  closures)))))
+                               ((#\")
+                                ;; A LilyPond string is ended by the
+                                ;; quote character unless the quote
+                                ;; character itself is escaped.  Note:
+                                ;; \"xxx" is a valid LilyPond
+                                ;; construct as well, so we don't need
+                                ;; to track leading backslashes here.
+                                (do ((c (copy-char) (copy-char)))
+                                    ((char=? c #\"))
+                                  (if (char=? c #\\)
+                                      (copy-char))))
+                               ((#\%)
+                                ;; LilyPond comments end at the end of
+                                ;; line unless they have the form
+                                ;; %{...%}
+                                (case (copy-char)
+                                  ((#\{)
+                                   (do ((c (copy-char) (copy-char)))
+                                       ((and (char=? c #\%) (peek-char port) #\})
+                                        (copy-char))))
+                                  ((#\nl) #f) ; empty comment
+                                  (else
+                                   (do ((c (copy-char) (copy-char)))
+                                       ((char=? c #\nl)))))))))))))
+
     (define (embedded-lilypond lily-string filename line closures)
       (let* ((clone (ly:parser-clone closures (*location*)))
              (result (ly:parse-string-expression clone lily-string
