c4d4e604c66e30029dc0cc8ae17c69c31ca8300a
Thomas Morley <thomasmorley65@gmail.com>
2017-11-05 15:36:03 +0100

    Issue 5231 Fix regression introduced with 5122 "Fix not scaling stem ..."
    commit eee677c480c78d58a5215e246575aa94ba2d1897
    only took text-font-size from current layout into account.
    Now text-font-size from $defaultpaper is respected as well.
    As a result note-by-number scales now properly for changes of
    set-global-staff-size, layout-set-staff-size and markup-fontsize.

--- scm/define-markup-commands.scm
+++ scm/define-markup-commands.scm
@@ -3720,10 +3720,18 @@ mensural-flags.  Both are supplied for convenience.
   (let* ((font (ly:paper-get-font layout (cons '((font-encoding . fetaMusic)
                                                  (font-name . #f))
                                                props)))
-         ;; default for text-font-size is 11
-         ;; hence we use (/ text-font-size 11) later, to ensure proper scaling
-         ;; of stem-length and thickness
-         (text-font-size (ly:output-def-lookup layout 'text-font-size 11))
+         ;; To make the stem scale properly with changes in
+         ;; set-global-staff-size and/or set-layout-staff-size, we need to catch
+         ;; text-font-size from current layout and $defaultpaper and scale
+         ;; stem-thickness and -length with the division
+         ;; (/ layout-text-font-size paper-text-font-size) later.
+         ;; Default for text-font-size is 11.
+         (layout-text-font-size
+           (ly:output-def-lookup layout 'text-font-size 11))
+         (paper-text-font-size
+           (ly:output-def-lookup
+             (ly:parser-lookup '$defaultpaper)
+             'text-font-size 11))
          (size-factor (magstep font-size))
          (blot (ly:output-def-lookup layout 'blot-diameter))
          (head-glyph-name
@@ -3745,11 +3753,16 @@ mensural-flags.  Both are supplied for convenience.
            (member style
                    '(mensural neomensural petrucci semipetrucci blackpetrucci)))
          (attach-indices (ly:note-head::stem-attachment font head-glyph-name))
-         (stem-length (* size-factor (max 3 (- log 1))))
+         (stem-length
+           (* size-factor
+              (/ layout-text-font-size paper-text-font-size)
+              (max 3 (- log 1))))
          ;; With ancient-flags we want a tighter stem
          (stem-thickness
-           (* size-factor (/ text-font-size 11) (if ancient-flags? 0.1 0.13)))
-         (stemy (* dir (/ text-font-size 11) stem-length))
+           (* size-factor
+              (/ layout-text-font-size paper-text-font-size)
+              (if ancient-flags? 0.1 0.13)))
+         (stemy (* dir stem-length))
          (attach-off (cons (interval-index
                             (ly:stencil-extent head-glyph X)
                             (* (sign dir) (car attach-indices)))
