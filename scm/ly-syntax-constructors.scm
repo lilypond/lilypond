0a73f468a79da57cb160c3ae01afa5dea4876edf
David Kastrup <dak@gnu.org>
2018-06-22 09:05:00 +0200

    Issue 5359: Simplify (@ (scm ly-syntax-constructors) create-script-function)
    There is no point in not using define-event-function here.

--- scm/ly-syntax-constructors.scm
+++ scm/ly-syntax-constructors.scm
@@ -115,16 +115,14 @@
          *unspecified*)))
 
 (define-public create-script-function
-  (ly:make-music-function
-   (list (cons ly:event? #f) ly:dir? scheme?)
-   (lambda (dir item)
-     (let ((res (create-script item)))
-       (if (ly:event? res)
-           (begin
-             (if (not (zero? dir))
-                 (set! (ly:music-property res 'direction) dir))
-             res)
-           (make-music 'PostEvents))))))
+  (define-event-function (dir item) (ly:dir? scheme?)
+    (let ((res (create-script item)))
+      (if (ly:event? res)
+          (begin
+            (if (not (zero? dir))
+                (set! (ly:music-property res 'direction) dir))
+            res)
+          (make-music 'PostEvents)))))
 
 (define-public (void-music)
   (ly:set-origin! (make-music 'Music)))
