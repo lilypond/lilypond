e0b074591ce3e571b7e645bfec51c913adb3bb3b
Werner Lemberg <wl@gnu.org>
2019-09-24 15:02:05 +0200

    run-and-check: Show output directory in case of error.
    This allows easy copy and paste.
    
    (cherry picked from commit 1d3105fce49125528a766938e4e9ae8c1d5d3be5)

--- scripts/build/run-and-check.sh
+++ scripts/build/run-and-check.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-# The nextline takes the value in parameter one, evaluates it
-# (necessary if it contains spaces) and runs it. > redirects
-# stdout to the logfile given in parameter 2.  2>&1 redirects
-# stderr to stdout (i.e. to the logfile).
+#
+# The next code line takes the value in parameter one, evaluates
+# it (necessary if it contains spaces), and runs it.
+#
+# `>' redirects stdout to the logfile given in parameter 2.
+# `2>&1' redirects stderr to stdout (i.e., to the logfile).
+
 eval $1 > $2 2>&1
-RetVal=$?  # captures the return value of the command
+RetVal=$? # capture return value of the just executed command
 if [ $RetVal -ne 0 ]; then
+	CurrDir=`pwd` # get current working directory
 	echo
-	echo "Please check the logfile" $2 "for errors"
+	echo "Please check the logfile"
+	echo
+	echo "  $CurrDir/$2"
+	echo
+	echo "for errors"
 	echo
 fi
 exit $RetVal
