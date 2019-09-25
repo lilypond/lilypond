71c4990e4762751aa7827d414c2a501b538eefd0
Werner Lemberg <wl@gnu.org>
2019-09-25 22:18:15 +0200

    run-and-check: Display correct log file directory.

--- scripts/build/run-and-check.sh
+++ scripts/build/run-and-check.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-#
-# The next code line takes the value in parameter one, evaluates
+
+# Get current working directory; this must be done first since
+# parameter 1 could contain a directory change.
+CurrDir=`pwd`
+
+# The next code line takes the value in parameter 1, evaluates
 # it (necessary if it contains spaces), and runs it.
 #
 # `>' redirects stdout to the logfile given in parameter 2.
 # `2>&1' redirects stderr to stdout (i.e., to the logfile).
-
 eval $1 > $2 2>&1
-RetVal=$? # capture return value of the just executed command
+
+# Capture return value of the just executed command.
+RetVal=$?
 if [ $RetVal -ne 0 ]; then
-	CurrDir=`pwd` # get current working directory
 	echo
 	echo "Please check the logfile"
 	echo
