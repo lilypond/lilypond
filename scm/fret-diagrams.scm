7930d8777ef2142c5189a5b9d8c8a163dff24529
Werner Lemberg <wl@gnu.org>
2019-09-26 16:53:51 +0200

    Issue 5558/2: NR: various minor corrections
    Some typos and minor textual improvements.
    Some conversions from subsubsections to snippet sections and vice versa.

--- scm/fret-diagrams.scm
+++ scm/fret-diagrams.scm
@@ -1135,13 +1135,8 @@ to string @var{end-string} at fret @var{fret-number}.
 Place a capo indicator (a large solid bar) across the entire fretboard
 at fret location @var{fret-number}.  Also, set fret @var{fret-number}
 to be the lowest fret on the fret diagram.
-@item
-(place-fret @var{string-number}
-            @var{fret-number}
-            [@var{finger-value}]
-            [@var{color-modifier}]
-            [@var{color}]
-            [@code{'parenthesized} [@code{'default-paren-color}]])
+
+@item (place-fret @var{string-number} @var{fret-number} [@var{finger-value}] [@var{color-modifier}] [@var{color}] ['parenthesized ['default-paren-color]])
 Place a fret playing indication on string @var{string-number} at fret
 @var{fret-number} with an optional fingering label @var{finger-value},
 an optional color modifier @var{color-modifier}, an optional color
