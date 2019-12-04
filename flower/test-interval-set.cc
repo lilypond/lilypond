0ea52e08bde09001641931246ee42b1f3b02b7f5
Dan Eble <nine.fierce.ballads@gmail.com>
2019-12-04 16:56:32 -0500

    Issue 5628: fix sign-compare warnings in flower unit tests

--- flower/test-interval-set.cc
+++ flower/test-interval-set.cc
@@ -32,28 +32,29 @@ FUNC (interval_set_union)
   ivs.push_back (Interval (0, 3));
   ivs.push_back (Interval (1, 2));
   Interval_set result = Interval_set::interval_union (ivs);
-  EQUAL (result.intervals ().size (), 1);
+  // TODO: Yaffut parameters are (expected, actual); these are reversed.
+  EQUAL (result.intervals ().size (), 1u);
   // Compare intervals using to_string, since yaffut doesn't know how to compare intervals.
   EQUAL (result.intervals ()[0].to_string (), Interval (-1, 3).to_string ());
 
   // Non-overlapping intervals.
   ivs.push_back (Interval (-5, -4));
   result = Interval_set::interval_union (ivs);
-  EQUAL (result.intervals ().size (), 2);
+  EQUAL (result.intervals ().size (), 2u);
   EQUAL (result.intervals ()[0].to_string (), Interval (-5, -4).to_string ());
   EQUAL (result.intervals ()[1].to_string (), Interval (-1, 3).to_string ());
 
   // Infinite intervals.
   ivs.push_back (Interval (-infinity_f, -4));
   result = Interval_set::interval_union (ivs);
-  EQUAL (result.intervals ().size (), 2);
+  EQUAL (result.intervals ().size (), 2u);
   EQUAL (result.intervals ()[0].to_string (), Interval (-infinity_f, -4).to_string ());
   EQUAL (result.intervals ()[1].to_string (), Interval (-1, 3).to_string ());
 
   // Empty intervals.
   ivs.push_back (Interval (infinity_f, -infinity_f));
   result = Interval_set::interval_union (ivs);
-  EQUAL (result.intervals ().size (), 2);
+  EQUAL (result.intervals ().size (), 2u);
 }
 
 FUNC (interval_set_nearest_point)
@@ -89,7 +90,7 @@ FUNC (interval_set_complement)
   ivs.push_back (Interval (-3, -1));
   ivs.push_back (Interval (1, 3));
   Interval_set set = Interval_set::interval_union (ivs).complement ();
-  EQUAL (set.intervals ().size (), 3);
+  EQUAL (set.intervals ().size (), 3u);
   EQUAL (set.intervals ()[0].to_string (), Interval (-infinity_f, -3).to_string ());
   EQUAL (set.intervals ()[1].to_string (), Interval (-1, 1).to_string ());
   EQUAL (set.intervals ()[2].to_string (), Interval (3, infinity_f).to_string ());
@@ -97,14 +98,14 @@ FUNC (interval_set_complement)
   // Half-infinite sets are handled correctly.
   ivs.push_back (Interval (-infinity_f, -2));
   set = Interval_set::interval_union (ivs).complement ();
-  EQUAL (set.intervals ().size (), 2);
+  EQUAL (set.intervals ().size (), 2u);
   EQUAL (set.intervals ()[0].to_string (), Interval (-1, 1).to_string ());
   EQUAL (set.intervals ()[1].to_string (), Interval (3, infinity_f).to_string ());
 
   // Full and empty sets are handled correctly.
   set = Interval_set ().complement ();
-  EQUAL (set.intervals ().size (), 1);
+  EQUAL (set.intervals ().size (), 1u);
   EQUAL (set.intervals ()[0].to_string (), Interval (-infinity_f, infinity_f).to_string ());
   set = set.complement ();
-  EQUAL (set.intervals ().size (), 0);
+  EQUAL (set.intervals ().size (), 0u);
 }
