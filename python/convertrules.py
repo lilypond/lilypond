lilypond/lilypond

commit a7d581df1611cf3a7d59bdd2985e31ab96d2be9f

Issue 5580/3: Replace string.join() by sep.join()
The method is available since at least Python 2.4 and "the default
value for sep is a single space character."

python/convertrules.py
@@ -3436,7 +3436,7 @@ grob_path = symbol_list + r"(?:\s+" + symbol_list + r")*"
 grob_spec = wordsyntax + r"(?:\s*\.\s*" + wordsyntax + r")?"
 
 def path_replace (m):
-    return m.group (1) + string.join (re.findall (wordsyntax, m.group (2)), ".")
+    return m.group (1) + ".".join (re.findall (wordsyntax, m.group (2)))
 
 # The following regexp appears to be unusually expensive to compile,
 # so we do it only once instead of for every file
@@ -3452,7 +3452,7 @@ footnotec = re.compile ("(" + matchfullmarkup + ")|"
 def conv (str):
     def patrep (m):
         def fn_path_replace (m):
-            x = string.join (re.findall (wordsyntax, m.group (2)), ".")
+            x = ".".join (re.findall (wordsyntax, m.group (2)))
             if x in ["TimeSignature", "KeySignature", "BarLine",
                      "Clef", "StaffSymbol", "OttavaBracket",
                      "LedgerLineSpanner"]:
@@ -3895,7 +3895,7 @@ def conv (str):
 @rule ((2, 19, 46), r"\context ... \modification -> \context ... \with \modification")
 def conv (str):
     word=r'(?:#?"[^"]*"|\b' + wordsyntax + r'\b)'
-    mods = string.join (re.findall ("\n(" + wordsyntax + r")\s*=\s*\\with(?:\s|\\|\{)", str)
+    mods = str.join (re.findall ("\n(" + wordsyntax + r")\s*=\s*\\with(?:\s|\\|\{)")
                         + ['RemoveEmptyStaves','RemoveAllEmptyStaves'], "|")
     str = re.sub (r"(\\(?:drums|figures|chords|lyrics|addlyrics|"
                   + r"(?:new|context)\s*" + word

python/musicexp.py
@@ -692,12 +692,12 @@ class NestedMusic(Music):
 
     def get_properties (self):
         return ("'elements (list %s)"
-            % string.join (map (lambda x: x.lisp_expression(),
+            % " ".join (map (lambda x: x.lisp_expression(),
                       self.elements)))
 
     def get_subset_properties (self, predicate):
         return ("'elements (list %s)"
-            % string.join (map (lambda x: x.lisp_expression(),
+            % " ".join (map (lambda x: x.lisp_expression(),
                       filter (predicate, self.elements))))
     def get_neighbor (self, music, dir):
         assert music.parent == self
@@ -1038,7 +1038,7 @@ class ChordEvent (NestedMusic):
                     basepitch = previous_pitch
             if stem:
                 printer (stem.ly_expression ())
-            printer ('<%s>' % string.join (pitches))
+            printer ('<%s>' % ' '.join (pitches))
             previous_pitch = basepitch
             duration = self.get_duration ()
             if duration:
@@ -1477,7 +1477,7 @@ class FretBoardEvent (NestedMusic):
           notes = []
           for n in fretboard_notes:
               notes.append (n.ly_expression ())
-          contents = string.join (notes)
+          contents = ' '.join (notes)
           printer ('<%s>%s' % (contents,self.duration))
 
 class FunctionWrapperEvent (Event):
@@ -1667,7 +1667,7 @@ class RhythmicEvent(Event):
         return [ev.pre_note_ly (is_chord_element) for ev in self.associated_events]
 
     def ly_expression_pre_note (self, is_chord_element):
-        res = string.join (self.pre_note_ly (is_chord_element), ' ')
+        res = ' '.join (self.pre_note_ly (is_chord_element))
         if res != '':
             res = res + ' '
         return res
@@ -1815,7 +1815,7 @@ class KeySignatureChange (Music):
         elif self.non_standard_alterations:
             alterations = [self.format_non_standard_alteration (a) for
                                         a in self.non_standard_alterations]
-            return "\\set Staff.keyAlterations = #`(%s)" % string.join (alterations, " ")
+            return "\\set Staff.keyAlterations = #`(%s)" % " ".join (alterations)
         else:
             return ''
 
@@ -1859,7 +1859,7 @@ class TimeSignatureChange (Music):
     def format_fraction (self, frac):
         if isinstance (frac, list):
             l = [self.format_fraction (f) for f in frac]
-            return "(" + string.join (l, " ") + ")"
+            return "(" + " ".join (l) + ")"
         else:
             return "%s" % frac
 
@@ -2076,7 +2076,7 @@ class FiguredBassEvent (NestedMusic):
           notes = []
           for x in figured_bass_events:
               notes.append (x.ly_expression ())
-          contents = string.join (notes)
+          contents = ' '.join (notes)
           if self.parentheses:
               contents = '[%s]' % contents
           printer ('<%s>' % contents)

python/musicxml.py
@@ -198,7 +198,7 @@ class Identification(Xml_node):
                 ret.append(result)
             else:
                 ret.append(text)
-        return string.join(ret, "\n")
+        return "\n".join(ret)
 
     # get contents of the source-element(usually used for publishing information).(These contents are saved in a custom variable named "source" in the header of the .ly file.)
     def get_source(self):
@@ -206,7 +206,7 @@ class Identification(Xml_node):
         ret = []
         for r in source:
           ret.append(r.get_text())
-        return string.join(ret, "\n")
+        return "\n".join(ret)
 
     def get_creator(self, type):
         creators = self.get_named_children('creator')

scripts/auxiliar/fixcc.py
@@ -323,7 +323,7 @@ def nitpick_file (outdir, file):
     #code = filter (lambda x: is_derived_class (x.__class__, Substring),
     #               chunks)
 
-    t = string.join (map (lambda x: x.filter_text (), chunks), '')
+    t = ''.join (map (lambda x: x.filter_text (), chunks))
     fixt = file
     if s != t:
         if not outdir:

scripts/lilymidi.py
@@ -221,7 +221,7 @@ def go ():
             if options.prefix:
                 sys.stdout.write ('%s ' % (options.prefix,))
             import string
-            sys.stdout.write (string.join (numbers, ','))
+            sys.stdout.write (','.join (numbers))
             sys.stdout.write ('\n')
     else:
         for n, name in info:

commit 33db456ed4ed863a7dfd457aba2631f87efd48f0

Fix broken convert-ly rule for 2.19.46
This was broken in commit a7d581df1611cf3a7d59bdd2985e31ab96d2be9f

python/convertrules.py
@@ -3895,8 +3895,8 @@ def conv (str):
 @rule ((2, 19, 46), r"\context ... \modification -> \context ... \with \modification")
 def conv (str):
     word=r'(?:#?"[^"]*"|\b' + wordsyntax + r'\b)'
-    mods = str.join (re.findall ("\n(" + wordsyntax + r")\s*=\s*\\with(?:\s|\\|\{)")
-                        + ['RemoveEmptyStaves','RemoveAllEmptyStaves'], "|")
+    mods = "|".join (re.findall ("\n(" + wordsyntax + r")\s*=\s*\\with(?:\s|\\|\{)", str)
+                     + ['RemoveEmptyStaves','RemoveAllEmptyStaves'])
     str = re.sub (r"(\\(?:drums|figures|chords|lyrics|addlyrics|"
                   + r"(?:new|context)\s*" + word
                   + r"(?:\s*=\s*" + word + r")?)\s*)(\\(?:" + mods + "))",