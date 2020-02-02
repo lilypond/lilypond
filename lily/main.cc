dc65755d47f9fad3edeb6b7aaa43b6121e17de78
Masamichi Hosoda <trueroad@trueroad.jp>
2020-02-02 11:42:58 +0100

    Issue 5725: Use x86 SSE2 for avoiding x86 FPU precision issues
    Remove x87 FPU precision setting
    
    This commit removes x87 FPU precision setting that is no longer needed
    because the x87 FPU is not used by the previous commit.
    
    Use x86 SSE2 for avoiding x86 FPU precision issues
    
    In x86 (32 bit) environments,
    floating point calculation caused some issues due to precision conversion.
    The x87 FPU registers have 80 bit precision,
    while floating point variables in the memory have 64 bit precision.
    On Linux x86 environment,
    we avoided the issues by manipulating the x87 FPU control word
    to limit the precision.
    However, in other x86 environment,
    the issues such as Issue 4943 and Issue 4975 were raised.
    
    This commit makes LilyPond for x86 platforms uses SSE2 instead of x87 FPU.
    The SSE2 registers have the same 64 bit precision
    as the variables in the memory.
    It can be expected that the issues will not raise
    since there is no precision conversion.
    However, external libraries may use the x87 FPU.
    So issues may remain with floating point calculation
    inside such libraries.
    
    On the other hand, in x86_64 (64 bit) environments,
    the issues are not raised because the x87 FPU is not used originally.
    This commit does not make any changes for x86_64 environments.
    
    (cherry picked from commit bea0cb75006fae7ebd7123829456772d8a03fa70)

--- lily/main.cc
+++ lily/main.cc
@@ -204,31 +204,6 @@ static Long_option_init options_static[]
 
 char const *LILYPOND_DATADIR = PACKAGE_DATADIR "/" TOPLEVEL_VERSION;
 
-/* x86 defaults to using 80-bit extended precision arithmetic. This can cause
-   problems because the truncation from 80 bits to 64 bits can occur in
-   unpredictable places. To get around this, we tell the x87 FPU to use only
-   double precision. Note that this is not needed for x86_64 because that uses
-   the SSE unit by default instead of the x87 FPU. */
-#if ((defined (__x86__) || defined (__i386__)) \
-  && defined (HAVE_FPU_CONTROL_H) && (HAVE_FPU_CONTROL_H == 1))
-
-#include <fpu_control.h>
-static void
-configure_fpu ()
-{
-  fpu_control_t fpu_control = 0x027f;
-  _FPU_SETCW (fpu_control);
-}
-
-#else
-
-static void
-configure_fpu ()
-{
-}
-
-#endif /* defined(__x86__) || defined(__i386__) */
-
 static void
 env_var_info (FILE *out, char const *key)
 /*
@@ -823,7 +798,6 @@ main (int argc, char **argv, char **envp)
  * envp:   Point to vector of OS environment variables
  */
 {
-  configure_fpu ();
   /*
     Process environment variables
   */
