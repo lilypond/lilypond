ee197383f4af552ed433c496617cb5ffe2a28dcf
David Kastrup <dak@gnu.org>
2020-02-26 02:07:27 +0100

    Issue 5822/2: Replace HTML size comments with human-readable size
    This calculates download sizes as the last step of HTML generation and
    puts them into the HTML files for reference.
    
    The work is done by scripts/build/fix-docsize.sh .
    
    It does that by replacing the size-requesting HTML comments in the
    files given on the command line by the respective human-readable size.
    
    scripts/build/fix-docsize.sh is run in the respective directories as
    the final step of generating the HTML versions.
    
    This script uses ls -sh in order to get a human-readable size spec.

---
+++ scripts/build/fix-docsize.sh
@@ -0,0 +1,68 @@
+#/bin/sh
+
+# Call with a number of HTML files as argument that may contain
+# comments of the form
+#
+# <!-- file-size:, href="dir/filename.ext"-->
+#
+# Those are going to be replaced (in this spelling) with a comma and
+# space, followed by the actual human-readable file size indicating
+# the size of the file for download.  The somewhat peculiar syntax is
+# chosen so that python/auxiliar/postprocess_html.py also does its
+# path processing on the size comments.
+
+# Only treat files actually having file-size source comments
+sourcefiles=$(fgrep -l '<!-- file-size:' "$@")
+
+# get out if there are no such files
+[ -z "$sourcefiles" ] && exit 0
+
+# Get list of referenced files and uniquify
+files=$(sed -n '/<!-- file-size:/s/^.*<!-- file-size:[^>]*href="\([^"]*\)".*$/\1/p' $sourcefiles | sort -u)
+
+# We build an sed command having one replacement for each
+# unique referenced file in a get-file-size comment.
+
+# Now build script over files accessible from cwd
+files=$(for filename in $files
+	do
+	    if ! [ -r "$filename" ] || ! [ -f "$filename" ]
+	    then
+		# If a file cannot be found from multiple places, we
+		# list those as a braced list, otherwise just as a
+		# single file
+		printf '%s: %s not accessible from %s/%s\n' \
+		       "$(basename $0)" "$filename" "$(pwd)" \
+		       "$(grep -l "<!-- file-size:[^>]*href=\"$filename\"" $sourcefiles |
+			     sed -n 's|^\./||
+			             1h
+				     1!H
+				     ${x
+				       /\n/{s/^/{/
+				            s/\n/,/g
+					    s/$/}/
+					   }
+				       p}')" >&2
+	    else
+		echo "$filename"
+	    fi
+	done)
+
+# Only do the replacement if there are actually accessible files
+# mentioned in the comments.
+[ -z "$files" ] && exit 0
+
+# echo "substituting size for:" $files >&2
+
+script=$(ls -sLh $files |
+	     sed 's/^[ 	]*\([^ 	]*\)[ 	]*\([^ 	]*\)[ 	]*$/'\
+'\/<!-- file-size:\/s|<!-- file-size:\\([^>]*\\)href="\2"\\([^>]*\\)-->|\\1\1\\2|/')
+
+# Using the same script for every file is efficient but it means that
+# every file here gets touched, even those containing only references
+# to inaccessible files.  If only such references remain in all files,
+# however, we don't touch anything.
+#
+# That's sort of good enough.
+
+sed -i -e "$script" $sourcefiles
