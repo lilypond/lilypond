2d40ef8ced63607cc1a9b2a0bc6a1fd1122af8c4
Han-Wen Nienhuys <hanwen@lilypond.org>
2020-03-27 19:20:32 +0100

    mf: use python scripting for generating Emmentaler fonts
    This gets rid of the generated FontForge scripts, and avoids
    intermediate files.
    
    Remove sequencing on |pfb . This was introduced in e6a5019 (Jun 28
    2012, "Clean fonts and docs makefiles"). The race conditions were
    fixed in c2eb8beac769a1bf30de357781601322d70095a6 ("mf/GNUmakefile:
    move logic to scripts")
    
    Remove XXX.fontname files. They appear to be unused.
    
    Remove XXX.otf-table files. The script reads .lisp directly.
    
    In mf-to-table:
    
     * Remove support for .enc files. Encodings are hardcoded in
       ps/encodingdefs.ps these days
    
     * Remove support for .dep files. They are generated from
       invoke-mf2pt1.sh
    
     * Remove various unused options
    
    Get rid of grouped rule for emmentaler-brace. GNU Make 4.2.1 (even
    with the "&:" operator) executes the recipe once for each target.
    
    https://sourceforge.net/p/testlilyissues/issues/5811
    http://codereview.appspot.com/553580043

---
+++ mf/gen-emmentaler-brace.fontforge.py
@@ -0,0 +1,76 @@
+import fontforge
+import getopt
+import os
+import psMat
+import re
+import sys
+
+(options, files) = \
+ getopt.getopt (sys.argv[1:],
+                '',
+                ['in=', 'out=', 'version='])
+
+version = "dev"
+indir = ""
+output = ""
+for opt in options:
+    o = opt[0]
+    a = opt[1]
+    if o == '--in':
+        indir = a
+    elif o == '--out':
+        output = a
+    elif o == '--version':
+        version = a
+    else:
+        print(o)
+        raise getopt.error
+
+font = fontforge.font()
+
+scale = 1.0
+subfonts = []
+for c in "abcdefghi" :
+    subfont = "feta-braces-%s" %c
+    subfonts.append(subfont)
+    f = fontforge.open(os.path.join(indir, subfont + ".pfb"))
+    f.selection.all()
+    f.transform(psMat.scale(scale))
+
+    # mergeFonts takes a font, but this is a recent innovation of
+    # b53e885e Aug 28, 2018 "Allow passing a font object to
+    # mergeFonts()"
+    tmp = "tmp.feta-brace-scaled.pfb"
+    f.generate(tmp)
+    font.mergeFonts(tmp)
+    os.remove(tmp)
+    scale += 1.0
+
+font.fontname= "Emmentaler-Brace"
+font.familyname = "Emmentaler-Brace"
+font.weight = "Regular"
+font.copyright = "GNU GPL"
+font.version = version
+
+# Set code points to PUA (Private Use Area)
+i = 0
+for glyph in font.glyphs():
+    glyph.unicode = i + 0xE000
+    i += 1
+
+subfonts_str = ' '.join (subfonts)
+
+
+lisp = b""
+for sub in subfonts:
+    lisp += open(os.path.join(indir, sub) + ".lisp", "rb").read()
+
+font.setTableData("LILF", subfonts_str.encode("ascii"))
+font.setTableData("LILC", lisp)
+font.setTableData("LILY", b'(design_size . 20)')
+
+font.generate(output)
+base, ext = os.path.splitext(output)
+
+font.generate(base + ".svg")
+font.generate(base + ".woff")
