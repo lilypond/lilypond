lilypond/lilypond

commit 68fb45e7e20071b10c43f2a6f64681d56122e67d

Rename \compressFullBarRests and document it.
- Rename \compressFullBarRests to \compressEmptyMeasures
as suggested by Trevor in #4375.

- Document the new command (and explain its difference
with \compressMMRests) by creating a new subsubsec in
NR 1.6.3 "Writing parts".

- Add index entries and links everywhere I can think of,
obviously starting with NR 1.2.2.3 "Full measure rests".

- Add convert rule and update syntax through the doc.

- Clarify the (in)famous progerror
"Multi_measure_rest::get_rods (): I am not spanned!"

- Address minor errors caught by Werner and Trevor.

Documentation/snippets/multi-measure-rest-length-control.ly
@@ -4,25 +4,24 @@
 % and then run scripts/auxiliar/makelsr.py
 %
 % This file is in the public domain.
-%% Note: this file works from version 2.19.80
 
-\version "2.19.80"
+\version "2.21.0"
 
 \header {
   lsrtags = "rhythms"
 
   texidoc = "
 Multi-measure rests have length according to their total duration which
-is under the control of @code{MultiMeasureRest.space-increment}.  Note
+is under the control of @code{MultiMeasureRest.space-increment}.  Note that
 the default value is @code{2.0}.
 
 "
-  doctitle = "Multi-measure length control"
+  doctitle = "Multi-measure rest length control"
 } % begin verbatim
 
 
 \relative c' {
-  \compressFullBarRests
+  \compressEmptyMeasures
   R1*2 R1*4 R1*64 R1*16
   \override Staff.MultiMeasureRest.space-increment = 2.5
   R1*2 R1*4 R1*64 R1*16

Documentation/snippets/new/multi-measure-rest-length-control.ly
@@ -1,5 +1,5 @@
 
-\version "2.19.80"
+\version "2.21.0"
 
 \header {
   lsrtags = "rhythms"
@@ -14,7 +14,7 @@ the default value is @code{2.0}.
 }
 
 \relative c' {
-  \compressFullBarRests
+  \compressEmptyMeasures
   R1*2 R1*4 R1*64 R1*16
   \override Staff.MultiMeasureRest.space-increment = 2.5
   R1*2 R1*4 R1*64 R1*16

Documentation/de/notation/changing-defaults.itely
@@ -1221,7 +1221,7 @@ des äußeren Kontextes neu positioniert, anstatt innerhalb von ihm
 gesetzt zu werden.
 
 Die @qq{accepts}-Liste eines Kontextes kann mit dem Befehlen
-@code{\accepts} und @code{\denies} verändert werden.  @code{\accepts} 
+@code{\accepts} und @code{\denies} verändert werden.  @code{\accepts}
 fügt einen Kontext zur @qq{accepts}-Liste, und @code{\denies}
 entfernt einen Kontext aus der Liste.  Akkordbezeichnungen sollen
 beispielsweise normalerweise nicht innerhalb eines @code{Staff}-Kontextes
@@ -1254,7 +1254,7 @@ geführt ist.  Wenn er aber benötigt wird, kann er hinzugefügt werden:
 @end lilypond
 
 @code{\denies} wird vorrangig eingesetzt, wenn ein neuer Kontext
-basierend auf einem existierenden erstellt wird, aber sein 
+basierend auf einem existierenden erstellt wird, aber sein
 Schachtelungsverhalten sich unterscheidet.  Der
 @code{VaticanaStaff}-Kontext beispielsweise basiert auf dem
 @code{Staff}-Kontext, hat aber den @code{VaticanaVoice}-Kontext
@@ -2844,7 +2844,7 @@ a
 
 @lilypond[verbatim,quote,relative=2]
 a1
-\compressFullBarRests
+\compressEmptyMeasures
 R1*23
 % increase the length of the rest bar
 \once \override MultiMeasureRest.minimum-length = #20

Documentation/de/notation/rhythms.itely
@@ -845,7 +845,7 @@ Noten eingegeben, wobei die Bezeichnung ein Großbuchstabe
 
 @lilypond[quote,verbatim,relative=2]
 % Rest measures contracted to single measure
-\compressFullBarRests
+\compressEmptyMeasures
 R1*4
 R1*24
 R1*4
@@ -858,7 +858,7 @@ eine ganze Anzahl an Taktlängen sein, weshalb Punktierungen
 und Brüche recht häufig eingesetzt werden müssen.
 
 @lilypond[quote,relative=2,verbatim]
-\compressFullBarRests
+\compressEmptyMeasures
 \time 2/4
 R1 | R2 |
 \time 3/4
@@ -886,10 +886,8 @@ R1*2 |
 @cindex Pausen, mehrere Takte komprimieren
 @cindex Mehrtaktpausen, komprimieren
 
-@funindex \expandFullBarRests
-@funindex \compressFullBarRests
-@funindex expandFullBarRests
-@funindex compressFullBarRests
+@funindex \expandEmptyMeasures
+@funindex \compressEmptyMeasures
 
 In den Standardeinstellungen werden mehrtaktige Pausen
 ausgeschrieben gesetzt, sodass sie die entsprechende Anzahl von
@@ -904,10 +902,10 @@ der Pausendauer über dem Pausenzeichen ausgegeben wird:
 \time 2/4 R2 |
 \time 4/4
 % Rest measures contracted to single measure
-\compressFullBarRests
+\compressEmptyMeasures
 r1 | R1*17 | R1*4 |
 % Rest measures expanded
-\expandFullBarRests
+\expandEmptyMeasures
 \time 3/4
 R2.*2 |
 @end lilypond
@@ -927,7 +925,7 @@ Textbeschriftung kann Mehrtaktpausen mit @code{\markup}
 hinzugefügt werden.
 
 @lilypond[quote,verbatim,relative=2]
-\compressFullBarRests
+\compressEmptyMeasures
 \time 3/4
 R2.*10^\markup { \italic "ad lib." }
 @end lilypond
@@ -960,14 +958,14 @@ Taktprüfungswarnungen nicht angezeigt.
 
 @funindex \textLengthOn
 @funindex \textLengthOff
-@funindex \compressFullBarRests
-@funindex \expandFullBarRests
+@funindex \compressEmptyMeasures
+@funindex \expandEmptyMeasures
 
 @predefined
 @code{\textLengthOn},
 @code{\textLengthOff},
-@code{\compressFullBarRests},
-@code{\expandFullBarRests}.
+@code{\compressEmptyMeasures},
+@code{\expandEmptyMeasures}.
 @endpredefined
 
 @snippets
@@ -1749,7 +1747,7 @@ verschoben werden.
 
 @subsubheading Zusammengesetzte Taktarten
 
-Taktarten aus mehreren Teilen werden mit der Funktion 
+Taktarten aus mehreren Teilen werden mit der Funktion
 @code{\compoundMeter} erstellt.  Die Syntax ist folgende:
 
 @example
@@ -2435,7 +2433,7 @@ nicht beendet wurde und weiterhin Noten erwartet, wird dieser
 letzte Balken nicht ausgegeben.  Das Gleiche gilt auch für
 polyphone Stimmen, die mit der
 @code{<< @dots{} \\ @dots{} >>}-Konstruktion notiert wurden.  Wenn eine
-polyphone Stimme endet, während ein Balken noch weitere Noten
+polyphone Stimme endet, während ein Balken noch weitere Noten
 erwartet, wird der Balken nicht gesetzt.  Eine Notlösung für
 dieses Probelm ist, den letzten Balken in der Stimme oder
 Partitur manuell zu setzen.

Documentation/de/notation/staff.itely
@@ -1082,7 +1082,7 @@ wird dann benutzt, um den Wechsel vorzunehmen:
 }
 \relative c' {
   \clef tenor
-  \compressFullBarRests
+  \compressEmptyMeasures
   c2 g'
   R1*16
   \instrumentSwitch "contrabassoon"

Documentation/ja/notation/rhythms.itely
@@ -936,7 +936,7 @@ R2.*2 |
 マークアップを複数小節にまたがる休符に付け加えることができます。
 
 @lilypond[quote,verbatim,relative=2]
-\compressFullBarRests
+\compressEmptyMeasures
 \time 3/4
 R2.*10^\markup { \italic "ad lib." }
 @end lilypond
@@ -976,8 +976,8 @@ R1\fermata
 
 @funindex \textLengthOn
 @funindex \textLengthOff
-@funindex \compressFullBarRests
-@funindex \expandFullBarRests
+@funindex \compressEmptyMeasures
+@funindex \expandEmptyMeasures
 
 @predefined
 @code{\textLengthOn},

Documentation/notation/ancient.itely
@@ -639,7 +639,7 @@ The following example demonstrates the @code{petrucci} style:
 @c separate the notes for easier identification.
 
 @lilypond[quote,fragment,ragged-right,verbatim]
-\set Score.skipBars = ##t
+\compressEmptyMeasures
 \autoBeamOff
 \override NoteHead.style = #'petrucci
 a'\maxima a'\longa a'\breve a'1 a'2 a'4 a'8 a'16 a'
@@ -716,7 +716,7 @@ ancient rests.  Supported ancient styles are
 The following example demonstrates these styles:
 
 @lilypond[quote,fragment,ragged-right,verbatim]
-\set Score.skipBars = ##t
+\compressEmptyMeasures
 \override Rest.style = #'mensural
 r\longa^"mensural" r\breve r1 r2 r4 r8 r16 s \break
 \override Rest.style = #'neomensural

commit e2614c48b6a30a842a5e98970f8f627797c0f194

Fix Documentation/snippets inconsistencies
Those were introduced by
68fb45e7e2 Rename \compressFullBarRests and document it.

Documentation/snippets/multi-measure-rest-length-control.ly
@@ -4,6 +4,7 @@
 % and then run scripts/auxiliar/makelsr.py
 %
 % This file is in the public domain.
+%% Note: this file works from version 2.21.0
 
 \version "2.21.0"
 

Documentation/snippets/new/multi-measure-rest-length-control.ly
@@ -6,11 +6,11 @@
 
   texidoc = "
 Multi-measure rests have length according to their total duration which
-is under the control of @code{MultiMeasureRest.space-increment}.  Note
+is under the control of @code{MultiMeasureRest.space-increment}.  Note that
 the default value is @code{2.0}.
 
 "
-  doctitle = "Multi-measure length control"
+  doctitle = "Multi-measure rest length control"
 }
 
 \relative c' {

Documentation/snippets/new/numbering-single-measure-rests.ly
@@ -1,9 +1,3 @@
-%% DO NOT EDIT this file manually; it is automatically
-%% generated from LSR http://lsr.di.unimi.it
-%% Make any changes in LSR itself, or in Documentation/snippets/new/ ,
-%% and then run scripts/auxiliar/makelsr.py
-%%
-%% This file is in the public domain.
 \version "2.21.0"
 
 \header {
@@ -15,8 +9,7 @@ measures. This can be changed by setting @code{restNumberThreshold}.
 
 "
   doctitle = "Numbering single measure rests"
-} % begin verbatim
-
+}
 {
   \compressEmptyMeasures
   R1 R1*10 R1*11 \bar "||"

Documentation/snippets/numbering-single-measure-rests.ly
@@ -1,9 +1,10 @@
-%% DO NOT EDIT this file manually; it is automatically
-%% generated from LSR http://lsr.di.unimi.it
-%% Make any changes in LSR itself, or in Documentation/snippets/new/ ,
-%% and then run scripts/auxiliar/makelsr.py
-%%
-%% This file is in the public domain.
+% DO NOT EDIT this file manually; it is automatically
+% generated from Documentation/snippets/new
+% Make any changes in Documentation/snippets/new/
+% and then run scripts/auxiliar/makelsr.py
+%
+% This file is in the public domain.
+%% Note: this file works from version 2.21.0
 \version "2.21.0"
 
 \header {