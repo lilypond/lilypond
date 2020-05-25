999946f573f9693c072265c5c6d5789ac47757ea
David Kastrup <dak@gnu.org>
2020-05-25 22:48:12 +0200

    event_type memoization for Percent_repeat_iterator::music_tail

--- lily/percent-repeat-iterator.cc
+++ lily/percent-repeat-iterator.cc
@@ -85,23 +85,23 @@ Percent_repeat_iterator::music_tail () const
 
   SCM child_list = SCM_EOL;
 
-  const char * event_type;
-  SCM slash_count = SCM_UNDEFINED;
+  SCM event_type = SCM_UNDEFINED;
+  SCM slash_count = SCM_UNDEFINED; // Only defined for RepeatSlashEvent
 
   if (starting_bar_ >= 0 && current_bar == starting_bar_ + 1)
-    event_type = "PercentEvent";
+    event_type = ly_symbol2scm ("PercentEvent");
   else if (starting_bar_ >= 0 && current_bar == starting_bar_ + 2)
-    event_type = "DoublePercentEvent";
+    event_type = ly_symbol2scm ("DoublePercentEvent");
   else
     {
       slash_count = Lily::calc_repeat_slash_count (child->self_scm ());
-      event_type = "RepeatSlashEvent";
+      event_type = ly_symbol2scm ("RepeatSlashEvent");
     }
 
   int repeats = scm_to_int (get_property (mus, "repeat-count"));
   for (int i = repeats; i > 1; i--)
     {
-      Music *percent = make_music_by_name (ly_symbol2scm (event_type));
+      Music *percent = make_music_by_name (event_type);
       percent->set_spot (*mus->origin ());
       set_property (percent, "length", length);
       if (repeats > 1)
