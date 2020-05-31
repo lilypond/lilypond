1afe198257162eec53e898462e1c19fdff272417
David Kastrup <dak@gnu.org>
2020-05-31 17:42:50 +0200

    Make Drul_array a private base class of Interval_t
    That stops accidental slicing assignments.  It also stops
    Interval::delta from being accessible, preventing the use of
    Interval in a manner not consistent with its semantics.

--- flower/include/interval.hh
+++ flower/include/interval.hh
@@ -33,9 +33,11 @@
    No invariants.  T must be a totally ordered ring (with division, anyway ..)
    At instantiation, the function infinity () has to be defined explicitly. */
 template<class T>
-struct Interval_t : public Drul_array<T>
+struct Interval_t : private Drul_array<T>
 {
   using Drul_array<T>::at;
+  using Drul_array<T>::operator [];
+  using Drul_array<T>::linear_combination;
 
   template <typename T2>
   Interval_t (Interval_t<T2> const &src) : Interval_t (src[LEFT], src[RIGHT])
