7f811f4d50537a87602eeee058de1dd801e500bd
Michael Käppler <xmichael-k@web.de>
2020-06-10 14:34:37 +0200

    Website post-processing: Reindent to < 80 cols
    * Try to follow PEP8 suggestions and avoid the
    backslash character to force line continuation.
    Use parentheses instead, where possible.
    See https://www.python.org/dev/peps/pep-0008/#maximum-line-length
    * Break before binary operators
    See https://www.python.org/dev/peps/pep-0008/#should-a-line-break-before-or-after-a-binary-operator

--- python/auxiliar/postprocess_html.py
+++ python/auxiliar/postprocess_html.py
@@ -56,9 +56,14 @@ web_footer = '''
 </div>
 ''' % sys.argv[0]
 
-footer_name_version = _doc ('This page is for %(package_name)s-%(package_version)s (%(branch_str)s).')
-# ugh, must not have "_doc" in strings because it is naively replaced with "_" in hacked gettext process
-footer_report_links = _doc ('We welcome your aid; please <a href="%(help_us_url)s">help us</a> by reporting errors to our <a href="%(bug_lilypond_url)s">bug list</a>.')
+footer_name_version = _doc ('This page is for %(package_name)s-'
+                            '%(package_version)s (%(branch_str)s).')
+# ugh, must not have "_doc" in strings because it is naively replaced with "_"
+# in hacked gettext process
+footer_report_links = _doc ('We welcome your aid; please '
+                            '<a href="%(help_us_url)s">help us</a> by '
+                            'reporting errors to our '
+                            '<a href="%(bug_lilypond_url)s">bug list</a>.')
 sidebar_version = _doc (' v%(package_version)s (%(branch_str)s).')
 
 bug_lilypond_url = 'https://lists.gnu.org/mailman/listinfo/bug-lilypond'
@@ -107,13 +112,16 @@ def source_links_replace (m, source_val):
     return 'href="' + os.path.join (source_val, m.group (1)) + '"'
 
 # More hardcoding, yay!
-split_docs_re = re.compile('(Documentation/out-www/(automated-engraving|essay|notation|changes|extending|music-glossary|usage|web|learning|snippets|contributor))/')
+split_docs_re = re.compile('(Documentation/out-www/(automated-engraving|'
+                           'essay|notation|changes|extending|music-glossary|'
+                           'usage|web|learning|snippets|contributor))/')
 lily_examples_re = re.compile ('(href|src)="(ly-examples/.*?)"')
 lily_snippets_re = re.compile ('(href|src)="([0-9a-f]{2}/lily-.*?)"')
 pictures_re = re.compile ('src="(pictures/.*?)"')
 
 docindex_link_re = re.compile (r'href="index.html"')
-manuals_page_link_re = re.compile (r'href="((?:\.\./)+)Documentation/web/manuals')
+manuals_page_link_re = re.compile (r'href="((?:\.\./)+)'
+                                   'Documentation/web/manuals')
 
 ## Windows does not support symlinks.
 # This function avoids creating symlinks for split HTML manuals
@@ -122,7 +130,8 @@ manuals_page_link_re = re.compile (r'href="((?:\.\./)+)Documentation/web/manuals
 def hack_urls (s, prefix, target, is_development_branch):
     depth = (prefix.count ('/') - 1) * '../'
     # fix css links
-    s = css_re.sub ('<link \\1href="%(rel)sDocumentation/css/\\2"\\3>' % {'rel': depth}, s)
+    s = css_re.sub ('<link \\1href="%(rel)sDocumentation/css/\\2"\\3>'
+                    % {'rel': depth}, s)
     # fix image links
     if split_docs_re.match (prefix):
         s = lily_examples_re.sub ('\\1="../\\2"', s)
@@ -130,21 +139,23 @@ def hack_urls (s, prefix, target, is_development_branch):
         s = pictures_re.sub ('src="../\\1"', s)
 
     # we also need to replace in the lsr, which is already processed above!
-    if 'input/' in prefix or 'Documentation/topdocs' in prefix or \
-            'Documentation/contributor' in prefix:
+    if ('input/' in prefix or 'Documentation/topdocs' in prefix or
+       'Documentation/contributor' in prefix):
         # fix the link from the regtest, lsr and topdoc pages to the doc index
         # (rewrite prefix to obtain the relative path of the doc index page)
         rel_link = re.sub (r'out-www/.*$', '', prefix)
         rel_link = re.sub (r'[^/]*/', '../', rel_link)
-        if 'input/regression' in prefix or 'Documentation/contributor' in prefix:
+        if ('input/regression' in prefix or
+           'Documentation/contributor' in prefix):
             indexfile = "Documentation/devel"
         else:
             indexfile = "index"
-        s = docindex_link_re.sub ('href="' + rel_link + indexfile + '.html\"', s)
+        s = docindex_link_re.sub ('href="' + rel_link + indexfile
+                                  + '.html\"', s)
     # make the "return to doc index" work with the online website.
     if target == 'online':
-        if (('Documentation/contributor' in prefix) or
-            is_development_branch):
+        if (('Documentation/contributor' in prefix)
+            or is_development_branch):
             manuals_page = 'development'
         else:
             manuals_page = 'manuals'
@@ -154,20 +165,22 @@ def hack_urls (s, prefix, target, is_development_branch):
     if not os.path.islink (source_path):
         return s
     source_val = os.readlink (source_path)
-    return re.sub ('href="source/(.*?)"', lambda m: source_links_replace (m, source_val), s)
+    return re.sub ('href="source/(.*?)"',
+                   lambda m: source_links_replace (m, source_val), s)
 
 body_tag_re = re.compile ('(?i)<body([^>]*)>')
 html_tag_re = re.compile ('(?i)<html>')
 doctype_re = re.compile ('(?i)<!DOCTYPE')
 doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n'
-css_re = re.compile ('(?i)<link ([^>]*)href="[^">]*?(lilypond.*\.css)"([^>]*)>')
+css_re = re.compile ('(?i)<link ([^>]*)href="[^">]*?'
+                     '(lilypond.*\.css)"([^>]*)>')
 end_head_tag_re = re.compile ('(?i)</head>')
-css_link = """    <link rel="stylesheet" type="text/css" title="Default design" href="%(rel)sDocumentation/css/lilypond-manuals.css">
-    <!--[if lte IE 7]>
-    <link href="%(rel)sDocumentation/css/lilypond-ie-fixes.css" rel="stylesheet" type="text/css">
-    <![endif]-->
-"""
-
+css_link = ('    <link rel="stylesheet" type="text/css" title="Default design"'
+            ' href="%(rel)sDocumentation/css/lilypond-manuals.css">\n'
+            '    <!--[if lte IE 7]>\n'
+            '    <link href="%(rel)sDocumentation/css/lilypond-ie-fixes.css"'
+            ' rel="stylesheet" type="text/css">\n'
+            '    <![endif]-->\n')
 
 def add_header (s, prefix):
     """Add header (<body>, doctype and CSS)"""
@@ -184,7 +197,8 @@ def add_header (s, prefix):
 
         if css_re.search (s) == None:
             depth = (prefix.count ('/') - 1) * '../'
-            s = end_head_tag_re.sub ((css_link % {'rel': depth}) + '</head>', s)
+            s = end_head_tag_re.sub ((css_link % {'rel': depth})
+                                     + '</head>', s)
     return s
 
 title_tag_re = re.compile ('.*?<title>(.*?)</title>', re.DOTALL)
@@ -206,7 +220,8 @@ end_html_re = re.compile ('(?i)</html>')
 
 def add_footer (s, footer_text):
     """add footer"""
-    (s, n) = footer_insert_re.subn (footer_text + '\n' + '<!-- FOOTER -->', s, 1)
+    (s, n) = footer_insert_re.subn (footer_text + '\n' + '<!-- FOOTER -->',
+                                    s, 1)
     if not n:
         (s, n) = end_body_re.subn (footer_text + '\n' + '</body>', s, 1)
     if not n:
@@ -224,9 +239,9 @@ def find_translations (prefix, lang_ext):
         if lang_ext != e:
             if e in pages_dict[prefix]:
                 available.append (l)
-            elif lang_ext == '' and l.enabled and reduce (operator.and_,
-                                                          [not prefix.startswith (s)
-                                                           for s in non_copied_pages]):
+            elif (lang_ext == '' and l.enabled and
+                 reduce (operator.and_, [not prefix.startswith (s)
+                                         for s in non_copied_pages])):
                 # English version of missing translated pages will be written
                 missing.append (e)
     return available, missing
@@ -234,20 +249,22 @@ def find_translations (prefix, lang_ext):
 online_links_re = re.compile ('''(href|src)=['"]\
 ((?!Compiling-from-source.html")[^/][.]*[^.:'"]*)\
 ([.]html)(#[^"']*|)['"]''')
-offline_links_re = re.compile ('href=[\'"]\
-((?!Compiling-from-source.html")(?![.]{2}/contributor)[^/][.]*[^.:\'"]*)([.]html)(#[^"\']*|)[\'"]')
+offline_links_re = re.compile ('''href=['"]\
+((?!Compiling-from-source.html")(?![.]{2}/contributor)[^/][.]*[^.:'"]*)\
+([.]html)(#[^"\']*|)[\'"]''')
 big_page_name_re = re.compile ('''(.+?)-big-page''')
 
 def process_i18n_big_page_links (match, prefix, lang_ext):
     big_page_name = big_page_name_re.match (match.group (1))
     if big_page_name:
-        destination_path = os.path.normpath (os.path.join (os.path.dirname (prefix),
-                                                           big_page_name.group (0)))
+        destination_path = os.path.normpath (os.path.join (
+                                                 os.path.dirname (prefix),
+                                                 big_page_name.group (0)))
         if not (destination_path in pages_dict and
                 lang_ext in pages_dict[destination_path]):
             return match.group (0)
-    return 'href="' + match.group (1) + '.' + lang_ext \
-        + match.group (2) + match.group (3) + '"'
+    return ('href="' + match.group (1) + '.' + lang_ext
+            + match.group (2) + match.group (3) + '"')
 
 def process_links (s, prefix, lang_ext, file_name, missing, target):
     page_flavors = {}
@@ -255,33 +272,40 @@ def process_links (s, prefix, lang_ext, file_name, missing, target):
         # Strip .html, suffix for auto language selection (content
         # negotiation).  The menu must keep the full extension, so do
         # this before adding the menu.
-        page_flavors[file_name] = \
-            [lang_ext, online_links_re.sub ('\\1="\\2\\4"', s)]
+        page_flavors[file_name] = [lang_ext,
+                                   online_links_re.sub ('\\1="\\2\\4"', s)]
     elif target == 'offline':
         # in LANG doc index: don't rewrite .html suffixes
         # as not all .LANG.html pages exist;
-        # the doc index should be translated and contain links with the right suffixes
+        # the doc index should be translated and contain links
+        # with the right suffixes
         # idem for NEWS
-        if prefix in ('Documentation/out-www/index', 'Documentation/topdocs/out-www/NEWS'):
+        if prefix in ('Documentation/out-www/index',
+                      'Documentation/topdocs/out-www/NEWS'):
             page_flavors[file_name] = [lang_ext, s]
         elif lang_ext == '':
             page_flavors[file_name] = [lang_ext, s]
             for e in missing:
-                page_flavors[langdefs.lang_file_name (prefix, e, '.html')] = \
-                    [e, offline_links_re.sub ('href="\\1.' + e + '\\2\\3"', s)]
+                page_flavors[langdefs.lang_file_name (
+                             prefix, e, '.html')] = [e, offline_links_re.sub (
+                                                         'href="\\1.' + e
+                                                         + '\\2\\3"', s)]
         else:
             # For saving bandwidth and disk space, we don't duplicate big pages
-            # in English, so we must process translated big pages links differently.
+            # in English, so we must process translated
+            # big pages links differently.
             if 'big-page' in prefix:
-                page_flavors[file_name] = \
-                    [lang_ext,
-                     offline_links_re.sub \
-                         (lambda match: process_i18n_big_page_links (match, prefix, lang_ext),
-                          s)]
+                page_flavors[file_name] = [lang_ext,
+                                           offline_links_re.sub (
+                                               lambda match:
+                                               process_i18n_big_page_links (
+                                                   match, prefix, lang_ext),
+                                               s)]
             else:
-                page_flavors[file_name] = \
-                    [lang_ext,
-                     offline_links_re.sub ('href="\\1.' + lang_ext + '\\2\\3"', s)]
+                page_flavors[file_name] = [lang_ext,
+                                           offline_links_re.sub (
+                                               'href="\\1.' + lang_ext
+                                                + '\\2\\3"', s)]
     return page_flavors
 
 def add_menu (page_flavors, prefix, available, target, translation):
@@ -330,8 +354,10 @@ def process_html_files (package_name = '',
 
     # Initialize dictionaries for string formatting
     subst = {}
-    subst[''] = dict ([i for i in list(globals ().items()) if type (i[1]) is str])
-    subst[''].update (dict ([i for i in list(locals ().items()) if type (i[1]) is str]))
+    subst[''] = dict ([i for i in list(globals ().items())
+                       if type (i[1]) is str])
+    subst[''].update (dict ([i for i in list(locals ().items())
+                             if type (i[1]) is str]))
     for l in translation:
         e = langdefs.LANGDICT[l].webext
         if e:
@@ -341,8 +367,10 @@ def process_html_files (package_name = '',
     # Do deeper string formatting as early as possible,
     # so only one '%' formatting pass is needed later
     for e in subst:
-        subst[e]['footer_name_version'] = subst[e]['footer_name_version'] % subst[e]
-        subst[e]['footer_report_links'] = subst[e]['footer_report_links'] % subst[e]
+        subst[e]['footer_name_version'] = (subst[e]['footer_name_version']
+                                           % subst[e])
+        subst[e]['footer_report_links'] = (subst[e]['footer_report_links']
+                                           % subst[e])
 
     for prefix, ext_list in list(pages_dict.items ()):
         for lang_ext in ext_list:
@@ -355,11 +383,13 @@ def process_html_files (package_name = '',
 
                 s = codecs.open (file_name, 'r', 'utf-8').read ()
                 s = s.replace ('%', '%%')
-                s = hack_urls (s, prefix, target, bool (int (versiontup[1]) %  2))
+                s = hack_urls (s, prefix, target, bool (int (versiontup[1])
+                                                        % 2))
                 s = add_header (s, prefix)
 
                 ### add sidebar information
-                s = s.replace ('<!-- Sidebar Version Tag  -->', sidebar_version)
+                s = s.replace ('<!-- Sidebar Version Tag  -->',
+                               sidebar_version)
 
                 ### add footer
                 if footer_tag_re.search (s) == None:
@@ -369,14 +399,21 @@ def process_html_files (package_name = '',
                         s = add_footer (s, footer_tag + footer)
 
                     available, missing = find_translations (prefix, lang_ext)
-                    page_flavors = process_links (s, prefix, lang_ext, file_name, missing, target)
-                    # Add menu after stripping: must not have autoselection for language menu.
-                    page_flavors = add_menu (page_flavors, prefix, available, target, translation)
+                    page_flavors = process_links (s, prefix, lang_ext,
+                                                  file_name, missing, target)
+                    # Add menu after stripping: must not have autoselection
+                    # for language menu.
+                    page_flavors = add_menu (page_flavors, prefix, available,
+                                             target, translation)
                 for k in page_flavors:
-                    page_flavors[k][1] = page_flavors[k][1] % subst[page_flavors[k][0]]
+                    page_flavors[k][1] = (page_flavors[k][1]
+                                          % subst[page_flavors[k][0]])
                     out_f = codecs.open (name_filter (k), 'w', 'utf-8')
                     out_f.write (page_flavors[k][1])
                     out_f.close()
-        # if the page is translated, a .en.html symlink is necessary for content negotiation
-        if target == 'online' and ext_list != [''] and not os.path.lexists (name_filter (prefix + '.en.html')):
-            os.symlink (os.path.basename (prefix) + '.html', name_filter (prefix + '.en.html'))
+        # if the page is translated, a .en.html symlink is necessary
+        # for content negotiation
+        if (target == 'online' and ext_list != ['']
+            and not os.path.lexists (name_filter (prefix + '.en.html'))):
+            os.symlink (os.path.basename (prefix) + '.html',
+                        name_filter (prefix + '.en.html'))
