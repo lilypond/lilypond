lilypond/lilypond

commit d79f0bb3738a3fffae211e8e8c550fad70cf55ec

Web: Replace URLs for gmane in footers
Issue 5213

Remove links for gmane
in footers for manuals.

Point to bug-lilypond
instead.

python/auxiliar/postprocess_html.py
@@ -60,7 +60,7 @@ footer_report_links = _doc ('We welcome your aid; please <a href="%(help_us_url)
 sidebar_version = _doc (' v%(package_version)s (%(branch_str)s).')
 
 
-mail_address = 'http://post.gmane.org/post.php?group=gmane.comp.gnu.lilypond.bugs'
+mail_address = 'https://lists.gnu.org/mailman/listinfo/bug-lilypond'
 help_us_url = 'http://lilypond.org/help-us.html'
 
 header_tag = '<!-- header_tag -->'
@@ -131,7 +131,7 @@ def hack_urls (s, prefix, target, is_development_branch):
     # we also need to replace in the lsr, which is already processed above!
     if 'input/' in prefix or 'Documentation/topdocs' in prefix or \
             'Documentation/contributor' in prefix:
-        # fix the link from the regtest, lsr and topdoc pages to the doc index 
+        # fix the link from the regtest, lsr and topdoc pages to the doc index
         # (rewrite prefix to obtain the relative path of the doc index page)
         rel_link = re.sub (r'out-www/.*$', '', prefix)
         rel_link = re.sub (r'[^/]*/', '../', rel_link)

commit 5040095ae8f5981873de3271fac4a1f69a21035e

Fix URL generation for bug-list in Doc footer
IIUC this has been broken since
d79f0bb3738a3fffae211e8e8c550fad70cf55ec
The logic in process_html_files() checks,
whether 'mail_address' refers to an actual
mail address or to a web page. It did only
check for http://, not for https://, however.

Since the link to the bug-lilypond web interface
is hardcoded, there is no point in checking
whether it is an email address or an URL, thus
removed the outdated branching code.

python/auxiliar/postprocess_html.py
@@ -58,11 +58,10 @@ web_footer = '''
 
 footer_name_version = _doc ('This page is for %(package_name)s-%(package_version)s (%(branch_str)s).')
 # ugh, must not have "_doc" in strings because it is naively replaced with "_" in hacked gettext process
-footer_report_links = _doc ('We welcome your aid; please <a href="%(help_us_url)s">help us</a> by reporting errors to our <a href="%(mail_address_url)s">bug list</a>.')
+footer_report_links = _doc ('We welcome your aid; please <a href="%(help_us_url)s">help us</a> by reporting errors to our <a href="%(bug_lilypond_url)s">bug list</a>.')
 sidebar_version = _doc (' v%(package_version)s (%(branch_str)s).')
 
-
-mail_address = 'https://lists.gnu.org/mailman/listinfo/bug-lilypond'
+bug_lilypond_url = 'https://lists.gnu.org/mailman/listinfo/bug-lilypond'
 help_us_url = 'https://lilypond.org/help-us.html'
 
 header_tag = '<!-- header_tag -->'
@@ -323,11 +322,6 @@ def process_html_files (package_name = '',
     """
     translation = langdefs.translation
     localtime = time.strftime ('%c %Z', time.localtime (time.time ()))
-
-    if "http://" in mail_address:
-        mail_address_url = mail_address
-    else:
-        mail_address_url= 'mailto:' + mail_address
 
     versiontup = package_version.split ('.')
     branch_str = _doc ('stable-branch')