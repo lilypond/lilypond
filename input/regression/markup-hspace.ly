lilypond/lilypond

commit 6d038b9cbc7a270f8277c4c17acbdf5f5cfc7f49

Use Interval::length rather than Interval::delta
Interval::delta assigns numeric meaning to empty intervals and is
an abuse of the Interval notion.

lily/stencil.cc
@@ -348,7 +348,7 @@ Stencil::stack (Axis a, Direction d, Stencil const &s, Real padding, Real mindis
   if (is_empty (other_axis (a)))
     {
       Stencil toadd (s);
-      Real offset = d * first_extent.delta ();
+      Real offset = d * first_extent.length ();
       toadd.translate_axis (offset, a);
       toadd.add_stencil (*this);
       expr_ = toadd.expr ();
@@ -368,7 +368,7 @@ Stencil::stack (Axis a, Direction d, Stencil const &s, Real padding, Real mindis
       expr_ = toadd.expr ();
       dim_ = toadd.extent_box ();
       dim_[a][-d] = first_extent[-d];
-      dim_[a][d] = first_extent[d] + d * next_extent.delta ();
+      dim_[a][d] = first_extent[d] + d * next_extent.length ();
       return;
     }
 

lily/lookup.cc
@@ -167,9 +167,9 @@ Lookup::filled_box (Box b)
 Stencil
 Lookup::round_filled_box (Box b, Real blotdiameter)
 {
-  Real width = b.x ().delta ();
+  Real width = b.x ().length ();
   blotdiameter = std::min (blotdiameter, width);
-  Real height = b.y ().delta ();
+  Real height = b.y ().length ();
   blotdiameter = std::min (blotdiameter, height);
 
   if (blotdiameter < 0.0)

commit 717f6c9569f8266faeb97341e341d33bec3a6ea2

Fix negative spacing when stacking stencils
This fixes a regression introduced by commit 6d038b9cbc
("Use Interval::length rather than Interval::delta") and
closes #6003.

[Jonas: Extend commit message and add regression test]
Signed-off-by: Jonas Hahnfeld <hahnjo@hahnjo.de>

lily/stencil.cc
@@ -348,7 +348,9 @@ Stencil::stack (Axis a, Direction d, Stencil const &s, Real padding, Real mindis
   if (is_empty (other_axis (a)))
     {
       Stencil toadd (s);
-      Real offset = d * first_extent.length ();
+      // Spacing assigns meaning to "intervals" with negative extent,
+      // so we cannot use first_extent.length () here
+      Real offset = first_extent[d] - first_extent[-d];
       toadd.translate_axis (offset, a);
       toadd.add_stencil (*this);
       expr_ = toadd.expr ();
@@ -368,7 +370,7 @@ Stencil::stack (Axis a, Direction d, Stencil const &s, Real padding, Real mindis
       expr_ = toadd.expr ();
       dim_ = toadd.extent_box ();
       dim_[a][-d] = first_extent[-d];
-      dim_[a][d] = first_extent[d] + d * next_extent.length ();
+      dim_[a][d] = first_extent[d] + next_extent[d] - next_extent[-d];
       return;
     }
 

input/regression/markup-hspace.ly
@@ -1,0 +1,10 @@
+\header {
+  texidoc = "Text markup using @code{\\hspace} with positive and
+negative arguments."
+}
+
+\version "2.21.3"
+
+{
+  c'1-\markup { \hspace #5 positive }-\markup { \hspace #-5 negative }
+}