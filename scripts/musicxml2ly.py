lilypond/lilypond

commit 21ea33e1cdd75f602b70071e69899370004db30b

Issue 5823: musicxml2ly: Emit bar checks for all voices
The script only added them for the first voice, without explanation.
As this change doesn't seem to cause any problems, it's probably fine
to lift this restriction and make the generated code easier to read.

Suggested privately by Kai Ruemmler.

scripts/musicxml2ly.py
@@ -2283,8 +2283,8 @@ def musicxml_voice_to_lily_voice(voice):
             voice_builder.add_multibar_rest(n._duration)
             continue
 
-        # print a bar check at the beginning of each measure!
-        if n.is_first() and n._measure_position == Rational(0) and n != voice._elements[0]:
+        # Print bar checks between measures.
+        if n._measure_position == Rational(0) and n != voice._elements[0]:
             try:
                 num = int(n.get_parent().number)
             except ValueError:

commit 7c65447ade8e4147b9ba8813443da11d57d6eed8

musicxml2ly: Bar check only before first note
If there are multiple elements on measure position 0, do not emit
multiple checks for the same bar, but only before the first note.
This avoids breaking chords and fixes #5994, a regression caused by
commit 21ea33e1cd ("musicxml2ly: Emit bar checks for all voices").

scripts/musicxml2ly.py
@@ -2241,6 +2241,7 @@ def musicxml_voice_to_lily_voice(voice):
         for number in list(lyrics.keys()):
             extracted_lyrics = extract_lyrics(voice, number, lyrics)
 
+    last_bar_check = -1
     for idx, n in enumerate(voice._elements):
         tie_started = False
         if n.get_name() == 'forward':
@@ -2310,11 +2311,12 @@ def musicxml_voice_to_lily_voice(voice):
                 num = int(n.get_parent().number)
             except ValueError:
                 num = 0
-            if num > 0:
+            if num > 0 and num > last_bar_check:
                 voice_builder.add_bar_check(num)
                 figured_bass_builder.add_bar_check(num)
                 chordnames_builder.add_bar_check(num)
                 fretboards_builder.add_bar_check(num)
+                last_bar_check = num
 
         if isinstance(n, musicxml.Direction):
             # check if Direction already has been converted in another voice.