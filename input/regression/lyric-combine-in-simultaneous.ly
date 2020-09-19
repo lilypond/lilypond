lilypond/lilypond

commit fbca1f879f95e21f4cb05f3af649830c71f1dd21

Handle an empty volta body more robustly
Sequential_iterator no longer pre-skips elements that are not OK.
This maintains the position of the elements, which is important for the
derived class Volta_repeat_iterator.

Volta_repeat_iterator issues commands to create repeat bars if and only
if the body music has some duration, including grace duration.

Closes #6025.

lily/sequential-iterator.cc
@@ -138,11 +138,6 @@ Sequential_iterator::create_children ()
       iter_ = unsmob<Music_iterator> (it_scm);
     }
 
-  // Why might a newly created iterator not be OK?  An example is a
-  // Sequential_iterator with no elements.
-  while (iter_ && !iter_->ok ())
-    next_element ();
-
   here_mom_ = get_music ()->start_mom ();
   la_.init (cursor_);
   la_.look_ahead ();
@@ -219,30 +214,33 @@ Sequential_iterator::process (Moment until)
 {
   while (iter_)
     {
-      const Grace_fixup *gf = la_.get_grace_fixup (here_mom_);
-      if (gf
-          && gf->start_ + gf->length_
-          + Moment (Rational (0), gf->grace_start_) == until)
-        {
-          /*
-            do the stuff/note/rest preceding a grace.
-          */
-          iter_->process (iter_->music_get_length ());
-        }
-      else
-        {
-          Moment w = until - here_mom_ + iter_->music_start_mom ();
-          iter_->process (w);
-        }
-
-      /*
-        if the iter is still OK, there must be events left that have
-
-        TIME > LEFT
-
-      */
       if (iter_->ok ())
-        return;
+        {
+          const Grace_fixup *gf = la_.get_grace_fixup (here_mom_);
+          if (gf
+              && gf->start_ + gf->length_
+              + Moment (Rational (0), gf->grace_start_) == until)
+            {
+              /*
+                do the stuff/note/rest preceding a grace.
+              */
+              iter_->process (iter_->music_get_length ());
+            }
+          else
+            {
+              Moment w = until - here_mom_ + iter_->music_start_mom ();
+              iter_->process (w);
+            }
+
+          /*
+            if the iter is still OK, there must be events left that have
+
+            TIME > LEFT
+
+          */
+          if (iter_->ok ())
+            return;
+        }
 
       descend_to_child (iter_->get_context ());
       next_element ();
@@ -257,7 +255,10 @@ Sequential_iterator::pending_moment () const
   if (!iter_)
     return Moment (Rational::infinity ());
 
-  Moment cp = iter_->pending_moment ();
+  // The only time iter_ might not be OK is before the first call to process().
+  // After that, process() skips over iterators that are not OK.
+  Moment cp
+    = iter_->ok () ? iter_->pending_moment () : iter_->music_start_mom ();
 
   /*
     Fix-up a grace note halfway in the music.

input/regression/repeat-volta-body-empty.ly
@@ -1,0 +1,32 @@
+\version "2.21.6"
+
+\header{
+  texidoc="If the body of a volta repeat is empty, the alternatives are
+still rendered with the expected volta notation."
+}
+
+body = { }
+
+\new Score {
+  \repeat volta 2 \body | R1
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body | R1
+}
+
+\new Score {
+  \repeat volta 2 \body \alternative { a'1 b' } | R1
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body \alternative { a'1 b' }
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body \alternative { a'1 b' } | R1
+}

input/regression/repeat-volta-body-grace.ly
@@ -1,0 +1,32 @@
+\version "2.21.6"
+
+\header{
+  texidoc="If the body of a volta repeat is only a grace note, it is still
+engraved as expected."
+}
+
+body = \grace f'8
+
+\new Score {
+  \repeat volta 2 \body | b'1
+}
+
+\new Score {
+  b'1 | \repeat volta 2 \body
+}
+
+\new Score {
+  b'1 | \repeat volta 2 \body | b'1
+}
+
+\new Score {
+  \repeat volta 2 \body \alternative { a'1 b' } | R1
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body \alternative { a'1 b' }
+}
+
+\new Score {
+  R1 | \repeat volta 2 \body \alternative { a'1 b' } | R1
+}

lily/volta-repeat-iterator.cc
@@ -39,6 +39,8 @@ protected:
   void derived_mark () const override;
 
   bool first_time_ = true;
+  // It seems silly not to have a body, but we're trying to be robust.
+  bool have_body_ = false;
   int alt_count_ = 0;
   int rep_count_ = 0;
   int done_count_ = 0;
@@ -62,6 +64,11 @@ Volta_repeat_iterator::get_music_list ()const
 void
 Volta_repeat_iterator::create_children ()
 {
+  if (auto *body = unsmob<Music> (get_property (get_music (), "element")))
+    {
+      have_body_ = (body->get_length () > 0) || body->start_mom ().grace_part_;
+    }
+
   Sequential_iterator::create_children ();
 
   SCM alts = get_property (get_music (), "elements");
@@ -126,7 +133,8 @@ Volta_repeat_iterator::next_element ()
 
           if (done_count_ - 1 < alt_count_)
             {
-              add_repeat_command (ly_symbol2scm ("end-repeat"));
+              if (have_body_)
+                add_repeat_command (ly_symbol2scm ("end-repeat"));
 
               if (from_scm<bool> (get_property (get_context (), "timing")))
                 {
@@ -163,7 +171,10 @@ Volta_repeat_iterator::next_element ()
                                         ly_string2scm (repstr)));
     }
   else
-    add_repeat_command (ly_symbol2scm ("end-repeat"));
+    {
+      if (have_body_)
+        add_repeat_command (ly_symbol2scm ("end-repeat"));
+    }
 }
 
 void
@@ -171,7 +182,8 @@ Volta_repeat_iterator::process (Moment m)
 {
   if (first_time_)
     {
-      add_repeat_command (ly_symbol2scm ("start-repeat"));
+      if (have_body_)
+        add_repeat_command (ly_symbol2scm ("start-repeat"));
       first_time_ = false;
     }
   Sequential_iterator::process (m);

commit 81a0b0c1001965c8124fb48f5595b3d7593cc4db

Sequential_iterator: treat an initial invalid iterator specially
This corrects a problem introduced by commit
fbca1f879f95e21f4cb05f3af649830c71f1dd21.

Closes #6036.

lily/include/sequential-iterator.hh
@@ -86,6 +86,7 @@ private:
   Moment here_mom_;
   SCM cursor_ = SCM_EOL;
   Lookahead la_;
+  bool first_time_ = true;
 };
 
 #endif /* SEQUENTIAL_ITERATOR_HH */

lily/sequential-iterator.cc
@@ -212,6 +212,8 @@ Sequential_iterator::next_element ()
 void
 Sequential_iterator::process (Moment until)
 {
+  first_time_ = false;
+
   while (iter_)
     {
       if (iter_->ok ())
@@ -255,10 +257,12 @@ Sequential_iterator::pending_moment () const
   if (!iter_)
     return Moment (Rational::infinity ());
 
-  // The only time iter_ might not be OK is before the first call to process().
-  // After that, process() skips over iterators that are not OK.
-  Moment cp
-    = iter_->ok () ? iter_->pending_moment () : iter_->music_start_mom ();
+  // Before the first call to process (), we might be looking at an iterator
+  // that will be skipped during the first call to process ().
+  if (first_time_ && !iter_->ok ())
+    return here_mom_;
+
+  Moment cp = iter_->pending_moment ();
 
   /*
     Fix-up a grace note halfway in the music.

input/regression/lyric-combine-in-sequential.ly
@@ -1,0 +1,11 @@
+\version "2.21.7"
+
+\header {
+  texidoc = "This tests @code{\\lyricsto} as the first element of
+sequential music."
+}
+
+<<
+  \context Voice = V f'1
+  \context Lyrics { \lyricsto V { \lyricmode { eff } } }
+>>

input/regression/lyric-combine-in-simultaneous.ly
@@ -1,0 +1,11 @@
+\version "2.21.7"
+
+\header {
+  texidoc = "This tests @code{\\lyricsto} as an element of
+simultaneous music."
+}
+
+<<
+  \context Voice = V f'1
+  \context Lyrics << \lyricsto V { \lyricmode { eff } } >>
+>>