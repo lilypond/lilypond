lilypond/lilypond

commit 7396a28bde310c5f43d7dcbd3dfc8f708726f3de

Issue 5720: Add enabling extension definitions for `-std=c++11` option
The compiler option `-std=c++11` means
to follow the strict C++11 standard,
and cannot use several extensions
such as POSIX functions/definitions
(e.g. M_PI, strdup, putenv, and chroot).

With the option, some environments (e.g. Linux)
can use the extensions as it is,
but other environments require definitions (e.g. `_GNU_SOURCE`)
to enable the extensions.

This commit makes
configure script checks the environments and adds the definitions.

configure.ac
@@ -80,6 +80,8 @@ fi
 AC_MSG_RESULT($LN_SRC_TO_BUILD)
 AC_SUBST(LN_SRC_TO_BUILD)
 
+AC_USE_SYSTEM_EXTENSIONS
+
 # must come before any header checks
 STEPMAKE_COMPILE
 

config.hh.in
@@ -85,5 +85,25 @@
 #define HAVE_LIBGUILE18_H 0
 
 /* define if cxxabi.h has workable demangler */
-
 #define HAVE_CXA_DEMANGLE 0
+
+/* define if enable extensions on GNU/Linux */
+#undef _GNU_SOURCE
+
+/* define if enable general extensions on Solaris */
+#undef __EXTENSIONS__
+
+/* define if enable threading extensions on Solaris */
+#undef _POSIX_PTHREAD_SEMANTICS
+
+/* define if enable extensions for the HP NonStop platform */
+#undef _TANDEM_SOURCE
+
+/* define if enable extensions for AIX 3, and for Interix */
+#undef _ALL_SOURCE
+
+/* define if enable Posix functions for Minix */
+#undef _POSIX_SOURCE
+
+/* define if enable additional Posix functions for Minix */
+#undef _POSIX_1_SOURCE

commit 5fcff89042e222baff432289e3c172357aa19fab

Fix configure flags to disable optimization or debugging
This broke with commit 7396a28bde ("Add enabling extension definitions
for `-std=c++11` option"), which added AC_USE_SYSTEM_EXTENSIONS before
STEPMAKE_COMPILE_BEFORE can set CFLAGS to an empty default value. This
led to execution of default code from autoconf which adds -O2 -g.

Closes #6056

aclocal.m4
@@ -240,6 +240,7 @@ AC_DEFUN(STEPMAKE_COMPILE_BEFORE, [
 AC_DEFUN(STEPMAKE_COMPILE, [
     AC_REQUIRE([STEPMAKE_COMPILE_BEFORE])
     AC_REQUIRE([AC_PROG_CC])
+    AC_REQUIRE([AC_USE_SYSTEM_EXTENSIONS])
 
     STEPMAKE_OPTIONAL_REQUIRED(CC, cc, $1)
 

configure.ac
@@ -70,8 +70,6 @@ AC_ARG_ENABLE(static-gxx,
     [LINK_GXX_STATICALLY=$enableval])
 AC_SUBST(LINK_GXX_STATICALLY)
 
-AC_USE_SYSTEM_EXTENSIONS
-
 # must come before any header checks
 STEPMAKE_COMPILE
 