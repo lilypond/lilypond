89e3243ead645fb05f0382374f82c2e1e184daae
Martin Joerg <martin.joerg@gmail.com>
2020-10-26 18:13:02 +0100

    lilypond-init.el: Correct auto-mode-alist entries for LilyPond-mode
    File name patterns for LilyPond-mode changed to match only at the end of filenames.
    
    (cherry picked from commit c9edc41c74f5aede8d0f678026255bfc975052bd)

--- elisp/lilypond-init.el
+++ elisp/lilypond-init.el
@@ -15,7 +15,7 @@
 ;(setq load-path (append (list (expand-file-name "~/site-lisp")) load-path))
 
 (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
-(add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
-(add-to-list 'auto-mode-alist '("\\.ily$" . LilyPond-mode))
+(add-to-list 'auto-mode-alist '("\\.ly\\'" . LilyPond-mode))
+(add-to-list 'auto-mode-alist '("\\.ily\\'" . LilyPond-mode))
 (add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
 
