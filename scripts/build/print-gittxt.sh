f0b63d8d1f7a07fc3dfd4af0ca6b43ddf8e08ef1
Jonas Hahnfeld <hahnjo@hahnjo.de>
2020-11-30 21:20:50 +0100

    Fix whitespace in print-gittxt.sh
    Replace tabs with 8 spaces.

--- scripts/build/print-gittxt.sh
+++ scripts/build/print-gittxt.sh
@@ -10,25 +10,25 @@ then
     BR=`git rev-parse --symbolic-full-name HEAD | sed s#^refs/heads/##`
     HD=`git rev-parse --verify HEAD`
     if git rev-parse -q origin/master ; then
-	FP=`git merge-base --octopus origin/master HEAD`
+        FP=`git merge-base --octopus origin/master HEAD`
     else
-	FP=""
+        FP=""
     fi
     echo "    BRANCH: $BR"
     echo "      HEAD: $HD"
     if [ ! -z $FP ]
     then
-	echo "MERGE_BASE: $FP"
-	echo
-	echo "   HISTORY:"
-	echo "   ========"
-	git log --pretty=format:"      HASH: %H%n   SUBJECT: %s%n" $FP~1..HEAD
+        echo "MERGE_BASE: $FP"
+        echo
+        echo "   HISTORY:"
+        echo "   ========"
+        git log --pretty=format:"      HASH: %H%n   SUBJECT: %s%n" $FP~1..HEAD
     else
-	echo "MERGE_BASE: unknown"
-	echo
-	echo "   HISTORY:"
-	echo "   ========"
-	git log --max-count=10 --pretty=format:"      HASH: %H%nSUBJECT: %s%n"
+        echo "MERGE_BASE: unknown"
+        echo
+        echo "   HISTORY:"
+        echo "   ========"
+        git log --max-count=10 --pretty=format:"      HASH: %H%nSUBJECT: %s%n"
     fi
     echo ""
     date
