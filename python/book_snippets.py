lilypond/lilypond

commit 38e1cad48d14f0b0f9286b4bb8891051a5f82d1e

Fix the linking of files from lilypond by lilypond-book.

python/book_snippets.py
@@ -621,7 +621,7 @@ printing diff against existing file." % filename)
     def link_all_output_files (self, output_dir, output_dir_files, destination):
         existing, missing = self.all_output_files (output_dir, output_dir_files)
         if missing:
-            print '\nMissing', missing
+            error (_ ('Missing files: %s') % ', '.join (missing))
             raise CompileError(self.basename())
         for name in existing:
             if (self.global_options.use_source_file_names
@@ -651,7 +651,7 @@ printing diff against existing file." % filename)
             except AttributeError:
                 shutil.copyfile (src, dst)
             except OSError:
-                print '\nCould not overwrite file', dst
+                error (_ ('Could not overwrite file %s') % dst)
                 raise CompileError(self.basename())
 
     def additional_files_to_consider (self, base, full):

scripts/lilypond-book.py
@@ -464,12 +464,15 @@ def do_process_cmd (chunks, input_name, options):
     else:
         progress (_ ("All snippets are up to date..."))
 
-    if options.lily_output_dir != options.output_dir:
-        output_files = split_output_files (options.lily_output_dir)
+    progress (_ ("Linking files..."))
+    abs_lily_output_dir = os.path.join (options.original_dir, options.lily_output_dir)
+    abs_output_dir = os.path.join (options.original_dir, options.output_dir)
+    if abs_lily_output_dir != abs_output_dir:
+        output_files = split_output_files (abs_lily_output_dir)
         for snippet in snippets:
-            snippet.link_all_output_files (options.lily_output_dir,
+            snippet.link_all_output_files (abs_lily_output_dir,
                                            output_files,
-                                           options.output_dir)
+                                           abs_output_dir)
 
 
 ###
@@ -640,6 +643,8 @@ def do_options ():
     if global_options.output_dir:
         global_options.output_dir = os.path.expanduser (global_options.output_dir)
         global_options.include_path.insert (0, inverse_relpath (original_dir, global_options.output_dir))
+
+    global_options.include_path.insert (0, ".%s" % os.path.sep)
 
     # Load the python packages (containing e.g. custom formatter classes)
     # passed on the command line

commit 28c055c30dba4d4a3ca0347769c5754d7f1facfb

lilypond-book: Remove snippet map
Support was broken ever since commit 38e1cad48d ("Fix the linking
of files from lilypond by lilypond-book.") prepended the current
directory as relative path to the list of includes in 2012. This
made LilyPond find the snippets as `./??/lily-*.ly' and the whole
mapping did not work. Now that commit 06cdcec72f ("lilypond-book:
Convert to absolute paths earlier") uses absolute paths and the
--lily-output-dir is not in the list of include paths, the mapping
came back to life and it becomes apparent why nobody missed it:

 * It's redundant since --use-source-file-names will replace the
   input file name with the original source file name.
 * Since the snippet map is added as a regular entry to the list
   of input files, it is subject to processing with multiple jobs.
   In particular this means:
     - without the argument -djob-count or a requested number of
       jobs greater than the number of input files (which also
       results in no jobs being forked), things might work;
     - if jobs are forked (and be it only one), the list of input
       files gets reversed and the map comes last after all files
       have been processed; and
     - even if that was fixed, only one job gets to see the map.

python/book_snippets.py
@@ -276,16 +276,10 @@ class Chunk:
         return False
 
     def __init__(self):
-        self._input_fullpath = ''
         self._output_fullpath = ''
 
-    def set_document_fullpaths(self, in_fp: str, out_fp: str):
-        self._input_fullpath = in_fp
+    def set_output_fullpath(self, out_fp: str):
         self._output_fullpath = out_fp
-
-    def input_fullpath(self) -> str:
-        """The input file path where this chunk comes from."""
-        return self._input_fullpath
 
     def output_fullpath(self) -> str:
         """The output file path that this chunk belongs to."""

scripts/lilypond-book.py
@@ -365,29 +365,12 @@ def process_snippets(cmd, outdated_dict,
     checksum = checksum.hexdigest()
 
     lily_output_dir = global_options.lily_output_dir
-    snippet_map_file = 'snippet-map-%s.ly' % checksum
-    snippet_map_path = os.path.join(lily_output_dir, snippet_map_file)
-
-    # Write snippet map.
-    with open(snippet_map_path, 'w', encoding='utf8') as snippet_map:
-        snippet_map.write("""
-
-#(define version-seen #t)
-#(define output-empty-score-list #f)
-""")
-
-        snippet_map.write("#(ly:add-file-name-alist '(\n")
-        for name in basenames:
-            snippet_map.write('("%s.ly" . "%s")\n' % (
-                name.replace('\\', '/'), outdated_dict[name].input_fullpath()))
-        snippet_map.write('))\n')
 
     # Write list of snippet names.
     snippet_names_file = 'snippet-names-%s.ly' % checksum
     snippet_names_path = os.path.join(lily_output_dir, snippet_names_file)
     with open(snippet_names_path, 'w', encoding='utf8') as snippet_names:
-        snippet_names.write('\n'.join(
-            [snippet_map_file] + [name + '.ly' for name in basenames]))
+        snippet_names.write('\n'.join([name + '.ly' for name in basenames]))
 
     # Run command.
     cmd = formatter.adjust_snippet_command(cmd)
@@ -397,7 +380,6 @@ def process_snippets(cmd, outdated_dict,
     system_in_directory(' '.join([cmd, snippet_names_arg]),
                         lily_output_dir,
                         logfile)
-    os.unlink(snippet_map_path)
     os.unlink(snippet_names_path)
 
 
@@ -522,7 +504,6 @@ def do_file(input_filename, included=False):
     input_absname = input_filename
     if not input_filename or input_filename == '-':
         in_handle = sys.stdin
-        input_fullname = '<stdin>'
     else:
         if os.path.exists(input_filename):
             input_fullname = input_filename
@@ -568,7 +549,7 @@ def do_file(input_filename, included=False):
         chunks = book_base.find_toplevel_snippets(
             source, global_options.formatter, global_options)
         for c in chunks:
-            c.set_document_fullpaths(input_fullname, output_filename)
+            c.set_output_fullpath(output_filename)
 
         # Let the formatter modify the chunks before further processing
         chunks = global_options.formatter.process_chunks(chunks)