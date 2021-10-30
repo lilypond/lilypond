f44ab2aa1d921bd25bbc4c98da3597d348e30903
Jonas Hahnfeld <hahnjo@hahnjo.de>
2021-10-30 14:50:14 +0200

    binaries: Fix pkg-config-static.sh for old pkg-config
    Old versions of pkgconf (such as version 1.6.3 found in Ubuntu 20.04)
    throw errors if querying --version with any other argument, so avoid
    passing --static in that case.

--- release/binaries/lib/pkg-config-static.sh
+++ release/binaries/lib/pkg-config-static.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-exec pkg-config --static "$@"
+static="--static"
+if [ "$*" = "--version" ]; then
+    # Old versions of pkgconf throw errors if querying --version with any other
+    # argument, so avoid passing --static in that case.
+    static=""
+fi
+exec pkg-config $static "$@"
