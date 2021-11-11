45d10476c8bb6842c30a01ea90dd8e14baa330f0
Jean Abou Samra <jean@abou-samra.fr>
2021-11-11 19:02:39 +0100

    Don't set non-musical in VoiceFollower definition
    It is a property of the item-interface and makes no sense for
    spanners.

--- scm/define-grobs.scm
+++ scm/define-grobs.scm
@@ -3069,7 +3069,6 @@
         (gap . 0.5)
         (left-bound-info . ,ly:line-spanner::calc-left-bound-info)
         (normalized-endpoints . ,ly:spanner::calc-normalized-endpoints)
-        (non-musical . #t)
         (right-bound-info . ,ly:line-spanner::calc-right-bound-info)
         (stencil . ,ly:line-spanner::print)
         (style . line)
