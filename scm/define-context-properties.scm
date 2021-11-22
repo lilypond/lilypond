8e39c61461fdf6d62b332488b145fb3193faa654
Dan Eble <nine.fierce.ballads@gmail.com>
2021-11-21 21:23:38 -0500

    Declare alternativeNumber as an index

--- scm/define-context-properties.scm
+++ scm/define-context-properties.scm
@@ -60,7 +60,7 @@ vertical alignment.")
      (alterationGlyphs ,list? "Alist mapping alterations to accidental glyphs.
 Alterations are given as exact numbers, e.g., -1/2 for flat.  This applies
 to all grobs that can print accidentals.")
-     (alternativeNumber ,integer? "When set, the index of the current
+     (alternativeNumber ,index? "When set, the index of the current
 @code{\\alternative} element, starting from one.  Not set outside of
 alternatives.  Note the distinction from volta number: an alternative
 may pertain to multiple volte.")
