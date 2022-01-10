lilypond/lilypond

commit 498a5503e9724f228d23d97114d454dda3e04383

Introduce -dlilypond-book-output.
If -dlilypond-book-output or -daux-files are given, Paper_book uses the
output-stencil routine to dump each system (or page, in case of \book)
separately, and then dumps a combined stencil of all the pages
concatenated.

Remove framework-eps, as lilypond-book output is now routed through
the PS backend. get_output_backend_name() remaps "eps" to "ps" to
maintain backward compatibility.

lily/paper-book.cc
@@ -197,67 +197,37 @@ Paper_book::output (SCM output_channel)
   dump_header_fields (output_channel, false);
   dump_signatures (output_channel);
 
-  string mod_nm = "lily framework-" + get_output_backend_name ();
-
-  SCM mod = scm_c_resolve_module (mod_nm.c_str ());
-
   if (get_program_option ("print-pages"))
     {
-      SCM framework = ly_module_lookup (mod, ly_symbol2scm ("output-stencils"));
-
-      if (scm_is_true (framework))
+      SCM stencils = SCM_EOL;
+      for (SCM s = pages (); scm_is_pair (s); s = scm_cdr (s))
         {
-          SCM func = scm_variable_ref (framework);
-
-          SCM stencils = SCM_EOL;
-          for (SCM s = pages (); scm_is_pair (s); s = scm_cdr (s))
-            {
-              stencils = scm_cons (get_property (unsmob<Prob> (scm_car (s)), "stencil"), stencils);
-            }
-          stencils = scm_reverse_x (stencils, SCM_EOL);
-
-          // TODO - paper or top_paper?
-          scm_call_4 (func, output_channel, stencils, header_,
-                      paper ()->self_scm ());
+          stencils = scm_cons (
+            get_property (unsmob<Prob> (scm_car (s)), "stencil"), stencils);
         }
-      else
-        warning (_f ("program option -dprint-pages not supported by backend `%s'",
-                     get_output_backend_name ()));
+      output_stencils (output_channel, scm_reverse_x (stencils, SCM_EOL));
     }
 
-  SCM framework = ly_module_lookup (mod, ly_symbol2scm ("output-stencil"));
-  if (scm_is_false (framework))
-    {
-      warning (_f ("program option -dclip-systems/-dcrop/-dpreview not "
-                   "supported by backend `%s'",
-                   get_output_backend_name ()));
-      return;
-    }
-
-  SCM func = scm_variable_ref (framework);
   if (get_program_option ("clip-systems"))
     {
       SCM name_stencil_alist
         = Lily::clipped_systems_stencils (output_channel, systems ());
 
       for (SCM p = name_stencil_alist; scm_is_pair (p); p = scm_cdr (p))
-        scm_call_3 (func, scm_caar (p), scm_cdar (p), paper_->self_scm ());
+        output_stencil (scm_caar (p), scm_cdar (p));
     }
 
   std::string basename = ly_scm2string (output_channel);
   if (get_program_option ("preview"))
     {
-
-      scm_call_3 (func, ly_string2scm (basename + ".preview"),
-                  Lily::generate_preview_stencil (self_scm ()),
-                  paper ()->self_scm ());
+      output_stencil (ly_string2scm (basename + ".preview"),
+                      Lily::generate_preview_stencil (self_scm ()));
     }
 
   if (get_program_option ("crop"))
     {
-      scm_call_3 (func, ly_string2scm (basename + ".cropped"),
-                  Lily::generate_crop_stencil (self_scm ()),
-                  paper ()->self_scm ());
+      output_stencil (ly_string2scm (basename + ".cropped"),
+                      Lily::generate_crop_stencil (self_scm ()));
     }
 
   if (get_program_option ("aux-files"))
@@ -323,25 +293,70 @@ Paper_book::classic_output (SCM output)
   dump_header_fields (output, true);
   dump_signatures (output);
 
-  string format = get_output_backend_name ();
-  string mod_nm = "lily framework-" + format;
+  output_stencils (output, Lily::generate_system_stencils (self_scm ()));
 
+  if (get_program_option ("aux-files"))
+    Lily::write_lilypond_book_aux_files (output, scm_length (systems ()));
+}
+
+void
+Paper_book::output_stencil (SCM out_name, SCM stencil)
+{
+  string mod_nm = "lily framework-" + get_output_backend_name ();
   SCM mod = scm_c_resolve_module (mod_nm.c_str ());
-  SCM func = scm_c_module_lookup (mod, "output-stencils");
-  if (scm_is_false (func))
+
+  SCM framework = ly_module_lookup (mod, ly_symbol2scm ("output-stencil"));
+  if (scm_is_false (framework))
     {
-      warning (_f ("lilypond-book output not supported by backend `%s'",
+      warning (_f ("program option -dclip-systems/-dcrop/-dpreview not "
+                   "supported by backend `%s'",
                    get_output_backend_name ()));
       return;
     }
 
-  func = scm_variable_ref (func);
+  SCM func = scm_variable_ref (framework);
+  scm_call_3 (func, out_name, stencil, paper_->self_scm ());
+}
 
-  SCM stencils = Lily::generate_system_stencils (self_scm ());
-  scm_call_4 (func, output, stencils, SCM_BOOL_F, paper ()->self_scm ());
+void
+Paper_book::output_stencils (SCM out_name, SCM stencils)
+{
+  if (get_program_option ("aux-files")
+      || get_program_option ("lilypond-book-output"))
+    {
+      Stencil acc;
+      std::string base = ly_scm2string (out_name);
+      for (SCM s = stencils; scm_is_pair (s); s = scm_cdr (s))
+        {
+          Stencil const *st = unsmob<Stencil const> (scm_car (s));
+          acc.add_at_edge (Y_AXIS, DOWN, *st, 2.0);
+        }
 
-  if (get_program_option ("aux-files"))
-    Lily::write_lilypond_book_aux_files (output, scm_length (systems ()));
+      output_stencil (out_name, acc.smobbed_copy ());
+      int i = 1;
+      for (SCM s = stencils; scm_is_pair (s); s = scm_cdr (s), i++)
+        {
+          output_stencil (ly_string2scm (base + "-" + std::to_string (i)),
+                          scm_car (s));
+        }
+    }
+  else
+    {
+      string format = get_output_backend_name ();
+      string mod_nm = "lily framework-" + format;
+
+      SCM mod = scm_c_resolve_module (mod_nm.c_str ());
+      SCM func = scm_c_module_lookup (mod, "output-stencils");
+      if (scm_is_false (func))
+        {
+          warning (_f ("multi-page output not supported by backend `%s'",
+                       get_output_backend_name ()));
+          return;
+        }
+
+      func = scm_variable_ref (func);
+      scm_call_4 (func, out_name, stencils, SCM_BOOL_F, paper ()->self_scm ());
+    }
 }
 
 /* TODO: resurrect more complex user-tweaks for titling?  */

Documentation/en/usage/running.itely
@@ -213,7 +213,7 @@ at the top of the @file{.ly} file.
 Generate EPS files.
 
 This option is equivalent to setting LilyPond's command line options
-@code{--ps}, @code{-dbackend=eps}, and @code{-daux-files='#f'}.
+@code{--ps} and @code{-dlilypond-book-output}.
 
 @cindex output, format
 @cindex format, output

lily/include/paper-book.hh
@@ -47,6 +47,9 @@ private:
   void add_score_title (SCM);
   SCM get_score_title (SCM);
 
+  void output_stencils (SCM, SCM);
+  void output_stencil (SCM, SCM);
+  
   Paper_book *parent_;
   Output_def *paper_;
 public:

lily/main.cc
@@ -647,7 +647,7 @@ parse_argv (int argc, char **argv)
 
         case 'E':
           add_output_format ("ps");
-          init_scheme_variables_global += "(backend . eps)\n";
+          init_scheme_variables_global += "(lilypond-book-output . #t)\n";
           break;
 
         case 'O':

lily/program-option.cc
@@ -22,10 +22,14 @@
 
 using std::string;
 
-string
+std::string
 get_output_backend_name ()
 {
-  return ly_symbol2string (ly_get_option (ly_symbol2scm ("backend")));
+  std::string name
+    = ly_symbol2string (ly_get_option (ly_symbol2scm ("backend")));
+  if (name == "eps")
+    name = "ps";
+  return name;
 }
 
 bool

ly/lilypond-book-preamble.ly
@@ -41,8 +41,6 @@
 
 #(set! output-empty-score-list #t)
 
-
-#(ly:set-option 'backend 'eps)
 #(ly:set-option (quote no-point-and-click))
 #(define inside-lilypond-book #t)
 #(define version-seen #t)

scm/framework-eps.scm
@@ -1,87 +1,0 @@
-;;;; This file is part of LilyPond, the GNU music typesetter.
-;;;;
-;;;; Copyright (C) 2004--2021 Han-Wen Nienhuys <hanwen@xs4all.nl>
-;;;;
-;;;; LilyPond is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation, either version 3 of the License, or
-;;;; (at your option) any later version.
-;;;;
-;;;; LilyPond is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
-
-(define-module (lily framework-eps))
-
-;;; this is still too big a mess.
-
-(use-modules (ice-9 regex)
-             (ice-9 string-fun)
-             (guile)
-             (lily framework-ps)
-             (lily paper-system)
-             (lily page)
-             (lily output-ps)
-             (srfi srfi-1)
-             (srfi srfi-13)
-             (lily))
-
-(define framework-eps-module
-  (current-module))
-
-
-(define (dump-stencils-as-EPSes stencils paper basename)
-  (define do-pdf
-    (member  "pdf" (ly:output-formats)))
-
-  (define create-aux-files
-    (ly:get-option 'aux-files))
-
-  (define (dump-counted-stencil stencil-count-pair)
-    "Return EPS filename."
-    (let* ((stencil (car stencil-count-pair))
-           (number (cdr stencil-count-pair))
-           (name (format #f "~a-~a" basename number)))
-      (dump-stencil-as-EPS paper stencil name
-                           (ly:get-option 'include-eps-fonts))
-      (string-append name ".eps")))
-
-  ;; main body
-  ;; First, create the output, then if necessary, individual staves and
-  ;; finally write some auxiliary files if desired
-  (dump-stencil-as-EPS paper (stack-stencils Y DOWN 2.0 stencils) basename #t)
-  (postprocess-output paper framework-eps-module (ly:tall-page-output-formats)
-                      basename
-                      (format #f "~a.eps" basename)
-                      #t)
-
-  ;; individual staves (*-1.eps etc.); only print if more than one stencil
-  ;; Otherwise the .eps and the -1.eps file will be identical and waste space
-  ;; Also always create if aux-files=##t
-  (if (or create-aux-files (< 1 (length stencils)))
-      (let* ((counted-systems  (count-list stencils))
-             (eps-files (map dump-counted-stencil counted-systems)))
-        (if do-pdf
-            (for-each (lambda (y) (postscript->pdf 0 0
-                                                   (dir-basename y ".eps")
-                                                   y #t))
-                      eps-files)))))
-
-(define-public (output-stencils basename stencils header paper) 
-  (dump-stencils-as-EPSes stencils
-                          paper
-                          basename))
-
-;; redefine to imports from framework-ps
-(define convert-to-pdf
-  convert-to-pdf)
-
-(define convert-to-ps
-  convert-to-ps)
-
-(define convert-to-png
-  convert-to-png)

scm/lily.scm
@@ -327,6 +327,9 @@ processed.")
     (job-count #f
                "Process in parallel, using the given number of
 jobs.")
+    (lilypond-book-output #f
+                          "Generate separate output files for
+individual pages/systems")
     (log-file #f
               "If string FOO is given as an argument, redirect
 output to log file `FOO.log'.")

commit 953b5cd38509c30f8dbd671e20366cb68d0f23d9

Reexport PDF metadata again
This functionality was broken in commit 498a5503 ("Introduce
-dlilypond-book-output.").

Catching this regression automatically needs significant rework of
output-distance (we'd have to convert the PS to PDF as well, extract the
metadata and diff that), so no test.

Fixes #6255

lily/paper-book.cc
@@ -418,7 +418,7 @@ Paper_book::output_stencils (SCM out_name, SCM stencils, SCM formats)
         }
 
       func = scm_variable_ref (func);
-      scm_apply_0 (func, scm_list_5 (out_name, stencils, SCM_BOOL_F,
+      scm_apply_0 (func, scm_list_5 (out_name, stencils, header_,
                                      paper ()->self_scm (), formats));
     }
 }