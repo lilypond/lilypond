lilypond/lilypond

commit 0ecab848b9fe1d71e483a7f49f0a3fb7166e0fa4

Simplify \version statement processing
There was duplication of functionality between ly:version? and the
Lilypond_version class.  Moving everything to Scheme deduplicates it
and obviates the clumsy manual parsing of the version string in C++.

scm/lily-library.scm
@@ -16,12 +16,12 @@
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
 
-;; for take, drop, take-while, list-index, and find-tail:
-(use-modules (srfi srfi-1))
-
-(use-modules (lily safe-utility-defs))
-
-(use-modules (ice-9 pretty-print))
+(use-modules
+ ;; for take, drop, take-while, list-index, and find-tail:
+ (srfi srfi-1)
+ (ice-9 pretty-print)
+ (ice-9 match)
+ (lily safe-utility-defs))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; constants.
@@ -917,6 +917,42 @@ it gets applied to @var{num}."
 version with a given version @var{ver}, which is passed as a list of
 numbers."
   (lexicographic-list-compare? op (ly:version) ver))
+
+(define (parse-lily-version str)
+  (match (string-split str #\.)
+   ((major minor patch my-patch)
+    (list (string->number major)
+          (string->number minor)
+          (string->number patch)
+          (string->symbol my-patch)))
+   ((major minor patch)
+    (list (string->number major)
+          (string->number minor)
+          (string->number patch)))
+   (else #f)))
+
+(define (lily-version-valid? str)
+  (let ((version (parse-lily-version str)))
+    (cond
+     ((or (not version)
+          ;; Are the components actually numbers?
+          (not (every identity version)))
+      (ly:non-fatal-error (_ "Invalid version string \"~a\"")
+                          str)
+      #f)
+     ((ly:version? < version)
+      (ly:non-fatal-error (_ "program too old: ~a (file requires: ~a)")
+                          (lilypond-version)
+                          str)
+      ;; Must consider invalid so that the lexer will emit the error
+      ;; token necessary to set a nonzero exit code.  The usage for
+      ;; the version returned is checking if it's too old (to suggest
+      ;; convert-ly), so not recording it if it's too new is no
+      ;; problem.
+      #f)
+     (else
+      #t))))
+
 
 ;;;;;;;;;;;;;;;;
 ;; broken spanner

lily/include/lily-imports.hh
@@ -84,6 +84,7 @@ extern Variable key_p;
 extern Variable key_list_p;
 extern Variable key_signature_interface_alteration_positions;
 extern Variable layout_extract_page_properties;
+extern Variable lily_version_valid_p;
 extern Variable lilypond_main;
 extern Variable line_markup;
 extern Variable f_location;

lily/include/lilypond-version.hh
@@ -1,45 +1,0 @@
-/*
-  This file is part of LilyPond, the GNU music typesetter.
-
-  Copyright (C) 1998--2022 Jan Nieuwenhuizen <janneke@gnu.org>
-
-  LilyPond is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  LilyPond is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef LILYPOND_VERSION_HH
-#define LILYPOND_VERSION_HH
-
-#include "std-string.hh"
-#include "compare.hh"
-
-struct Lilypond_version
-{
-  Lilypond_version (int major, int minor, int patch);
-  Lilypond_version (const std::string &str);
-
-  std::string to_string () const;
-  operator bool () const;
-  static int compare (const Lilypond_version &, const Lilypond_version &);
-
-  int major_;
-  int minor_;
-  int patch_;
-  std::string extra_patch_string_;
-};
-
-INSTANTIATE_COMPARE (const Lilypond_version &, Lilypond_version::compare);
-
-extern Lilypond_version oldest_version;
-
-#endif // LILYPOND_VERSION_HH

lily/lexer.ll
@@ -59,7 +59,6 @@
 #include "lily-guile.hh"
 #include "lily-lexer.hh"
 #include "lily-parser.hh"
-#include "lilypond-version.hh"
 #include "main.hh"
 #include "music.hh"
 #include "music-function.hh"
@@ -98,7 +97,6 @@ void strip_leading_white (string&);
 string lyric_fudge (string s);
 SCM lookup_markup_command (string s);
 SCM lookup_markup_list_command (string s);
-bool is_valid_version (string s);
 
 
 #define start_quote() do {                      \
@@ -253,7 +251,7 @@ FIG_ALT_EXPR	{WHITE}*{FIG_ALT_SYMB}({FIG_ALT_SYMB}|{WHITE})*
 		scm_module_define (top_scope, ly_symbol2scm ("version-seen"), SCM_BOOL_T);
 	}
 
-	if (!is_valid_version (s)) {
+	if (!from_scm<bool> (Lily::lily_version_valid_p (ly_string2scm (s)))) {
                 yylval = SCM_UNSPECIFIED;
 		return INVALID;
         }
@@ -1318,25 +1316,6 @@ strip_trailing_white (string&s)
 	s = s.substr (0, i + 1);
 }
 
-
-
-bool
-is_valid_version (string s)
-{
-  Lilypond_version current ( MAJOR_VERSION "." MINOR_VERSION "." PATCH_LEVEL );
-  Lilypond_version ver (s);
-  if (!ver)
-  {
-	  non_fatal_error (_f ("Invalid version string \"%s\"", s));
-	  return false;
-  }
-  if (ver > current)
-	{
-		non_fatal_error (_f ("program too old: %s (file requires: %s)",  current.to_string (), ver.to_string ()));
-		return false;
-	}
-  return true;
-}
 
 
 /*

lily/lily-imports.cc
@@ -75,6 +75,7 @@ Variable key_p ("key?");
 Variable key_list_p ("key-list?");
 Variable key_signature_interface_alteration_positions ("key-signature-interface::alteration-positions");
 Variable layout_extract_page_properties ("layout-extract-page-properties");
+Variable lily_version_valid_p ("lily-version-valid?");
 Variable lilypond_main ("lilypond-main");
 Variable line_markup ("line-markup");
 Variable f_location ("%location");

lily/lilypond-version.cc
@@ -1,98 +1,0 @@
-/*
-  This file is part of LilyPond, the GNU music typesetter.
-
-  Copyright (C) 1998--2022 Jan Nieuwenhuizen <janneke@gnu.org>
-
-  LilyPond is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  LilyPond is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include "lilypond-version.hh"
-#include "std-vector.hh"
-#include "misc.hh"
-
-#include <ctype.h>
-#include <string>
-
-using std::string;
-using std::vector;
-
-Lilypond_version::Lilypond_version (int major, int minor, int patch)
-{
-  major_ = major;
-  minor_ = minor;
-  patch_ = patch;
-}
-
-Lilypond_version::Lilypond_version (const string &str)
-{
-  major_ = -1;
-  minor_ = 0;
-  patch_ = 0;
-
-  vector<string> version;
-  const char *digits = "0123456789";
-  version = string_split (str, '.');
-
-  switch (version.size ())
-    {
-    case 4:
-      extra_patch_string_ = version[3];
-      if (version[2].empty ())
-        return;
-    // fallthrough
-    case 3:
-      if (version[2].find_first_not_of (digits) != string::npos
-          || version[1].empty ())
-        return;
-      patch_ = std::stoi (version[2]);
-    // fallthrough
-    case 2:
-      if (version[1].find_first_not_of (digits) != string::npos
-          || version[1].empty () || version[0].empty ())
-        return;
-      minor_ = std::stoi (version[1]);
-      if (version[0].find_first_not_of (digits) != string::npos)
-        return;
-      major_ = std::stoi (version[0]);
-    }
-}
-
-string
-Lilypond_version::to_string () const
-{
-  if (major_ < 0)
-    return "invalid";
-  return std::to_string (major_)
-         + "." + std::to_string (minor_)
-         + "." + std::to_string (patch_);
-}
-
-Lilypond_version::operator bool () const
-{
-  return !(major_ < 0);
-}
-
-int
-Lilypond_version::compare (const Lilypond_version &a, const Lilypond_version &b)
-{
-  if (!a || !b)
-    return 0;
-  if (a.major_ != b.major_)
-    return a.major_ > b.major_ ? 1 : -1;
-  if (a.minor_ != b.minor_)
-    return a.minor_ > b.minor_ ? 1 : -1;
-  if (a.patch_ != b.patch_)
-    return a.patch_ > b.patch_ ? 1 : -1;
-  return 0;
-}

lily/warn-scheme.cc
@@ -45,6 +45,22 @@ any).
   return SCM_UNSPECIFIED;
 }
 
+LY_DEFINE (ly_non_fatal_error, "ly:non-fatal-error",
+           1, 0, 1, (SCM str, SCM rest),
+           R"(
+A Scheme callable function to issue the error @var{str}.  The error is
+formatted with @code{format}; @var{rest} holds the formatting
+arguments (if any).  When using this function, some way of signalling
+the error should be employed in order for the compilation to
+eventually result in a nonzero return code.
+)")
+{
+  LY_ASSERT_TYPE (scm_is_string, str, 1);
+  str = scm_simple_format (SCM_BOOL_F, str, rest);
+  non_fatal_error (ly_scm2string (str));
+  return SCM_UNSPECIFIED;
+}
+
 LY_DEFINE (ly_programming_error, "ly:programming-error",
            1, 0, 1, (SCM str, SCM rest),
            R"(

commit 56f10679eeac753cf513d66737b159e756d0adf4

Accept incomplete version statements within stable releases
Commit 0ecab848b9 inadvertently changed behavior by rejecting
incomplete version statements like \version "1" or \version "2.22".
On the other hand, convert-ly has always rejected such versions.  This
brings consistency by accepting partial versions iff they have two
digits, and the second is even.  This is because it can make sense to
have \version "2.22", as syntax is supposed not to change within a
stable release, whereas \version "2.23" is not useful to convert-ly
because the syntax does change within an unstable release.

scm/lily-library.scm
@@ -933,17 +933,46 @@ numbers."
   (lexicographic-list-compare? op (ly:version) ver))
 
 (define (parse-lily-version str)
-  (match (string-split str #\.)
-   ((major minor patch my-patch)
-    (list (string->number major)
-          (string->number minor)
-          (string->number patch)
-          (string->symbol my-patch)))
-   ((major minor patch)
-    (list (string->number major)
-          (string->number minor)
-          (string->number patch)))
-   (else #f)))
+  (let* ((warned #f)
+         (output
+          (match (string-split str #\.)
+           ((major minor patch my-patch)
+            (list (string->number major)
+                  (string->number minor)
+                  (string->number patch)
+                  (string->symbol my-patch)))
+           ((major minor patch)
+            (list (string->number major)
+                  (string->number minor)
+                  (string->number patch)))
+           ((major minor)
+            ;; Accept \version "x.y" without third component, but only
+            ;; if y is even, i.e. it's a stable release series.  Within
+            ;; a development release series, syntax changes with point
+            ;; releases so convert-ly needs the specific one.
+            (let ((major-int (string->number major))
+                  (minor-int (string->number minor)))
+              (cond
+               ((not (and major-int minor-int))
+                #f)
+               ((odd? minor-int)
+                (ly:non-fatal-error (G_ "version with third number omitted is only allowed for stable releases \
+(when the second number is even)"))
+                (set! warned #t)
+                #f)
+               (else
+                (list major-int minor-int)))))
+           (else #f))))
+    (cond
+     (warned
+      #f)
+     ((or (not output)
+          ;; Are the number components actually numbers?
+          (not (every identity output)))
+      (ly:non-fatal-error (G_ "Invalid version string \"~a\"")
+                          str)
+      #f)
+     (else output))))
 
 (define (parse-and-check-version str)
   "Parse the given version string and check its validity.  If valid,
@@ -952,11 +981,7 @@ version is greater than the running version of LilyPond, a warning is
 emitted and the version is considered invalid."
   (let ((version (parse-lily-version str)))
     (cond
-     ((or (not version)
-          ;; Are the components actually numbers?
-          (not (every identity version)))
-      (ly:non-fatal-error (G_ "Invalid version string \"~a\"")
-                          str)
+     ((not version)
       #f)
      ((ly:version? < version)
       (ly:non-fatal-error (G_ "program too old: ~a (file requires: ~a)")
@@ -1001,13 +1026,15 @@ compatibility is preserved."
    ((major minor patch . _)
     (match
      file-version
-     ((file-major file-minor file-patch)
+     ((file-major file-minor . rest)
       (or (> major file-major)
           (and (eqv? major file-major)
                (or (> minor file-minor)
                    (and (eqv? minor file-minor)
                         (odd? minor)
-                        (> patch file-patch))))))))))
+                        (match rest
+                         (() #f)
+                         ((file-patch . _) (> patch file-patch))))))))))))
 
 (define-public (suggest-convert-ly-message version-seen)
   "Internally used when the file has an error, to suggest usage of

scripts/convert-ly.py
@@ -41,7 +41,7 @@ import lilylib as ly
 lilypond_version_re_str = '\\\\version *\"([0-9.]+)"'
 lilypond_version_re = re.compile(lilypond_version_re_str)
 
-lilypond_version_strict_re_str = '\\\\version *\"([0-9]+[.][0-9]+[.][0-9]+)"'
+lilypond_version_strict_re_str = '\\\\version *\"([0-9]+(?:[.]([0-9]+))([.][0-9]+)?)"'
 lilypond_version_strict_re = re.compile(lilypond_version_strict_re_str)
 
 help_summary = (
@@ -170,19 +170,6 @@ def str_to_tuple(s):
 def tup_to_str(t):
     return '.'.join(['%s' % x for x in t])
 
-def version_cmp(t1, t2):
-    for x in [0, 1, 2]:
-        if t1[x] - t2[x]:
-            return t1[x] - t2[x]
-    return 0
-
-
-def get_conversions(from_version, to_version):
-    def is_applicable(v, f=from_version, t=to_version):
-        return version_cmp(v[0], f) > 0 and version_cmp(v[0], t) <= 0
-    return list(filter(is_applicable, convertrules.conversions))
-
-
 def latest_version():
     return convertrules.conversions[-1][0]
 
@@ -198,7 +185,7 @@ def do_conversion(s, from_version, to_version):
 tuple (LAST,LASTCHANGED,STR,ERRORS), with the last applied conversion,
 the last conversion resulting in a change, the resulting
 string and the number of errors."""
-    conv_list = get_conversions(from_version, to_version)
+    conv_list = [conv for conv in convertrules.conversions if from_version < conv[0] <= to_version]
 
     ly.progress(_("Applying conversion: "), newline=False)
 
@@ -229,7 +216,10 @@ string and the number of errors."""
 
 def guess_lilypond_version(input):
     m = lilypond_version_strict_re.search(input)
-    if m:
+    # Accept a missing third component if the second component
+    # is even.  That works because we don't have conversion rules
+    # within stable releases, as the syntax doesn't change.
+    if m and (m.group(3) is not None or int(m.group(2))%2 == 0):
         return m.group(1)
     m = lilypond_version_re.search(input)
     if m:
@@ -301,9 +291,6 @@ def do_one_file(infile_name):
         to_version = global_options.to_version
     else:
         to_version = latest_version()
-
-    if len(from_version) != 3:
-        raise InvalidVersion(".".join([str(n) for n in from_version]))
 
     (last, last_change, result, errors) = \
         do_conversion(input, from_version, to_version)

input/regression/other/version-incomplete-bad.ly
@@ -1,0 +1,8 @@
+\header {
+  texidoc = "An incomplete version statement, with the third number omitted, is
+rejected if the second number is odd."
+}
+
+expect-error = ##t
+
+\version "2.23"

input/regression/other/version-incomplete-good.ly
@@ -1,0 +1,10 @@
+\header {
+  texidoc = "An incomplete version statement, with the third number omitted,
+is accepted if the second number is even.  This also tests the message
+suggesting @command{convert-ly} in such cases."
+}
+
+expect-error = ##t
+
+\version "2.22"
+\nonexistentFunction