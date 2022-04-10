4fb91834298745cae514a516ac4fbd067d742160
Werner Lemberg <wl@gnu.org>
2022-04-10 16:21:58 +0200

    IR: Circumvent texi2html 1.82 bug
    Reported by Harm in
    
      https://lists.gnu.org/archive/html/bug-lilypond/2022-04/msg00012.html

--- scm/document-functions.scm
+++ scm/document-functions.scm
@@ -33,13 +33,16 @@
 
 ;; If there is `::` in the function name, insert a breakpoint to avoid
 ;; overlong index entries that would otherwise stick out to the right
-;; in the two-column output of the PDF documentation.
+;; in the two-column output of the PDF documentation.  Alas, this
+;; triggers a bug in `texi2html` 1.82; we thus have to temporarily
+;; enclose `prettier-name` with `@code` until an upgrade to a recent
+;; `texi2any` version.
 (define (document-function name arguments doc-string is-macro)
   (let* ((cmd (if is-macro "defmac" "defun"))
          (str-name (symbol->string name))
          (prettier-name
           (regexp-substitute/global #f "::" str-name 'pre "::@/" 'post)))
-    (format #f "@~a ~a ~a\n~a\n@end ~a\n\n"
+    (format #f "@~a @code{~a} ~a\n~a\n@end ~a\n\n"
             cmd prettier-name arguments doc-string cmd)))
 
 ;; Map function names (as strings) to full documentation entries
