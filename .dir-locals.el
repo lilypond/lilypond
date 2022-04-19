1e495e1fdbff835d730a8246c7361d4d72c9f17e
Jean Abou Samra <jean@abou-samra.fr>
2022-04-19 08:04:47 +0200

    Prevent fixscm.sh from badly reformatting a few forms
    A match form should be formatted as
    
    (match subject
      (pattern1 expr1)
      (pattern2 expr2)
      ...)
    
    and not
    
    (match subject
           (pattern1 expr1)
           (pattern2 expr2)
           ...)
    
    Same goes for a few other forms.
    
    This is implemented via .dir-locals.el, so those developers who use
    Emacs also get the right behavior out-of-the-box.
    
    This has also been submitted as a patch to Emacs, which has been
    merged and will thus be included in the next release:
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=55033

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,27 @@
   (c-file-style . "gnu")
   (indent-tabs-mode . nil))
  (scheme-mode
-  (indent-tabs-mode . nil))
+  (indent-tabs-mode . nil)
+  (eval .
+        ;; Teach Emacs about a few Scheme forms that it would
+        ;; otherwise reformat badly.  This can be removed when
+        ;; the patch
+        ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=55033
+        ;; occurs in a released version that becomes reasonably
+        ;; current. --JeanAS
+        (progn
+          "This is code run by LilyPond's .dir-locals.el to fix indentation \
+according to our standards.  If Emacs is warning you that this 'may not be \
+safe', just accept running it."
+          (put 'match 'scheme-indent-function 1)
+          (put 'match-lambda 'scheme-indent-function 0)
+          (put 'match-lambda* 'scheme-indent-function 0)
+          (put 'match-let 'scheme-indent-function 'scheme-let-indent)
+          (put 'match-let* 'scheme-indent-function 1)
+          (put 'match-letrec 'scheme-indent-function 1)
+          (put 'and-let* 'scheme-indent-function 1)
+          (put 'with-syntax 'scheme-indent-function 1)
+          (put 'eval-when 'scheme-indent-function 1))))
  (shell-script-mode
   (indent-tabs-mode . nil))
  (texinfo-mode
--- scripts/auxiliar/fixscm.sh
+++ scripts/auxiliar/fixscm.sh
@@ -4,11 +4,19 @@
 # filenames in the command line), according to
 # LilyPond source style standards.
 
-elisp_expression='(progn
-  (delete-trailing-whitespace)
-  (indent-region (point-min) (point-max) nil)
-  (untabify (point-min) (point-max))
-  (save-buffer))'
+# This is needed in order to load our .dir-locals.el, which contains
+# configuration relevant for Scheme indentation.
+load_dir_locals='
+     (setq enable-local-variables :all)'
+
+reformat_file='(progn
+     (delete-trailing-whitespace)
+     (indent-region (point-min) (point-max) nil)
+     (untabify (point-min) (point-max))
+     (save-buffer))'
 for f in "$@"; do
-  emacs -batch "$f" --eval "${elisp_expression}"
+  # First --eval to load .dir-locals.el before file is loaded, so
+  # the options take effect, then second --eval after file is loaded,
+  # to process it.
+  emacs --batch --eval "${load_dir_locals}" "$f" --eval "${reformat_file}"
 done
