790071bc31d4b0267c958bb1d9877f695853ddee
Jean Abou Samra <jean@abou-samra.fr>
2022-07-13 01:26:12 +0200

    page-breaking-scheme.cc: don't segfault with bad argument types

--- lily/page-breaking-scheme.cc
+++ lily/page-breaking-scheme.cc
@@ -26,70 +26,76 @@
 #include "minimal-page-breaking.hh"
 
 LY_DEFINE (ly_page_turn_breaking, "ly:page-turn-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
-Optimally break (pages and lines) the @code{Paper_book} object @var{pb} such
-that page turns only happen in specified places, returning its pages.
+Optimally break (pages and lines) the @code{Paper_book} object @var{paper-book}
+such that page turns only happen in specified places, returning its pages.
            )")
 {
-  Page_turn_page_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  Page_turn_page_breaking b (pb);
   return b.solve ();
 }
 
 LY_DEFINE (ly_optimal_breaking, "ly:optimal-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
-Optimally break (pages and lines) the @code{Paper_book} object @var{pb} to
-minimize badness for both vertical and horizontal spacing.
+Optimally break (pages and lines) the @code{Paper_book} object @var{paper-book}
+to minimize badness for both vertical and horizontal spacing.
            )")
 {
-  Optimal_page_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  Optimal_page_breaking b (pb);
   return b.solve ();
 }
 
 LY_DEFINE (ly_minimal_breaking, "ly:minimal-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
-Break (pages and lines) the @code{Paper_book} object @var{pb} without looking
-for optimal spacing: stack as many lines on a page before moving to the next
-one.
+Break (pages and lines) the @code{Paper_book} object @var{paper-book} without
+looking for optimal spacing: stack as many lines on a page before moving to the
+next one.
            )")
 {
-  Minimal_page_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  Minimal_page_breaking b (pb);
   return b.solve ();
 }
 
 LY_DEFINE (ly_one_page_breaking, "ly:one-page-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
 Put each score on a single page.  The @code{paper-height} settings are modified
 so each score fits on one page, and the height of the page matches the height
 of the full score.
            )")
 {
-  One_page_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  One_page_breaking b (pb);
   return b.solve ();
 }
 
 LY_DEFINE (ly_one_line_breaking, "ly:one-line-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
 Put each score on a single line, and put each line on its own page.  Modify the
 @code{paper-width} setting so that every page is wider than the widest line.
            )")
 {
-  One_line_page_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  One_line_page_breaking b (pb);
   return b.solve ();
 }
 
 LY_DEFINE (ly_one_line_auto_height_breaking, "ly:one-line-auto-height-breaking",
-           1, 0, 0, (SCM pb),
+           1, 0, 0, (SCM paper_book),
            R"(
 Put each score on a single line, and put each line on its own page.  Modify the
 @code{paper-width} setting so that every page is wider than the widest line.
 Modify the @code{paper-height} setting to fit the height of the tallest line.
            )")
 {
-  One_line_auto_height_breaking b (unsmob<Paper_book> (pb));
+  auto *const pb = LY_ASSERT_SMOB (Paper_book, paper_book, 1);
+  One_line_auto_height_breaking b (pb);
   return b.solve ();
 }
