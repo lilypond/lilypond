lilypond/lilypond

commit f511d98c65e46aa30dbd27c0d93cde1d24b974f9

Fix page references to labels straddling at a page break
They were handled inconsistently when the book contained several
bookparts because the whole alist was reversed instead of just the
part for the local bookpart, leading to several reversals for the same
alist part.  Merge the two loops, addressing a TODO and incidentally
leading to nothing special needing to be done for ensuring a
consistent order.

Fixes #6301

lily/page-breaking.cc
@@ -133,6 +133,7 @@
 #include "item.hh"
 #include "lily-imports.hh"
 #include "line-interface.hh"
+#include "ly-scm-list.hh"
 #include "output-def.hh"
 #include "page-layout-problem.hh"
 #include "page-spacing.hh"
@@ -601,15 +602,18 @@ Page_breaking::make_pages (const vector<vsize> &lines_per_page, SCM systems)
   if (scm_is_null (systems))
     return SCM_EOL;
 
-  int first_page_number
-    = from_scm (book_->paper ()->c_variable ("first-page-number"), 1);
-  SCM ret = SCM_EOL;
-  bool reset_footnotes_on_new_page = from_scm<bool> (book_->top_paper ()->c_variable ("reset-footnotes-on-new-page"));
+  // Note that the order in which pages are iterated matters for
+  // building this table.  For a label straddling at a page break, we
+  // want to choose the last occurrence.
   SCM label_page_table = book_->top_paper ()->c_variable ("label-page-table");
   if (SCM_UNBNDP (label_page_table))
     label_page_table = SCM_EOL;
 
-  // Build a list of (systems configuration . footnote-count) triples.
+  int first_page_number
+    = from_scm (book_->paper ()->c_variable ("first-page-number"), 1);
+  SCM ret = SCM_EOL;
+  bool reset_footnotes_on_new_page = from_scm<bool> (book_->top_paper ()->c_variable ("reset-footnotes-on-new-page"));
+
   // Note that we lay out the staves and find the configurations,
   // but we do not draw anything in this function.  It is important
   // that all staves are laid out vertically before any are drawn; some
@@ -617,7 +621,6 @@ Page_breaking::make_pages (const vector<vsize> &lines_per_page, SCM systems)
   // themselves.  If this happens before the neighbouring staves have
   // been laid out, bad side-effects could happen (in particular,
   // Align_interface::align_to_ideal_distances might be called).
-  SCM systems_configs_fncounts = SCM_EOL;
   vsize footnote_count = 0;
   Real last_page_force = 0;
 
@@ -663,56 +666,35 @@ Page_breaking::make_pages (const vector<vsize> &lines_per_page, SCM systems)
       else
         last_page_force = layout.force ();
 
-      systems_configs_fncounts = scm_cons (scm_cons (lines, config), systems_configs_fncounts);
-      footnote_count += fn_lines;
-      systems = scm_list_tail (systems, line_count);
-    }
-
-  // TODO: previously, the following loop caused the systems to be
-  // drawn.  Now that we no longer draw anything in Page_breaking,
-  // it is safe to merge these two loops.  However, the merged loop
-  // should iterate over page numbers backwards (as in the second
-  // loop).  If page numbers are iterated forwards (as in the first
-  // loop), the order of pages is reversed.
-  int page_num = first_page_number + static_cast<int> (page_count) - 1;
-  for (SCM s = systems_configs_fncounts; scm_is_pair (s); s = scm_cdr (s))
-    {
-      SCM lines = scm_caar (s);
-      SCM config = scm_cdar (s);
-
-      bool bookpart_last_page = scm_is_eq (s, systems_configs_fncounts);
       SCM page = draw_page (lines, config, page_num, bookpart_last_page);
+
       /* collect labels */
       SCM page_num_scm = to_scm (page_num);
-      for (SCM l = lines; scm_is_pair (l); l = scm_cdr (l))
+      for (SCM line : as_ly_scm_list (lines))
         {
           SCM labels = SCM_EOL;
-          if (Grob *line = unsmob<Grob> (scm_car (l)))
+          if (System *sys = unsmob<System> (line))
             {
-              System *system = dynamic_cast<System *> (line);
-              labels = get_property (system, "labels");
+              labels = get_property (sys, "labels");
             }
-          else if (Prob *prob = unsmob<Prob> (scm_car (l)))
+          else if (Prob *prob = unsmob<Prob> (line))
             labels = get_property (prob, "labels");
 
           SCM table = SCM_EOL;
-          for (SCM lbls = labels; scm_is_pair (lbls); lbls = scm_cdr (lbls))
-            table = scm_cons (scm_cons (scm_car (lbls), page_num_scm),
-                              table);
+          for (SCM label : as_ly_scm_list (labels))
+            table = scm_acons (label, page_num_scm, table);
 
           label_page_table = scm_reverse_x (table, label_page_table);
         }
 
       ret = scm_cons (page, ret);
-      --page_num;
+
+      footnote_count += fn_lines;
+      systems = scm_list_tail (systems, line_count);
     }
 
-  // By reversing the table, we ensure that duplicated labels (e.g. those
-  // straddling a page turn) will appear in the table with their last
-  // occurrence first.
-  label_page_table = scm_reverse_x (label_page_table, SCM_EOL);
   book_->top_paper ()->set_variable (ly_symbol2scm ("label-page-table"), label_page_table);
-  return ret;
+  return scm_reverse_x (ret, SCM_EOL);
 }
 
 void

input/regression/label-straddling-page-break-bookparts.ly
@@ -1,0 +1,33 @@
+\version "2.23.11"
+
+\header {
+  texidoc = "When a label straddles at a page break, the chosen
+page is the second one.  This also works when there are several
+bookparts.
+
+Note: you need to compile this regtest on its own to check it, as
+the @command{lilypond-book} setup does not work for page references."
+}
+
+\markup \column {
+  \line { Should be 3: \page-ref #'labI "  " "??" }
+  \line { Should be 5: \page-ref #'labII "  " "??" }
+}
+
+\bookpart {
+  {
+    c'1
+    \pageBreak
+    \label labI
+    c'1
+  }
+}
+
+\bookpart {
+  {
+    c'1
+    \pageBreak
+    \label labII
+    c'1
+  }
+}

commit 17697940f2d8c5627a2cbd41dfeabccb7b4d4125

Re-fix order of PDF bookmarks on same page
This got accidentally broken in f511d98c65.

Also extend the test toc-multiple-entries-on-same-page.ly so that it
covers TOC entries from multiple systems drawn on the same page.

Fixes #6410

lily/page-breaking.cc
@@ -649,7 +649,9 @@ Page_breaking::make_pages (const vector<vsize> &lines_per_page, SCM systems)
 
       /* collect labels */
       SCM page_num_scm = to_scm (page_num);
-      for (SCM line : as_ly_scm_list (lines))
+      // Iterate over lines backwards so that labels come out in the
+      // same order as the lines.  This matters for PDF bookmarks.
+      for (SCM line : ly_scm_list (scm_reverse (lines)))
         {
           SCM labels = SCM_EOL;
           if (System *sys = unsmob<System> (line))
@@ -659,11 +661,10 @@ Page_breaking::make_pages (const vector<vsize> &lines_per_page, SCM systems)
           else if (Prob *prob = unsmob<Prob> (line))
             labels = get_property (prob, "labels");
 
-          SCM table = SCM_EOL;
+          // The list of labels is in reverse order.  By aconsing elements one
+          // by one onto label_page_table, we get them in forward order.
           for (SCM label : as_ly_scm_list (labels))
-            table = scm_acons (label, page_num_scm, table);
-
-          label_page_table = scm_reverse_x (table, label_page_table);
+            label_page_table = scm_acons (label, page_num_scm, label_page_table);
         }
 
       ret = scm_cons (page, ret);

input/regression/toc-multiple-entries-on-same-page.ly
@@ -15,12 +15,15 @@ documents.)"
       \tocItem \markup "1"
       \tocItem \markup "2"
       c'
+      \break
+      \tocItem \markup "3"
+      c'
     }
   }
   \score {
     {
-      \tocItem \markup "3"
       \tocItem \markup "4"
+      \tocItem \markup "5"
       c'
     }
   }