lilypond/lilypond

commit 61cd3bc1f3254b430bf04acd587c4082253602d4

Make articulation-type a symbol? instead of a string?
This was an ancient TODO (see note from 2003 in scm/define-music-properties.scm).
It does not seem to make much sense to rely on string comparison for
distinguishing a well-defined set of articulation types. Also, the
articulation-type is used as an alist index, for which symbols are much more
natural than strings.

This also makes it easier to (for instance) let users enter lists of articulation
types to use in custom code, since nowadays the parser turns

    types = tenuto, fermata, espressivo

into a symbol-list?.

This commit also starts an obsoletion cycle for the outdated articulation
definition style: As a legacy feature, it used to be possible to do

    dashDash = "tenuto"

instead of the modern variants

    dashDash = \tenuto
    dashDash = #(make-articulation 'tenuto)

which are more in line with current LilyPond syntax principles.

The legacy input still works but emits an obsoletion warning that may be
removed later (e.g. for 2.26).

Lastly, the code handling scripts is made more robust: Choosing non-existing
scripts now tends to emit warnings/programming errors instead of terminating.

scm/define-music-display-methods.scm
@@ -171,7 +171,7 @@ expression."
 (define-display-method ArticulationEvent (event) #t
   (let* ((articulation  (ly:music-property event 'articulation-type))
          (shorthand
-          (case (string->symbol articulation)
+          (case articulation
             ((marcato) "^")
             ((stopped) "+")
             ((tenuto)    "-")

Documentation/ca/notation/percussion.itely
@@ -433,8 +433,8 @@ vostra pròpia llista al principi del vostre fitxer.
          (bassdrum        default   #f           -1)
          (snare           default   #f           0)
          (hihat           cross     #f           1)
-         (halfopenhihat   cross     "halfopen"   1)
-         (pedalhihat      xcircle   "stopped"    2)
+         (halfopenhihat   cross     halfopen     1)
+         (pedalhihat      xcircle   stopped      2)
          (lowtom          diamond   #f           3)))
 up = \drummode { hh8 hh hhho hhho hhp4 hhp }
 down = \drummode { bd4 sn bd toml8 toml }
@@ -460,8 +460,8 @@ bloc alt) i wbl (@emph{woodblock-low} bloc baix.)
 % Aquestes línies defineixen la posició dels blocs de fusta al pentagrama;
 % si ho voleu, podeu canviar-lo o podeu usar caps de nota especials
 % per als blocs de fusta.
-#(define mydrums '((hiwoodblock default #t  3)
-                   (lowoodblock default #t -2)))
+#(define mydrums '((hiwoodblock default #f  3)
+                   (lowoodblock default #f -2)))
 
 woodstaff = {
   % Això defineix un pentagrama amb sols dues línies.
@@ -496,7 +496,7 @@ més informació sobre aquests temes delicats, consulteu
 Una pandereta, escrita mitjaç+ant @q{tamb} (@emph{tambourine}):
 
 @lilypond[verbatim,quote]
-#(define mydrums '((tambourine default #t 0)))
+#(define mydrums '((tambourine default #f 0)))
 
 \new DrumStaff \with { instrumentName = "Tambourine" }
 
@@ -517,7 +517,7 @@ Una pandereta, escrita mitjaç+ant @q{tamb} (@emph{tambourine}):
 Música per a gong, introduïda amb @q{tt} (tam-tam):
 
 @lilypond[quote,verbatim]
-#(define mydrums '((tamtam default #t 0)))
+#(define mydrums '((tamtam default #f 0)))
 
 \new DrumStaff \with { instrumentName = "Tamtam" }
 
@@ -534,8 +534,8 @@ Dues campanes, introduïdes amb @q{cb} (@emph{cowbell}, esquella) i
 @q{rb} (@emph{ridebell}, campana normal)
 
 @lilypond[quote,verbatim]
-#(define mydrums '((ridebell default #t  3)
-                   (cowbell  default #t -2)))
+#(define mydrums '((ridebell default #f  3)
+                   (cowbell  default #f -2)))
 
 \new DrumStaff \with { instrumentName = "Different Bells" }
 
@@ -554,9 +554,9 @@ Aquí hi ha un exemple breu procedent de la @q{Història del soldat} de
 Stravinsky:
 
 @lilypond[quote,verbatim]
-#(define mydrums '((bassdrum   default #t  4)
-                   (snare      default #t -4)
-                   (tambourine default #t  0)))
+#(define mydrums '((bassdrum   default #f  4)
+                   (snare      default #f -4)
+                   (tambourine default #f  0)))
 
 global = {
   \time 3/8 s4.

Documentation/de/extending/programming-interface.itely
@@ -1313,7 +1313,7 @@ wie @code{\displayMusic} benutzt wird, hilft hier weiter.
 
 @example
 F = #(let ((m (make-music 'ArticulationEvent
-                          'articulation-type "flageolet")))
+                          'articulation-type 'flageolet)))
        (set! (ly:music-property m 'tweaks)
              (acons 'font-size -3
                     (ly:music-property m 'tweaks)))

Documentation/de/extending/scheme-tutorial.itely
@@ -904,7 +904,7 @@ geschrieben hätte:
 
 In allen diesen Formen findet die Auswertung des Scheme-Codes statt,
 während die Eingabe noch gelesen wird, entweder im Lexer oder im Parser.
-Wenn man es später ausgeführt haben möchte, muss man 
+Wenn man es später ausgeführt haben möchte, muss man
 @ref{Leere Scheme-Funktionen} benutzen oder es in einem Makro speichern:
 
 @example
@@ -1411,8 +1411,7 @@ Ausgabe examiniert:
   'articulations
   (list (make-music
           'ArticulationEvent
-          'articulation-type
-          "accent"))
+          'articulation-type 'accent))
   'duration
   (ly:make-duration 2 0 1/1)
   'pitch
@@ -1443,7 +1442,7 @@ Um diese Funktion zu bauen, wird folgendermaßen begonnen:
   which is supposed to be a NoteEvent expression."
   (set! (ly:music-property note-event 'articulations)
         (cons (make-music 'ArticulationEvent
-                'articulation-type "accent")
+                'articulation-type 'accent)
               (ly:music-property note-event 'articulations)))
   note-event)
 @end example
@@ -1521,7 +1520,7 @@ der aus der Ausgabe von @code{\displayMusic} kopiert werden kann:
 
 @example
 (cons (make-music 'ArticulationEvent
-        'articulation-type "accent")
+        'articulation-type 'accent)
       (ly:music-property result-event-chord 'articulations))
 @end example
 
@@ -1547,7 +1546,7 @@ addAccent = #(define-music-function (note-event)
   which is supposed to be a NoteEvent expression."
   (set! (ly:music-property note-event 'articulations)
         (cons (make-music 'ArticulationEvent
-                'articulation-type "accent")
+                'articulation-type 'accent)
               (ly:music-property note-event 'articulations)))
   note-event)
 @end example

Documentation/de/notation/percussion.itely
@@ -396,8 +396,8 @@ eigene Liste der Positionen und Notenköpfe am Anfang ihrer Datei erstellen.
          (bassdrum        default   #f           -1)
          (snare           default   #f           0)
          (hihat           cross     #f           1)
-         (halfopenhihat   cross     "halfopen"   1)
-         (pedalhihat      xcircle   "stopped"    2)
+         (halfopenhihat   cross     halfopen     1)
+         (pedalhihat      xcircle   stopped      2)
          (lowtom          diamond   #f           3)))
 up = \drummode { hh8 hh hhho hhho hhp4 hhp }
 down = \drummode { bd4 sn bd toml8 toml }
@@ -420,8 +420,8 @@ Zwei Holzblöcke, notiert mit wbh (hoch) und wbl (tief)
 % These lines define the position of the woodblocks in the stave;
 % if you like, you can change it or you can use special note heads
 % for the woodblocks.
-#(define mydrums '((hiwoodblock default #t  3)
-                   (lowoodblock default #t -2)))
+#(define mydrums '((hiwoodblock default #f  3)
+                   (lowoodblock default #f -2)))
 
 woodstaff = {
   % This defines a staff with only two lines.
@@ -454,7 +454,7 @@ auch definiert werden.
 Tamburin, notiert mit @code{tamb}:
 
 @lilypond[quote,verbatim]
-#(define mydrums '((tambourine default #t 0)))
+#(define mydrums '((tambourine default #f 0)))
 
 tambustaff = {
   \override Staff.StaffSymbol.line-positions = #'( 0 )
@@ -480,7 +480,7 @@ tambustaff = {
 Noten für Tam-Tam (notiert mit @code{tt}):
 
 @lilypond[quote,verbatim]
-#(define mydrums '((tamtam default #t 0)))
+#(define mydrums '((tamtam default #f 0)))
 
 tamtamstaff = {
   \override Staff.StaffSymbol.line-positions = #'( 0 )
@@ -502,8 +502,8 @@ Zwei Glocken, notiert mit @code{cb} (Kuhglocke) und @code{rb}
 (Reiterglocke)
 
 @lilypond[quote,verbatim]
-#(define mydrums '((ridebell default #t  3)
-                   (cowbell  default #t -2)))
+#(define mydrums '((ridebell default #f  3)
+                   (cowbell  default #f -2)))
 
 bellstaff = {
   \override DrumStaff.StaffSymbol.line-positions = #'(-2 3)
@@ -522,12 +522,12 @@ bellstaff = {
 }
 @end lilypond
 
-Hier ein kurzes Beispiel von Stravinsky (aus @qq{L'histoire du Soldat}):
+Hier ein kurzes Beispiel von Stravinsky (aus @qq{L'Histoire du Soldat}):
 
 @lilypond[quote,verbatim]
-#(define mydrums '((bassdrum   default #t  4)
-                   (snare      default #t -4)
-                   (tambourine default #t  0)))
+#(define mydrums '((bassdrum   default #f  4)
+                   (snare      default #f -4)
+                   (tambourine default #f  0)))
 
 global = {
   \time 3/8 s4.

Documentation/en/extending/programming-interface.itely
@@ -1430,7 +1430,7 @@ especially how to use @code{\displayMusic} as a helping guide.
 
 @example
 F = #(let ((m (make-music 'ArticulationEvent
-                          'articulation-type "flageolet")))
+                          'articulation-type 'flageolet)))
        (set! (ly:music-property m 'tweaks)
              (acons 'font-size -3
                     (ly:music-property m 'tweaks)))

Documentation/en/extending/scheme-tutorial.itely
@@ -1404,8 +1404,7 @@ Scheme.  We begin by examining our input and desired output.
   'articulations
   (list (make-music
           'ArticulationEvent
-          'articulation-type
-          "accent"))
+          'articulation-type 'accent))
   'duration
   (ly:make-duration 2 0 1/1)
   'pitch
@@ -1425,7 +1424,7 @@ To build this function, we begin with
   which is supposed to be a NoteEvent expression."
   (set! (ly:music-property note-event 'articulations)
         (cons (make-music 'ArticulationEvent
-                'articulation-type "accent")
+                'articulation-type 'accent)
               (ly:music-property note-event 'articulations)))
   note-event)
 @end example
@@ -1498,7 +1497,7 @@ former @code{'articulations} property, with an extra item: the
 
 @example
 (cons (make-music 'ArticulationEvent
-        'articulation-type "accent")
+        'articulation-type 'accent)
       (ly:music-property result-event-chord 'articulations))
 @end example
 
@@ -1521,7 +1520,7 @@ addAccent = #(define-music-function (note-event) (ly:music?)
   which is supposed to be a NoteEvent expression."
   (set! (ly:music-property note-event 'articulations)
         (cons (make-music 'ArticulationEvent
-                'articulation-type "accent")
+                'articulation-type 'accent)
               (ly:music-property note-event 'articulations)))
   note-event)
 @end example

Documentation/en/notation/percussion.itely
@@ -497,8 +497,8 @@ be converted into a Scheme hash table, using the
          (bassdrum        default   #f           -1)
          (snare           default   #f           0)
          (hihat           cross     #f           1)
-         (halfopenhihat   cross     "halfopen"   1)
-         (pedalhihat      xcircle   "stopped"    2)
+         (halfopenhihat   cross     halfopen     1)
+         (pedalhihat      xcircle   stopped      2)
          (lowtom          diamond   #f           3)))
 
 up = \drummode { hh8 hh hhho hhho hhp4 hhp }
@@ -566,11 +566,11 @@ drumPitchNames.dsm        = #'dslapmute
 
 #(define djembe-style
   '((dbass      default  #f         -2)
-    (dbassmute  default  "stopped"  -2)
+    (dbassmute  default  stopped    -2)
     (dopen      default  #f          0)
-    (dopenmute  default  "stopped"   0)
+    (dopenmute  default  stopped     0)
     (dslap      default  #f          2)
-    (dslapmute  default  "stopped"   2)))
+    (dslapmute  default  stopped     2)))
 
 midiDrumPitches.dbass     = g
 midiDrumPitches.dbassmute = fis

commit 6e75c7ea05850683e7577b3a501532f9c5430de2

Fix MultiMeasureArticulationEvent display method
It has been broken since 61cd3bc1f3254b430bf04acd587c4082253602d4.
Use the ArticulationEvent method to simplify it.  Also do the same
simplification in MultiMeasureTextEvent.

scm/define-music-display-methods.scm
@@ -185,22 +185,8 @@ expression."
             shorthand
             (or shorthand articulation))))
 
-(define-display-method MultiMeasureArticulationEvent (event) #t
-  (let* ((articulation  (ly:music-property event 'articulation-type))
-         (shorthand
-          (case (string->symbol articulation)
-            ((marcato) "^")
-            ((stopped) "+")
-            ((tenuto)    "-")
-            ((staccatissimo) "!")
-            ((accent) ">")
-            ((staccato) ".")
-            ((portato) "_")
-            (else #f))))
-    (format #f "~a~:[\\~;~]~a"
-            (event-direction->lily-string event shorthand)
-            shorthand
-            (or shorthand articulation))))
+(define-display-method MultiMeasureArticulationEvent (event)
+  (music->lily-string (make-music 'ArticulationEvent event)))
 
 (define-post-event-display-method FingeringEvent (event) #t
   (ly:music-property event 'digit))
@@ -208,8 +194,8 @@ expression."
 (define-post-event-display-method TextScriptEvent (event) #t
   (markup->lily-string (ly:music-property event 'text)))
 
-(define-post-event-display-method MultiMeasureTextEvent (event) #t
-  (markup->lily-string (ly:music-property event 'text)))
+(define-display-method MultiMeasureTextEvent (event)
+  (music->lily-string (make-music 'TextScriptEvent event)))
 
 (define-post-event-display-method BendAfterEvent (event) #f
   (format #f "\\bendAfter #~a " (ly:music-property event 'delta-step)))

input/regression/other/display-lily-tests.ly
@@ -1,4 +1,4 @@
-\version "2.23.11"
+\version "2.23.14"
 #(use-modules (srfi srfi-13)
               (ice-9 format))
 
@@ -147,6 +147,7 @@ are reported on the stderr of this run."
 \test ##[ { c4-. c4^. c4_. } #]
 \test ##[ { c4-_ c4^_ c4__ } #]
 \test ##[ { c4\trill c4^\trill c4_\trill } #]
+\test ##[ { R1-! R1-- R1\trill R1^"a" R1^\markup \italic "foo" } #]
 \test ##[ { c4-1 c4^2 c4_3 } #]				% FingerEvent
 \test ##[ { c4-"foo" c4^"foo" c4_"foo" } #]		% TextScriptEvent
 \test ##[ { R1*4-"foo" R1*4^"foo" R1*4_"foo" } #]	% MultiMeasureTextEvent