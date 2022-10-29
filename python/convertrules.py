lilypond/lilypond

commit 03855a333fbfe7b2550039afae28aeead16d4c94

Restate convert-ly rule for \override syntax in 2.23.11
While \override and \revert syntax was introduced in 2.17.6, a warning was added
for the old syntax in 2.23.11 with aee38e53e39116b0cd30c3996f24ba3efb296c0e,
which for a number of people is the first time they realize some of their
overrides are using old syntax, and some copied things from snippets found on
the net or mailing list answers, which bypasses convert-ly.  Therefore, this
restates the convert-ly rule that was used in 2.17.6 for 2.23.11, so people who
run convert-ly won't ever see this warning.

python/convertrules.py
@@ -3616,9 +3616,11 @@ grob_path = symbol_list + r"(?:\s+" + symbol_list + r")*"
 grob_spec = wordsyntax + r"(?:\s*\.\s*" + wordsyntax + r")?"
 
 
-def path_replace(m):
-    return m.group(1) + ".".join(re.findall(wordsyntax, m.group(2)))
-
+def convert_overrides_to_dots(s):
+    def path_replace(m):
+        return m.group(1) + ".".join(re.findall(wordsyntax, m.group(2)))
+    return re.sub(r"(\\(?:override|revert)\s+)(" + grob_spec + r"\s+" + grob_path + ")",
+                  path_replace, s)
 
 # The following regexp appears to be unusually expensive to compile,
 # so we do it only once instead of for every file
@@ -3626,7 +3628,6 @@ footnotec = re.compile("(" + matchfullmarkup + ")|"
                        + r"(\\footnote(?:\s*"
                        + matchmarkup + ")?" + matcharg + ")(" + matcharg
                        + r")?(\s+" + matchmarkup + r")(\s+\\default)?")
-
 
 @rule((2, 17, 6), r"""\accidentalStyle #'Context "style" -> \accidentalStyle Context.style
 \alterBroken "Context.grob" -> \alterBroken Context.grob
@@ -3668,8 +3669,7 @@ def conv(s):
                  + matcharg + ")", r"\1\3\2", s)
     s = re.sub(r"(\\overrideProperty\s+)(" + grob_spec + r"\s+" + grob_path + ")",
                  path_replace, s)
-    s = re.sub(r"(\\(?:override|revert)\s+)(" + grob_spec + r"\s+" + grob_path + ")",
-                 path_replace, s)
+    s = convert_overrides_to_dots(s)
     return s
 
 
@@ -4630,6 +4630,9 @@ def conv(s):
         r'S\.\|:']  # S.|:
     s = re.sub(r'\\bar\s*"(' + '|'.join(changed_bar_types) + ')"',
                r'\\bar "\1-|"', s)
+    # New syntax was introduced in 2.17.6, but this version adds a warning,
+    # which is more insistent.
+    s = convert_overrides_to_dots(s)
     return s
 
 fine_iteration_warning = _(r"""

commit f768801e452777406ca536f98b7373de4be6f52f

convertrules.py: define path_replace on toplevel
Fixes #6446 (bug introduced in 03855a33)

python/convertrules.py
@@ -3615,10 +3615,10 @@ grob_path = symbol_list + r"(?:\s+" + symbol_list + r")*"
 
 grob_spec = wordsyntax + r"(?:\s*\.\s*" + wordsyntax + r")?"
 
+def path_replace(m):
+    return m.group(1) + ".".join(re.findall(wordsyntax, m.group(2)))
 
 def convert_overrides_to_dots(s):
-    def path_replace(m):
-        return m.group(1) + ".".join(re.findall(wordsyntax, m.group(2)))
     return re.sub(r"(\\(?:override|revert)\s+)(" + grob_spec + r"\s+" + grob_path + ")",
                   path_replace, s)
 