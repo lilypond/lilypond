d21fcee74a3c563d28f3ade3784120e3ded643f8
Werner Lemberg <wl@gnu.org>
2022-11-21 07:31:44 +0100

    Make generated web pages valid HTML
    From https://lists.w3.org/Archives/Public/www-validator/2014Oct/0015.html:
    
    > The token `</` cannot appear inside an element [...].
    >
    > Use `<\/a>` if you need to represent an HTML end tag in a JavaScript
    > string.

--- python/auxiliar/postprocess_html.py
+++ python/auxiliar/postprocess_html.py
@@ -185,7 +185,7 @@ def process_links(pages_dict, content, prefix, lang_ext, file_name, target):
 # About the @license comments, see
 # https://www.gnu.org/software/librejs/free-your-javascript.html
 syntax_highlighting_code = '''
-<script>
+<script type='text/javascript'>
     // @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
     (function () {
         let handled = false;
@@ -210,13 +210,13 @@ syntax_highlighting_code = '''
         document.body.classList.add('highlight');
         enabled = true;
         document.getElementById('highlighting-active-link').innerHTML =
-            "<a href='javascript:disable_highlighting()'>%(disable_text)s</a>";
+            "<a href='javascript:disable_highlighting()'>%(disable_text)s<\\/a>";
     }
     function disable_highlighting() {
         document.body.classList.remove('highlight');
         enabled = false;
         document.getElementById('highlighting-active-link').innerHTML =
-            "<a href='javascript:enable_highlighting()'>%(enable_text)s</a>";
+            "<a href='javascript:enable_highlighting()'>%(enable_text)s<\\/a>";
     }
 
     function save_preference() {
