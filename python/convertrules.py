bcad9c44bf7a017df3a86a67c36f1819b940c7ac
Jonas Hahnfeld <hahnjo@hahnjo.de>
2022-12-10 13:11:32 +0100

    Add convertrule for 2.24 stable release

--- python/convertrules.py
+++ python/convertrules.py
@@ -4745,6 +4745,10 @@ is the opposite of the convention used previously.
         stderr_write(UPDATE_MANUALLY)
     return s
 
+@rule((2, 24, 0), _("bump version for release"))
+def conv(s):
+    return s
+
 
 # Guidelines to write rules (please keep this at the end of this file)
 #
