lilypond/lilypond

commit e12ee4107c02a7a2550bfa860f6c279592aa3c58

Don't return SCM_UNDEFINED to Scheme layer
SCM_UNDEFINED is reserved as a C API value indicating no valid Scheme
value.  This fixes a few instances returning it by mistake.  It doesn't
give the GS API the same treatment as it is under active consideration
right now.

lily/parse-scm.cc
@@ -63,7 +63,7 @@ struct Parse_start
     if (scm_ilength (args) > 2)
       scm_display_error_message (scm_cadr (args), scm_caddr (args), scm_current_error_port ());
 
-    return SCM_UNDEFINED;
+    return SCM_UNSPECIFIED;
   }
 };
 

lily/include/callback.hh
@@ -123,7 +123,7 @@ public:
     LY_ASSERT_SMOB (T, target, 1);
 
     (t->*callback) (ev);
-    return SCM_UNDEFINED;
+    return SCM_UNSPECIFIED;
   }
 
   // The more complex trampolines are defined near their use cases.

lily/include/listener.hh
@@ -135,7 +135,7 @@ public:
     LY_ASSERT_SMOB (T, target, 1);
 
     (t->*callback) (ev);
-    return SCM_UNDEFINED;
+    return SCM_UNSPECIFIED;
   }
 
   template <class T, void (T::*callback) (SCM)>

lily/lily-modules.cc
@@ -60,7 +60,7 @@ call_trampoline (void *self)
   // One more indirection since void * can only be safely cast to
   // pointers to data rather than pointers to function.
   (*static_cast <void (* *) ()> (self)) ();
-  return SCM_UNDEFINED;
+  return SCM_UNSPECIFIED;
 }
 
 void

commit e76ddc705367cdaa03c0bb28371fceb0473fd730

Exit with non-zero return code if Scheme errors were found
This was broken in e12ee410.

Also amend tests that were relying on this using 'expect-error = ##t'
(this is not separate because expect-error complains if no error was
found while one was expected).

Fixes #6344

lily/parse-scm.cc
@@ -189,8 +189,7 @@ struct Parse_start
     if (!parse_protect_global)
       exit (1);
 
-    // This *unspecified* is unimportant.
-    return SCM_UNSPECIFIED;
+    return SCM_UNSPECIFIED; // unimportant
   }
 
   // The outer handler, which just specifies that any Scheme expression whose
@@ -200,8 +199,7 @@ struct Parse_start
   static SCM handle_error_after_unwinding (void * /*data*/, SCM /*tag*/,
                                            SCM /*args*/)
   {
-    // This *unspecified* is important, it's the value returned to LilyPond.
-    return SCM_UNSPECIFIED;
+    return SCM_UNDEFINED; // lexer.ll catches this to set the lexer's error_level_
   }
 };
 

input/regression/other/scheme-error-locations-include-files.ly
@@ -1,10 +1,12 @@
-\version "2.23.14"
+\version "2.25.4"
 
 \header {
   texidoc = "In @code{-dcompile-scheme-code} mode, source locations
 are printed for Scheme errors, even if the faulty Scheme code
 is in an include file."
 }
+
+expect-error = ##t
 
 #(ly:set-option 'compile-scheme-code)
 

input/regression/other/scheme-error-locations-included-string.ly
@@ -1,4 +1,4 @@
-\version "2.23.14"
+\version "2.25.4"
 
 \header {
   texidoc = "@code{-dcompile-scheme-code} also works with
@@ -8,6 +8,8 @@ two Scheme errors.  (However, providing error messages that
 point to the precise form that fails is not currently
 implemented for included strings of code.)"
 }
+
+expect-error = ##t
 
 #(ly:set-option 'compile-scheme-code)
 

input/regression/other/scheme-error-locations.ly
@@ -1,10 +1,12 @@
-\version "2.23.14"
+\version "2.25.4"
 
 \header {
   texidoc = "When using the @code{-dcompile-scheme-code} option,
 Scheme errors point at the precise form where the error occurs
 (this is limited by the precision of the information Guile provides)."
 }
+
+expect-error = ##t
 
 % Error will point to "#(begin" line
 #(begin