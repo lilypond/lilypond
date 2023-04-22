lilypond/lilypond

commit d29a682be9a05b2490559a66294a3bf2f4fefa35

Cairo: support PDF outlines
This mimics the existing implementation in framework-ps.scm.

The TOC infrastructure is extended to make each TOC entry contain the ID
of its parent, since that is how Cairo allows to define the nesting,
unlike PostScript's pdfmark, which expects each parent to announce its
number of children.

Just like PDF metadata, this works in Cairo ≥ 1.16.

Fixes #6501

lily/cairo.cc
@@ -32,6 +32,7 @@
 #include "lily-imports.hh"
 #include "lily-version.hh"
 #include "ly-module.hh"
+#include "ly-scm-list.hh"
 #include "main.hh"
 #include "modified-font-metric.hh"
 #include "open-type-font.hh"
@@ -351,6 +352,7 @@ public:
   void create_surface (Stencil const *);
   void finish_page ();
   void handle_metadata (SCM header);
+  void handle_outline (Output_def *paper);
   void close ();
 };
 
@@ -945,7 +947,7 @@ Cairo_outputter::eps_file (std::string const &content, std::vector<int> bbox,
      make a 1x1 image. However, the pattern does not necessarily
      have a 1:1 aspect ratio. Avoid scaling magic by creating the
      image exactly to bbox size.
-   */
+  */
   int height = bbox[3] - bbox[1];
   cairo_surface_t *image = cairo_image_surface_create (
     CAIRO_FORMAT_ARGB32, bbox[2] - bbox[0], height);
@@ -1327,6 +1329,108 @@ Cairo_outputter::handle_metadata (SCM module)
     }
 }
 
+void
+Cairo_outputter::handle_outline (Output_def *paper)
+{
+  if (format_ != PDF)
+    return;
+
+  if (scm_is_false (ly_get_option (ly_symbol2scm ("outline-bookmarks"))))
+    return;
+
+#if CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 16, 0)
+
+  SCM toc_alist = paper->lookup_variable (ly_symbol2scm ("label-alist-table"));
+  SCM page_numbers
+    = paper->lookup_variable (ly_symbol2scm ("label-page-table"));
+  for (SCM entry : as_ly_scm_list (page_numbers))
+    {
+      if (!scm_is_pair (entry))
+        {
+          programming_error ("non-pair in label-page-table");
+          return;
+        }
+    }
+
+  // This is OK because the entries are GC-protected through their presence in
+  // page_numbers.
+  std::vector<SCM> page_numbers_vec
+    = from_scm_list<std::vector<SCM>> (page_numbers);
+
+  // Entries for the same page are in the right order.  Entries for different
+  // pages are in reverse order.  By doing a stable sort, we reorder pages while
+  // keeping entries in the right order within the same page.
+  std::stable_sort (page_numbers_vec.begin (), page_numbers_vec.end (),
+                    [&] (SCM entry1, SCM entry2) {
+                      return from_scm<int> (scm_cdr (entry1))
+                             < from_scm<int> (scm_cdr (entry2));
+                    });
+
+  // If a label straddles at a page break, we want to choose the occurrence at
+  // the beginning of the new page.  label-page-table is in the right order for
+  // that, but it will contain two occurrences for the label in question, so
+  // make sure we skip the first one.
+  SCM label_to_chosen_index_table = scm_c_make_hash_table (0);
+  vsize i = 0;
+  for (SCM entry : page_numbers_vec)
+    {
+      SCM id_sym = scm_car (entry);
+      scm_hashv_set_x (label_to_chosen_index_table, id_sym, to_scm (i));
+      i++;
+    }
+  i = 0;
+
+  // Maps TOC IDs (symbols) to bookmark IDs (integers) returned by Cairo.
+  SCM bookmark_cairo_id_table = scm_c_make_hash_table (0);
+
+  for (SCM entry : page_numbers_vec)
+    {
+      SCM id_sym = scm_car (entry);
+
+      // Skip this occurrence if there is a later one.
+      vsize chosen_index = from_scm<vsize> (
+        scm_hashv_ref (label_to_chosen_index_table, id_sym, SCM_BOOL_F));
+
+      if (chosen_index == i)
+        {
+          int page_number = from_scm<int> (scm_cdr (entry));
+
+          SCM alist = ly_assoc_get (id_sym, toc_alist, SCM_EOL);
+          std::string toc_text = ly_scm2string (Lily::markup_to_string (
+            ly_assoc_get (ly_symbol2scm ("text"), alist, SCM_BOOL_F)));
+
+          SCM parent
+            = ly_assoc_get (ly_symbol2scm ("parent"), alist, SCM_BOOL_F);
+          int parent_cairo_id
+            = scm_is_false (parent)
+                ? CAIRO_PDF_OUTLINE_ROOT
+                : from_scm<int> (
+                  scm_hashq_ref (bookmark_cairo_id_table, parent, SCM_BOOL_F));
+
+          std::string attributes = String_convert::form_string (
+            "page=%d pos=[0.0 0.0]", page_number);
+
+          int new_item_id = cairo_pdf_surface_add_outline (
+            surface_->cairo_surface (), parent_cairo_id, toc_text.c_str (),
+            attributes.c_str (), static_cast<cairo_pdf_outline_flags_t> (0));
+
+          scm_hashq_set_x (bookmark_cairo_id_table, id_sym,
+                           to_scm (new_item_id));
+        }
+
+      i++;
+    }
+
+  scm_remember_upto_here (page_numbers);
+
+#else
+
+  (void) paper;
+  debug_output ("Skipping PDF outline, as Cairo is too old.");
+
+#endif // CAIRO_VERSION >= CAIRO_VERSION_ENCODE(1, 16, 0)
+}
+
 SCM
 Cairo_outputter::output (SCM expr)
 {
@@ -1476,6 +1580,7 @@ dump book through cairo backend
       Cairo_outputter outputter (format, base, odef, false);
       outputter.create_surface (unsmob<const Stencil> (scm_car (stencils)));
       outputter.handle_metadata (header);
+      outputter.handle_outline (odef);
 
       for (SCM p = stencils; scm_is_pair (p); p = scm_cdr (p))
         {

ly/toc-init.ly
@@ -55,6 +55,7 @@
                                 (ly:warning (_i "Invalid toc label: ~a")
                                             raw-path))
                                (list id))))
+                  (parent #f) ; set below
                   (level
                    ;; Find which existing TOC entry, if any, to attach this entry to.
                    ;; The principle is that the first element of path is interpreted specially:
@@ -71,26 +72,28 @@
                       (hashq-set! toc-name-id-hashtab single id)
                       0)
                      ((head . tail)
-                      (let* ((node-id (hashq-ref toc-name-id-hashtab head))
-                             (entry (and node-id (hashq-ref toc-hashtab node-id))))
+                      (let* ((root-id (hashq-ref toc-name-id-hashtab head)))
                         (let loop ((path path)
-                                   ;; entry corresponds to the entry for the first element
-                                   ;; in the path.  path still contains its name so a warning
-                                   ;; can be emitted if entry is #f.
-                                   (entry entry)
-                                   (level (and entry (1+ (assq-ref entry 'level)))))
-                          (if entry
-                              (let ((children (assq-ref entry 'children)))
+                                   ;; nested-id is the TOC ID of the first element in the path.  path still
+                                   ;; contains its name so a warning can be emitted if nested-id is #f.
+                                   (ancestor-id root-id)
+                                   ;; Start with the level of the root, which may be anywhere
+                                   ;; in the tree.
+                                   (level (let ((entry (hashq-ref toc-hashtab root-id)))
+                                            (and entry (1+ (assq-ref entry 'level))))))
+                          (if ancestor-id
+                              (let* ((entry (hashq-ref toc-hashtab ancestor-id))
+                                     (children (assq-ref entry 'children)))
                                 (match path
                                   ((head name)
                                    ;; The last component is a newly created node.
                                    (hashq-set! children name id)
                                    (hashq-set! toc-name-id-hashtab name id)
+                                   (set! parent ancestor-id)
                                    level)
                                   ((head . (and remaining (child . rest)))
                                    (loop remaining
-                                         (let ((child-id (hashq-ref children child)))
-                                           (and child-id (hashq-ref toc-hashtab child-id)))
+                                         (hashq-ref children child)
                                          (1+ level)))))
                               (begin
                                (ly:warning (G_ "TOC node ~a not defined")
@@ -103,7 +106,8 @@
                    `((text . ,text)
                      (toc-markup . ,markup-symbol)
                      (children . ,(make-hash-table))
-                     (level . ,level))))
+                     (level . ,level)
+                     (parent . ,parent))))
              ;; Register the new entry.
              (hashq-set! toc-hashtab id alist)
              (set! toc-alist (acons id alist toc-alist))

commit 434848d463f73c9f8fbcc7661762d771607e2afa

Cairo: Fix error when TOC items are present but no table of contents
This was caused by an oversight in d29a682be9 while replicating the
Scheme logic in C++.  ly:output-def-lookup falls back to '() aka
SCM_EOL, but Output_def::lookup_variable uses SCM_UNDEFINED instead.

Fixes #6583

lily/cairo.cc
@@ -1403,6 +1403,9 @@ Cairo_outputter::handle_outline (Output_def *paper)
     return;
 
   SCM toc_alist = paper->lookup_variable (ly_symbol2scm ("label-alist-table"));
+  if (SCM_UNBNDP (toc_alist))
+    toc_alist = SCM_EOL;
+
   SCM page_numbers
     = paper->lookup_variable (ly_symbol2scm ("label-page-table"));
   for (SCM entry : as_ly_scm_list (page_numbers))

input/regression/toc-items-no-toc.ly
@@ -1,0 +1,10 @@
+\version "2.25.4"
+
+\header {
+  texidoc = "There is no error when a file contains
+TOC items but no TOC."
+}
+
+% This used to error out in the Cairo backend.
+\tocItem \markup foo
+{ c' }