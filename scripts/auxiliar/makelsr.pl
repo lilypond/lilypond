lilypond/lilypond

commit be51451b4362eede0d97c24a4745aaea8385881f

makelsr.pl: Add option `-D` (`--diff-version-update`)
This passes `--diff-version-update` to `convert-ly`.

In the long run, this helps LSR updates become smaller because version
numbers are only updated if a new `convert-ly` rule affects a snippet.

scripts/auxiliar/makelsr.pl
@@ -74,6 +74,9 @@ Use the LSR database to regenerate LilyPond's 'snippets' directory.
                            database
                          if WHAT is 'no', don't use a dump file (this
                            implies '--no-lsr')
+  -D, --diff-version-update
+                         only update '\version' number of a snippet if
+                           it is modified
   -f, --no-snippet-list  don't update snippet list files
   -h, --help             display this help and exit
   -j, --jobs=N           use N jobs for creating snippet files in
@@ -144,6 +147,7 @@ EOT
 
 
 # Option handling.
+my $diff_version_update = 0;
 my $dump = "";
 my $help = 0;
 my $jobs = 1;
@@ -159,6 +163,7 @@ my $top_source = ".";
 my $verbose = 0;
 
 GetOptions(
+  "diff-version-update|D" => \$diff_version_update,
   "dump|d=s" => \$dump,
   "help|h" => \$help,
   "jobs|j=i" => \$jobs,
@@ -237,6 +242,8 @@ unless ($no_convert) {
   print "Using '$CONVERT_LY'\n" if $verbose;
 }
 
+my $convert_ly_options = $diff_version_update ? "--diff-version-update"
+                                              : "";
 
 # Check the actual snippet directories.
 my $snippet_dir = catdir("Documentation", "snippets");
@@ -936,7 +943,8 @@ sub convert_ly {
   my @err;
 
   eval {
-    run3 [$CONVERT_LY, "--loglevel=$loglevel", "-"],
+    run3 [$CONVERT_LY,
+          "--loglevel=$loglevel", "$convert_ly_options", "-"],
       \$in, \$out, \@err,
       {binmode_stdin => ":encoding(UTF-8)",
        binmode_stdout => ":encoding(UTF-8)"};

commit 9af92c35746c5d6de70ae5b3af5c696741f603cd

makelsr.pl: Correctly handle option `--new`
Don't pass `""` as an argument to `convert-ly`.

Bug introduced in be51451b4.

Fixes #6608.

scripts/auxiliar/makelsr.pl
@@ -242,9 +242,6 @@ unless ($no_convert) {
   print "Using '$CONVERT_LY'\n" if $verbose;
 }
 
-my $convert_ly_options = $diff_version_update ? "--diff-version-update"
-                                              : "";
-
 # Check the actual snippet directories.
 my $snippet_dir = catdir("Documentation", "snippets");
 my $new_snippet_dir = catdir($snippet_dir, "new");
@@ -941,11 +938,15 @@ sub convert_ly {
   my ($in) = @_;
   my $out;
   my @err;
+  my @args;
+
+  push @args, $CONVERT_LY;
+  push @args, "--loglevel=$loglevel";
+  push @args, "--diff-version-update" if $diff_version_update;
+  push @args, "-";
 
   eval {
-    run3 [$CONVERT_LY,
-          "--loglevel=$loglevel", "$convert_ly_options", "-"],
-      \$in, \$out, \@err,
+    run3 \@args, \$in, \$out, \@err,
       {binmode_stdin => ":encoding(UTF-8)",
        binmode_stdout => ":encoding(UTF-8)"};
   };