lilypond/lilypond

commit 85d50b72dfdd1f50ff9d2b0467a6ad258dc9cf40

musicxml2ly: Handle `print-object` attribute of `<clef>`
Also fix handling the `<sign>` element's value `none` (which is deprecated
in MusicXML 4.0): The old code produced the non-sensical `\clef "None"`,
causing a LilyPond warning.  While doing so, add some warning messages for
unsupported clefs.

python/musicexp.py
@@ -2369,29 +2369,50 @@ class ClefChange(Music):
         self.type = 'G'
         self.position = 2
         self.octave = 0
+        self.visible = True
 
     def octave_modifier(self):
         return {1: "^8", 2: "^15", -1: "_8", -2: "_15"}.get(self.octave, '')
 
-    def clef_name(self):
-        return {('G', 2): "treble",
-                ('G', 1): "french",
-                ('C', 1): "soprano",
-                ('C', 2): "mezzosoprano",
-                ('C', 3): "alto",
-                ('C', 4): "tenor",
-                ('C', 5): "baritone",
-                ('F', 3): "varbaritone",
-                ('F', 4): "bass",
-                ('F', 5): "subbass",
-                ("percussion", 2): "percussion",
-                # Workaround: MuseScore uses PERC instead of percussion
-                ("PERC", 2): "percussion",
-                ("TAB", 5): get_tab_clef()}.get((self.type, self.position),
-                                                None)
+    supported_clefs = ['G', 'F', 'C', 'percussion', 'TAB']
+
+    lily_clef_dict = {
+        ('G', 2): "treble",
+        ('G', 1): "french",
+        ('C', 1): "soprano",
+        ('C', 2): "mezzosoprano",
+        ('C', 3): "alto",
+        ('C', 4): "tenor",
+        ('C', 5): "baritone",
+        ('F', 3): "varbaritone",
+        ('F', 4): "bass",
+        ('F', 5): "subbass",
+        ("percussion", 2): "percussion",
+        # Old MuseScore versions used `PERC` instead of `percussion`.
+        ("PERC", 2): "percussion",
+        ("TAB", 5): "tab",
+    }
 
     def ly_expression(self):
-        return r'\clef "%s%s"' % (self.clef_name(), self.octave_modifier())
+        if self.type in self.supported_clefs:
+            clef_name = self.lily_clef_dict.get((self.type, self.position),
+                                                None)
+            if clef_name == 'tab':
+                clef_name = get_tab_clef()
+            elif not clef_name:
+                ly.warning(_("Non-standard clef positions are not supported "
+                             "yet, using 'treble' instead"))
+                clef_name = 'treble'
+        else:
+            if self.type == 'none':
+                # Deprecated in MusicXML version 4.0.
+                clef_name = 'treble'
+            else:
+                ly.warning(_("Unsupported clef '%s', using 'treble' instead")
+                           % clef_name)
+                clef_name = 'treble'
+
+        return r'\clef "%s%s"' % (clef_name, self.octave_modifier())
 
     clef_dict = {
         "G": ("clefs.G", -2, -6),

python/musicxml.py
@@ -548,9 +548,10 @@ class Attributes(Measure_element):
                 _("Unable to interpret time signature! Falling back to 4/4."))
             return (4, 4)
 
-    # returns clef information in the form("cleftype", position, octave-shift)
+    # Return clef information in the form `("cleftype", position,
+    # octave-shift, print-object)`.
     def get_clef_information(self):
-        clefinfo = ['G', 2, 0]
+        clefinfo = ['G', 2, 0, True]
         mxl = self.get_named_attribute('clef')
         if not mxl:
             return clefinfo
@@ -563,6 +564,7 @@ class Attributes(Measure_element):
         octave = mxl.get_maybe_exist_named_child('clef-octave-change')
         if octave:
             clefinfo[2] = int(octave.get_text())
+        clefinfo[3] = (getattr(mxl, 'print-object', 'yes') == 'yes')
         return clefinfo
 
     def get_key_signature(self):

scripts/musicxml2ly.py
@@ -906,7 +906,7 @@ def group_tuplets(music_list, events):
 
 def musicxml_clef_to_lily(attributes):
     change = musicexp.ClefChange()
-    (change.type, change.position, change.octave) = \
+    (change.type, change.position, change.octave, change.visible) = \
         attributes.get_clef_information()
     return change
 
@@ -2526,6 +2526,8 @@ def musicxml_voice_to_lily_voice(voice):
     return_value = VoiceData()
     return_value.voicedata = voice
 
+    clef_visible = True
+
     # Track pitch alterations for cautionary accidentals without parentheses
     # (to be realized with LilyPond's `!` pitch modifier) that are not
     # represented with `<accidental cautionary="yes" parentheses="no">`.
@@ -2723,8 +2725,27 @@ def musicxml_voice_to_lily_voice(voice):
                 if isinstance(a, musicexp.KeySignatureChange):
                     alterations = a.get_alterations()
                     current_alterations = alterations
+
                 elif isinstance(a, musicexp.MeasureStyleEvent):
                     multibar_count = a.multiple_rest_length
+
+                elif isinstance(a, musicexp.ClefChange):
+                    clef_visible_new = a.visible
+                    if a.type == 'none':
+                        clef_visible_new = False
+
+                    if clef_visible and not clef_visible_new:
+                        voice_builder.add_command(
+                            musicexp.OmitEvent('Staff.Clef'))
+                    elif not clef_visible and clef_visible_new:
+                        voice_builder.add_command(
+                            musicexp.OmitEvent('Staff.Clef', undo=True))
+                        voice_builder.add_command(
+                            musicexp.SetEvent('Staff.forceClef', '##t',
+                                              once=True))
+
+                    clef_visible = clef_visible_new
+
                 voice_builder.add_command(a)
             measure_length = measure_length_from_attributes(
                 n, current_measure_length)

commit d04ae0e77dcb509daf690a66b1d9276cf4ec70fb

musicxmp.ly: Fix typo
Introduced in commit 85d50b72dfd.

python/musicexp.py
@@ -2506,7 +2506,7 @@ class ClefChange(Music):
                 clef_name = 'treble'
             else:
                 ly.warning(_("Unsupported clef '%s', using 'treble' instead")
-                           % clef_name)
+                           % self.type)
                 clef_name = 'treble'
 
         return r'\clef "%s%s"' % (clef_name, self.octave_modifier())