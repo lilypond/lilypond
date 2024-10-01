lilypond/lilypond

commit 23a3778f2ed6a12b2e41026d3e6646f7177f0a29

Rewrite handling of tied tab note heads
If a tab note head is right bound of a tie (or has a repeat tie)
and a spanner starts, always print it parenthesized.
If a Tie is split, print the tied tab note head due to the
settings of tied-properties.
Same for a tied tab note holding a `RepeatTie`.

This was already intended by the first implementation
by 3417b5edd0596f128bc5faca6c600c3b11915709
Though, using break-visibility for a note head made no sense.

dcafb42fa6c1cf234a8916ec221a2b006d3582b9 deleted break-visibility,
but introduced a regression.

This patch rewrites most of the functionality:
- extend the engraver, port it to scheme
- introduce a new context-property tabFullNotation
- introduce new sub-properties ties/repeat-tied, for
  TabNoteHead.details.tied-properties.
  Delete TabNoteHead.details.repeat-tied-properties.
- Move functionality from (Repeat)Tie.after-line-breaking to
  TabNoteHead.after-line-breaking.
- Extend tab-note-head interface
- Adjust hide/showSplitTiedTabNotes

Closes #6733

scm/tablature.scm
@@ -138,68 +138,6 @@ This markup command is used to implement @code{\\clef moderntab} within a
         -0.75
         0.75)))
 
-;; FIXME: some of the below routines have side effects that sound
-;; like they could cause dependency on callback order. --JeanAS
-
-;; the handler for ties in tablature; according to TabNoteHead #'details,
-;; the 'tied to' note is handled differently after a line break
-(define-public (tie::handle-tab-note-head grob)
-  (let* ((original (ly:grob-original grob))
-         (tied-tab-note-head (ly:spanner-bound grob RIGHT))
-         (spanner-start (ly:grob-property tied-tab-note-head 'span-start #f))
-         (siblings (if (ly:grob? original)
-                       (ly:spanner-broken-into original) '())))
-
-    (if spanner-start
-        ;; tab note head is right bound of a tie and left of spanner,
-        ;; -> parenthesize it at all events
-        (begin
-          (ly:grob-set-property! tied-tab-note-head 'display-cautionary #t)
-          (ly:grob-set-property! tied-tab-note-head 'stencil tab-note-head::print))
-        ;; otherwise, check whether tie is split:
-        (if (and (>= (length siblings) 2)
-                 (eq? (car (last-pair siblings)) grob))
-            ;; tie is split -> get TabNoteHead #'details
-            (let* ((details (ly:grob-property tied-tab-note-head 'details))
-                   (tied-properties (assoc-get 'tied-properties details '()))
-                   (tab-note-head-parenthesized (assoc-get 'parenthesize tied-properties #t)))
-
-              (if tab-note-head-parenthesized
-                  (begin
-                    (ly:grob-set-property! tied-tab-note-head 'display-cautionary #t)
-                    (ly:grob-set-property! tied-tab-note-head 'stencil tab-note-head::print))))
-
-            ;; tie is not split
-            (ly:grob-set-property! tied-tab-note-head 'transparent #t)))))
-
-
-
-;; repeat ties occur within alternatives in a repeat construct;
-;; TabNoteHead #'details handles the appearance in this case
-(define-public (repeat-tie::handle-tab-note-head grob)
-  (let* ((tied-tab-note-head (ly:grob-object grob 'note-head))
-         (spanner-start (ly:grob-property tied-tab-note-head 'span-start #f)))
-    (if spanner-start
-        ;; tab note head is between a tie and a slur/glissando
-        ;; -> parenthesize it at all events
-        (begin
-          (ly:grob-set-property! tied-tab-note-head 'display-cautionary #t)
-          (ly:grob-set-property! tied-tab-note-head 'stencil tab-note-head::print))
-        ;; otherwise check 'details
-        (let* ((details (ly:grob-property tied-tab-note-head 'details))
-               (repeat-tied-properties (assoc-get 'repeat-tied-properties details '()))
-               (tab-note-head-visible (assoc-get 'note-head-visible repeat-tied-properties #t))
-               (tab-note-head-parenthesized (assoc-get 'parenthesize repeat-tied-properties #t)))
-
-          (if tab-note-head-visible
-              ;; tab note head is visible
-              (if tab-note-head-parenthesized
-                  (begin
-                    (ly:grob-set-property! tied-tab-note-head 'display-cautionary #t)
-                    (ly:grob-set-property! tied-tab-note-head 'stencil tab-note-head::print)))
-              ;; tab note head is invisible
-              (ly:grob-set-property! tied-tab-note-head 'transparent #t))))))
-
 ;; The slurs should not be too far apart from the corresponding fret number, so
 ;; we move the slur towards the TabNoteHeads.
 (define-public slur::move-closer-to-tab-note-heads
@@ -243,7 +181,48 @@ This markup command is used to implement @code{\\clef moderntab} within a
   (ly:grob-set-property! grob 'text (make-vcenter-markup fret))
   (tab-note-head::print grob))
 
+
+(define-public (tab-note-head::handle-ties grob)
+  "Handle tab note heads ending a @code{Tie}, deal with @code{RepeatTie}.
+
+If a @code{Slur} or @code{Glissando} starts at a tie-ending @code{TabNoteHead}
+always print the @code{TabNoteHead} parenthesized.
+
+If a tie-ending @code{TabNoteHead} occurs at the beginning of a line print it
+parenthesized unless sub-properties @code{note-head-visible} and
+@code{parenthesize} of @code{details.tied-properties} are set @code{#f}, which
+can be done manually or using @code{hideSplitTiedTabNotes}.
+
+A @code{TabNoteHead} with @code{\\repeatTie} is printed parenthesized as well,
+the same holds if @code{\\repeatTie} is applied to a chord. This is useful for
+seconda volta blocks. This behaviour can be switched off with
+@code{hideSplitTiedTabNotes}."
+;; TODO move behaviour for repeatTie and 'span-start to the stencil-procedure?
+  (let* (;; get PaperColumn
+         (pc (ly:item-get-column grob))
+         ;; get NonMusicalPaperColumn
+         (nmpc (ly:grob-object pc 'left-neighbor))
+         (at-line-begin? (positive? (ly:item-break-dir nmpc)))
+         (span-start? (ly:grob-property grob 'span-start #f))
+         (details (ly:grob-property grob 'details '()))
+         (tied-props (assoc-get 'tied-properties details '()))
+         (tied? (assoc-get 'tied tied-props #f))
+         (repeat-tied? (assoc-get 'repeat-tied tied-props #f))
+         (tied-visible? (assoc-get 'note-head-visible tied-props #t))
+         (tied-parenthesized? (assoc-get 'parenthesize tied-props #t)))
+
+    (cond ((or span-start?
+               (and repeat-tied? tied-visible? tied-parenthesized?)
+               (and tied? at-line-begin? tied-visible? tied-parenthesized?))
+             (ly:grob-set-property! grob 'display-cautionary #t)
+             ;; The stencil procedure needs to be run again, otherwise
+             ;; the 'display-cautionary would have no effect.
+             (ly:grob-set-property! grob 'stencil tab-note-head::print))
+          ((or tied? repeat-tied?)
+            (ly:grob-set-property! grob 'transparent #t)))))
+
 (define-public (tab-note-head::print grob)
+  "Print a tab note head."
   (define (is-harmonic? grob)
     (let ((arts (ly:event-property (event-cause grob) 'articulations)))
       (or (pair? (filter (lambda (a)
@@ -284,6 +263,7 @@ This markup command is used to implement @code{\\clef moderntab} within a
                                            cautionary-width
                                            cautionary-angularity
                                            cautionary-padding)))
+
     (ly:stencil-translate-axis
      (ly:stencil-aligned-to output-grob X CENTER)
      column-offset

input/regression/tablature-tie-spanner.ly
@@ -1,4 +1,4 @@
-\version "2.15.19"
+\version "2.25.19"
 
 \header {
   texidoc = "If a @code{Slur} or a @code{Glissando} follows a @code{Tie}, the

lily/tab-tie-follow-engraver.cc
@@ -1,139 +1,0 @@
-/*
-  This file is part of LilyPond, the GNU music typesetter.
-
-  Copyright (C) 2010--2023 Carl D. Sorensen
-
-  LilyPond is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  LilyPond is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include "engraver.hh"
-
-#include "context.hh"
-#include "item.hh"
-#include "spanner.hh"
-
-#include "translator.icc"
-
-#include <cctype>
-#include <cstdio>
-
-/*
-   Change tab-note-head properties when a tie is followed by a
-   slurs or glissando.
-*/
-class Tab_tie_follow_engraver final : public Engraver
-{
-  std::vector<Spanner *> slurs_;
-  std::vector<Spanner *> glissandi_;
-  std::vector<Item *> note_heads_;
-
-public:
-  TRANSLATOR_DECLARATIONS (Tab_tie_follow_engraver);
-
-protected:
-  void acknowledge_glissando (Grob_info_t<Spanner>);
-  void acknowledge_slur (Grob_info_t<Spanner>);
-  void acknowledge_tab_note_head (Grob_info_t<Item>);
-
-  void stop_translation_timestep ();
-};
-
-Tab_tie_follow_engraver::Tab_tie_follow_engraver (Context *c)
-  : Engraver (c)
-{
-}
-
-void
-Tab_tie_follow_engraver::acknowledge_glissando (Grob_info_t<Spanner> info)
-{
-  glissandi_.push_back (info.grob ());
-}
-
-void
-Tab_tie_follow_engraver::acknowledge_tab_note_head (Grob_info_t<Item> info)
-{
-  note_heads_.push_back (info.grob ());
-}
-
-void
-Tab_tie_follow_engraver::acknowledge_slur (Grob_info_t<Spanner> info)
-{
-  slurs_.push_back (info.grob ());
-}
-
-void
-Tab_tie_follow_engraver::stop_translation_timestep ()
-{
-  for (vsize k = 0; k < note_heads_.size (); k++)
-    {
-      bool spanner_start = false;
-      for (vsize j = 0; j < slurs_.size (); j++)
-        {
-          Item *left_item = slurs_[j]->get_bound (LEFT);
-          if (left_item)
-            {
-              SCM left_cause = get_property (left_item, "cause");
-              Item *slur_cause = unsmob<Item> (left_cause);
-              if (slur_cause == note_heads_[k])
-                {
-                  set_property (note_heads_[k], "span-start", SCM_BOOL_T);
-                  spanner_start = true;
-                  break;
-                }
-            }
-        }
-      if (!spanner_start)
-        for (vsize j = 0; j < glissandi_.size (); j++)
-          {
-            Item *left_bound = glissandi_[j]->get_bound (LEFT);
-            if (left_bound == note_heads_[k])
-              {
-                set_property (note_heads_[k], "span-start", SCM_BOOL_T);
-                break;
-              }
-          }
-    }
-  slurs_.clear ();
-  glissandi_.clear ();
-  note_heads_.clear ();
-}
-
-void
-Tab_tie_follow_engraver::boot ()
-{
-  ADD_ACKNOWLEDGER (slur);
-  ADD_ACKNOWLEDGER (glissando);
-  ADD_ACKNOWLEDGER (tab_note_head);
-}
-
-ADD_TRANSLATOR (Tab_tie_follow_engraver,
-                /* doc */
-                R"(
-Adjust TabNoteHead properties when a tie is followed by a slur or glissando.
-                )",
-
-                /* create */
-                R"(
-
-                )",
-
-                /* read */
-                R"(
-
-                )",
-
-                /* write */
-                R"(
-
-                )");

ly/engraver-init.ly
@@ -1253,9 +1253,6 @@ contexts and handles the line spacing, the tablature clef etc. properly."
   \override LaissezVibrerTie.stencil = ##f
   \override Slur.control-points = #slur::move-closer-to-tab-note-heads
   \override PhrasingSlur.stencil = ##f
-  %% 'tied to' fret numbers become invisible or parenthesized, respectively)
-  \override Tie.after-line-breaking = #tie::handle-tab-note-head
-  \override RepeatTie.after-line-breaking = #repeat-tie::handle-tab-note-head
   %% ... and all kinds of markups, spanners etc.
   \override TupletBracket.stencil = ##f
   \override TupletNumber.stencil = ##f

ly/property-init.ly
@@ -684,6 +684,8 @@ arabicStringNumbers = {
 
 % switch to full notation
 tabFullNotation = {
+  % signal we are using `tabFullNotation`
+  \set TabStaff.tabFullNotation = ##t
   % time signature
   \revert TabStaff.TimeSignature.stencil
   % stems (the half note gets a double stem)
@@ -702,9 +704,7 @@ tabFullNotation = {
   \revert TabStaff.StemTremolo.stencil
   \revert TabStaff.Dots.stencil
   \revert TabStaff.Tie.stencil
-  \revert TabStaff.Tie.after-line-breaking
   \revert TabStaff.RepeatTie.stencil
-  \revert TabStaff.RepeatTie.after-line-breaking
   \revert TabStaff.LaissezVibrerTie.stencil
   \revert TabStaff.Slur.control-points
   \revert TabStaff.PhrasingSlur.stencil
@@ -733,17 +733,13 @@ tabFullNotation = {
 
 %tie/repeat tie behavior
 hideSplitTiedTabNotes = {
-  \override TabVoice.TabNoteHead.details.tied-properties.break-visibility = #all-invisible
+  \override TabVoice.TabNoteHead.details.tied-properties.note-head-visible = ##f
   \override TabVoice.TabNoteHead.details.tied-properties.parenthesize = ##f
-  \override TabVoice.TabNoteHead.details.repeat-tied-properties.note-head-visible = ##f
-  \override TabVoice.TabNoteHead.details.repeat-tied-properties.parenthesize = ##f
 }
 
 showSplitTiedTabNotes = {
-  \override TabVoice.TabNoteHead.details.tied-properties.break-visibility = #begin-of-line-visible
+  \override TabVoice.TabNoteHead.details.tied-properties.note-head-visible = ##t
   \override TabVoice.TabNoteHead.details.tied-properties.parenthesize = ##t
-  \override TabVoice.TabNoteHead.details.repeat-tied-properties.note-head-visible = ##t
-  \override TabVoice.TabNoteHead.details.repeat-tied-properties.parenthesize = ##t
 }
 
 %% text length

scm/define-context-properties.scm
@@ -773,6 +773,7 @@ of the system/@/staff?  Set to @code{SystemStartBrace},
 the nesting of a start delimiters.")
 
 
+     (tabFullNotation ,boolean? "Flag whether @code{\\tabFullNotation} is used")
      (tablatureFormat ,procedure? "A function formatting a tablature
 note head.  Called with three arguments: context, string number and,
 fret number.  It returns the text as a markup.")

scm/define-grob-interfaces.scm
@@ -666,24 +666,18 @@ The maximum horizontal extent of a parenthesis.
 Move all tablature numbers horizontally.  The value is given as a multiple of a
 single-digit number width.
 
-@item repeat-tied-properties
-An alist with the following elements.
-
-@table @code
-@item note-head-visible
-If set to @code{#t}, show a number for a note with @code{\\repeatTie}.
-@item parenthesize
-If set to @code{#t}, parenthesize the number for a note with @code{\\repeatTie}.
-@end table
-
 @item tied-properties
 An alist with the following elements.
 
 @table @code
-@item break-visibility
-A vector of 3@tie{}booleans, @code{#(@var{end-of-line} @var{unbroken}
-@var{begin-of-line})}, to control the visibility of ties over a line break.
-@code{#t} means visible, @code{#f} means killed.
+@item tied
+Keep track whether a @code{Tie} ends at this note.
+@item repeat-tied
+Does this note hold a @code{RepeatTie}? @code{'repeat-tied} is also set if the
+note is part of a chord with set @code{RepeatTie}.
+@item note-head-visible
+If set to @code{#t}, show a number for a note with @code{\\repeatTie} or a
+note ending a @code{Tie}.
 @item parenthesize
 If set to @code{#t}, parenthesize the number for a tied note after a line break.
 @end table

scm/define-grobs.scm
@@ -3504,6 +3504,7 @@ start delimiter.")))))
 
     (TabNoteHead
      . (
+        (after-line-breaking . ,tab-note-head::handle-ties)
         (bend-me . ())
         (details . ((cautionary-properties . ((angularity . 0.4)
                                               (half-thickness . 0.075)
@@ -3516,9 +3517,10 @@ start delimiter.")))))
                                             (procedure . ,parenthesize-stencil)
                                             (width . 0.25)))
                     (head-offset . 3/5)
-                    (repeat-tied-properties . ((note-head-visible . #t)
-                                               (parenthesize . #t)))
-                    (tied-properties . ((parenthesize . #t)))))
+                    (tied-properties . ((note-head-visible . #t)
+                                        (parenthesize . #t)
+                                        (repeat-tied . #f)
+                                        (tied . #f)))))
         (direction . ,CENTER)
         (duration-log . ,note-head::calc-duration-log)
         (font-series . bold)

commit a33073f95cc5740882274f6556b793e57322e4be

Fix spacer after line-break in tablature
A regression was introduced by
23a3778f2ed6a12b2e41026d3e6646f7177f0a29
if a spacer starts a new line.
Fixed by checking whether a relevant column is found at all.

Closes #6752

scm/tablature.scm
@@ -201,8 +201,8 @@ seconda volta blocks. This behaviour can be switched off with
   (let* (;; get PaperColumn
          (pc (ly:item-get-column grob))
          ;; get NonMusicalPaperColumn
-         (nmpc (ly:grob-object pc 'left-neighbor))
-         (at-line-begin? (positive? (ly:item-break-dir nmpc)))
+         (nmpc (ly:grob-object pc 'left-neighbor #f))
+         (at-line-begin? (and nmpc (positive? (ly:item-break-dir nmpc))))
          (span-start? (ly:grob-property grob 'span-start #f))
          (details (ly:grob-property grob 'details '()))
          (tied-props (assoc-get 'tied-properties details '()))

input/regression/tablature-new-line-spacer.ly
@@ -1,0 +1,8 @@
+\version "2.25.21"
+
+\header {
+  texidoc = "A spacer at begin of a new line does not error."
+}
+
+\new TabStaff { b1 \break s2 b }
+