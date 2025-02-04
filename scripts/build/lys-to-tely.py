54b41fd797beacc420e94d5012c50d319cc59962
Werner Lemberg <wl@gnu.org>
2025-02-04 08:55:04 +0100

    lys-to-tely.py: Replace 'getopt' with 'argparse'
    Also improve help output.

--- scripts/build/lys-to-tely.py
+++ scripts/build/lys-to-tely.py
@@ -22,73 +22,153 @@
 TODO:
 
  * Add @nodes, split at sections?
-
- * -o --output   listed in help is not implemented?!
 '''
 
 
 import sys
 import os
-import getopt
+import argparse
 import re
 import glob
+import textwrap
 
 from pathlib import Path
+from argparse import HelpFormatter
 
-
-program_name = 'lys-to-tely'
 
 include_snippets = '@lysnippets'
-fragment_options = 'printfilename,texidoc'
-help_text = r"""Usage: %(program_name)s [OPTIONS]... LY-FILE...
-Construct tely doc from LY-FILEs.
-
-Options:
- -h, --help                  print this help
- -f, --fragment-options=OPTIONS
-                             use OPTIONS as lilypond-book fragment options
- -o, --output=OUTPUT         write tely doc to OUTPUT
-     --prefix=PREFIX         prefix filenames with PREFIX
- -i, --input-filenames=NAME  read list of files from a file instead of stdin
- -g, --glob-input=GLOB       a string which will be passed to glob.glob(GLOB)
- -t, --title=TITLE           set tely doc title to TITLE
- -a, --author=AUTHOR         set tely author to AUTHOR
-     --template=TEMPLATE     use TEMPLATE as texinfo template file instead of
-                               standard template; TEMPLATE should contain a
-                               marker '%(include_snippets)s' to indicate
-                               where to insert LY-FILEs.  When this option is
-                               used, TITLE gets ignored
-
-If a lilypond-book options file exists for an input file (stripping off the
-file name's suffix and appending `.lybook`) that gets eventually included
-with `@musicxmlfile` or `@lilypondfile`, use its contents as additional
-fragment options (separated by whitespace) for this input file.
-"""
 
 
-def help(text):
-    sys.stdout.write(text)
-    sys.exit(0)
+# A wrapper around `textwrap.wrap()` that keeps newlines in the input string
+# intact.  Taken from https://stackoverflow.com/questions/3853722.
+def wrap_paragraphs(text, width, indent):
+    lines = []
+    for i in text.splitlines():
+        paragraph_lines = textwrap.wrap(i, width,
+                                        initial_indent=indent,
+                                        subsequent_indent=indent)
+        # `textwrap.wrap()` returns an empty list when passed an empty
+        # string (which happens when there are two consecutive line breaks
+        # in the input string).  This would lead to those line breaks being
+        # collapsed into a single line break, effectively removing empty
+        # lines from the input.  Thus, we add an empty line in that case.
+        lines.extend(paragraph_lines or [''])
+    return lines
 
 
-(options, files) = getopt.getopt(sys.argv[1:], 'f:hn:t:',
-                                 ['fragment-options=', 'help', 'name=',
-                                  'title=', 'author=', 'template=',
-                                  'input-filenames=', 'glob-input=',
-                                  'prefix=', 'output='])
+class CustomHelpFormatter(HelpFormatter):
+    # Sort options alphabetically, ignoring case ...
+    def add_arguments(self, actions):
+        # ... but don't sort '--zzz' before '-a'.
+        actions = \
+          sorted(actions,
+                 key=lambda x:
+                         x.option_strings[0].replace('--', '-').lower()
+                             if x.option_strings else None)
+        super(CustomHelpFormatter, self).add_arguments(actions)
 
-output = "ly-doc.tely"
-title = "Ly Doc"
-author = "The LilyPond development team"
-input_filename = ""
-glob_input = ""
-template = r'''\input texinfo
+    # Preserve newlines in `help` arguments.
+    def _split_lines(self, text, width):
+        return wrap_paragraphs(text, width, '')
+
+    # Preserve newlines in `description` and `epilog` arguments.
+    def _fill_text(self, text, width, indent):
+        return '\n'.join(wrap_paragraphs(text, width, indent))
+
+
+p = argparse.ArgumentParser(
+        formatter_class=CustomHelpFormatter,
+        usage='%(prog)s [OPTION]... [FILE]...',
+        description="Construct a 'tely' doc file from LilyPond files.",
+        epilog="If a lilypond-book options file exists for an input file"
+               " (stripping off the file name's suffix and appending"
+               " `.lybook`) that gets eventually included with"
+               " `@musicxmlfile` or `@lilypondfile`, use its contents as"
+               " additional fragment options (separated by whitespace) for"
+               " this input file.")
+
+p.add_argument(
+    '-f', '--fragment-options',
+    type=str,
+    default='printfilename,texidoc',
+    metavar='OPTIONS',
+    help="use %(metavar)s as lilypond-book fragment options"
+         " (default: '%(default)s')")
+p.add_argument(
+    '-o', '--output',
+    type=str,
+    default='ly-doc.tely',
+    metavar='FILE',
+    help="write 'tely' doc to %(metavar)s (default: '%(default)s')")
+p.add_argument(
+    '--prefix',
+    type=str,
+    default='',
+    metavar='PREFIX',
+    help='prefix file names with %(metavar)s')
+p.add_argument(
+    '-i', '--input-filenames',
+    type=str,
+    default='',
+    metavar='FILE',
+    help='read list of files from %(metavar)s instead of stdin')
+p.add_argument(
+    '-g', '--glob-input',
+    type=str,
+    metavar='GLOB',
+    help='a string to be passed as `glob.glob(%(metavar)s)` for getting a'
+         ' list of files, ignoring stdin; setting this option overrides'
+         ' `--input-filenames`')
+p.add_argument(
+    '-t', '--title',
+    type=str,
+    default='Ly Doc',
+    metavar='TITLE',
+    help="set 'tely' doc title to %(metavar)s (default: '%(default)s')")
+p.add_argument(
+    '-a', '--author',
+    type=str,
+    default='The LilyPond development team',
+    metavar='AUTHOR',
+    help="set 'tely' doc author to %(metavar)s (default: '%(default)s')")
+p.add_argument(
+    '--template',
+    type=str,
+    metavar='FILE',
+    help=f'use %(metavar)s as a custom Texinfo template file instead of the'
+         f' default template; %(metavar)s should contain a marker'
+         f' `{include_snippets}` to indicate where to insert the LilyPond'
+         f' source files.\n'
+         f'\n'
+         f'If a custom template is used, options `--title` and `--author`'
+         f' are ignored')
+
+p.add_argument(
+    'files',
+    nargs='*',
+    metavar='FILEs',
+    help='LilyPond source files. Depending on the file extensions they are'
+         ' handled as follows.\n'
+         '`.[i]html`, `.info`, `.[i]pdf`, `.[i][la]tex`: use HTML link.\n'
+         '`.[i]tely`, `.[i]texi`, `.[i]texinfo`: use `@include`.\n'
+         '`.[i]xml`, `.[i]mxl`: use `@musicxmlfile`.\n'
+         'Use `@lilypondfile` for everything else.')
+
+options = p.parse_args()
+
+files = options.files
+if options.glob_input:
+    files = glob.glob(options.glob_input)
+elif options.input_filenames:
+    files = open(options.input_filenames, encoding='utf-8').read().split()
+
+template_default = rf'''\input texinfo
 
 @c This file was autogenerated
 @c     from: %s
 @c     by:   %s
 
-@settitle %%(title)s
+@settitle {options.title}
 
 @documentencoding UTF-8
 @afourpaper
@@ -98,48 +178,25 @@ template = r'''\input texinfo
 @titlepage
 @title LilyPond
 @subtitle The music typesetter
-@titlefont{%%(title)s}
-@author %%(author)s
+@titlefont{{{options.title}}}
+@author {options.author}
 @end titlepage
 
 @ifnottex
 @node Top
-@top %%(title)s
+@top {options.title}
 @end ifnottex
 
-%s
+{include_snippets}
 
 @bye
-''' % (", ".join(files), sys.argv[0], include_snippets)
+''' % (", ".join(files), sys.argv[0])
 
-prefix = ''
+if options.template:
+    template = open(options.template, 'r', encoding='utf-8').read()
+else:
+    template = template_default
 
-for opt in options:
-    o = opt[0]
-    a = opt[1]
-    if o == '-h' or o == '--help':
-        # We can't use vars () inside a function, as that only contains all
-        # local variables and none of the global variables! Thus we have to
-        # generate the help text here and pass it to the function...
-        help(help_text % vars())
-    elif o == '-o' or o == '--output':
-        output = a
-    elif o == '-t' or o == '--title':
-        title = a
-    elif o == '-a' or o == '--author':
-        author = a
-    elif o == '-i' or o == '--input-filenames':
-        input_filename = a
-    elif o == '-g' or o == '--glob-input':
-        glob_input = a
-    elif o == '-f' or o == '--fragment-options':
-        fragment_options = a
-    elif o == '--prefix':
-        prefix = a
-    elif o == '--template':
-        template = open(a, 'r', encoding='utf-8').read()
-    else:
-        raise Exception('unknown option: ' + o)
 
 html_file_re = re.compile(r'.*\.i?html?$')
 info_file_re = re.compile(r'.*\.info$')
@@ -151,13 +208,13 @@ xml_file_re = re.compile(r'.*\.i?(xm|mx)l$')
 
 def name2line(n):
     file_name = Path(n)
-    options_file = file_name.with_suffix('.lybook')
+    fragment_options_file = file_name.with_suffix('.lybook')
 
-    options_string = ''
-    if os.path.isfile(options_file):
-        options = open(options_file, encoding='utf-8').read().split()
-        if options:
-            options_string = ',' + ','.join(options)
+    fragment_options_string = ''
+    if os.path.isfile(fragment_options_file):
+        fragment_options = open(fragment_options_file, encoding='utf-8').read().split()
+        if fragment_options:
+            fragment_options_string = ',' + ','.join(fragment_options)
 
     if texi_file_re.match(n):
         # We have a Texinfo file; simply include it.
@@ -185,7 +242,9 @@ def name2line(n):
 @end ifhtml
 
 @musicxmlfile[%s]{%s}
-""" % (os.path.basename(n), fragment_options + options_string, prefix + n)
+""" % (os.path.basename(n),
+       options.fragment_options + fragment_options_string,
+       options.prefix + n)
 
     else:
         # Assume it's a LilyPond file -> create image, etc.
@@ -197,20 +256,16 @@ def name2line(n):
 @end ifhtml
 
 @lilypondfile[%s]{%s}
-""" % (os.path.basename(n), fragment_options + options_string, prefix + n)
+""" % (os.path.basename(n),
+       options.fragment_options + fragment_options_string,
+       options.prefix + n)
     return s
 
 
-if glob_input:
-    files = glob.glob(glob_input)
-elif input_filename:
-    files = open(input_filename, encoding='utf-8').read().split()
-
 if files:
-    template = template % vars()
     s = "\n".join(map(name2line, files))
     s = template.replace(include_snippets, s, 1)
-    h = open(output, "w", encoding="utf8")
+    h = open(options.output, "w", encoding="utf8")
     h.write(s)
     h.close()
 else:
