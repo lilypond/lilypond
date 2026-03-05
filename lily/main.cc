lilypond/lilypond

commit c95d569e9013276fd0595366c79eebdf772bea77

Make `--help` emit top-level directory of LilyPond's data tree
Also update documentation in the LM.

lily/main.cc
@@ -168,26 +168,33 @@ env_var_info (FILE *out, char const *key)
 static void
 dir_info (FILE *out)
 {
-  fputs ("\n", out);
-  env_var_info (out, "LILYPOND_DATADIR");
-  env_var_info (out, "LILYPOND_LOCALEDIR");
-  env_var_info (out, "LILYPOND_RELOCDIR");
+  if (is_loglevel (LOG_DEBUG))
+    {
+      env_var_info (out, "LILYPOND_DATADIR");
+      env_var_info (out, "LILYPOND_LOCALEDIR");
+      env_var_info (out, "LILYPOND_RELOCDIR");
+    }
 
   fputs (_f ("\n"
-             "Effective prefix: '%s'\n",
+             "Top-level directory of LilyPond's data tree:\n"
+             "  '%s'\n"
+             "\n",
              lilypond_datadir)
            .c_str (),
          out);
 
-  env_var_info (out, "FONTCONFIG_FILE");
-  env_var_info (out, "FONTCONFIG_PATH");
-  env_var_info (out, "GS_FONTPATH");
-  env_var_info (out, "GS_LIB");
-  env_var_info (out, "GUILE_LOAD_PATH");
-  env_var_info (out, "PANGO_RC_FILE");
-  env_var_info (out, "PANGO_PREFIX");
-  env_var_info (out, "PATH");
-  fputs ("\n", out);
+  if (is_loglevel (LOG_DEBUG))
+    {
+      env_var_info (out, "FONTCONFIG_FILE");
+      env_var_info (out, "FONTCONFIG_PATH");
+      env_var_info (out, "GS_FONTPATH");
+      env_var_info (out, "GS_LIB");
+      env_var_info (out, "GUILE_LOAD_PATH");
+      env_var_info (out, "PANGO_RC_FILE");
+      env_var_info (out, "PANGO_PREFIX");
+      env_var_info (out, "PATH");
+      fputs ("\n", out);
+    }
 }
 
 static std::string
@@ -417,8 +424,7 @@ main_with_guile (void *, int, char **)
     scm_c_lookup ("%compile-fallback-path"),
     scm_from_locale_string ((lilypond_datadir + "/guile-bytecode").c_str ()));
 
-  if (is_loglevel (LOG_DEBUG))
-    dir_info (stderr);
+  dir_info (stderr);
 
   init_scheme_code_global = "(begin " + init_scheme_code_global + ")";
 
@@ -659,8 +665,8 @@ parse_argv (int argc, char **argv)
   if (show_help)
     {
       ly_usage ();
-      if (is_loglevel (LOG_DEBUG))
-        dir_info (stdout);
+      setup_paths (argv[0]);
+      dir_info (stdout);
       exit (0);
     }
 }

Documentation/en/learning/tweaks.itely
@@ -4746,28 +4746,20 @@ file with @code{\include "../global.ily"}, which contains
 The Internals Reference documentation contains a lot of information
 about LilyPond, but even more information can be gathered by
 looking at the internal LilyPond files.  To explore these, you must
-first find the directory appropriate to your system.  The location
-of this directory depends on whether you obtained LilyPond
-by downloading a precompiled binary from @code{lilypond.org}
-or whether you installed it from a package manager (i.e.,
-distributed with GNU/Linux, or installed under MacPorts, Homebrew,
-Cygwin, Chocolatey, etc.) or compiled it from source.
+first find the directory appropriate to your system.  If you say
 
-@itemize
-@item Downloaded from @code{lilypond.org}
-
-Navigate to
 @example
-@file{@var{INSTALLDIR}/lilypond-@version{}/share/lilypond/@version{}/}
+lilypond --help
 @end example
 
-@item Installed from a package manager or compiled from source
+@noindent
+on the command line, the top-level directory of LilyPond's data
+tree is shown near the end of the help output, for example
 
-Navigate to
-@file{@var{PREFIX}/share/lilypond/@version{}/}, where
-@var{PREFIX} is set by your package manager or @code{configure}
-script.
-@end itemize
+@example
+Top-level directory of LilyPond's data tree:
+  '/usr/local/share/lilypond/@version{}'
+@end example
 
 Within this directory the two interesting subdirectories are
 

commit 7a006071b87c2bbce0fb099ce890f598be9cf210

main.cc: Fix thinko
Problem introduced in c95d569e901, reported by Dan.  Without this fix, the
top-level data dir is always shown in the log output.

lily/main.cc
@@ -424,7 +424,8 @@ main_with_guile (void *, int, char **)
     scm_c_lookup ("%compile-fallback-path"),
     scm_from_locale_string ((lilypond_datadir + "/guile-bytecode").c_str ()));
 
-  dir_info (stderr);
+  if (is_loglevel (LOG_DEBUG))
+    dir_info (stderr);
 
   init_scheme_code_global = "(begin " + init_scheme_code_global + ")";
 