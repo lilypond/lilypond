lilypond/lilypond

commit c043783aac6c35926d633aa2daf60ed4f2e7ca7a

Rewrite glissando-engraver in scheme

scm/scheme-engravers.scm
@@ -2483,3 +2483,115 @@ adapted for typesetting within a chord grid.")))
    (description . "Read the @code{toeHeelStyle} context property and use it to
 style @code{\\rtoe} and its siblings, based on the data in the
 @code{toe-heel-styles} alist.")))
+
+(define-public Glissando_engraver
+  (lambda (context)
+    (let ((glissando-event #f)
+          (start-glissando-line #f)
+          (stop-glissando-line #f)
+          (current-glissando-grobs '())
+          (gliss-map #f))
+
+      (define (bounds lst grobs start-stop)
+        ;; `lst` is supposed to be the number-pair-list from `glissandoMap`
+        ;; `grobs` are usually note-head grobs
+        ;; `start-stop` is car or cdr of a pair
+        ;;
+        ;; Selects possible left/right bounds for Glissando.
+        (if (number-pair-list? lst)
+            (let* ((user-start-stops (map start-stop lst))
+                   (start-stops
+                     (filter
+                       (lambda (n)
+                         (and (index? n) (<= n (1- (length grobs)))))
+                       user-start-stops)))
+              (map
+                (lambda (start-stop) (list-ref grobs start-stop))
+                start-stops))
+            grobs))
+
+      (make-engraver
+        (listeners
+           ((glissando-event this-engraver event)
+             (set! glissando-event event)
+             (set! start-glissando-line #t)))
+
+        (acknowledgers
+          ((note-column-interface this-engraver grob source-engraver)
+           (let* ((note-heads-array (ly:grob-object grob 'note-heads #f))
+                  (note-heads
+                    (if note-heads-array
+                        (ly:grob-array->list note-heads-array)
+                        '()))
+                  (glissando-skip?
+                    (ly:grob-property grob 'glissando-skip #f)))
+
+        ;; Do not start a new Glissando, if the last is not yet ended.
+        ;; This may happen if 'glissando-skip is used.
+        (when (and glissando-skip? start-glissando-line stop-glissando-line)
+          (ly:warning "G_overwriting glissando")
+          (set! glissando-event #f)
+          (set! start-glissando-line #f))
+
+        (when (and (not glissando-skip?) stop-glissando-line)
+          ;; set 'glissando-index
+          (for-each
+            (lambda (gliss i) (ly:grob-set-property! gliss 'glissando-index i))
+            current-glissando-grobs
+            (iota (length note-heads)))
+
+          (let ((possible-right-bounds (bounds gliss-map note-heads cdr)))
+
+            ;; Set right bound.
+            (for-each
+              (lambda (bound gliss )
+                (ly:spanner-set-bound! gliss RIGHT bound)
+                (ly:engraver-announce-end-grob this-engraver gliss bound))
+              possible-right-bounds
+              current-glissando-grobs)
+
+            ;; Throw away Glissandi not right-bounded by a rhythmic grob.
+            (for-each
+              (lambda (gliss)
+                (when (not (ly:grob? (ly:spanner-bound gliss RIGHT)))
+                  (ly:grob-suicide! gliss)))
+              current-glissando-grobs)
+
+            (set! stop-glissando-line #f)
+            (set! current-glissando-grobs '())))
+
+        (when start-glissando-line
+          (set! gliss-map (ly:context-property context 'glissandoMap #f))
+          (let ((possible-left-bounds (bounds gliss-map note-heads car)))
+            (set! start-glissando-line #f)
+            (set! stop-glissando-line #t)
+            (set! current-glissando-grobs
+                  (append
+                    (map
+                      (lambda (bound)
+                        (let ((gliss
+                                (ly:engraver-make-grob
+                                  this-engraver 'Glissando glissando-event)))
+                          (ly:spanner-set-bound! gliss LEFT bound)
+                          gliss))
+                      possible-left-bounds)
+                    current-glissando-grobs))
+            (set! glissando-event #f))))))
+        ((finalize this-engraver)
+         (when (pair? current-glissando-grobs)
+           (for-each
+             (lambda (gliss)
+               (ly:warning "unterminated Glissando")
+               (ly:grob-suicide! gliss))
+             current-glissando-grobs)
+           (set! current-glissando-grobs '())
+           (set! gliss-map #f)
+           (set! stop-glissando-line #f)))))))
+
+(ly:register-translator
+ Glissando_engraver 'Glissando_engraver
+ '((grobs-created . (Glissando))
+   (events-accepted . (glissando-event))
+   (properties-read . (glissandoMap))
+   (properties-written . ())
+   (description . "Engrave glissandi.")))

lily/glissando-engraver.cc
@@ -1,198 +1,0 @@
-/*
-  This file is part of LilyPond, the GNU music typesetter.
-
-  Copyright (C) 2000--2023 Jan Nieuwenhuizen <janneke@gnu.org>
-
-  LilyPond is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  LilyPond is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with LilyPond.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include "engraver.hh"
-
-#include "international.hh"
-#include "pointer-group-interface.hh"
-#include "rhythmic-head.hh"
-#include "spanner.hh"
-#include "stream-event.hh"
-#include "warn.hh"
-#include "item.hh"
-
-#include "translator.icc"
-
-class Glissando_engraver final : public Engraver
-{
-public:
-  TRANSLATOR_DECLARATIONS (Glissando_engraver);
-
-protected:
-  void listen_glissando (Stream_event *);
-  void acknowledge_note_column (Grob_info_t<Item>);
-  void finalize () override;
-
-  void stop_translation_timestep ();
-  void process_music ();
-
-private:
-  std::vector<Spanner *> lines_;
-  std::vector<Spanner *> kill_me_;
-  bool start_glissandi_ = false;
-  bool stop_glissandi_ = false;
-
-  Stream_event *event_ = nullptr;
-  std::vector<vsize> note_column_1;
-  std::vector<vsize> note_column_2;
-};
-
-Glissando_engraver::Glissando_engraver (Context *c)
-  : Engraver (c)
-{
-}
-
-void
-Glissando_engraver::listen_glissando (Stream_event *ev)
-{
-  assign_event_once (event_, ev);
-}
-
-void
-Glissando_engraver::process_music ()
-{
-  if (event_)
-    start_glissandi_ = true;
-}
-
-void
-Glissando_engraver::acknowledge_note_column (Grob_info_t<Item> info)
-{
-  auto *const g = info.grob ();
-  if (from_scm<bool> (get_property (g, "glissando-skip")))
-    return;
-
-  if (stop_glissandi_)
-    {
-      extract_grob_set (g, "note-heads", note_heads);
-      int glissando_index = 0;
-      for (vsize i = 0; i < note_column_1.size (); i++)
-        {
-          if (note_column_2[i] >= note_heads.size ())
-            {
-              kill_me_.push_back (lines_[i]);
-              announce_end_grob (lines_[i], SCM_EOL);
-            }
-          else
-            {
-              lines_[i]->set_bound (RIGHT, note_heads[note_column_2[i]]);
-              set_property (lines_[i], "glissando-index",
-                            to_scm (glissando_index));
-              glissando_index++;
-              announce_end_grob (lines_[i],
-                                 note_heads[note_column_2[i]]->self_scm ());
-            }
-        }
-      lines_.clear ();
-      note_column_1.clear ();
-      note_column_2.clear ();
-      stop_glissandi_ = false;
-    }
-
-  if (start_glissandi_)
-    {
-      extract_grob_set (g, "note-heads", note_heads);
-      SCM map = get_property (this, "glissandoMap");
-      if (scm_is_null (map))
-        for (vsize i = 0; i < note_heads.size (); i++)
-          {
-            note_column_1.push_back (i);
-            note_column_2.push_back (i);
-          }
-      else
-        for (SCM m = map; scm_is_pair (m); m = scm_cdr (m))
-          {
-            SCM candidate = scm_car (m);
-            if (!scm_is_pair (candidate))
-              continue;
-            int n1 = from_scm (scm_car (candidate), -1);
-            int n2 = from_scm (scm_cdr (candidate), -1);
-            if ((n1 < 0) || (n2 < 0) || (size_t (n1) >= note_heads.size ()))
-              continue;
-            note_column_1.push_back (vsize (n1));
-            note_column_2.push_back (vsize (n2));
-          }
-      for (vsize i = 0; i < note_column_1.size (); i++)
-        {
-          lines_.push_back (make_spanner ("Glissando", event_->self_scm ()));
-          lines_.back ()->set_bound (LEFT, note_heads[note_column_1[i]]);
-        }
-    }
-}
-
-void
-Glissando_engraver::stop_translation_timestep ()
-{
-  if (start_glissandi_)
-    {
-      if (stop_glissandi_)
-        programming_error ("overwriting glissando");
-      stop_glissandi_ = true;
-      start_glissandi_ = false;
-    }
-  event_ = nullptr;
-}
-
-void
-Glissando_engraver::finalize ()
-{
-  if (!lines_.empty ())
-    {
-      std::string msg = _ ("unterminated glissando");
-
-      if (event_)
-        event_->warning (msg);
-      else
-        warning (msg);
-
-      for (vsize i = 0; i < lines_.size (); i++)
-        lines_[i]->suicide ();
-    }
-
-  for (vsize i = 0; i < kill_me_.size (); i++)
-    kill_me_[i]->suicide ();
-}
-
-void
-Glissando_engraver::boot ()
-{
-  ADD_LISTENER (glissando);
-  ADD_ACKNOWLEDGER (note_column);
-}
-
-ADD_TRANSLATOR (Glissando_engraver,
-                /* doc */
-                R"(
-Engrave glissandi.
-                )",
-
-                /* create */
-                R"(
-Glissando
-                )",
-
-                /* read */
-                R"(
-glissandoMap
-                )",
-
-                /* write */
-                R"(
-
-                )");

commit 77fb1478762ae988188f40f8c586b1b227f01f6d

Fix issue 6946 - dead Glissando prevents automatic line break
This was a regression introduced with c043783a
"Rewrite glissando-engraver in scheme", leading to recurrence of
issue #1639
The fix here is the same as for #1639, i.e. delay killing unneeded
Glissando grobs until other engravers had the opportunity to
acknowledge the end of the spanner.
While the fix for #1639 added a regtest already, it was not
sufficient to spot the problem with c043783a.
Thus adding a new one.

Closes #6946

scm/scheme-engravers.scm
@@ -2490,6 +2490,7 @@ style @code{\\rtoe} and its siblings, based on the data in the
           (start-glissando-line #f)
           (stop-glissando-line #f)
           (current-glissando-grobs '())
+          (kill-me '())
           (gliss-map #f))
 
       (define (bounds lst grobs start-stop)
@@ -2550,11 +2551,15 @@ style @code{\\rtoe} and its siblings, based on the data in the
                 possible-right-bounds
                 current-glissando-grobs)
 
-               ;; Throw away Glissandi not right-bounded by a rhythmic grob.
+               ;; Throw away unneeded Glissandi, but postpone it until other
+               ;; engravers have acknowledged the end of the spanner.
+               ;; This avoids problems like issue 6946
                (for-each
                 (lambda (gliss)
                   (when (not (ly:grob? (ly:spanner-bound gliss RIGHT)))
-                    (ly:grob-suicide! gliss)))
+                    (ly:engraver-announce-end-grob this-engraver gliss
+                      (ly:context-property context 'currentMusicalColumn))
+                    (set! kill-me (cons gliss kill-me))))
                 current-glissando-grobs)
 
                (set! stop-glissando-line #f)
@@ -2577,6 +2582,9 @@ style @code{\\rtoe} and its siblings, based on the data in the
                        possible-left-bounds)
                       current-glissando-grobs))
                (set! glissando-event #f))))))
+       ((stop-translation-timestep this-engraver)
+        (for-each ly:grob-suicide! kill-me)
+        (set! kill-me '()))
        ((finalize this-engraver)
         (when (pair? current-glissando-grobs)
           (for-each

input/regression/glissando-chord-to-note.ly
@@ -1,0 +1,12 @@
+\version "2.27.2"
+
+\header {
+ texidoc = "A glissando from a chord to a single note does not disturb automatic
+line breaking.
+The expected output is a visible single glissando, with a proper line break
+later."
+}
+
+%% I.e. check for #6946 "glissando and line break issue"
+
+\relative { <c'' f>2.\glissando a'4 \repeat unfold 64 g16 }