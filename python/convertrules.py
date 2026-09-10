lilypond/lilypond

commit ed0071be6146d8d4bc303ad783f682416ad1a4e8

Document book/bookpart type split in changes, add convert-ly rule

python/convertrules.py
@@ -5697,6 +5697,10 @@ def conv(s):
     s = re.sub(r'\\set\s+stanza\s+=\s*', r'\\stanza ', s)
     return s
 
+@rule((2, 27, 3), "ly:book? -> ly:book-or-bookpart?")
+def conv(s):
+    s = re.sub(r'ly:book\?',r'ly:book-or-bookpart?')
+    return s
 
 # Guidelines to write rules (please keep this at the end of this file)
 #

commit af0b952fcee2162c2ecac74147d3903f26620701

Fix regression in convert-ly caused by MR !3141
This problem was introduced in
commit ed0071be6146d8d4bc303ad783f682416ad1a4e8 .

python/convertrules.py
@@ -5699,7 +5699,7 @@ def conv(s):
 
 @rule((2, 27, 3), "ly:book? -> ly:book-or-bookpart?")
 def conv(s):
-    s = re.sub(r'ly:book\?',r'ly:book-or-bookpart?')
+    s = re.sub(r'ly:book\?',r'ly:book-or-bookpart?', s)
     return s
 
 # Guidelines to write rules (please keep this at the end of this file)